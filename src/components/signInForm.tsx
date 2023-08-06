7a43d799804220eba0d330bee8dc9d46963b48b9
junhcha <quffl9612@naver.com>
2023-08-06 17:56:19 +0900

    feat: add base signIn html structs

--- src/components/signInForm.tsx
+++ src/components/signInForm.tsx
@@ -3,9 +3,17 @@ import React from "react";
 class SignIn extends React.Component {
     render() {
         return (
-        <div>
-            <h1>Sign In</h1>
-        </div>
+            <div className="login-wrapper">
+                <h2>Login</h2>
+                <form method="post" action="서버의url" id="login-form">
+                    <input type="text" name="userName" placeholder="Email" />
+                    <input type="password" name="userPassword" placeholder="Password" />
+                    <label htmlFor="remember-check">
+                        <input type="checkbox" id="remember-check" />아이디 저장하기 
+                    </label>
+                    <input type="submit" value="Login" />
+                </form>
+            </div>
         );
     }
 }
