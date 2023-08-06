e216a6aadcc0caffaad1a1f1ba50b0fd4c263fb3
junhcha <quffl9612@naver.com>
2023-08-07 01:25:11 +0900

    style: complete stylesheet.

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -1,45 +1,88 @@
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 class LoginForm extends React.Component {
-    render() {
-        return (
-            <div className="login-wrapper">
-                <h2>Login</h2>
-                
-                <form method="post" action="서버의url" id="login-form">
-                    <Input type="text" name="userName" placeholder="Email" />
-                    <Input type="password" name="userPassword" placeholder="Password" />
-                    <label htmlFor="remember-check">
-                        <Input type="checkbox" id="remember-check" />아이디 저장하기 
-                    </label>
-                    <SubmitInput type="submit" value="Login" />
-                </form>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <Wrapper>
+        <Title>Login</Title>
+        <form>
+          <AccountInput type="text" name="userName" placeholder="Email" />
+          <AccountInput
+            type="password"
+            name="userPassword"
+            placeholder="Password"
+          />
+          <RememberCheckLabel htmlFor="remember-check">
+            <CheckboxInput type="checkbox" id="remember-check" />
+            아이디 저장하기
+          </RememberCheckLabel>
+          <Submit
+            type="submit"
+            value="Submit"
+            onClick={() => window.alert("Login info has submitted.")}
+          />
+        </form>
+      </Wrapper>
+    );
+  }
 }
-
-
-export const Input = styled.input`
-    width: 100%;
-    height: 48px;
-    padding: 0 10px;
-    box-sizing: border-box;
-    margin-bottom: 16px;
-    border-radius: 6px;
-    background-color: #F8F8F8;
-
-    &::placeholder {
-        color: #D2D2D2;
-    }
+export const Wrapper = styled.div`
+  width: 100%;
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 40px;
+  border: None;
+  box-sizing: border-box;
+  text-align: center;
+  font-size: 14px;
+  font-family: "Roboto", sans-serif;
 `;
 
-export const SubmitInput = styled(Input)`
-    color: #fff;
-    font-size: 16px;
-    background-color: #6A24FE;
-    margin-top: 20px;
+export const Title = styled.h2`
+  margin-bottom: 15px;
+  font-size: 30px;
+  font-weight: 900;
+  color: #6a24fe;
+`;
+
+export const AccountInput = styled.input`
+  width: 100%;
+  height: 45px;
+  padding: 0 10px;
+  box-sizing: border-box;
+  margin-bottom: 10px;
+  border-radius: 6px;
+  background-color: #f8f8f8;
+
+  &::placeholder {
+    color: #d2d2d2;
+  }
+`;
+
+export const RememberCheckLabel = styled.label`
+  height: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  margin-bottom: 10px;
+  font-size: 14px;
+  color: #999999;
+`;
+
+export const CheckboxInput = styled(AccountInput)`
+  width: auto;
+  height: auto;
+  margin-right: 10px;
+`;
+
+export const Submit = styled.input`
+  width: 100%;
+  height: 45px;
+  color: #fff;
+  font-size: 16px;
+  background-color: #6a24fe;
+  margin-top: 10px;
 `;
 
 export default LoginForm;
