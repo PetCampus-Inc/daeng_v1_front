c750715e8d70f48e31f780e0acc3f0b4ce652b4c
junhcha <quffl9612@naver.com>
2023-08-06 18:28:39 +0900

    style: apply stylesheet

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -1,21 +1,45 @@
 import React from "react";
+import styled from 'styled-components';
 
 class LoginForm extends React.Component {
     render() {
         return (
             <div className="login-wrapper">
                 <h2>Login</h2>
+                
                 <form method="post" action="서버의url" id="login-form">
-                    <input type="text" name="userName" placeholder="Email" />
-                    <input type="password" name="userPassword" placeholder="Password" />
+                    <Input type="text" name="userName" placeholder="Email" />
+                    <Input type="password" name="userPassword" placeholder="Password" />
                     <label htmlFor="remember-check">
-                        <input type="checkbox" id="remember-check" />아이디 저장하기 
+                        <Input type="checkbox" id="remember-check" />아이디 저장하기 
                     </label>
-                    <input type="submit" value="Login" />
+                    <SubmitInput type="submit" value="Login" />
                 </form>
             </div>
         );
     }
 }
+
+
+export const Input = styled.input`
+    width: 100%;
+    height: 48px;
+    padding: 0 10px;
+    box-sizing: border-box;
+    margin-bottom: 16px;
+    border-radius: 6px;
+    background-color: #F8F8F8;
+
+    &::placeholder {
+        color: #D2D2D2;
+    }
+`;
+
+export const SubmitInput = styled(Input)`
+    color: #fff;
+    font-size: 16px;
+    background-color: #6A24FE;
+    margin-top: 20px;
+`;
 
 export default LoginForm;
