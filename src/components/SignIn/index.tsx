571511097b4baa42590b1fce953f42fa53b75101
hyerani <hran9404@naver.com>
2023-09-14 20:38:10 +0900

    feat: sign in style setting

--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -1,8 +1,39 @@
 import { memo } from "react";
-import { Container } from "./styles";
+import {
+  AppleButton,
+  ButtonWrapper,
+  Container,
+  GoogleButton,
+  KakaoButton,
+  Text,
+  TextWrapper,
+  TryButton,
+} from "./styles";
 
 const SignIn = () => {
-  return <Container>signin</Container>;
+  return (
+    <Container>
+      <TextWrapper>
+        <Text>반려견의 유치원</Text>
+        <Text>생활을 보러 갈까요?</Text>
+      </TextWrapper>
+      <ButtonWrapper>
+        <KakaoButton>
+          <img src="images/kakao-logo.png" alt="logo" />
+          카카오로 계속하기
+        </KakaoButton>
+        <GoogleButton>
+          <img src="images/google-logo.png" alt="logo" />
+          구글로 계속하기
+        </GoogleButton>
+        <AppleButton>
+          <img src="images/apple-logo.png" alt="logo" />
+          Apple로 로그인
+        </AppleButton>
+        <TryButton>서비스 체험하기</TryButton>
+      </ButtonWrapper>
+    </Container>
+  );
 };
 
 export default memo(SignIn);
