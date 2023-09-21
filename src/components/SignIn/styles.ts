adb66fe2a68deb09543e3a5e8811f1285a82d6d6
kor-kms <68898456+kor-kms@users.noreply.github.com>
2023-09-21 19:12:05 +0900

    Feat: add social login page (#5)
    * Style: change code name
    
    * Feat: header style change, add images
    
    * Refactor: add memo
    
    * feat: add sign-in page
    
    * feat: sign in style setting
    
    * Feat: add common board component:
    
    ---------
    
    Co-authored-by: hyerani <hran9404@naver.com>

---
+++ src/components/SignIn/styles.ts
@@ -0,0 +1,76 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  padding-top: 40%;
+  padding-left: 5%;
+  padding-right: 5%;
+  background-color: white;
+`;
+
+export const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Text = styled.text`
+  font-weight: 700;
+  font-size: 24px;
+  margin-bottom: 5px;
+`;
+
+export const ButtonWrapper = styled.div`
+  height: 65%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const KakaoButton = styled.button`
+  width: 100%;
+  height: 10%;
+  margin-bottom: 10px;
+  border-radius: 7px;
+  border-style: none;
+  background-color: #fee500;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const GoogleButton = styled.button`
+  width: 100%;
+  height: 10%;
+  margin-bottom: 10px;
+  border: solid 1px #cccccc;
+  border-radius: 7px;
+  background-color: white;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const AppleButton = styled.button`
+  width: 100%;
+  height: 10%;
+  margin-bottom: 10px;
+  border-radius: 7px;
+  border-style: none;
+  background-color: black;
+  color: white;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TryButton = styled.button`
+  width: 100%;
+  height: 10%;
+  border-style: none;
+  background-color: white;
+  color: #525252;
+`;
