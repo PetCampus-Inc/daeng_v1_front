a0758df9d16fcd7439c612b5e25625ff6b1b775b
hyerani <hran9404@naver.com>
2023-11-04 21:51:11 +0900

    Feat: add postcode api (#18) (#19)

--- src/components/SignUp/Step/step4.tsx
+++ src/components/SignUp/Step/step4.tsx
@@ -11,6 +11,7 @@ import InputBoxAndText from "components/SignIn/InputBoxAndText";
 import Button from "components/common/Button";
 import { REGISTRATION_REGEX, SCHOOL_PHONE_REGEX } from "constants/validCheck";
 import { ThemeConfig } from "styles/ThemeConfig";
+import Postcode from "components/common/Postcode";
 
 interface Props {
   currentStep: number;
@@ -40,6 +41,7 @@ const Step4 = ({
   const [isSchoolPhoneValid, setIsSchoolPhoneValid] = useState(false);
   const [isRegistrationValid, setIsRegistrationValid] = useState(false);
   const [isClicked, setIsClicked] = useState(false);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
 
   const handleValidCheck = () => {
     setIsClicked(true);
@@ -79,7 +81,6 @@ const Step4 = ({
     if (value.length > 12) {
       value = value.substring(0, 12);
     }
-
     setSchoolNum(
       value
         .replace(/[^0-9]/g, "")
@@ -89,106 +90,130 @@ const Step4 = ({
   };
 
   useEffect(() => {
+    schoolAddress !== "" && setIsPopupOpen(false);
     SCHOOL_PHONE_REGEX.test(schoolPhone)
       ? setIsSchoolPhoneValid(true)
       : setIsSchoolPhoneValid(false);
-  }, [schoolPhone]);
+  }, [schoolPhone, schoolAddress]);
 
   return (
-    <Container padding_top="35%">
-      <Header
-        type="back"
-        handleClick={() => {
-          setCurrentStep(currentStep - 1);
-        }}
-      />
-      <TextWrapper margin_bottom="8%">
-        <Text
-          text={"박유빈 원장님"}
-          size="1.4rem"
-          weight="bold"
-          height="2rem"
-        />
-        <Text
-          text={"유치원 정보를 입력해 주세요"}
-          size="1.4rem"
-          weight="bold"
-          height="2rem"
-        />
-      </TextWrapper>
-      <InputBoxWrapper height="70%">
-        <InputBoxAndText
-          text="유치원명"
-          type="text"
-          placeholder="유치원 이름을 입력해 주세요"
-          inputValue={schoolName}
-          setInputValue={setSchoolName}
-        />
-        <InputBoxAndText
-          text="유치원 연락처"
-          placeholder="유치원 연락처를 입력해 주세요"
-          type="text"
-          inputValue={schoolPhone}
-          setInputValue={setSchoolPhone}
-          onChange={handleSchoolPhoneChange}
-        />
-        <InputBoxAndText
-          text="유치원 주소"
-          placeholder="주소를 검색해 주세요"
-          type="search"
-          inputValue={schoolAddress}
-          setInputValue={setSchoolAddress}
-        />
-        <InputBoxAndText
-          text="사업자 등록번호"
-          placeholder="사업자 등록번호를 입력해 주세요"
-          type="check"
-          inputValue={schoolNum}
-          setInputValue={setSchoolNum}
-          handleClick={handleValidCheck}
-          onChange={handleSchoolNumChange}
-          errorText={
-            isClicked
-              ? isRegistrationValid
-                ? ""
-                : "올바르지 않은 사업자 등록 번호입니다."
-              : ""
-          }
-        />
-      </InputBoxWrapper>
-      <StyledBottomWrapper>
-        <Button
-          width="90%"
-          height="70%"
-          text={"등록하기"}
-          weight="bold"
-          size="1.1rem"
-          handleClick={() => {
-            schoolName !== "" &&
-              schoolAddress !== "" &&
-              isSchoolPhoneValid &&
-              isRegistrationValid &&
-              setCurrentStep(currentStep + 1);
-          }}
-          backcolor={
-            schoolName === "" ||
-            !isSchoolPhoneValid ||
-            !isRegistrationValid ||
-            schoolAddress === ""
-              ? ThemeConfig.gray_5
-              : ThemeConfig.primaryColor
-          }
-          textcolor={
-            schoolName === "" ||
-            !isSchoolPhoneValid ||
-            !isRegistrationValid ||
-            schoolAddress === ""
-              ? ThemeConfig.gray_3
-              : ThemeConfig.white
-          }
-        />
-      </StyledBottomWrapper>
-    </Container>
+    <>
+      {isPopupOpen && (
+        <Container padding_top="25%">
+          <Header
+            type="text"
+            text="주소 검색"
+            handleClick={() => setIsPopupOpen(false)}
+          />
+          <Postcode
+            schoolAddress={schoolAddress}
+            setSchoolAddress={setSchoolAddress}
+          />
+        </Container>
+      )}
+      {!isPopupOpen && (
+        <Container padding_top="35%">
+          <Header
+            type="back"
+            handleClick={() => {
+              setCurrentStep(currentStep - 1);
+            }}
+          />
+
+          <TextWrapper margin_bottom="8%">
+            <Text
+              text={"박유빈 원장님"}
+              size="1.4rem"
+              weight="bold"
+              height="2rem"
+            />
+            <Text
+              text={"유치원 정보를 입력해 주세요"}
+              size="1.4rem"
+              weight="bold"
+              height="2rem"
+            />
+          </TextWrapper>
+          <InputBoxWrapper height="70%">
+            <InputBoxAndText
+              text="유치원명"
+              type="text"
+              placeholder="유치원 이름을 입력해 주세요"
+              inputValue={schoolName}
+              setInputValue={setSchoolName}
+            />
+            <InputBoxAndText
+              text="유치원 연락처"
+              placeholder="유치원 연락처를 입력해 주세요"
+              type="text"
+              inputValue={schoolPhone}
+              setInputValue={setSchoolPhone}
+              onChange={handleSchoolPhoneChange}
+            />
+            <InputBoxAndText
+              text="유치원 주소"
+              placeholder="주소를 검색해 주세요"
+              type="search"
+              inputValue={schoolAddress}
+              setInputValue={setSchoolAddress}
+              handleClick={() => {
+                setSchoolAddress("");
+                setIsPopupOpen(!isPopupOpen);
+              }}
+            />
+
+            <InputBoxAndText
+              text="사업자 등록번호"
+              placeholder="사업자 등록번호를 입력해 주세요"
+              type="check"
+              inputValue={schoolNum}
+              setInputValue={setSchoolNum}
+              handleClick={handleValidCheck}
+              onChange={handleSchoolNumChange}
+              errorText={
+                isClicked
+                  ? isRegistrationValid
+                    ? ""
+                    : "올바르지 않은 사업자 등록 번호입니다."
+                  : ""
+              }
+            />
+          </InputBoxWrapper>
+          <StyledBottomWrapper>
+            <Button
+              width="90%"
+              height="70%"
+              text={"등록하기"}
+              weight="bold"
+              size="1.1rem"
+              handleClick={() => {
+                schoolName !== "" &&
+                  schoolAddress !== "" &&
+                  isSchoolPhoneValid &&
+                  isRegistrationValid &&
+                  setCurrentStep(currentStep + 1);
+              }}
+              backcolor={
+                schoolName === "" ||
+                !isSchoolPhoneValid ||
+                !isRegistrationValid ||
+                schoolAddress === ""
+                  ? ThemeConfig.gray_5
+                  : ThemeConfig.primaryColor
+              }
+              textcolor={
+                schoolName === "" ||
+                !isSchoolPhoneValid ||
+                !isRegistrationValid ||
+                schoolAddress === ""
+                  ? ThemeConfig.gray_3
+                  : ThemeConfig.white
+              }
+            />
+          </StyledBottomWrapper>
+        </Container>
+      )}
+    </>
   );
 };
 
