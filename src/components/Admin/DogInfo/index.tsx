35ce1da6c65adfca74f4960d8ebc5e047d31b4ea
hyerani <hran9404@naver.com>
2023-12-13 17:11:35 +0900

    Refactor: remove comment

--- src/components/Admin/DogInfo/index.tsx
+++ src/components/Admin/DogInfo/index.tsx
@@ -93,14 +93,14 @@ const DogInfo = () => {
         <StyledCalendar
           onChange={onChange}
           value={value}
-          formatDay={(locale, date) => moment(date).format("D")} // 일 제거 숫자만 보이게
-          formatYear={(locale, date) => moment(date).format("YYYY")} // 네비게이션 눌렀을때 숫자 연도만 보이게
-          formatMonthYear={(locale, date) => moment(date).format("YYYY. MM")} // 네비게이션에서 2023. 12 이렇게 보이도록 설정
-          calendarType="gregory" // 일요일 부터 시작
-          showNeighboringMonth={false} // 전달, 다음달 날짜 숨기기
-          next2Label={null} // +1년 & +10년 이동 버튼 숨기기
-          prev2Label={null} // -1년 & -10년 이동 버튼 숨기기
-          minDetail="year" // 10년단위 년도 숨기기
+          formatDay={(locale, date) => moment(date).format("D")}
+          formatYear={(locale, date) => moment(date).format("YYYY")}
+          formatMonthYear={(locale, date) => moment(date).format("YYYY. MM")}
+          calendarType="gregory"
+          showNeighboringMonth={false}
+          next2Label={null}
+          prev2Label={null}
+          minDetail="year"
           tileContent={({ date, view }) => {
             let html = [];
             if (
--- src/components/Admin/DogInfo/styles.ts
+++ src/components/Admin/DogInfo/styles.ts
@@ -95,52 +95,43 @@ export const StyledCalendarWrapper = styled.div`
     background-color: white;
   }
 
-  // 전체 폰트 컬러
   .react-calendar__month-view {
     abbr {
       color: ${(props) => props.theme.gray_1};
     }
   }
 
-  // 네비게이션 가운데 정렬
   .react-calendar__navigation {
     justify-content: center;
   }
 
-  // 네비게이션 폰트 설정
   .react-calendar__navigation button {
     font-weight: 800;
     font-size: 1rem;
   }
 
-  // 네비게이션 버튼 컬러
   .react-calendar__navigation button:focus {
     background-color: white;
   }
 
-  // 네비게이션 비활성화 됐을때 스타일
   .react-calendar__navigation button:disabled {
     background-color: white;
     color: ${(props) => props.theme.darkBlack};
   }
 
-  // 년/월 상단 네비게이션 칸 크기 줄이기
   .react-calendar__navigation__label {
     flex-grow: 0 !important;
   }
 
-  // 요일 밑줄 제거
   .react-calendar__month-view__weekdays abbr {
     text-decoration: none;
     font-weight: 800;
   }
 
-  // 일요일에만 빨간 폰트
   .react-calendar__month-view__weekdays__weekday--weekend abbr[title="일요일"] {
     color: ${(props) => props.theme.red_1};
   }
 
-  // 오늘 날짜 폰트 컬러
   .react-calendar__tile--now {
     background: none;
     abbr {
@@ -148,14 +139,12 @@ export const StyledCalendarWrapper = styled.div`
     }
   }
 
-  // 네비게이션 월 스타일 적용
   .react-calendar__year-view__months__month {
     border-radius: 0.8rem;
     background-color: ${(props) => props.theme.gray_5};
     padding: 0;
   }
 
-  // 네비게이션 현재 월 스타일 적용
   .react-calendar__tile--hasActive {
     background-color: ${(props) => props.theme.primary_2};
     abbr {
@@ -163,13 +152,11 @@ export const StyledCalendarWrapper = styled.div`
     }
   }
 
-  //일 날짜 간격
   .react-calendar__tile {
     padding: 5px 0px 18px;
     position: relative;
   }
 
-  // 네비게이션 월 스타일 적용
   .react-calendar__year-view__months__month {
     flex: 0 0 calc(33.3333% - 10px) !important;
     margin-inline-start: 5px !important;
@@ -181,7 +168,6 @@ export const StyledCalendarWrapper = styled.div`
     color: ${(props) => props.theme.gray_1};
   }
 
-  // 선택한 날짜 스타일 적용
   .react-calendar__tile:enabled:hover,
   .react-calendar__tile:enabled:focus,
   .react-calendar__tile--active {
