dd3c4621d677ec4c0b4775fa08662b8f79eda265
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-02-05 07:13:56 +0900

    Feat: add PATH constants and update router configuration

---
+++ src/constants/path.ts
@@ -0,0 +1,14 @@
+export const PATH = {
+  ROOT: "/",
+  HOME: "/home",
+  LOGIN: "/signIn",
+  SIGNUP: "/signUp",
+  ADMIN: "/admin",
+  ADMIN_ATTENDANCE: "/admin/attendance",
+  ADMIN_ATTEND_CARE: "/admin/attendCare",
+  ADMIN_DOG_INFO: "/admin/dogInfo",
+  ADMIN_SCHOOL_MANAGE: "/admin/schoolManage",
+  OWNER: "/owner",
+  OWNER_MA: "/owner/membershipApplication",
+  REDIRECT: "/auth/:provider"
+} as const;
