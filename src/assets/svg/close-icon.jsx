55e9f8285f4dd8fa71717bf12b1904c59c8bb6b0
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-02-05 04:52:05 +0900

    Feat: add CloseIcon and UploadIcon

---
+++ src/assets/svg/close-icon.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const CloseIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="none" viewBox="0 0 25 25">
+      <circle cx="12.311" cy="12.365" r="8.096" fill="#B5B5B5" />
+      <path
+        fill="#E9E9E9"
+        fillRule="evenodd"
+        d="M8.94 8.992a.954.954 0 0 0 0 1.35l2.024 2.024L8.94 14.39a.954.954 0 0 0 1.349 1.35l2.024-2.025 2.025 2.024a.954.954 0 1 0 1.349-1.35l-2.024-2.023 2.023-2.024a.954.954 0 0 0-1.349-1.35l-2.024 2.024-2.023-2.024a.954.954 0 0 0-1.35 0Z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+};
+
+export default CloseIcon;
