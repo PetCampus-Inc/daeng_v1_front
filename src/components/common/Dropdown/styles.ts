374ee8f4f6b6c8787d61abce9eea4b5f73e7310f
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-02-05 03:13:46 +0900

    Feat: Add DropDown component

---
+++ src/components/common/Dropdown/styles.ts
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const List = styled.ul<{ width: string }>`
+  position: absolute;
+  background-color: ${({ theme }) => theme.colors.white};
+  border: 1px solid ${({ theme }) => theme.colors.gray_4};
+  border-radius: 8px;
+  width: ${({ width }) => width};
+  max-height: 192px;
+
+  overflow: scroll;
+  scroll-snap-type: x mandatory;
+  scroll-snap-stop: always;
+  scrollbar-width: thin;
+
+  &.no-list {
+    overflow: auto;
+    padding: 30px 16px 16px;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+  }
+`;
+export const BoldText = styled.span`
+  ${({ theme }) => theme.typo.body2_16_B}
+  color: ${({ theme }) => theme.colors.gray_1};
+`;
+export const ThinText = styled.span`
+  ${({ theme }) => theme.typo.label2_14_R}
+  color: ${({ theme }) => theme.colors.gray_2};
+`;
+
+export const ListItem = styled.li`
+  display: flex;
+  padding: 12px 12px 12px 16px;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 25px;
+  height: 48px;
+  color: ${({ theme }) => theme.colors.gray_3};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray_4};
+  scroll-snap-align: start;
+  scroll-snap-stop: always;
+  ${({ theme }) => theme.typo.body1_16_R}
+`;
