e6cda236b7409515ef3e55f780fd068cc5dcd25b
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-02-06 07:22:54 +0900

    Refactor: refactor Checkbox component

--- src/components/common/Checkbox/index.tsx
+++ src/components/common/Checkbox/index.tsx
@@ -1,16 +1,23 @@
 import { BsCheck } from "react-icons/bs";
+import { useController, UseControllerProps } from "react-hook-form";
 
 import * as S from "./styles";
+import { ChangeEvent } from "react";
 
-interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
+export interface CheckboxProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "defaultValue">,
+    UseControllerProps {
+  isChecked: boolean;
+  isRequired?: boolean;
   ariaLabel?: string;
   ariaLabelledby?: string;
   ariaDescribedby?: string;
-  isChecked?: boolean;
-  name?: string;
-  children?: React.ReactNode;
-  id?: string;
+  children: React.ReactNode;
   disabled?: boolean;
+  variant?: "square" | "default";
+  id?: string;
+  name: string;
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
 const Checkbox = ({
@@ -18,38 +25,58 @@ const Checkbox = ({
   ariaLabelledby,
   ariaDescribedby,
   isChecked = false,
-  name,
   value,
   children,
-  id,
   disabled = false,
-  ...props
-}: CheckboxProps) => (
-  <S.CheckboxContainer htmlFor={id}>
-    <S.HiddenCheckbox
-      type="checkbox"
-      aria-label={ariaLabel}
-      aria-labelledby={ariaLabelledby}
-      aria-describedby={ariaDescribedby}
-      id={id}
-      checked={isChecked}
-      name={name}
-      disabled={disabled}
-      {...props}
-    />
-    <S.Checkbox
+  variant = "default",
+  isRequired = false,
+  id,
+  onChange,
+  ...useControllerProps
+}: CheckboxProps) => {
+  const { field } = useController(useControllerProps);
+  const { onChange: fieldOnChange, ...restFieldProps } = field;
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    fieldOnChange(e);
+
+    if (onChange) {
+      onChange(e);
+    }
+  };
+
+  return (
+    <S.CheckboxContainer
+      htmlFor={id}
+      variant={variant}
       className={isChecked ? "checked" : ""}
       aria-disabled={disabled ? "true" : undefined}
     >
-      <BsCheck className="checkbox-icon" />
-    </S.Checkbox>
-    <S.LabelText
-      className={isChecked ? "checked" : ""}
-      aria-disabled={disabled ? "true" : undefined}
-    >
-      {children}
-    </S.LabelText>
-  </S.CheckboxContainer>
-);
+      <S.HiddenCheckbox
+        type="checkbox"
+        aria-label={ariaLabel}
+        aria-labelledby={ariaLabelledby}
+        aria-describedby={ariaDescribedby}
+        id={id}
+        checked={field.value ?? false}
+        disabled={disabled}
+        onChange={handleChange}
+        {...restFieldProps}
+      />
+      <S.Checkbox
+        className={isChecked ? "checked" : ""}
+        aria-disabled={disabled ? "true" : undefined}
+      >
+        <BsCheck className="checkbox-icon" />
+      </S.Checkbox>
+      <S.LabelText
+        className={isChecked ? "checked" : ""}
+        aria-disabled={disabled ? "true" : undefined}
+      >
+        {children}
+      </S.LabelText>
+    </S.CheckboxContainer>
+  );
+};
 
 export default Checkbox;
