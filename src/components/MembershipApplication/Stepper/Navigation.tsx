d81f9c9d84cf1ff40a05e59d8b090c233dc556ce
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-02-05 14:06:35 +0900

    Feat: add Navigation component to MembershipApplication Stepper

---
+++ src/components/MembershipApplication/Stepper/Navigation.tsx
@@ -0,0 +1,39 @@
+import * as S from "./styles";
+
+interface NavigationProps {
+  currentStep: number;
+  stepsLength: number;
+  nextStep: () => void;
+  prevStep: () => void;
+}
+
+const Navigation = ({ currentStep, stepsLength, nextStep, prevStep }: NavigationProps) => {
+  let captions;
+  if (currentStep === stepsLength - 1) {
+    captions = [
+      "정보를 모두 입력해야 가입신청이 가능합니다.",
+      "제출하신 후에는 수정이 불가하니, 꼼꼼히 확인해 주세요."
+    ];
+  } else {
+    captions = ["정보를 모두 입력해야 가입신청이 가능합니다."];
+  }
+
+  return (
+    <S.ButtonContainer>
+      <S.Caption>
+        {captions.map((text, index) => (
+          <p key={index}>{text}</p>
+        ))}
+      </S.Caption>
+      <S.ButtonWrapper>
+        {currentStep > 0 && currentStep < stepsLength - 1 && (
+          <S.PrevButton onClick={prevStep}>이전</S.PrevButton>
+        )}
+        {currentStep < stepsLength - 1 && <S.Button onClick={nextStep}>다음</S.Button>}
+        {currentStep === stepsLength - 1 && <S.Button type="submit">제출하기</S.Button>}
+      </S.ButtonWrapper>
+    </S.ButtonContainer>
+  );
+};
+
+export default Navigation;
