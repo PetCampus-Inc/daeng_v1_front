8f4dcd339a92aac9a56d934ffc9e53447fa0d1f6
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-02-06 00:15:55 +0900

    Refactor: Change QueryClient settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,21 @@
 import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
-import { BrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import { ThemeConfig } from "./styles/ThemeConfig";
 import { ThemeProvider } from "styled-components";
 import router from "router";
-import App from "App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const queryClient = new QueryClient();
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <ThemeProvider theme={ThemeConfig}>
-    <RecoilRoot>
-      <GlobalStyle />
-      <RouterProvider router={router} />
-    </RecoilRoot>
-  </ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={ThemeConfig}>
+      <RecoilRoot>
+        <GlobalStyle />
+        <RouterProvider router={router} />
+      </RecoilRoot>
+    </ThemeProvider>
+  </QueryClientProvider>
 );
