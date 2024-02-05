5e0f8452950ae3d8b1d669130d6eeb3d293ef6f0
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-02-05 17:18:48 +0900

    Refactor: refactor navigation component

--- src/libs/common/captions.ts
+++
@@ -1,8 +0,0 @@
-export const getCaptions = (currentStep: number, stepsLength: number) => {
-  return currentStep === stepsLength - 1
-    ? [
-        "정보를 모두 입력해야 가입신청이 가능합니다.",
-        "제출하신 후에는 수정이 불가하니, 꼼꼼히 확인해 주세요."
-      ]
-    : ["정보를 모두 입력해야 가입신청이 가능합니다."];
-};
