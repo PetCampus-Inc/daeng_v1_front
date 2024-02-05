138f4a85a8281d9597513644a5b5efc9d4f0c993
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-02-05 14:07:46 +0900

    Feat: add MembershipApplication component Layout

--- src/components/MembershipApplication/index.tsx
+++ src/components/MembershipApplication/index.tsx
@@ -1,38 +1,83 @@
 import { FormProvider, useForm } from "react-hook-form";
 import useStep from "hooks/useStep";
 
-import DogInfo from "./Form/DogInfo";
 import DogOwnerInfo from "./Form/DogOwnerInfo";
+import DogInfo from "./Form/DogInfo";
 import NoticeInfo from "./Form/NoticeInfo";
 import PickDropInfo from "./Form/PickDropInfo";
 import TicketInfo from "./Form/TicketInfo";
-import StepNavigation from "./Form/StepNavigation";
+import Navigation from "./Stepper/Navigation";
 
 import * as S from "./styles";
+import Indicator from "./Stepper/Indicator";
 
-const stepsComponents = [DogInfo, DogOwnerInfo, NoticeInfo, PickDropInfo, TicketInfo];
+// FIXME: 이 부분 위치 수정 필요합니다.
+const step = [
+  {
+    title: "견주 정보를 입력해 주세요",
+    subtitle: "아래 내용을 빠짐없이 입력해 주세요",
+    indicator: "견주 정보",
+    component: DogOwnerInfo
+  },
+  {
+    title: "강아지 정보를 입력해 주세요",
+    subtitle: "아래 내용을 빠짐없이 입력해 주세요",
+    indicator: "강아지 정보",
+    component: DogInfo
+  },
+  {
+    title: "이용권 종류를 선택해 주세요",
+    subtitle: "아래 내용을 빠짐없이 입력해 주세요",
+    indicator: "이용권 정보",
+    component: TicketInfo
+  },
+  {
+    title: "유의사항을 확인해주세요",
+    subtitle: "필수입력에 동의하지 않으면 가입이 어려울 수 있어요",
+    indicator: "유의사항",
+    component: NoticeInfo
+  },
+  {
+    title: "픽드랍 정보를 입력해 주세요",
+    subtitle: "아래 내용을 빠짐없이 입력해 주세요",
+    indicator: "픽드랍",
+    component: PickDropInfo
+  }
+];
 
 const MembershipApplication = () => {
   const methods = useForm({
     mode: "onBlur",
     shouldUnregister: false
   });
-  const { currentStep, nextStep, prevStep } = useStep(0, stepsComponents.length - 1);
+  const { currentStep, nextStep, prevStep, setStep } = useStep(0, step.length - 1);
 
-  const CurrentComponent = stepsComponents[currentStep];
+  const CurrentComponent = step[currentStep].component;
+  const currentTitle = step[currentStep].title;
+  const currentSubtitle = step[currentStep].subtitle;
+  const indicators: string[] = step.map((s) => s.indicator);
 
   return (
-    <>
+    <S.Container>
+      <S.TopWrapper>
+        <S.TitleWrapper>
+          <S.Title>{currentTitle}</S.Title>
+          <S.SubTitle>{currentSubtitle}</S.SubTitle>
+        </S.TitleWrapper>
+        <Indicator indicators={indicators} currentStep={currentStep} goToStep={setStep} />
+      </S.TopWrapper>
       <FormProvider {...methods}>
-        <CurrentComponent />
-        <StepNavigation
+        <S.ContentWrapper>
+          <CurrentComponent />
+        </S.ContentWrapper>
+        <Navigation
           currentStep={currentStep}
-          stepsLength={stepsComponents.length}
+          stepsLength={step.length}
           nextStep={nextStep}
           prevStep={prevStep}
         />
       </FormProvider>
-    </>
+    </S.Container>
   );
 };
 
--- src/components/MembershipApplication/styles.ts
+++ src/components/MembershipApplication/styles.ts
@@ -1,1 +1,42 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  padding: 28px 12px 42px;
+  flex-direction: column;
+  gap: 28px;
+
+  background-color: ${({ theme }) => theme.colors.BGray};
+`;
+
+export const TopWrapper = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+
+  gap: 12px;
+`;
+
+export const TitleWrapper = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+
+  gap: 3px;
+`;
+
+export const Title = styled.h1`
+  ${({ theme }) => theme.typo.title2_20_B}
+`;
+export const SubTitle = styled.h3`
+  ${({ theme }) => theme.typo.body2_16_R};
+  color: ${({ theme }) => theme.colors.gray_3};
+`;
+
+export const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 4px;
+
+  border-radius: 12px;
+  background-color: ${({ theme }) => theme.colors.white};
+`;
