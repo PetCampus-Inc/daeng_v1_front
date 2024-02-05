e4a5cecae3b534366e4c4271c16c38318dd412b0
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-02-06 03:54:36 +0900

    Feat: Add temporary dogOwnerInfo page

--- src/components/MembershipApplication/Form/DogOwnerInfo.tsx
+++ src/components/MembershipApplication/Form/DogOwnerInfo.tsx
@@ -1,7 +1,64 @@
-import React from "react";
+import { useState } from "react";
+import { Card } from "./styles";
+import Title from "components/common/Title";
+import InputBox from "components/common/InputBox";
+import SingleRadio from "components/common/Select/SingleRadio";
 
 const DogOwnerInfo = () => {
-  return <div>DogOwnerInfo</div>;
+  const [name, setName] = useState<string>("");
+  const [address, setAddress] = useState<string>("");
+  const [phoneNum, setPhoneNum] = useState<string>("");
+  const [emergencyNum, setEmergencyNum] = useState<string>("");
+
+  return (
+    <>
+      <Card>
+        <Title>이름</Title>
+        <InputBox
+          name="memberName"
+          width="100%"
+          height="49px"
+          inputValue={name}
+          setInputValue={setName}
+        />
+      </Card>
+      <Card>
+        <Title>성별</Title>
+        <SingleRadio name="memberGender" radiosText={["남", "여"]} />
+      </Card>
+      <Card>
+        <Title>주소</Title>
+        <InputBox
+          name="address"
+          type="search"
+          width="100%"
+          height="49px"
+          inputValue={address}
+          setInputValue={setAddress}
+        />
+      </Card>
+      <Card>
+        <Title>연락처</Title>
+        <InputBox
+          name="phoneNumber"
+          width="100%"
+          height="49px"
+          inputValue={phoneNum}
+          setInputValue={setPhoneNum}
+        />
+      </Card>
+      <Card>
+        <Title>비상 연락처</Title>
+        <InputBox
+          name="phoneNumber"
+          width="100%"
+          height="49px"
+          inputValue={emergencyNum}
+          setInputValue={setEmergencyNum}
+        />
+      </Card>
+    </>
+  );
 };
 
 export default DogOwnerInfo;
