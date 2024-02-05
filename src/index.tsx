8d93b40cb764b218a0fb9c4d3508b569351e3382
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-02-06 07:16:27 +0900

    Refactor: fix App and index files

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,24 @@
 import ReactDOM from "react-dom/client";
+import router from "router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RecoilRoot } from "recoil";
-import { BrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import { ThemeConfig } from "./styles/ThemeConfig";
 import { ThemeProvider } from "styled-components";
-import router from "router";
-import App from "App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <ThemeProvider theme={ThemeConfig}>
-    <RecoilRoot>
-      <GlobalStyle />
-      <RouterProvider router={router} />
-    </RecoilRoot>
-  </ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={ThemeConfig}>
+      <RecoilRoot>
+        <GlobalStyle />
+        <RouterProvider router={router} />
+      </RecoilRoot>
+    </ThemeProvider>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
