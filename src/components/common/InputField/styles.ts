096c696f559cdcd5bdb43c292a4941e02d8c4b1f
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-02-06 22:39:47 +0900

    Feat: add InputField component

---
+++ src/components/common/InputField/styles.ts
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  border: 1px solid ${({ theme }) => theme.colors.gray_4};
+
+  padding: 12px 18px;
+  border-radius: 8px;
+
+  color: ${({ theme }) => theme.colors.gray_1};
+  ${({ theme }) => theme.typo.body2_16_R};
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.gray_3};
+  }
+
+  &:focus {
+    outline: none;
+    border: 1px solid ${({ theme }) => theme.colors.br_3};
+    box-shadow: ${({ theme }) => theme.colors.br_3} 0px 0px 0px 1px;
+  }
+
+  outline: transparent solid 2px;
+  outline-offset: 2px;
+  overflow-y: hidden;
+
+  transition:
+    border,
+    border-color,
+    box-shadow 0.2s ease-out;
+
+  &::-webkit-outer-spin-button,
+  &::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+  }
+
+  &[type="number"] {
+    -moz-appearance: textfield;
+  }
+`;
