2f35ff87c894c9cdf9aa9691d67318d579d30894
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-02-07 04:02:04 +0900

    Feat: Linkage of dog owner registration application data

--- src/components/MembershipApplication/Form/DogInfo.tsx
+++ src/components/MembershipApplication/Form/DogInfo.tsx
@@ -4,38 +4,35 @@ import BreedInput from "../ChooseBreed";
 import SelectNumber from "components/common/Select/SelectNumber";
 import { daysArray, monthsArray, yearsArray } from "constants/date";
 import Title from "components/common/Title";
-import InputBox from "components/common/InputBox";
 import TextArea from "components/common/TextArea";
-import { Card } from "./styles";
+import { Caption, Card } from "./styles";
+import { useFormContext } from "react-hook-form";
+import { ITEM_KEYS } from "constants/item";
+import InputField from "components/common/InputField";
+import ImageUpload from "components/common/ImageUpload";
 
 interface DogInfoProps {
   requiredItems: Map<number, boolean>;
 }
 
 const DogInfo = ({ requiredItems }: DogInfoProps) => {
-  const [name, setName] = useState<string>("");
   const [inputValue, setInputValue] = useState<string>("");
   const [chosenBreedId, setChosenBreedId] = useState<number | null>(null);
   //post할 때 chosenBreedId가 있으면 이걸로 보내고(꼭 얘를 먼저 검증), 없으면 inputValue를 보내면 된다.
+  const { register, control, watch } = useFormContext();
 
   return (
     <>
       <Card>
-        <Title>이름</Title>
-        <InputBox
-          name="dogName"
-          width="100%"
-          height="49px"
-          inputValue={name}
-          setInputValue={setName}
-        />
+        <Title isRequired={requiredItems.get(ITEM_KEYS.DOG_NAME)}>이름</Title>
+        <InputField control={control} name="dogName" placeholder="강아지 이름을 입력해주세요" />
       </Card>
       <Card>
-        <Title>성별</Title>
+        <Title isRequired={requiredItems.get(ITEM_KEYS.DOG_GENDER)}>성별</Title>
         <SingleRadio name="dogGender" radiosText={["수컷", "암컷"]} />
       </Card>
       <Card>
-        <Title>크기</Title>
+        <Title isRequired={requiredItems.get(ITEM_KEYS.DOG_SIZE)}>크기</Title>
         <SingleRadio
           name="dogSize"
           caption="~7kg 소형견 / ~ 15kg 중형견 / 15kg 이상 대형견"
@@ -43,7 +40,7 @@ const DogInfo = ({ requiredItems }: DogInfoProps) => {
         />
       </Card>
       <Card>
-        <Title>견종</Title>
+        <Title isRequired={requiredItems.get(ITEM_KEYS.DOG_BREED)}>견종</Title>
         <BreedInput
           inputValue={inputValue}
           setInputValue={setInputValue}
@@ -52,7 +49,7 @@ const DogInfo = ({ requiredItems }: DogInfoProps) => {
         />
       </Card>
       <Card>
-        <Title>생일</Title>
+        <Title isRequired={requiredItems.get(ITEM_KEYS.DOG_BIRTHDAY)}>생일</Title>
         <div style={{ display: "flex", gap: "5px" }}>
           <SelectNumber numberList={yearsArray} initialValue={"2000"} />
           <SelectNumber numberList={monthsArray} initialValue={"01"} />
@@ -60,18 +57,27 @@ const DogInfo = ({ requiredItems }: DogInfoProps) => {
         </div>
       </Card>
       <Card>
-        <Title>중성화 여부</Title>
+        <Title isRequired={requiredItems.get(ITEM_KEYS.NEUTRALIZATION)}>중성화 여부</Title>
         <SingleRadio name="neutralization" radiosText={["했어요", "안했어요"]} />
       </Card>
       <Card>
-        <Title>예방접종 여부</Title>
+        <Title isRequired={requiredItems.get(ITEM_KEYS.VACCINATION)}>예방접종 여부</Title>
         <SingleRadio name="vaccination" radiosText={["했어요", "안했어요"]} />
       </Card>
+      {watch("vaccination") === "했어요" && (
+        <Card>
+          <Title isRequired={requiredItems.get(ITEM_KEYS.VACCINATION_FILE)}>
+            예방접종 파일 첨부
+          </Title>
+          <Caption>최근 1년 내 접종 기록 증명을 위해 jpg, png 형태로 업로드해 주세요</Caption>
+          <ImageUpload />
+        </Card>
+      )}
       <Card>
-        <Title>알러지 및 질병 유무</Title>
+        <Title isRequired={requiredItems.get(ITEM_KEYS.ALLERGY_DISEASE)}>알러지 및 질병 유무</Title>
         <TextArea
+          register={register}
           name="allergyDisease"
-          autoResize
           placeholder="알러지나 질병이 있다면 상세히 입력해주세요."
         />
       </Card>
--- src/components/MembershipApplication/Form/MemberInfo.tsx
+++ src/components/MembershipApplication/Form/MemberInfo.tsx
@@ -1,4 +1,11 @@
+import { Card } from "./styles";
+import Title from "components/common/Title";
+import SingleRadio from "components/common/Select/SingleRadio";
 import type { IMemberDto } from "types/School.type";
+import InputField from "components/common/InputField";
+import { useFormContext } from "react-hook-form";
+import SearchInputField from "components/common/InputField/SearchInputField";
+import { ITEM_KEYS } from "constants/item";
 
 interface MemberInfoProps {
   info: IMemberDto;
@@ -6,7 +13,43 @@ interface MemberInfoProps {
 }
 
 const MemberInfo = ({ info, requiredItems }: MemberInfoProps) => {
-  return <div>{info.memberName}</div>;
+  const { control } = useFormContext();
+
+  return (
+    <>
+      <Card>
+        <Title isRequired={requiredItems.get(ITEM_KEYS.MEMBER_NAME)}>이름</Title>
+        <InputField control={control} name="memberName" placeholder="견주 이름을 입력해주세요" />
+      </Card>
+      <Card>
+        <Title>성별</Title>
+        <SingleRadio name="memberGender" radiosText={["남", "여"]} />
+      </Card>
+      <Card>
+        <Title isRequired={requiredItems.get(ITEM_KEYS.MEMBER_ADDRESS)}>주소</Title>
+        <SearchInputField
+          control={control}
+          name="address"
+          placeholder="주소를 입력해주세요"
+          onSearch={() => {
+            console.log("클릭");
+          }}
+        />
+      </Card>
+      <Card>
+        <Title isRequired={requiredItems.get(ITEM_KEYS.MEMBER_PHONE)}>연락처</Title>
+        <InputField control={control} name="phoneNumber" placeholder="연락처를 입력해주세요" />
+      </Card>
+      <Card>
+        <Title isRequired={requiredItems.get(ITEM_KEYS.EMERGENCY_NUMBER)}>비상 연락처</Title>
+        <InputField
+          control={control}
+          name="emergencyNumber"
+          placeholder="비상 연락처를 입력해주세요"
+        />
+      </Card>
+    </>
+  );
 };
 
 export default MemberInfo;
