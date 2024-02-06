5bbe70dd444239a306680cf79bdad8a695c7735f
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-02-06 22:40:08 +0900

    Feat: add SearchInputField component

---
+++ src/components/common/InputField/SearchInputField/styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const SearchInputWrapper = styled.div`
+  width: 100%;
+  position: relative;
+`;
+
+export const SearchInputButton = styled.button`
+  all: unset;
+
+  height: 100%;
+  width: 17%;
+  margin-right: 5%;
+  aspect-ratio: 1/1;
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  right: 0%;
+  transform: translateY(-50%);
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
