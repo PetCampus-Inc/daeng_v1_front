37fdbc7cc4e3e44801dd2c2141a9087a7a86e043
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-02-06 23:30:15 +0900

    Feat: Add DropDown Component and API integration (#42)
    * Chore: Add @ariakit/react and lodash dependencies
    
    * Refactor: Refactor InputBox Icon Size
    
    * Feat: Add DropDown component
    
    * Refactor: Refactor Dropdown component by adding ChosenBreedId
    
    * Feat: Add BreedInput component and useDetectClose hook
    
    * Feat: Add birthday select dropdown component
    
    * Feat: Add QueryClientProvider and useGetBreed hook
    
    * Refactor: Change file name MultiCheckBox to DayMultiCheck component and refactor SingleRadio component
    
    * Chore: Remove unused dependencies
    
    * Fix: Control Duplicate click & Time adjustment to prevent flickering
    
    * Refactor: Remove unused import in Dropdown component

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import styled from "styled-components";
 import { Outlet } from "react-router-dom";
 import Navbar from "components/common/NavBar";
 import Header from "components/common/Header";
+import { QueryClient } from "@tanstack/query-core";
+import { QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <>
-      <Header type="main" />
-      <Main>
-        <Outlet />
-      </Main>
-      <Navbar />
+      <QueryClientProvider client={queryClient}>
+        <Header type="main" />
+        <Main>
+          <Outlet />
+        </Main>
+        <Navbar />
+      </QueryClientProvider>
     </>
   );
 };
