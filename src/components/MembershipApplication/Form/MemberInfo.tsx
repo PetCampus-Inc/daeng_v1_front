8877ceb34dac12dab8292e0ab4082c8c9e6083b0
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-02-08 00:31:52 +0900

    Refactor: update MemberInfo component to include validation

--- src/components/MembershipApplication/Form/MemberInfo.tsx
+++ src/components/MembershipApplication/Form/MemberInfo.tsx
@@ -1,11 +1,15 @@
-import { Card } from "./styles";
+import { useFormContext } from "react-hook-form";
+
 import Title from "components/common/Title";
 import SingleRadio from "components/common/Select/SingleRadio";
-import type { IMemberDto } from "types/School.type";
 import InputField from "components/common/InputField";
-import { useFormContext } from "react-hook-form";
 import SearchInputField from "components/common/InputField/SearchInputField";
 import { ITEM_KEYS } from "constants/item";
+import { NAME_REGEX, PHONE_REGEX } from "constants/validCheck";
+import { formatPhoneNumber } from "utils/formatter";
+
+import { Card } from "./styles";
+import type { IMemberDto } from "types/School.type";
 
 interface MemberInfoProps {
   info: IMemberDto;
@@ -13,13 +17,30 @@ interface MemberInfoProps {
 }
 
 const MemberInfo = ({ info, requiredItems }: MemberInfoProps) => {
-  const { control } = useFormContext();
+  const { control, setValue } = useFormContext();
+
+  const handleChange = (field: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    const formattedValue = formatPhoneNumber(value);
+    setValue(field, formattedValue);
+  };
 
   return (
     <>
       <Card>
         <Title isRequired={requiredItems.get(ITEM_KEYS.MEMBER_NAME)}>이름</Title>
-        <InputField control={control} name="memberName" placeholder="견주 이름을 입력해주세요" />
+        <InputField
+          control={control}
+          name="memberName"
+          rules={{
+            required: requiredItems.get(ITEM_KEYS.MEMBER_NAME),
+            pattern: {
+              value: NAME_REGEX,
+              message: "한글 1~10자로 입력해주세요"
+            }
+          }}
+          placeholder="견주 이름을 입력해주세요"
+        />
       </Card>
       <Card>
         <Title>성별</Title>
@@ -38,13 +59,33 @@ const MemberInfo = ({ info, requiredItems }: MemberInfoProps) => {
       </Card>
       <Card>
         <Title isRequired={requiredItems.get(ITEM_KEYS.MEMBER_PHONE)}>연락처</Title>
-        <InputField control={control} name="phoneNumber" placeholder="연락처를 입력해주세요" />
+        <InputField
+          control={control}
+          name="phoneNumber"
+          rules={{
+            required: requiredItems.get(ITEM_KEYS.MEMBER_PHONE),
+            pattern: {
+              value: PHONE_REGEX,
+              message: "휴대폰 번호를 입력해주세요 (ex. 010-1234-5678)"
+            }
+          }}
+          onChange={handleChange("phoneNumber")}
+          placeholder="연락처를 입력해주세요"
+        />
       </Card>
       <Card>
         <Title isRequired={requiredItems.get(ITEM_KEYS.EMERGENCY_NUMBER)}>비상 연락처</Title>
         <InputField
           control={control}
           name="emergencyNumber"
+          rules={{
+            required: requiredItems.get(ITEM_KEYS.EMERGENCY_NUMBER),
+            pattern: {
+              value: PHONE_REGEX,
+              message: "휴대폰 번호를 입력해주세요 (ex. 010-1234-5678)"
+            }
+          }}
+          onChange={handleChange("emergencyNumber")}
           placeholder="비상 연락처를 입력해주세요"
         />
       </Card>
