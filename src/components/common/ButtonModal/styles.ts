ed4477e37cede54c27659684cb6c4e87de7a5ef6
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-02-09 15:52:55 +0900

    Refactor: refactor ButtonModal Component

--- src/components/common/ButtonModal/styles.ts
+++ src/components/common/ButtonModal/styles.ts
@@ -14,7 +14,7 @@ export const Container = styled.div`
   align-items: center;
 `;
 
-export const StyledMainWrapper = styled.div<{
+export const MainWrapper = styled.div<{
   height?: string;
   paddingtop?: string;
 }>`
@@ -29,11 +29,49 @@ export const StyledMainWrapper = styled.div<{
   align-items: center;
 `;
 
-export const StyledButtonWrapper = styled.div`
+export const TextWrapper = styled.div`
   display: flex;
-  height: 100%;
-  width: 100%;
-  justify-content: space-between;
+  flex-direction: column;
+  align-items: center;
+  height: 80%;
 
-  padding-top: 10%;
+  gap: 4px;
+`;
+
+export const MainText = styled.p`
+  ${({ theme }) => theme.typo.title2_20_B};
+  color: ${({ theme }) => theme.colors.black};
+`;
+
+export const SubText = styled.p`
+  ${({ theme }) => theme.typo.body2_16_R};
+  color: ${({ theme }) => theme.colors.gray_2};
+`;
+
+export const ButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  gap: 8px;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  border-radius: 8px;
+
+  padding: 3%;
+`;
+
+export const CloseButton = styled(Button)`
+  ${({ theme }) => theme.typo.label1_16_R};
+
+  background-color: ${({ theme }) => theme.colors.gray_4};
+  color: ${({ theme }) => theme.colors.gray_2};
+`;
+
+export const ActButton = styled(Button)`
+  ${({ theme }) => theme.typo.label1_16_B};
+
+  background-color: ${({ theme }) => theme.colors.primaryColor};
+  color: ${({ theme }) => theme.colors.white};
 `;
