29cb509eafaaf56f34d9346c42c9f47558c4b7ad
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-02-09 12:50:42 +0900

    Typo: Edit fill-rule, clip-rule

--- src/components/common/Modal/styles.ts
+++ src/components/common/Modal/styles.ts
@@ -40,7 +40,7 @@ export const StyledCloseImage = styled.div`
   margin-right: 12px;
   margin-top: 12px;
   cursor: pointer;
-  background: url("data:image/svg+xml,%3Csvg width='16px' height='16px' viewBox='0 0 16 16' class='bi bi-x' fill='currentColor' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill-rule='evenodd' d='M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z'/%3E%3C/svg%3E")
+  background: url("data:image/svg+xml,%3Csvg width='16px' height='16px' viewBox='0 0 16 16' class='bi bi-x' fill='currentColor' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fillRule='evenodd' d='M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z'/%3E%3C/svg%3E")
     no-repeat right top;
   background-size: 36px;
   width: 36px;
