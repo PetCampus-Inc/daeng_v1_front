f11c64d5f4365efafe5c813cc3622b21219d8683
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-02-10 08:32:17 +0900

    Feat: add TicketCounter component

---
+++ src/components/SchoolManage/TicketCounter/index.tsx
@@ -0,0 +1,34 @@
+import type { Dispatch, SetStateAction } from "react";
+import AddIcon from "assets/svg/add-icon";
+import SubtractIcon from "assets/svg/subtract-icon";
+import * as S from "../styles";
+
+interface Props {
+  initial: number;
+  counter: number;
+  setCounter: Dispatch<SetStateAction<number>>;
+}
+
+const TicketCounter = ({ initial, counter, setCounter }: Props) => {
+  return (
+    <S.ContentWrapper>
+      <S.TextWrapper>
+        <S.Title>회차권 유형을 추가해 주세요</S.Title>
+        <S.ErrorMessage>이미 추가되어 있어요 횟수를 변경해 주세요</S.ErrorMessage>
+      </S.TextWrapper>
+      <S.Counter>
+        <S.Button onClick={() => setCounter((prev) => (prev > initial ? prev - 1 : prev))}>
+          <SubtractIcon />
+        </S.Button>
+        <S.Text>
+          {counter} <span>회</span>
+        </S.Text>
+        <S.Button onClick={() => setCounter((prev) => prev + 1)}>
+          <AddIcon />
+        </S.Button>
+      </S.Counter>
+    </S.ContentWrapper>
+  );
+};
+
+export default TicketCounter;
