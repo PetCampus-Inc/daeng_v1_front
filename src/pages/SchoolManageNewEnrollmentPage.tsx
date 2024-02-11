6123c80a94b10482da0c49e65931459dfd136721
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-02-11 21:10:17 +0900

    Feat: New registration page router settings

---
+++ src/pages/SchoolManageNewEnrollmentPage.tsx
@@ -0,0 +1,12 @@
+import Header from "components/common/Header"
+
+const SchoolManageNewEnrollmentPage = () => {
+  return (
+    <>
+        <Header type="text" text="신규가입"/>
+        <div>SchoolManageNewEnrollmentPage</div>
+    </>
+  )
+}
+
+export default SchoolManageNewEnrollmentPage
