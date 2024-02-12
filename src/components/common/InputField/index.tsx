502002c9dca391bbda5c5fc4d0df0b5c41a485cb
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-02-13 06:10:30 +0900

    Feat: Change Input design & Add disabled attribute

--- src/components/common/InputField/index.tsx
+++ src/components/common/InputField/index.tsx
@@ -4,16 +4,16 @@ import { Control, UseControllerProps, useController } from "react-hook-form";
 import * as S from "./styles";
 
 export interface InputFieldProps
-  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "defaultValue" | "name">,
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "defaultValue" | "name" | "disabled">,
     UseControllerProps {
   control: Control;
 }
 
-const InputField = ({ control, name, rules, defaultValue, ...inputProps }: InputFieldProps) => {
+const InputField = ({ control, name, rules, defaultValue, disabled=false, ...inputProps }: InputFieldProps) => {
   const { field } = useController({ control, name, rules, defaultValue });
 
   return (
-    <S.Input {...field} id={field.name} value={field.value ? field.value : ""} {...inputProps} />
+    <S.Input {...field} id={field.name} value={field.value ? field.value : ""} disabled={disabled} {...inputProps} />
   );
 };
 
