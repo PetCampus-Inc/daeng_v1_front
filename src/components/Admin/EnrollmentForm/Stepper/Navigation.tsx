13a3c1550202e6771ed2164658304a7eb9b20ac8
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-02-13 05:28:28 +0900

    Refactor: separate description constants form Navigation

--- src/components/Admin/EnrollmentForm/Stepper/Navigation.tsx
+++ src/components/Admin/EnrollmentForm/Stepper/Navigation.tsx
@@ -20,19 +20,13 @@ const Navigation = ({ currentStep, stepsLength, nextStep, prevStep }: Navigation
   };
 
   return (
-    <S.ButtonContainer>
-      <S.Caption>
-        <p>정보를 모두 입력해야 가입신청이 가능합니다.</p>
-        {isLastStep && <p>제출하신 후에는 수정이 불가하니, 꼼꼼히 확인해 주세요.</p>}
-      </S.Caption>
-      <S.ButtonWrapper>
-        {showPrevButton(currentStep, stepsLength) && (
-          <S.PrevButton onClick={prevStep}>이전</S.PrevButton>
-        )}
-        {showNextButton(currentStep, stepsLength) && <S.Button onClick={nextStep}>다음</S.Button>}
-        {isLastStep && <SubmitButton />}
-      </S.ButtonWrapper>
-    </S.ButtonContainer>
+    <S.ButtonWrapper>
+      {showPrevButton(currentStep, stepsLength) && (
+        <S.PrevButton onClick={prevStep}>이전</S.PrevButton>
+      )}
+      {showNextButton(currentStep, stepsLength) && <S.Button onClick={nextStep}>다음</S.Button>}
+      {isLastStep && <SubmitButton />}
+    </S.ButtonWrapper>
   );
 };
 
