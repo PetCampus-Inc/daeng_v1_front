8ef1297edde5f59a91eba5232f70ef9e1a1a4644
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-02-12 14:57:38 +0900

    Feat: Add TitleWthIcon component

--- src/pages/SchoolManageNewEnrollmentPage.tsx
+++ src/pages/SchoolManageNewEnrollmentPage.tsx
@@ -1,10 +1,14 @@
 import Header from "components/common/Header"
+import TitleWithIcon from "components/Admin/SchoolManage/TitleWithIcon"
+import ListIcon from "assets/svg/list-icon"
 
 const SchoolManageNewEnrollmentPage = () => {
   return (
     <>
-        <Header type="text" text="신규가입"/>
-        <div>SchoolManageNewEnrollmentPage</div>
+      <Header type="text" text="신규가입"/>
+      <div style={{margin:`calc(5vh + 32px) 16px` }}>
+        <TitleWithIcon title="신규가입" icon={<ListIcon/>} />
+      </div>
     </>
   )
 }
