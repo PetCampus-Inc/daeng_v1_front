1c141251013cf7ea0156e6a4397809130ae232cb
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-02-12 14:57:38 +0900

    Feat: Add TitleWthIcon component

---
+++ src/assets/svg/list-icon.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const ListIcon = () => {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="24" height="24" rx="4" fill="#EEE3D9" />
+      <path
+        d="M4 6C4 4.89543 4.89543 4 6 4H18C19.1046 4 20 4.89543 20 6V18C20 19.1046 19.1046 20 18 20H6C4.89543 20 4 19.1046 4 18V6Z"
+        fill="white"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M6 3H18C19.6569 3 21 4.34315 21 6V18C21 19.6569 19.6569 21 18 21H6C4.34315 21 3 19.6569 3 18V6C3 4.34315 4.34315 3 6 3ZM6 4C4.89543 4 4 4.89543 4 6V18C4 19.1046 4.89543 20 6 20H18C19.1046 20 20 19.1046 20 18V6C20 4.89543 19.1046 4 18 4H6Z"
+        fill="#E4CAB1"
+      />
+      <path d="M18 7L18 8L9 8L9 7L18 7Z" fill="#858585" />
+      <path d="M18 10L18 11L9 11L9 10L18 10Z" fill="#858585" />
+      <path d="M18 13L18 14L9 14L9 13L18 13Z" fill="#858585" />
+      <path d="M18 16L18 17L9 17L9 16L18 16Z" fill="#858585" />
+      <circle cx="6.5" cy="7.5" r="0.5" fill="#956F4C" />
+      <circle cx="6.5" cy="10.5" r="0.5" fill="#956F4C" />
+      <circle cx="6.5" cy="13.5" r="0.5" fill="#956F4C" />
+      <circle cx="6.5" cy="16.5" r="0.5" fill="#956F4C" />
+    </svg>
+  );
+};
+
+export default ListIcon;
