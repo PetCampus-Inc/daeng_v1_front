c6e00b6ae94588451928f01e0824e5d772999701
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-02-13 00:05:40 +0900

    Refactor: separate ticket form and add useTicketFieldArray hook

--- src/components/SchoolManage/Form/TicketInfo.tsx
+++ src/components/SchoolManage/Form/TicketInfo.tsx
@@ -1,45 +1,16 @@
-import { useState } from "react";
-import { useForm, useFieldArray } from "react-hook-form";
-
-import XIcon from "assets/svg/x-icon";
-import BottomSheet from "components/common/BottomSheet";
-import EditableRadioGroup from "components/common/Select/EditableRadioGroup";
-import TicketCounter from "../TicketCounter";
-
-import * as S from "./styles";
+import { useForm } from "react-hook-form";
+import RoundTicketType from "./RoundTicketType";
+import MonthlyTicketType from "./MonthlyTicketType";
 
 // FIXME: 실제 폼 형태로 수정 필요
 const TicketInfo = () => {
-  const initialCounter = 2;
   const { control, handleSubmit } = useForm();
-  const [isBottomSheetVisible, setBottomSheetVisible] = useState(false);
-  const [counter, setCounter] = useState<number>(initialCounter);
-
-  const { fields, append, remove } = useFieldArray({
-    control,
-    name: "myRadioGroup"
-  });
-
-  const handleAddRadio = () => {
-    append({ value: counter.toString(), label: `${counter}회` });
-    setBottomSheetVisible(false);
-    setCounter(initialCounter);
-  };
 
   return (
     <div>
-      <button onClick={() => setBottomSheetVisible(true)}>직접 추가하기</button>
-      {isBottomSheetVisible && (
-        <BottomSheet onClose={() => setBottomSheetVisible(false)}>
-          <S.CloseButton type="button" onClick={() => setBottomSheetVisible(false)}>
-            <XIcon />
-          </S.CloseButton>
-          <TicketCounter initial={initialCounter} counter={counter} setCounter={setCounter} />
-          <S.ConfirmButton onClick={handleAddRadio}>추가</S.ConfirmButton>
-        </BottomSheet>
-      )}
       <form onSubmit={handleSubmit((data) => console.log(data))}>
-        <EditableRadioGroup control={control} name="myRadioGroup" fields={fields} remove={remove} />
+        <RoundTicketType control={control} />
+        <MonthlyTicketType control={control} />
         <input type="submit" />
       </form>
     </div>
