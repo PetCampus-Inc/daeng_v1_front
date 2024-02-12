3477ef139739dce8e02fab9289d2ae658d6f5351
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-02-13 06:45:43 +0900

    Feat: Add useAdminEnrollQuery hook and update EnrollmentForm component

--- src/components/Admin/EnrollmentForm/index.tsx
+++ src/components/Admin/EnrollmentForm/index.tsx
@@ -1,4 +1,5 @@
 import { FormProvider, useForm } from "react-hook-form";
+import { useAdminEnrollQuery } from "hooks/api/useAdminEnrollQuery";
 import useStep from "hooks/common/useStep";
 
 import MemberInfo from "./Form/MemberInfo";
@@ -8,26 +9,25 @@ import PickDropInfo from "./Form/PickDropInfo";
 import TicketInfo from "./Form/TicketInfo";
 import Navigation from "./Stepper/Navigation";
 import Indicator from "./Stepper/Indicator";
-
-import * as S from "./styles";
-import { ADMIN_CREATE_FORM_STEP, ADMIN_READ_FORM_STEP } from "constants/step";
 import SubmitButton from "./Stepper/SubmitButton";
+
 import { FORM_DESC } from "constants/desc";
+import { ADMIN_CREATE_FORM_STEP, ADMIN_READ_FORM_STEP } from "constants/step";
+import * as S from "./styles";
 
 interface EnrollmentFormProps {
   type: "READ" | "CREATE" | "EDIT";
 }
-
+// FIXME: CREATE | READ & EDIT 으로 분리 고려해볼 것.
 const EnrollmentForm = ({ type }: EnrollmentFormProps) => {
+  const { data } = useAdminEnrollQuery({ type, formId: "1" });
   const methods = useForm({
     mode: "onBlur",
     shouldUnregister: false
   });
 
   const currentSteps = type === "READ" ? ADMIN_READ_FORM_STEP : ADMIN_CREATE_FORM_STEP;
-
   const { currentStep, nextStep, prevStep, setStep } = useStep(0, currentSteps.length - 1);
-
   const currentTitle = currentSteps[currentStep].title;
   const currentSubtitle = currentSteps[currentStep].subtitle;
   const indicators: string[] = currentSteps.map((s) => s.indicator);
@@ -44,11 +44,17 @@ const EnrollmentForm = ({ type }: EnrollmentFormProps) => {
       </S.TopWrapper>
       <FormProvider {...methods}>
         <S.ContentWrapper>
-          {currentStep === 0 && <MemberInfo />}
-          {currentStep === 1 && <DogInfo />}
-          {currentStep === 2 && <TicketInfo />}
-          {currentStep === 3 && <PolicyInfo />}
-          {currentStep === 4 && <PickDropInfo />}
+          {currentStep === 0 && <MemberInfo requiredItems={data?.requiredItemsMap} />}
+          {currentStep === 1 && <DogInfo requiredItems={data?.requiredItemsMap} />}
+          {currentStep === 2 && (
+            <TicketInfo info={data?.ticketInfo} requiredItems={data?.requiredItemsMap} />
+          )}
+          {currentStep === 3 && (
+            <PolicyInfo info={data?.policyInfo} requiredItems={data?.requiredItemsMap} />
+          )}
+          {currentStep === 4 && (
+            <PickDropInfo info={data?.pickDropInfo} requiredItems={data?.requiredItemsMap} />
+          )}
         </S.ContentWrapper>
         {type !== "READ" && (
           <S.ButtonContainer>
---
+++ src/hooks/api/useAdminEnrollQuery.ts
@@ -0,0 +1,72 @@
+import { useQuery } from "@tanstack/react-query";
+import { handleGetAdminEnrollment } from "apis/school.api";
+import type { IAdminEnrollmentProps } from "apis/school.api";
+import type {
+  IEnrollment,
+  IMemberDto,
+  IPickDropInfo,
+  IPolicyInfo,
+  ITicketInfo
+} from "types/School.type";
+
+interface AdminEnrollQueryProps extends IAdminEnrollmentProps {
+  type: "READ" | "CREATE" | "EDIT";
+}
+
+interface EnrollmentProps {
+  requiredItemsMap: Map<number, boolean>;
+  memberInfo: IMemberDto | null;
+  ticketInfo: ITicketInfo;
+  policyInfo: IPolicyInfo;
+  pickDropInfo: IPickDropInfo;
+}
+
+export const useAdminEnrollQuery = ({ type, formId }: AdminEnrollQueryProps) => {
+  const enlistmentQuery = useQuery<IEnrollment, Error, EnrollmentProps>({
+    queryKey: ["enrollment", formId],
+    queryFn: () => handleGetAdminEnrollment({ formId }),
+    enabled: type !== "CREATE",
+    select: (data) => {
+      // 이용권 정보
+      const selectTicketInfo = () => ({
+        priceInfo: data.priceInfo,
+        ticketType: data.ticketType,
+        roundTicketNumber: data.roundTicketNumber,
+        openDays: data.openDays,
+        monthlyTicketNumber: data.monthlyTicketNumber,
+        ticketInfo: data.ticketInfo
+      });
+
+      // 정책 정보
+      const selectPolicyInfo = () => ({
+        limitsInfo: data.limitsInfo,
+        accidentInfo: data.accidentInfo,
+        abandonmentInfo: data.abandonmentInfo
+      });
+
+      // 픽드랍 정보
+      const selectPickDrop = () => ({
+        pickDropState: data.pickDropState,
+        pickDropMemo: data.pickDropMemo,
+        pickDropInfo: data.pickDropInfo,
+        pickDropNotice: data.pickDropNotice
+      });
+
+      // 필수 항목 리스트
+      const requiredItemsList = JSON.parse(data.requiredItemList);
+      const requiredItemsMap: Map<number, boolean> = new Map(
+        requiredItemsList.map((itemNumber: number) => [itemNumber, true])
+      );
+
+      return {
+        requiredItemsMap,
+        memberInfo: data.memberDto,
+        ticketInfo: selectTicketInfo(),
+        policyInfo: selectPolicyInfo(),
+        pickDropInfo: selectPickDrop()
+      };
+    }
+  });
+
+  return enlistmentQuery;
+};
