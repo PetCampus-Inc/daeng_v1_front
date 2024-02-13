3d3e3ebfb8da81bfcfe520e0403af44e3759edb1
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-02-13 16:19:04 +0900

    Feat: Add SimpleApplication component

---
+++ src/components/Admin/SchoolManage/SimpleMembershipApplication/styles.ts
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 19px 20px;
+  background-color: ${({ theme }) => theme.colors.white};
+  border-radius: 8px;
+  border: 1px solid ${({ theme }) => theme.colors.gray_4};
+  box-shadow: ${({ theme }) => theme.shadows.card};
+`;
+
+export const LeftBox = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Image = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 12px;
+`;
+
+export const TextWrapper = styled.div`
+  margin-left: 16px;
+  gap: 2px;
+`;
+
+export const Title = styled.p`
+  ${({ theme }) => theme.typo.body2_16_B};
+  color: ${({ theme }) => theme.colors.gray_1};
+  white-space: nowrap;
+`;
+
+export const MiddleText = styled.p`
+  ${({ theme }) => theme.typo.label2_14_R};
+  color: ${({ theme }) => theme.colors.gray_2};
+  white-space: nowrap;
+`;
+
+export const Date = styled.p`
+  ${({ theme }) => theme.typo.caption1_12_R};
+  color: ${({ theme }) => theme.colors.gray_3};
+  white-space: nowrap;
+`;
