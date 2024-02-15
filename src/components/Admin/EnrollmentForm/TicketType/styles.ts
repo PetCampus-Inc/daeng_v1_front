9b6599563d921d5198e5b8648ca1a753f2c620d5
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-02-15 23:16:37 +0900

    Refactor: separate ticket type components and styles

--- src/components/Admin/EnrollmentForm/TicketType/styles.ts
+++ src/components/Admin/EnrollmentForm/TicketType/styles.ts
@@ -26,3 +26,35 @@ export const ConfirmButton = styled.button`
 
   ${({ theme }) => theme.typo.label1_16_B};
 `;
+
+export const AddButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 15px 0px;
+  gap: 4px;
+
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.colors.br_4};
+  color: ${({ theme }) => theme.colors.primaryColor};
+
+  ${({ theme }) => theme.typo.caption1_12_R};
+  line-height: 0;
+
+  & > svg {
+    color: ${({ theme }) => theme.colors.br_2};
+  }
+
+  &:disabled {
+    border: 1px solid ${({ theme }) => theme.colors.gray_4};
+    background-color: ${({ theme }) => theme.colors.gray_5};
+
+    color: ${({ theme }) => theme.colors.gray_4};
+  }
+
+  &:disabled > svg {
+    color: ${({ theme }) => theme.colors.gray_3};
+  }
+
+  transition: background-color 0.2s ease-in-out;
+`;
