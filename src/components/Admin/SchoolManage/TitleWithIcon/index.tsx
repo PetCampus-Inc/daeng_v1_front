9caf7972e38733783d5d981acc0288cadd0b19a5
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-02-16 04:17:15 +0900

    Feat: Update TitleWithIcon and ButtonBadge component

--- src/components/Admin/SchoolManage/TitleWithIcon/index.tsx
+++ src/components/Admin/SchoolManage/TitleWithIcon/index.tsx
@@ -7,11 +7,13 @@ interface ITitleWithIcon {
   title: string;
   icon: ReactNode;
   handleClick?: () => void;
+  children?: ReactNode;
 }
 
-const TitleWithIcon = ({ title, icon, handleClick }: ITitleWithIcon) => {
+const TitleWithIcon = ({ title, icon, handleClick, children }: ITitleWithIcon) => {
   return (
-    <StyledTextWrapper style={{ margin: "0 0 12px" }}>
+    <StyledTextWrapper style={{ margin: "0 0 12px", width: "100%" }}>
+      {children}
       <Title>{title}</Title>
       <IconWrapper onClick={handleClick}>{icon}</IconWrapper>
     </StyledTextWrapper>
