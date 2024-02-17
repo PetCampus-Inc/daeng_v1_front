3c3b13da532f114e92a29cba3ff267ef3f7196bf
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-02-17 17:20:25 +0900

    Feat: Update MultiCheck (preview mode, defaultChecked, validation available)

--- src/components/common/Select/MultiCheck/index.tsx
+++ src/components/common/Select/MultiCheck/index.tsx
@@ -4,25 +4,40 @@ import * as S from "../styles";
 
 interface IMulticheck extends ISelect, Omit<React.InputHTMLAttributes<HTMLInputElement>, "name"> {
   radiosText: string[];
+  defaultSelect?: string;
+  isRequired?: boolean;
+  isPreviewMode?: boolean;
 }
 
-const MultiCheck = ({ radiosText, name, disabled, ...props }: IMulticheck) => {
+const MultiCheck = ({
+  radiosText,
+  name,
+  disabled,
+  defaultSelect,
+  isRequired = false,
+  isPreviewMode = false,
+  ...props
+}: IMulticheck) => {
   const { register } = useFormContext();
 
   return (
     <S.Container>
       <S.RadioContainer>
-        {radiosText.map((text: any) => (
+        {radiosText.map((text: string) => (
           <div style={{ width: "100%" }} key={text}>
             <S.StyledInput
               id={text}
               type="checkbox"
-              {...register(`${name}`)}
+              {...register(`${name}`, { required: isRequired })}
               value={text}
               disabled={disabled}
+              defaultChecked={defaultSelect === text}
+              className={isPreviewMode ? "preview" : ""}
               {...props}
             />
-            <S.StyledLabel htmlFor={text}>{text}</S.StyledLabel>
+            <S.StyledLabel htmlFor={text} className={isPreviewMode ? "preview" : ""}>
+              {text}
+            </S.StyledLabel>
           </div>
         ))}
       </S.RadioContainer>
