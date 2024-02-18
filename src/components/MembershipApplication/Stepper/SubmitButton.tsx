84b208954d53eb013e6e41210fbb88602739e046
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-02-19 00:53:46 +0900

    Refactor: refactor form submission logic and add form transformer

--- src/components/MembershipApplication/Stepper/SubmitButton.tsx
+++ src/components/MembershipApplication/Stepper/SubmitButton.tsx
@@ -1,13 +1,12 @@
 import { useFormContext } from "react-hook-form";
 import { useEnrollMutation } from "hooks/api/useEnrollMutation";
-import { formatDate, extractNumber, getMapValue } from "utils/formatter";
-import type { IRequestEnrollment, TPickDropRequest, TTicketType } from "types/School.type";
-import type { ItemMapValue } from "constants/item";
 
 import * as S from "./styles";
+import { EnrollmentFormTransformer } from "utils/formTransformer";
 
 const SubmitButton = () => {
   const {
+    control,
     handleSubmit,
     formState: { isValid }
   } = useFormContext();
@@ -16,47 +15,19 @@ const SubmitButton = () => {
 
   // FIXME: schoolFormId, memberId, fileUrl 추가 필요
   const onSubmit = handleSubmit((data) => {
-    const address = [data.address.street, data.address.detail].filter(Boolean).join(" ");
-    const requestData: IRequestEnrollment = {
-      schoolFormId: 1,
-      memberId: 1,
-      breedId: data.breedId,
-      newBreed: data.breedId ? "" : data.dogBreed,
-      fileUrl: "",
-      memberName: data.memberName,
-      address,
-      phoneNumber: data.phoneNumber,
-      emergencyNumber: data.emergencyNumber || "",
-      dogName: data.dogName,
-      allergyDisease: data.allergyDisease,
-      attendanceDays: data.attendanceDays,
-      pickDropMemo: data.pickDropMemo,
-      birthDate: formatDate(data.year, data.month, data.day),
-      memberGender: getMapValue(data.memberGender) as string,
-      dogGender: getMapValue(data.dogGender) as string,
-      dogSize: getMapValue(data.dogSize) as string,
-      neutralization: (getMapValue(data.neutralization) as ItemMapValue).neutralization,
-      vaccination: (getMapValue(data.vaccination) as ItemMapValue).vaccination,
-      pickDropRequest: getMapValue(data.pickDropRequest) as TPickDropRequest,
-      pickDropType: getMapValue(data.pickDropType) as string,
-      ticketType: getMapValue(data.ticketType) as TTicketType,
-      monthlyTicketNumber: extractNumber(data.monthlyTicketNumber),
-      roundTicketNumber: extractNumber(data.roundTicketNumber)
-    };
+    const transformer = new EnrollmentFormTransformer(data);
+    const requestData = transformer.transform();
 
     console.log(requestData);
     // enrollMutation(requestData);
   });
 
   return (
-    <S.Button
-      type="submit"
-      onClick={onSubmit}
-      aria-disabled={isValid ? "true" : undefined}
-      aria-label="제출하기"
-    >
-      제출하기
-    </S.Button>
+    <>
+      <S.Button type="submit" onClick={onSubmit} aria-label="제출하기">
+        제출하기
+      </S.Button>
+    </>
   );
 };
 
