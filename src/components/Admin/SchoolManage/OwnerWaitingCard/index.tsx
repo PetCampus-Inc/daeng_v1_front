e0d0ff812baa2e631c039a7bcba0281ca30573f5
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-02-18 22:51:33 +0900

    Refactor: Approve and deny Button component separation

--- src/components/Admin/SchoolManage/OwnerWaitingCard/index.tsx
+++ src/components/Admin/SchoolManage/OwnerWaitingCard/index.tsx
@@ -2,33 +2,16 @@ import ListIconSmallRound from "assets/svg/list-icon-small-round";
 import { TextWrapper, Name, Contour, PhoneNum } from "../TeacherInfo/styles";
 import * as S from "./styles";
 import RightArrow from "assets/svg/right-arrow";
-import { useState } from "react";
-import { AnimatePresence, easeIn, easeOut } from "framer-motion";
-import { ThemeConfig } from "styles/ThemeConfig";
+import { AnimatePresence } from "framer-motion";
 import { IWaitingOwnerInfo } from "types/Admin.type";
+import ApproveDenyButton from "../ApproveDenyButton";
 
 interface IOwnerWaitingCard {
   data: IWaitingOwnerInfo;
 }
 
 const OwnerWaitingCard = ({ data }: IOwnerWaitingCard) => {
-  const [button1Visible, setButton1Visible] = useState(true);
-  const [button2Visible, setButton2Visible] = useState(true);
   const { dogName, memberName } = data;
-
-  const handleButton1Click = () => {
-    setButton2Visible(false);
-  };
-
-  const handleButton2Click = () => {
-    setButton1Visible(false);
-  };
-
-  const customVariants = {
-    greenScaleUp: { backgroundColor: ThemeConfig.colors.green, width: "120px" },
-    redScaleUp: { backgroundColor: ThemeConfig.colors.red_1, width: "120px", color: "white" },
-    disappear: { opacity: 0, display: "none" }
-  };
 
   return (
     <AnimatePresence>
@@ -39,39 +22,7 @@ const OwnerWaitingCard = ({ data }: IOwnerWaitingCard) => {
             <Contour>|</Contour>
             <PhoneNum>{dogName}</PhoneNum>
           </TextWrapper>
-          <S.ButtonWrapper>
-            {button1Visible && (
-              <S.ButtonWrapper>
-                <S.Button
-                  variants={customVariants}
-                  initial={{ opacity: 1, scale: 1 }}
-                  animate={!button2Visible && "greenScaleUp"}
-                  exit={{ opacity: 0, y: -20 }}
-                  transition={{ ease: "easeInOut", duration: 0.4 }}
-                  onClick={handleButton1Click}
-                >
-                  승인
-                </S.Button>
-              </S.ButtonWrapper>
-            )}
-
-            {button2Visible && (
-              <S.ButtonWrapper style={{ justifyContent: "flex-end" }}>
-                <S.Button
-                  key="button2"
-                  className="second"
-                  variants={customVariants}
-                  initial={{ opacity: 1, scale: 1 }}
-                  animate={!button1Visible && "redScaleUp"}
-                  exit={{ opacity: 0, y: -20, backgroundColor: "blue" }}
-                  transition={{ ease: "easeInOut", duration: 0.4 }}
-                  onClick={handleButton2Click}
-                >
-                  거절
-                </S.Button>
-              </S.ButtonWrapper>
-            )}
-          </S.ButtonWrapper>
+          <ApproveDenyButton />
         </S.UpperWrapper>
         <S.LowerWrapper>
           <S.TextWrapper>
