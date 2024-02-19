dcc609574dd3dafe7da2bce8213da570579057f3
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-02-20 05:20:20 +0900

    Refactor: Add cases in Badge component

--- src/components/common/Badge/styles.ts
+++ src/components/common/Badge/styles.ts
@@ -5,7 +5,10 @@ import type { BadgeProps } from "./index";
 const badgeStyles = (theme: DefaultTheme) => ({
   required: `background-color: ${theme.colors.yellow_3}; color: ${theme.colors.primaryColor};`,
   adminRequired: `background-color: ${theme.colors.yellow_3}; color: ${theme.colors.primaryColor};`,
-  optional: `background-color: ${theme.colors.gray_5}; color: ${theme.colors.gray_2};`
+  optional: `background-color: ${theme.colors.gray_5}; color: ${theme.colors.gray_2};`,
+  neutralized: `background-color: ${theme.colors.yellow_3}; color: ${theme.colors.primaryColor};`,
+  vaccinated: `background-color: #fff2e8; color: ${theme.colors.primary_3};`,
+  pickdrop: `background-color: ${theme.colors.br_5}; color: ${theme.colors.primaryColor};`
 });
 
 export const Badge = styled.span<{
@@ -19,6 +22,7 @@ export const Badge = styled.span<{
 
   min-width: 50px;
   border-radius: 50px;
+  padding: 2px 10px;
 
   ${({ theme }) => theme.typo.caption1_12_R}
 
