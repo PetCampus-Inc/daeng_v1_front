94e65dd94fe10d0f1496bb43a1149faa98e98d77
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-02-20 05:20:20 +0900

    Refactor: Add cases in Badge component

--- src/components/common/Badge/index.tsx
+++ src/components/common/Badge/index.tsx
@@ -1,14 +1,17 @@
 import * as S from "./styles";
 
 export interface BadgeProps {
-  type: "required" | "optional" | "adminRequired";
+  type: "required" | "optional" | "adminRequired" | "neutralized" | "vaccinated" | "pickdrop";
 }
 
 const Badge = ({ type }: BadgeProps) => {
   const text = new Map([
     ["required", "필수 입력"],
     ["optional", "선택 입력"],
-    ["adminRequired", "원장 입력사항"]
+    ["adminRequired", "원장 입력사항"],
+    ["neutralized", "중성화 완료"],
+    ["vaccinated", "예방접종 완료"],
+    ["pickdrop", "픽드랍 신청"]
   ]);
 
   return <S.Badge type={type}>{text.get(type)}</S.Badge>;
