9addb40529541e25994d81c452bece5c31a929c5
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-02-20 06:33:26 +0900

    Refactor: apply FormToServerAdapter on SubmitButton component

--- src/components/Admin/EnrollmentForm/Stepper/SubmitButton.tsx
+++ src/components/Admin/EnrollmentForm/Stepper/SubmitButton.tsx
@@ -1,16 +1,15 @@
-import { useFormContext } from "react-hook-form";
-
+import { useState } from "react";
+import { FieldValues, useFormContext } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
 import { enrollmentFormAtom } from "store/form";
-
-import { extractTicketValues, getMapValue } from "utils/formatter";
-import { useNavigate } from "react-router-dom";
+import { Adapter } from "libs/Adapter";
+import { FormToServerAdapter } from "libs/Adapter/FormToServerAdapter";
+import ButtonModal from "components/common/ButtonModal";
 import { PATH } from "constants/path";
 
-import type { IRequestForm, TPickDropState, TTicketType } from "types/School.type";
+import type { IRequestAdminEnrollment } from "types/School.type";
 import * as S from "./styles";
-import ButtonModal from "components/common/ButtonModal";
-import { useState } from "react";
 interface SubmitButtonProps {
   type?: "READ" | "CREATE" | "EDIT";
 }
@@ -27,34 +26,13 @@ const SubmitButton = ({ type }: SubmitButtonProps) => {
   const text = type === "EDIT" ? "수정 완료" : "가입 신청서 등록";
 
   const onSubmit = handleSubmit((data) => {
-    console.log(isValid);
     if (!isValid) setIsModalOpen(true);
 
-    const requiredFields = Object.entries(data.requiredItemList)
-      .filter(([key, value]) => value)
-      .map(([key]) => parseInt(key, 10));
+    const requestData = Adapter.from(data).to<FieldValues, IRequestAdminEnrollment>((item) =>
+      new FormToServerAdapter(item).adapt()
+    );
 
-    const monthlyTicketNumbers = extractTicketValues(data.monthlyTicketNumber || []);
-    const roundTicketNumbers = extractTicketValues(data.roundTicketNumber || []);
-
-    const requestData: IRequestForm = {
-      schoolId: 1,
-      adminId: 2,
-      formName: null,
-      requiredItemList: requiredFields || [],
-      priceInfo: data.priceInfo || "",
-      ticketType: [getMapValue(data.ticketType) as TTicketType],
-      roundTicketNumber: roundTicketNumbers,
-      openDays: data.openDays || [],
-      monthlyTicketNumber: monthlyTicketNumbers,
-      ticketInfo: data.ticketInfo || "",
-      limitsInfo: data.limitsInfo || "",
-      accidentInfo: data.accidentInfo || "",
-      abandonmentInfo: data.abandonmentInfo || "",
-      pickDropState: getMapValue(data.pickDropState) as TPickDropState,
-      pickDropInfo: data.pickDropInfo || "",
-      pickDropNotice: data.pickDropNotice || ""
-    };
+    console.log(requestData);
 
     setEnrollmentForm(requestData);
     navigate(PATH.ADMIN_SUBMIT_FORM);
