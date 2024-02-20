4a98da7cd991fbeafe6fb37e099efe2c94d078b6
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-02-20 18:04:25 +0900

    Refactor: TextAreaModal doesn't need to import useForm

--- src/components/common/TextAreaModal/index.tsx
+++ src/components/common/TextAreaModal/index.tsx
@@ -1,14 +1,14 @@
 import Portal from "../Modal/portal";
 import TextArea from "../TextArea";
 import * as S from "../ButtonModal/styles";
-import { FieldValues, UseFormRegister, useForm } from "react-hook-form";
+import { FieldValues, UseFormRegister } from "react-hook-form";
 
 interface Props {
   children?: React.ReactNode;
   closebutton?: string;
   actionbutton?: string;
   closefunc?: () => void | Promise<void>;
-  actionfunc?: (text: string) => void | Promise<void>;
+  actionfunc?: () => void | Promise<void>;
   name: string;
   register: UseFormRegister<FieldValues>;
 }
@@ -22,13 +22,6 @@ const TextAreaModal = ({
   name,
   register
 }: Props) => {
-  const { handleSubmit } = useForm();
-
-  const onSubmit = handleSubmit((data) => {
-    actionfunc && actionfunc(data.name);
-    closefunc && closefunc();
-  });
-
   return (
     <Portal>
       <S.BackDrop>
@@ -43,7 +36,7 @@ const TextAreaModal = ({
 
           <S.ButtonWrapper>
             {closebutton && <S.CloseButton onClick={closefunc}>{closebutton}</S.CloseButton>}
-            {actionbutton && <S.ActButton onClick={onSubmit}>{actionbutton}</S.ActButton>}
+            {actionbutton && <S.ActButton onClick={actionfunc}>{actionbutton}</S.ActButton>}
           </S.ButtonWrapper>
         </S.MainWrapper>
       </S.BackDrop>
