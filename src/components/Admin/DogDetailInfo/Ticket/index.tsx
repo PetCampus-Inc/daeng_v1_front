8e3e38167ac021a73aabe677cad5d4ae4ff8db63
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-02-23 15:41:49 +0900

    Feat: Add Ticket Detail component in Dog Detail page

--- src/components/Admin/DogDetailInfo/Ticket/index.tsx
+++ src/components/Admin/DogDetailInfo/Ticket/index.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { DogDetailInfoText } from "../DogInfo/styles";
+import { FlexWrapper, InnerContainer } from "../styles";
+import TicketCard from "./TicketCard";
 
 const Ticket = () => {
-  return <div>Ticket</div>;
+  return (
+    <>
+      <InnerContainer>
+        <FlexWrapper>
+          <DogDetailInfoText className="big">이용권 상세정보</DogDetailInfoText>
+          <TicketCard />
+        </FlexWrapper>
+        <FlexWrapper>
+          <DogDetailInfoText className="big">과거 이용권 정보</DogDetailInfoText>
+        </FlexWrapper>
+      </InnerContainer>
+    </>
+  );
 };
 
 export default Ticket;
