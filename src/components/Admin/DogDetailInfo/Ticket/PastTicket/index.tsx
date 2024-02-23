4a2fe8e4fd5892a1b754fbdd86a49ef11246cb90
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-02-23 16:40:35 +0900

    Feat: Add past ticket component

---
+++ src/components/Admin/DogDetailInfo/Ticket/PastTicket/index.tsx
@@ -0,0 +1,12 @@
+import * as S from "../PastTicketCard/styles";
+
+const PastTicket = () => {
+  return (
+    <S.List>
+      <S.ListInnerBox className="left">정기권 12주</S.ListInnerBox>
+      <S.ListInnerBox>2020.0909-2021.09.10</S.ListInnerBox>
+    </S.List>
+  );
+};
+
+export default PastTicket;
