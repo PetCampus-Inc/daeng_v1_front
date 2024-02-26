759e3538096a1baf8b12503cdaf82b162a2fbdd2
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-02-27 04:20:26 +0900

    Feat: Add new SVG icons and update existing ones

--- src/assets/svg/search-icon.jsx
+++ src/assets/svg/search-icon.jsx
@@ -1,6 +1,13 @@
-const SearchIcon = () => {
+const SearchIcon = ({ className = "" }) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="25"
+      height="25"
+      viewBox="0 0 25 25"
+      fill="none"
+      className={className}
+    >
       <path
         fillRule="evenodd"
         clipRule="evenodd"
