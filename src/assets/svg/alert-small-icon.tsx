62af773b8f99377bfe1fa9cc8df3930a473d8d16
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-02-27 04:20:26 +0900

    Feat: Add new SVG icons and update existing ones

---
+++ src/assets/svg/alert-small-icon.tsx
@@ -0,0 +1,26 @@
+interface AlertIconProps {
+  color: "red" | "gray" | "brown";
+}
+
+const AlertSmallIcon = ({ color }: AlertIconProps) => {
+  const colorMap = new Map<string, string[]>([
+    ["red", ["#FAE7E3", "#DD5435", "#FAE7E3"]],
+    ["gray", ["#F7F7F7", "#B5B5B5", "#F7F7F7"]],
+    ["brown", ["#FFF7E1", "#956F4C", "#EEE3D9"]]
+  ]);
+
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none">
+      <rect width="16" height="16" fill={colorMap.get(color)?.[0]} rx="4" />
+      <circle cx="8.001" cy="7.999" r="5.333" fill={colorMap.get(color)?.[1]} />
+      <path
+        fill={colorMap.get(color)?.[2]}
+        fillRule="evenodd"
+        d="M7.999 4.707a.667.667 0 0 0-.667.667V8.04a.667.667 0 0 0 1.333 0V5.374A.667.667 0 0 0 8 4.707Zm0 5.404a.591.591 0 1 0 0 1.183.591.591 0 0 0 0-1.183Z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+};
+
+export default AlertSmallIcon;
---
+++ src/assets/svg/more-icon.tsx
@@ -0,0 +1,12 @@
+const MoreIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+      <path
+        fill="currentColor"
+        d="M9 12a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"
+      />
+    </svg>
+  );
+};
+
+export default MoreIcon;
