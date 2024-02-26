230beeab79affcddda6feeabcd5f5c61a6f560b8
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-02-27 04:36:17 +0900

    Refactor: Add SortSelectBox component for admin attendance sorting

---
+++ src/components/Admin/Attendance/SortSelectBox/index.tsx
@@ -0,0 +1,71 @@
+import { type SetStateAction } from "react";
+import useBottomSheet from "hooks/common/useBottomSheet";
+import BottomSheet from "components/common/BottomSheet";
+import { LIST } from "constants/option";
+import ArrowDownIcon from "assets/svg/arrow-down-icon";
+import { ConfirmButton } from "styles/StyleModule";
+import * as S from "./styles";
+
+type SortSelectBoxProps = {
+  sortName: string;
+  setSortName: React.Dispatch<SetStateAction<string>>;
+};
+
+const SortSelectBox = ({ sortName, setSortName }: SortSelectBoxProps) => {
+  const { isVisible, open, close } = useBottomSheet(false);
+
+  return (
+    <>
+      {isVisible && (
+        <BottomSheet onClose={close}>
+          <S.Container>
+            <S.Title>정렬</S.Title>
+            <S.ListWrapper>
+              <S.ListItem
+                className={sortName === LIST.REGISTERED ? "active" : ""}
+                onClick={async () => {
+                  setSortName(LIST.REGISTERED);
+                }}
+              >
+                {LIST.REGISTERED}
+              </S.ListItem>
+              <S.ListItem
+                className={sortName === LIST.PAYMENT ? "active" : ""}
+                onClick={async () => {
+                  setSortName(LIST.PAYMENT);
+                }}
+              >
+                {LIST.PAYMENT}
+              </S.ListItem>
+              <S.ListItem
+                className={sortName === LIST.DATE ? "active" : ""}
+                onClick={async () => {
+                  setSortName(LIST.DATE);
+                }}
+              >
+                {LIST.DATE}
+              </S.ListItem>
+              <S.ListItem
+                className={sortName === LIST.CHARGE ? "active" : ""}
+                onClick={async () => {
+                  setSortName(LIST.CHARGE);
+                }}
+              >
+                {LIST.CHARGE}
+              </S.ListItem>
+            </S.ListWrapper>
+            <ConfirmButton onClick={() => close()}>닫기</ConfirmButton>
+          </S.Container>
+        </BottomSheet>
+      )}
+      <S.SelectBox onClick={() => open()}>
+        <S.Text>{sortName}</S.Text>
+        <S.ArrowDownButton type="button">
+          <ArrowDownIcon />
+        </S.ArrowDownButton>
+      </S.SelectBox>
+    </>
+  );
+};
+
+export default SortSelectBox;
