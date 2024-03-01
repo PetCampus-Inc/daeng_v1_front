90ef1c148dc5fcefba74289bb8aca656fd81d792
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-01 16:31:43 +0900

    Refactor: Add AttendanceMain component and remove DogSearch component

---
+++ src/components/Admin/Attendance/AttendanceMain.tsx
@@ -0,0 +1,58 @@
+import { type SetStateAction, useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import useDogSearchQuery from "hooks/api/useDogSearchQuery";
+import DogList from "./DogList";
+import AttendanceSearchInput from "./AttendanceSearchInput";
+import DogSearchList from "./DogSearchList";
+import { Spacing } from "./styles";
+
+interface AttendanceMainProps {
+  schoolId: number;
+  adminId: number;
+  isFocus: boolean;
+  setIsFocus: React.Dispatch<SetStateAction<boolean>>;
+}
+
+const AttendanceMain = ({ schoolId, adminId, isFocus, setIsFocus }: AttendanceMainProps) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchText, setSearchText] = useState(searchParams.get("dogName") || "");
+  const { data, isLoading, isFetching } = useDogSearchQuery(schoolId, searchText);
+
+  const handleSearch = (value: React.SetStateAction<string>) => {
+    setSearchText(value);
+  };
+
+  useEffect(() => {
+    const cleanedSearchParams = Object.fromEntries(
+      Object.entries({ dogName: searchText }).filter(([, value]) => !!value)
+    );
+    setSearchParams(new URLSearchParams(cleanedSearchParams));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchText]);
+
+  const showSearchResults = isLoading || isFetching || data || searchText;
+
+  if (isLoading) return <div>로딩중...</div>;
+
+  return (
+    <>
+      <AttendanceSearchInput
+        name="dogSearch"
+        placeholder="검색"
+        onSearch={handleSearch}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
+      />
+      {showSearchResults ? (
+        <>
+          <Spacing />
+          <DogSearchList data={data} />
+        </>
+      ) : (
+        <DogList schoolId={schoolId} adminId={adminId} isFocus={isFocus} />
+      )}
+    </>
+  );
+};
+
+export default AttendanceMain;
