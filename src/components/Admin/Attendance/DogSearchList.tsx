24d975316febfe88e45ed186b87a2bc5120e9733
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-01 16:33:39 +0900

    Feat: Add error handling and empty search result message to DogSearchList component

--- src/components/Admin/Attendance/DogSearchList.tsx
+++ src/components/Admin/Attendance/DogSearchList.tsx
@@ -1,26 +1,35 @@
-import { ListWrapper, CardListWrapper } from "./styles";
+import { ListWrapper, CardListWrapper, EmptyText } from "./styles";
 import DogCard from "./DogCard";
 
 type DogSearchListProps = {
-  data: any;
+  data?: any[];
 };
 
 const DogSearchList = ({ data }: DogSearchListProps) => {
+  // TODO: 데이터패칭이 실패한 경우, 또는 패칭이 안된 경우 에러 핸들링 필요!
+  if (!data) return;
+
   return (
-    <ListWrapper>
-      <CardListWrapper>
-        {data.map((item: any) => (
-          <DogCard
-            key={item.dogId}
-            dogId={item.dogId}
-            name={item.dogName}
-            allRounds={item.allRounds}
-            rounds={item.currentRounds}
-            monthly={item.monthlyTicket}
-          />
-        ))}
-      </CardListWrapper>
-    </ListWrapper>
+    <>
+      {data.length > 0 ? (
+        <ListWrapper>
+          <CardListWrapper>
+            {data.map((item) => (
+              <DogCard
+                key={item.dogId}
+                dogId={item.dogId}
+                name={item.dogName}
+                allRounds={item.allRounds}
+                rounds={item.currentRounds}
+                monthly={item.monthlyTicket}
+              />
+            ))}
+          </CardListWrapper>
+        </ListWrapper>
+      ) : (
+        <EmptyText>검색 결과와 일치하는 강아지가 없어요</EmptyText>
+      )}
+    </>
   );
 };
 
