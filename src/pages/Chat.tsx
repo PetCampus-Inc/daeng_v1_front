a06cc9937c31f9c0ba2664eb8b2f6c9483f58793
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-04 19:23:13 +0900

    Refactor: update Chat component to use useS3Image hook

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,11 +1,7 @@
-import NavBar from "components/common/NavBar";
-import { PATH } from "constants/path";
-import { useState, useEffect } from "react";
-import { S3Client, GetObjectCommand, S3ClientConfig } from "@aws-sdk/client-s3";
+import { S3Client, S3ClientConfig } from "@aws-sdk/client-s3";
+import useS3Image from "hooks/common/useS3Image";
 
 const Chat = () => {
-  const [imageUrl, setImageUrl] = useState("");
-
   const awsRegion = process.env.REACT_APP_AWS_REGION;
   const awsAccessKeyId = process.env.REACT_APP_AWS_ACCESS_KEY_ID as string;
   const awsSecretAccessKey = process.env.REACT_APP_AWS_SECRET_ACCESS_KEY as string;
@@ -23,41 +19,17 @@ const Chat = () => {
   const bucketName = "pet-campus-s3";
   const objectKey = "test_images/test-pet.png";
 
-  useEffect(() => {
-    const fetchImage = async () => {
-      const command = new GetObjectCommand({
-        Bucket: bucketName,
-        Key: objectKey
-      });
-
-      try {
-        const response = await s3Client.send(command);
-        const { Body } = response || {};
-        if (Body instanceof ReadableStream) {
-          const blob = await new Response(Body).blob();
-          const url = URL.createObjectURL(blob);
-          setImageUrl(url);
-        }
-      } catch (error) {
-        console.error("Image fetch error:", error);
-      }
-    };
-
-    fetchImage();
-  }, [bucketName, objectKey]);
+  const { imageUrl } = useS3Image({ s3Client, bucketName, objectKey });
 
   return (
     <>
       <div>Chat</div>
-      <div>
-        {imageUrl && <img src={imageUrl} alt="S3 content" />}
-        {imageUrl && (
-          <a href={imageUrl} download={`${objectKey}.jpg`}>
-            Download
-          </a>
-        )}
-      </div>
-      <NavBar type="admin" attendance={PATH.ADMIN_CHAT} />
+      {imageUrl && <img src={imageUrl} alt="S3 content" />}
+      {imageUrl && (
+        <a href={imageUrl} download={`${objectKey}.jpg`}>
+          다운로드
+        </a>
+      )}
     </>
   );
 };
