8d51332ccb8b1456d4d1b207ec83a81e10812a1d
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-04 15:51:51 +0900

    Refactor: refactor AttendancePage component

--- src/pages/AttendancePage.tsx
+++ src/pages/AttendancePage.tsx
@@ -1,15 +1,44 @@
-import Attendance from "components/Admin/Attendance";
+import { useState } from "react";
 import Navbar from "components/common/NavBar";
 import Header from "components/common/Header";
-import { useState } from "react";
+import AttendanceTop from "components/Admin/Attendance/AttendanceTop";
+import AttendanceSearch from "components/Admin/Attendance/AttendanceSearch";
+import { PageContainer } from "styles/StyleModule";
+import { adminLoginInfoAtom } from "store/admin";
+import { useRecoilValue } from "recoil";
+import AttendanceMain from "components/Admin/Attendance/AttendanceMain";
 
 const AttendancePage = () => {
-  const [isNavHidden, setIsNavHidden] = useState(false);
+  const { schoolId, adminId } = useRecoilValue(adminLoginInfoAtom).data;
+  const [mode, setMode] = useState<"DEFAULT" | "ATTENDANCE">("DEFAULT");
+  const [isFocus, setIsFocus] = useState<boolean>(false);
+
+  const isAttendMode = mode === "ATTENDANCE";
+  const isDefaultMode = mode === "DEFAULT";
+
   return (
     <>
       <Header type="notice" text="출석부" />
-      <Attendance setIsNavHidden={setIsNavHidden} />
-      <Navbar type="admin" show={isNavHidden ? "none" : "flex"} />
+      <PageContainer $paddingTop="32px" style={{ paddingBottom: "78px" }}>
+        <AttendanceTop mode={mode} setMode={setMode} isFocus={isFocus} />
+        {isDefaultMode && (
+          <AttendanceMain
+            schoolId={schoolId}
+            adminId={adminId}
+            isFocus={isFocus}
+            setIsFocus={setIsFocus}
+          />
+        )}
+        {isAttendMode && (
+          <AttendanceSearch
+            schoolId={schoolId}
+            isFocus={isFocus}
+            setIsFocus={setIsFocus}
+            setMode={setMode}
+          />
+        )}
+      </PageContainer>
+      <Navbar type="admin" />
     </>
   );
 };
