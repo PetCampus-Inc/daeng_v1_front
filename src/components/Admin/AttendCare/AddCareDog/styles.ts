a44befad615b784ab607212e8c69d4e2f4040c39
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-06 02:30:08 +0900

    Feat: Add AddDogList and AddDogCard components

---
+++ src/components/Admin/AttendCare/AddCareDog/styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+export const ListTitle = styled.p`
+  ${({ theme }) => theme.typo.body2_16_R};
+  color: ${({ theme }) => theme.colors.gray_2};
+`;
+
+export const ListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
+
+export const AvatarList = styled.div`
+  display: flex;
+  gap: 16px;
+`;
---
+++ src/components/Admin/AttendCare/CareCard/AddDogCard.tsx
@@ -0,0 +1,49 @@
+import AlertRed from "assets/svg/alert-red";
+import Badge from "components/common/Badge";
+import Checkbox from "components/common/Checkbox";
+import React from "react";
+
+import { CheckboxWrapper, ListItem, ListItemImg, ListItemTitle, ListItemWrapper } from "./styles";
+
+interface AddDogCardProps {
+  dogId: number;
+  dogName: string;
+  adminName: string | null;
+  isChecked: boolean;
+  onClick: () => void;
+}
+
+const AddDogCard = React.memo(
+  ({ dogId, dogName, adminName, isChecked, onClick }: AddDogCardProps) => {
+    const isCared = adminName ? true : false;
+
+    return (
+      <ListItem key={dogId}>
+        <ListItemWrapper>
+          <ListItemImg>
+            <img
+              src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="dog"
+            />
+          </ListItemImg>
+          <ListItemTitle>{dogName}</ListItemTitle>
+          {isCared && <Badge variant="yellow" text={adminName + "T"} />}
+        </ListItemWrapper>
+        <CheckboxWrapper>
+          {isCared ? (
+            <AlertRed />
+          ) : (
+            <Checkbox
+              id={dogId.toString()}
+              isChecked={isChecked}
+              variant="outline"
+              onChange={onClick}
+            />
+          )}
+        </CheckboxWrapper>
+      </ListItem>
+    );
+  }
+);
+
+export default AddDogCard;
