0bb8124958df7bc1730c7297ca2bebc2e0c5ad4e
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-06 14:31:04 +0900

    Feat: Add AttendCareInit component and update styles

---
+++ src/components/Admin/AttendCare/AttendCareInit.tsx
@@ -0,0 +1,61 @@
+import { useRecoilState } from "recoil";
+import { attendCareDogListAtom } from "store/admin";
+
+import AddDogAvatar from "./AddCareDog/AddDogAvatar";
+import AddDogList from "./AddCareDog/AddDogList";
+import { ListWrapper, SubTitle, Title } from "./styles";
+
+const data = [
+  {
+    attendanceId: 47,
+    dogId: 1,
+    dogName: "쥐빵이",
+    adminName: "김민수",
+    lastPhotoTime: null,
+    agendaWriting: null
+  },
+  {
+    attendanceId: 57,
+    dogId: 2,
+    dogName: "꽃개",
+    adminName: null,
+    lastPhotoTime: null,
+    agendaWriting: null
+  },
+  {
+    attendanceId: 67,
+    dogId: 3,
+    dogName: "냠냠이",
+    adminName: null,
+    lastPhotoTime: null,
+    agendaWriting: null
+  },
+  {
+    attendanceId: 49,
+    dogId: 4,
+    dogName: "호빵맨",
+    adminName: null,
+    lastPhotoTime: null,
+    agendaWriting: null
+  }
+];
+
+const AttendCareInit = () => {
+  const [dogList, setDogList] = useRecoilState(attendCareDogListAtom);
+  const handleItemRemove = (dogId: number) => {
+    setDogList((prev) => prev.filter((dog) => dog.dogId !== dogId));
+  };
+
+  return (
+    <>
+      <Title>오늘 관리할 강아지</Title>
+      <SubTitle>관리할 강아지를 먼저 선택해 주세요</SubTitle>
+      {dogList.length > 0 && <AddDogAvatar selectedDogs={dogList} onRemove={handleItemRemove} />}
+      <ListWrapper>
+        <AddDogList data={data} />
+      </ListWrapper>
+    </>
+  );
+};
+
+export default AttendCareInit;
---
+++ src/components/Admin/AttendCare/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  ${({ theme }) => theme.typo.title2_20_B};
+  color: ${({ theme }) => theme.colors.gray_1};
+`;
+
+export const SubTitle = styled.h3`
+  ${({ theme }) => theme.typo.body2_16_R};
+  color: ${({ theme }) => theme.colors.gray_2};
+`;
+
+export const ListWrapper = styled.div`
+  margin: 24px 0;
+`;
