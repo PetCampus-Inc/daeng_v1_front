18ce5344ff951e1b6a7a9ad955d8ef2e1dbc5667
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-03-07 01:23:27 +0900

    Chore: favicon, title 추가, eslint config 업데이트 (#78)
    * Add overflow: hidden to Content component
    
    * Chore: Update linting and formatting scripts in package.json
    
    * Typo: Fix .eslintrc file name
    
    * Chore: Update import/order rule to warn instead of error
    
    * Chore: Update favicon.ico and change page title to "똑독"

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
@@ -14,7 +14,7 @@ module.exports = {
     "import/prefer-default-export": "off",
     "import/extensions": ["off", "error", "ignorePackages"],
     "import/order": [
-      "error",
+      "warn",
       {
         groups: [
           "builtin",
