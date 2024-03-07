7cda5a6aa12fc6e651b370e0a116c2227e055903
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-08 01:45:02 +0900

    Refactor: Add admin login info to Recoil state

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
-import ReactDOM from "react-dom/client";
-import Router from "router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { StyledContainer } from "components/common/CustomToast/styles";
+import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
+import Router from "router";
+import { ThemeProvider } from "styled-components";
+import mediaQueries from "styles/MediaQuery";
+
 import { GlobalStyle } from "./styles/GlobalStyle";
 import { ThemeConfig } from "./styles/ThemeConfig";
-import { ThemeProvider } from "styled-components";
-import { StyledContainer } from "components/common/CustomToast/styles";
-import mediaQueries from "styles/MediaQuery";
 
 const queryClient = new QueryClient();
 
