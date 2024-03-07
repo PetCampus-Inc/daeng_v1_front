d167c26b054089be9f3ab0fccc2229f943e7e27f
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-08 01:49:59 +0900

    Refactor: Update TicketCounter component and BottomSheet in TicketType component

--- src/components/Admin/EnrollmentForm/TicketCounter/index.tsx
+++ src/components/Admin/EnrollmentForm/TicketCounter/index.tsx
@@ -1,7 +1,7 @@
+import type { Dispatch, SetStateAction } from "react";
+
 import AddBIcon from "assets/svg/add-b-icon";
 import SubtractBIcon from "assets/svg/subtract-b-icon";
-
-import type { Dispatch, SetStateAction } from "react";
 
 import * as S from "./styles";
 interface Props {
@@ -17,25 +17,27 @@ const TicketCounter = ({ type, isDuplication, initial, counter, setCounter }: Pr
   const times = type === "ROUND" ? "회" : "주";
 
   return (
-    <S.ContentWrapper>
+    <>
       <S.TextWrapper>
         <S.Title>{text} 유형을 추가해 주세요</S.Title>
         {isDuplication && (
           <S.ErrorMessage>이미 추가되어 있어요. 횟수를 변경해 주세요.</S.ErrorMessage>
         )}
       </S.TextWrapper>
-      <S.Counter>
-        <S.Button onClick={() => setCounter((prev) => (prev > initial ? prev - 1 : prev))}>
-          <SubtractBIcon />
-        </S.Button>
-        <S.Text>
-          {counter} <span>{times}</span>
-        </S.Text>
-        <S.Button onClick={() => setCounter((prev) => prev + 1)}>
-          <AddBIcon />
-        </S.Button>
-      </S.Counter>
-    </S.ContentWrapper>
+      <S.CounterWrapper>
+        <S.Counter>
+          <S.Button onClick={() => setCounter((prev) => (prev > initial ? prev - 1 : prev))}>
+            <SubtractBIcon />
+          </S.Button>
+          <S.Text>
+            {counter} <span>{times}</span>
+          </S.Text>
+          <S.Button onClick={() => setCounter((prev) => prev + 1)}>
+            <AddBIcon />
+          </S.Button>
+        </S.Counter>
+      </S.CounterWrapper>
+    </>
   );
 };
 
