4521ef0fc0ef7bbc826ef9fe3ff19bffdcd52423
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-08 02:15:34 +0900

    Refactor: Update SortSelectBox component to use BottomSheet component

--- src/components/Admin/Attendance/SortSelectBox/index.tsx
+++ src/components/Admin/Attendance/SortSelectBox/index.tsx
@@ -18,48 +18,46 @@ const SortSelectBox = ({ sortName, setSortName }: SortSelectBoxProps) => {
 
   return (
     <>
-      {isVisible && (
-        <BottomSheet onClose={close}>
-          <S.Container>
-            <S.Title>정렬</S.Title>
-            <S.ListWrapper>
-              <S.ListItem
-                className={sortName === LIST.REGISTERED ? "active" : ""}
-                onClick={async () => {
-                  setSortName(LIST.REGISTERED);
-                }}
-              >
-                {LIST.REGISTERED}
-              </S.ListItem>
-              <S.ListItem
-                className={sortName === LIST.PAYMENT ? "active" : ""}
-                onClick={async () => {
-                  setSortName(LIST.PAYMENT);
-                }}
-              >
-                {LIST.PAYMENT}
-              </S.ListItem>
-              <S.ListItem
-                className={sortName === LIST.DATE ? "active" : ""}
-                onClick={async () => {
-                  setSortName(LIST.DATE);
-                }}
-              >
-                {LIST.DATE}
-              </S.ListItem>
-              <S.ListItem
-                className={sortName === LIST.CHARGE ? "active" : ""}
-                onClick={async () => {
-                  setSortName(LIST.CHARGE);
-                }}
-              >
-                {LIST.CHARGE}
-              </S.ListItem>
-            </S.ListWrapper>
-            <ConfirmButton onClick={() => close()}>닫기</ConfirmButton>
-          </S.Container>
-        </BottomSheet>
-      )}
+      <BottomSheet isOpen={isVisible} onClose={close}>
+        <BottomSheet.Content>
+          <BottomSheet.Title variant="body">정렬</BottomSheet.Title>
+          <S.ListWrapper>
+            <S.ListItem
+              className={sortName === LIST.REGISTERED ? "active" : ""}
+              onClick={async () => {
+                setSortName(LIST.REGISTERED);
+              }}
+            >
+              {LIST.REGISTERED}
+            </S.ListItem>
+            <S.ListItem
+              className={sortName === LIST.PAYMENT ? "active" : ""}
+              onClick={async () => {
+                setSortName(LIST.PAYMENT);
+              }}
+            >
+              {LIST.PAYMENT}
+            </S.ListItem>
+            <S.ListItem
+              className={sortName === LIST.DATE ? "active" : ""}
+              onClick={async () => {
+                setSortName(LIST.DATE);
+              }}
+            >
+              {LIST.DATE}
+            </S.ListItem>
+            <S.ListItem
+              className={sortName === LIST.CHARGE ? "active" : ""}
+              onClick={async () => {
+                setSortName(LIST.CHARGE);
+              }}
+            >
+              {LIST.CHARGE}
+            </S.ListItem>
+          </S.ListWrapper>
+          <ConfirmButton onClick={() => close()}>닫기</ConfirmButton>
+        </BottomSheet.Content>
+      </BottomSheet>
       <S.SelectBox onClick={() => open()}>
         <S.Text>{sortName}</S.Text>
         <S.ArrowDownButton type="button">
