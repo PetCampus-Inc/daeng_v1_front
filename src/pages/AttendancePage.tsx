4f08c445ca6462970f2053e6e9aa40ed2172dc61
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-08 01:41:34 +0900

    Chore: Remove unnecessary code and refactor AttendancePage component

--- src/pages/AttendancePage.tsx
+++ src/pages/AttendancePage.tsx
@@ -4,19 +4,9 @@ import AttendanceTop from "components/Admin/Attendance/AttendanceTop";
 import Header from "components/common/Header";
 import Navbar from "components/common/NavBar";
 import { useState } from "react";
-import { useRecoilValue } from "recoil";
-import { adminLoginInfoAtom } from "store/admin";
 import { PageContainer } from "styles/StyleModule";
 
 const AttendancePage = () => {
-  const { schoolId, adminId } = useRecoilValue(adminLoginInfoAtom).data;
-  const [mode, setMode] = useState<"DEFAULT" | "ATTENDANCE">("DEFAULT");
-  const [isFocus, setIsFocus] = useState<boolean>(false);
-
-  const isAttendMode = mode === "ATTENDANCE";
-  const isDefaultMode = mode === "DEFAULT";
-
-  const { schoolId, adminId } = useRecoilValue(adminLoginInfoAtom).data;
   const [mode, setMode] = useState<"DEFAULT" | "ATTENDANCE">("DEFAULT");
   const [isFocus, setIsFocus] = useState<boolean>(false);
 
@@ -28,42 +18,8 @@ const AttendancePage = () => {
       <Header type="notice" text="출석부" />
       <PageContainer $paddingTop="32px" style={{ paddingBottom: "78px" }}>
         <AttendanceTop mode={mode} setMode={setMode} isFocus={isFocus} />
-        {isDefaultMode && (
-          <AttendanceMain
-            schoolId={schoolId}
-            adminId={adminId}
-            isFocus={isFocus}
-            setIsFocus={setIsFocus}
-          />
-        )}
-        {isAttendMode && (
-          <Attendance
-            schoolId={schoolId}
-            isFocus={isFocus}
-            setIsFocus={setIsFocus}
-            setMode={setMode}
-          />
-        )}
-      </PageContainer>
-      <Navbar type="admin" />
-      <PageContainer $paddingTop="32px" style={{ paddingBottom: "78px" }}>
-        <AttendanceTop mode={mode} setMode={setMode} isFocus={isFocus} />
-        {isDefaultMode && (
-          <AttendanceMain
-            schoolId={schoolId}
-            adminId={adminId}
-            isFocus={isFocus}
-            setIsFocus={setIsFocus}
-          />
-        )}
-        {isAttendMode && (
-          <Attendance
-            schoolId={schoolId}
-            isFocus={isFocus}
-            setIsFocus={setIsFocus}
-            setMode={setMode}
-          />
-        )}
+        {isDefaultMode && <AttendanceMain isFocus={isFocus} setIsFocus={setIsFocus} />}
+        {isAttendMode && <Attendance isFocus={isFocus} setIsFocus={setIsFocus} setMode={setMode} />}
       </PageContainer>
       <Navbar type="admin" />
     </>
