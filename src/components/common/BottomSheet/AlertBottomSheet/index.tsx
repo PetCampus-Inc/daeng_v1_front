f2de72c220e18b6710395d0a641e910cd048d146
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-08 02:32:59 +0900

    Design: Add IconWrapper in AlertBottomSheet component

--- src/components/common/BottomSheet/AlertBottomSheet/index.tsx
+++ src/components/common/BottomSheet/AlertBottomSheet/index.tsx
@@ -1,6 +1,6 @@
 import AlertIcon from "assets/svg/alert-icon";
+import styled from "styled-components";
 
-import { TextContainer } from "./styles";
 import BottomSheet from "../index";
 
 import type { BottomSheetButtonProps } from "../BottomSheetButton";
@@ -28,11 +28,11 @@ const AlertBottomSheet = ({
     <BottomSheet isOpen={isOpen} onClose={onClose}>
       <BottomSheet.Content>
         {hasControl && <BottomSheet.Control />}
-        <AlertIcon />
-        <TextContainer>
-          <BottomSheet.Title>{title}</BottomSheet.Title>
-          <BottomSheet.Subtitle>{subtitle}</BottomSheet.Subtitle>
-        </TextContainer>
+        <IconWrapper>
+          <AlertIcon />
+        </IconWrapper>
+        <BottomSheet.Title>{title}</BottomSheet.Title>
+        <BottomSheet.Subtitle>{subtitle}</BottomSheet.Subtitle>
         <BottomSheet.Button
           closeText={closeText}
           closeFn={closeFn}
@@ -45,3 +45,11 @@ const AlertBottomSheet = ({
 };
 
 export default AlertBottomSheet;
+
+const IconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  margin-top: 28px;
+`;
