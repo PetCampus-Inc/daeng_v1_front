f85cab2a75a583053a57581461869d91d1552c1f
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-07 16:29:06 +0900

    Refactor: update school api and attendance api

--- src/apis/admin.api.ts
+++ src/apis/admin.api.ts
@@ -1,5 +1,5 @@
+import axios from "axios";
 import customAxios from "libs/CustomAxios";
-import { IResponse, ITeacherSubmitResponse } from "types/Response.type";
 import {
   IAdminLoginInfo,
   INewEnrollmentList,
@@ -9,12 +9,12 @@ import {
   ITeacherSignUpInfo,
   IWaitingOwnerInfo
 } from "types/Admin.type";
-import axios from "axios";
 import { IAdminLoginResponse } from "types/Attendance.type";
+import { IResponse, ITeacherSubmitResponse } from "types/Response.type";
 
 // 아이디 중복확인
 export const handleCheckId = async (id: string): Promise<number> => {
-  const url: string = `admin/checkid?id=${id}`;
+  const url = `admin/checkid?id=${id}`;
   const { data } = await customAxios.get(url);
   return data.status;
 };
@@ -23,7 +23,7 @@ export const handleCheckId = async (id: string): Promise<number> => {
 export const handleAdminLoginResult = async (
   req: IAdminLoginInfo
 ): Promise<IAdminLoginResponse> => {
-  const url: string = `admin/login`;
+  const url = `admin/login`;
   const { data } = await customAxios.post(url, {
     id: req.inputId,
     pwd: req.inputPw
@@ -33,7 +33,7 @@ export const handleAdminLoginResult = async (
 
 // 사업자번호 확인 api
 export const handleCheckRegistrationNumber = async (req: string): Promise<string> => {
-  const url: string = `https://api.odcloud.kr/api/nts-businessman/v1/status?serviceKey=${process.env.REACT_APP_BUSINESS_API_KEY}`;
+  const url = `https://api.odcloud.kr/api/nts-businessman/v1/status?serviceKey=${process.env.REACT_APP_BUSINESS_API_KEY}`;
   const { data } = await axios.post(url, {
     b_no: [req]
   });
@@ -42,7 +42,7 @@ export const handleCheckRegistrationNumber = async (req: string): Promise<string
 
 // 원장 회원가입
 export const handleOwnerSignUpResult = async (req: IOwnerSignUpInfo): Promise<IResponse> => {
-  const url: string = `admin/join/owner`;
+  const url = `admin/join/owner`;
   const { data } = await customAxios.post(url, {
     id: req.userId,
     pwd: req.userPw,
@@ -60,7 +60,7 @@ export const handleOwnerSignUpResult = async (req: IOwnerSignUpInfo): Promise<IR
 export const handleTeacherSignUpSubmit = async (
   req: ITeacherSignUpInfo
 ): Promise<ITeacherSubmitResponse> => {
-  const url: string = `admin/submit/teacher/approval`;
+  const url = `admin/submit/teacher/approval`;
   const { data } = await customAxios.post(url, {
     id: req.userId,
     pwd: req.userPw,
@@ -73,14 +73,14 @@ export const handleTeacherSignUpSubmit = async (
 
 // 선생님 회원가입 요청 취소
 export const handleTeacherSignUpCancel = async (adminId: number): Promise<IResponse> => {
-  const url: string = `admin/cancel/teacher/approval`;
+  const url = `admin/cancel/teacher/approval`;
   const { data } = await customAxios.post(url, adminId);
   return data;
 };
 
 // 선생님 가입 승인
 export const handleTeacherApprove = async (req: ITeacherApprove): Promise<IResponse> => {
-  const url: string = `admin/approve/teacher/approval`;
+  const url = `admin/approve/teacher/approval`;
   const { data } = await customAxios.post(url, {
     adminId: req.submittedAdminId,
     schoolId: req.submittedSchoolId
@@ -90,35 +90,31 @@ export const handleTeacherApprove = async (req: ITeacherApprove): Promise<IRespo
 
 // 선생님 가입 거절
 export const handleTeacherDeny = async (adminId: number): Promise<IResponse> => {
-  const url: string = `admin/deny/teacher/approval`;
+  const url = `admin/deny/teacher/approval`;
   const { data } = await customAxios.post(url, adminId);
   return data;
 };
 
 // 견주 가입 승인
 export const handleMemberApprove = async (memberId: number): Promise<IResponse> => {
-  const url: string = `admin/approve/member/approval`;
+  const url = `admin/approve/member/approval`;
   const { data } = await customAxios.post(url, memberId);
   return data;
 };
 
 // 견주 가입 거절
 export const handleMemberDeny = async (memberId: number): Promise<IResponse> => {
-  const url: string = `admin/deny/member/approval`;
+  const url = `admin/deny/member/approval`;
   const { data } = await customAxios.post(url, memberId);
   return data;
 };
 
 // 원장 선생님 목록 조회
-export const handleGetTeacherList = async (
-  adminId: number,
-  schoolId: number
-): Promise<ITeacherList> => {
-  const url: string = `admin/teachers/main`;
+export const handleGetTeacherList = async (adminId: number): Promise<ITeacherList> => {
+  const url = `admin/teachers/main`;
   const { data } = await customAxios.get(url, {
     params: {
-      adminId,
-      schoolId
+      adminId
     }
   });
   return data.data;
@@ -129,7 +125,7 @@ export const handleGetNewEnrollment = async (
   adminId: number,
   schoolId: number
 ): Promise<INewEnrollmentList> => {
-  const url: string = `admin/enrollment/main`;
+  const url = `admin/enrollment/main`;
   const { data } = await customAxios.get(url, {
     params: {
       adminId,
@@ -143,7 +139,7 @@ export const handleGetNewEnrollment = async (
 export const handleGetWaitingOwnersList = async (
   schoolId: number
 ): Promise<IWaitingOwnerInfo[]> => {
-  const url: string = `admin/enrollment/list`;
+  const url = `admin/enrollment/list`;
   const { data } = await customAxios.get(url, {
     params: {
       schoolId
@@ -154,35 +150,35 @@ export const handleGetWaitingOwnersList = async (
 
 // 가입신청서 승인
 export const handlePostApproveForm = async (enrollmentFormId: number): Promise<IResponse> => {
-  const url: string = `admin/enrollment/approve?enrollmentFormId=${enrollmentFormId}`;
+  const url = `admin/enrollment/approve?enrollmentFormId=${enrollmentFormId}`;
   const { data } = await customAxios.post(url);
   return data;
 };
 
 // 가입신청서 거절
 export const handlePostDenyForm = async (enrollmentFormId: number): Promise<IResponse> => {
-  const url: string = `admin/enrollment/deny?enrollmentFormId=${enrollmentFormId}`;
+  const url = `admin/enrollment/deny?enrollmentFormId=${enrollmentFormId}`;
   const { data } = await customAxios.post(url);
   return data;
 };
 
 // 선생님 승인
 export const handlePostApproveTeacher = async (adminId: number): Promise<IResponse> => {
-  const url: string = `admin/approve/teacher/approval?adminId=${adminId}`;
+  const url = `admin/approve/teacher/approval?adminId=${adminId}`;
   const { data } = await customAxios.post(url);
   return data;
 };
 
 // 선생님 거절
 export const handlePostDenyTeacher = async (adminId: number): Promise<IResponse> => {
-  const url: string = `admin/deny/teacher/approval?adminId=${adminId}`;
+  const url = `admin/deny/teacher/approval?adminId=${adminId}`;
   const { data } = await customAxios.post(url);
   return data;
 };
 
 // 선생님 삭제 (유치원 admin에서 삭제)
 export const handleDeleteTeacher = async (adminId: number): Promise<IResponse> => {
-  const url: string = `admin/delete/teacher?adminId=${adminId}`;
+  const url = `admin/delete/teacher?adminId=${adminId}`;
   const { data } = await customAxios.post(url);
   return data;
 };
--- src/apis/attendance.ts
+++ src/apis/attendance.ts
@@ -12,7 +12,7 @@ import {
 import { IResponse } from "types/Response.type";
 
 export const handleGetDogs = async (schoolId: number): Promise<IAttendanceInfo> => {
-  const url: string = `admin/attendance?schoolId=${schoolId}`;
+  const url = `admin/attendance?schoolId=${schoolId}`;
   const { data } = await customAxios.get(url);
   return data.data;
 };
@@ -21,19 +21,13 @@ export const handleGetSearchDogs = async (
   schoolId: number,
   searchText: string
 ): Promise<IAttendanceInfo> => {
-  const url: string = `admin/attendance/dog/search?schoolId=${schoolId}&searchText=${searchText}`;
-  const { data } = await customAxios.get(url);
-  return data.data;
-};
-
-export const handleSortRegistered = async (schoolId: number): Promise<IAttendanceInfo> => {
-  const url: string = `admin/attendance/dog/sort/registered?schoolId=${schoolId}`;
+  const url = `admin/attendance/dog/search?schoolId=${schoolId}&searchText=${searchText}`;
   const { data } = await customAxios.get(url);
   return data.data;
 };
 
 export const handleSortPayment = async (schoolId: number): Promise<IAttendanceInfo> => {
-  const url: string = `admin/attendance/dog/sort/payment?schoolId=${schoolId}`;
+  const url = `admin/attendance/dog/sort/payment?schoolId=${schoolId}`;
   const { data } = await customAxios.get(url);
   return data.data;
 };
@@ -42,37 +36,37 @@ export const handleSortCharge = async (
   schoolId: number,
   adminId: number
 ): Promise<IAttendanceInfo> => {
-  const url: string = `admin/attendance/dog/sort/charge?schoolId=${schoolId}&adminId=${adminId}`;
+  const url = `admin/attendance/dog/sort/charge?schoolId=${schoolId}&adminId=${adminId}`;
   const { data } = await customAxios.get(url);
   return data.data;
 };
 
 export const handleSortDate = async (schoolId: number): Promise<IAttendanceInfo> => {
-  const url: string = `admin/attendance/dog/sort/date?schoolId=${schoolId}`;
+  const url = `admin/attendance/dog/sort/date?schoolId=${schoolId}`;
   const { data } = await customAxios.get(url);
   return data.data;
 };
 
 export const handleCallMember = async (dogId: number): Promise<IMemberCallInfo> => {
-  const url: string = `admin/attendance/callowner/${dogId}`;
+  const url = `admin/attendance/callowner/${dogId}`;
   const { data } = await customAxios.get(url);
   return data.data;
 };
 
 export const handleSendAlarm = async (dogId: number) => {
-  const url: string = `admin/attendance/send/alarm/${dogId}`;
+  const url = `admin/attendance/send/alarm/${dogId}`;
   const { data } = await customAxios.get(url);
   return data.data;
 };
 
 export const handleDeleteDog = async (dogId: number): Promise<IResponse> => {
-  const url: string = `admin/attendance/delete/dog?dogId=${dogId}`;
+  const url = `admin/attendance/delete/dog?dogId=${dogId}`;
   const { data } = await customAxios.post(url);
   return data.data;
 };
 
 export const handlePostAttend = async (req: IAttendInfo): Promise<IResponse> => {
-  const url: string = `admin/attendance/attend`;
+  const url = `admin/attendance/attend`;
   const { data } = await customAxios.post(url, {
     schoolId: req.schoolId,
     attendanceIdList: req.selectedDogIds
@@ -81,7 +75,7 @@ export const handlePostAttend = async (req: IAttendInfo): Promise<IResponse> => 
 };
 
 export const handleGetAttendDogs = async (schoolId: number): Promise<IAttendDogsInfo> => {
-  const url: string = `admin/attendance/attend?schoolId=${schoolId}`;
+  const url = `admin/attendance/attend?schoolId=${schoolId}`;
   const { data } = await customAxios.get(url);
   return data.data;
 };
@@ -90,19 +84,19 @@ export const handleGetAttendSearchDogs = async (
   schoolId: number,
   searchText: string
 ): Promise<IAttendDogLists[]> => {
-  const url: string = `admin/attendance/attend/search?schoolId=${schoolId}&searchText=${searchText}`;
+  const url = `admin/attendance/attend/search?schoolId=${schoolId}&searchText=${searchText}`;
   const { data } = await customAxios.get(url);
   return data.data;
 };
 
 export const handleGetAttendCareDogs = async (schoolId: number): Promise<IAttendCareDogInfo> => {
-  const url: string = `admin/attendance/attend/dog/care?schoolId=${schoolId}`;
+  const url = `admin/attendance/attend/dog/care?schoolId=${schoolId}`;
   const { data } = await customAxios.get(url);
   return data;
 };
 
 export const handlePostAttendCareDogs = async (req: IAttendCareInfo): Promise<IResponse> => {
-  const url: string = `admin/attendance/attend/dog/care`;
+  const url = `admin/attendance/attend/dog/care`;
   const { data } = await customAxios.post(url, {
     adminId: req.adminId,
     attendanceIdList: req.selectedDogId
@@ -111,7 +105,7 @@ export const handlePostAttendCareDogs = async (req: IAttendCareInfo): Promise<IR
 };
 
 export const handleGetDogDetails = async (dogId: number, date: string): Promise<IDogDetails> => {
-  const url: string = `admin/attendance/dog/info?dogId=${dogId}&date=${date}`;
+  const url = `admin/attendance/dog/info?dogId=${dogId}&date=${date}`;
   const { data } = await customAxios.get(url);
   return data;
 };
