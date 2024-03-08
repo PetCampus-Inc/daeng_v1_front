29a149a9c8cd50b9f18b7e5adc3568567acad5a4
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-08 16:32:28 +0900

    Refactor: Apply CallBottomSheet in CallMemberBottomSheet component

--- src/components/Admin/Attendance/AttendanceModal/CallMemberBottomSheet.tsx
+++ src/components/Admin/Attendance/AttendanceModal/CallMemberBottomSheet.tsx
@@ -1,5 +1,4 @@
-import BasicPhoneIcon from "assets/svg/phone-basic";
-import BottomSheet from "components/common/BottomSheet";
+import CallBottomSheet from "components/common/BottomSheet/CallBottomSheet";
 import { memo } from "react";
 
 import type { IMemberCallInfo } from "types/Attendance.type";
@@ -11,7 +10,6 @@ interface CallMemberBottomSheetProps {
 }
 
 const CallMemberBottomSheet = memo(({ info, isOpen, close }: CallMemberBottomSheetProps) => {
-  if (!isOpen) return null;
   if (!info) return null;
 
   const handleCallMember = (info: IMemberCallInfo) => {
@@ -19,18 +17,15 @@ const CallMemberBottomSheet = memo(({ info, isOpen, close }: CallMemberBottomShe
     // TODO: 해당 견주의 전화번호가 입력된 전화앱으로 바로 이동.
     close();
   };
+
   return (
-    <BottomSheet isOpen={isOpen} onClose={() => close()}>
-      <BottomSheet.Content>
-        <BottomSheet.Control />
-        <BottomSheet.Title>
-          <BasicPhoneIcon />
-          <span>{info?.dogName} 견주</span>
-        </BottomSheet.Title>
-        <BottomSheet.Subtitle>{info?.phoneNumber}</BottomSheet.Subtitle>
-        <BottomSheet.Button actionText="전화 걸기" actionFn={() => handleCallMember(info)} />
-      </BottomSheet.Content>
-    </BottomSheet>
+    <CallBottomSheet
+      isOpen={isOpen}
+      dogName={info.dogName}
+      phoneNumber={info.phoneNumber}
+      onClose={close}
+      handleCall={() => handleCallMember(info)}
+    />
   );
 });
 
