45704253689b23a15ec60b40d1aca4d4e0eb088f
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-08 23:23:29 +0900

    Chore: Add add-thin-icon  and update props in RightArrow icon

--- src/assets/svg/right-arrow.jsx
+++ src/assets/svg/right-arrow.jsx
@@ -1,8 +1,8 @@
-const RightArrow = () => {
+const RightArrow = ({ w = "16", h = "16" }) => {
   return (
     <svg
-      width="16"
-      height="16"
+      width={w}
+      height={h}
       viewBox="0 0 16 16"
       fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
