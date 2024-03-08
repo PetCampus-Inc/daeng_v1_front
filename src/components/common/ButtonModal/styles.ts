26b2ace9d394c581a5c503f73fb524d9e73d962c
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-08 22:21:30 +0900

    Feat: Add Modal component use to Compound Pattern

--- src/components/common/ButtonModal/styles.ts
+++ src/components/common/ButtonModal/styles.ts
@@ -1,82 +1,78 @@
+import { motion } from "framer-motion";
 import styled from "styled-components";
 
-export const BackDrop = styled.div.withConfig({
-  shouldForwardProp: (prop) => !["isOpen"].includes(prop)
-})<{
-  isOpen: boolean;
-}>`
-  display: ${({ isOpen }) => (isOpen ? "flex" : "none")};
+import type { ModalButtonVariant } from "./type";
+export { Container, BackDrop } from "styles/StyleModule";
 
-  width: 100vw;
-  height: 100vh;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  position: fixed;
-  left: 0;
-  top: 0;
-
-  background-color: rgba(0, 0, 0, 0.7);
-  z-index: 5;
+export const StyledModal = styled(motion.div)`
+  width: 90%;
+  background-color: ${({ theme }) => theme.colors.white};
+  border-radius: 0.8rem;
+  flex-direction: column;
+  position: absolute;
+  z-index: 10;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -60%);
 `;
 
-export const MainWrapper = styled.div`
-  width: 90%;
-  min-height: 22%;
-  display: flex;
-  background-color: white;
-  border-radius: 0.8rem;
-  padding: 9% 5% 5%;
-  flex-direction: column;
-  justify-content: space-between;
+export const StyledContent = styled.div.withConfig({
+  shouldForwardProp: (prop) => !["variant"].includes(prop)
+})<{ variant: ModalButtonVariant }>`
+  width: 100%;
+  padding: ${({ variant }) => (variant === "one" ? "36px 14px 16px" : "36px 12px 14px")};
+`;
+
+const Text = styled.p`
   align-items: center;
+  text-wrap: balance;
+  word-break: keep-all;
+`;
+
+export const MainText = styled(Text)`
+  ${({ theme }) => theme.typo.title2_20_B};
+  color: ${({ theme }) => theme.colors.black};
+`;
+
+export const SubText = styled(Text)`
+  ${({ theme }) => theme.typo.body2_16_R};
+  color: ${({ theme }) => theme.colors.gray_2};
+  padding: 0 10px;
 `;
 
 export const TextWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 4px;
   align-items: center;
+  gap: 4px;
   height: 80%;
-  margin: 0 0 6%;
-  text-wrap: balance;
-  word-break: keep-all;
+  padding-bottom: 28px;
 `;
 
-export const MainText = styled.p`
-  ${({ theme }) => theme.typo.title2_20_B};
-  color: ${({ theme }) => theme.colors.black};
-`;
-
-export const SubText = styled.p`
-  ${({ theme }) => theme.typo.body2_16_R};
-  color: ${({ theme }) => theme.colors.gray_2};
-`;
-
-export const ButtonWrapper = styled.div`
-  width: 100%;
+export const ButtonGroup = styled.div`
   display: flex;
   justify-content: space-between;
   gap: 8px;
 `;
 
 const Button = styled.button`
+  display: flex;
   width: 100%;
-  padding: 3%;
+  padding: 11px;
+  justify-content: center;
+  align-items: center;
 
   border-radius: 8px;
+
+  ${({ theme }) => theme.typo.label1_16_B};
 `;
 
 export const CloseButton = styled(Button)`
-  ${({ theme }) => theme.typo.label1_16_R};
-
   background-color: ${({ theme }) => theme.colors.gray_4};
   color: ${({ theme }) => theme.colors.gray_2};
 `;
 
 export const ActButton = styled(Button)`
-  ${({ theme }) => theme.typo.label1_16_B};
-
   background-color: ${({ theme }) => theme.colors.primaryColor};
   color: ${({ theme }) => theme.colors.white};
 `;
