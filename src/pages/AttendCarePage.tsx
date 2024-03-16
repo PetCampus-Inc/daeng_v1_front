17657a85dd12826308da83eaf133e806b578f55c
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-16 18:14:43 +0900

    Feat: Enhance isFirstEntry detection using location state and data

--- src/pages/AttendCarePage.tsx
+++ src/pages/AttendCarePage.tsx
@@ -1,19 +1,27 @@
-import AttendCare from "components/Admin/AttendCare";
 import AttendCareInit from "components/Admin/AttendCare/AttendCareInit";
+import AttendCareMain from "components/Admin/AttendCare/AttendCareMain";
 import Header from "components/common/Header";
 import NavBar from "components/common/NavBar";
+import { useGetCareDogList } from "hooks/api/caredogQuery";
+import { useLocation } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { adminLoginInfoAtom } from "store/admin";
 import { PageContainer } from "styles/StyleModule";
 
-type AttendCarePageProps = { type: "main" | "init" };
+const AttendCarePage = () => {
+  const { adminId } = useRecoilValue(adminLoginInfoAtom).data;
+  const { data } = useGetCareDogList(adminId);
+  const location = useLocation();
+  const isFirstEntry = location.state?.isFirstEntry ?? data.some((dog) => dog.adminName === null);
 
-const AttendCarePage = ({ type }: AttendCarePageProps) => {
-  const isFirstEntry = type === "init";
+  // FIXME: get 강아지관리 api 수정되면 []로 최초진입 구분하기!!
+  // const isFirstEntry = data.length === 0;
 
   return (
     <>
       <Header type="notice" text="강아지 관리" />
-      <PageContainer $padding="32px 0 78px">
-        {isFirstEntry ? <AttendCareInit /> : <AttendCare />}
+      <PageContainer color={isFirstEntry ? "white" : "BGray"} pt={2}>
+        {isFirstEntry ? <AttendCareInit /> : <AttendCareMain data={data} />}
       </PageContainer>
       <NavBar />
     </>
