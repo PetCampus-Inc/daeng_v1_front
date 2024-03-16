c052b7eb2b094f59eebb94ced7ab80da489395f7
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-16 18:12:18 +0900

    Refactor: Add padding options to SimpleButton component

--- src/components/common/Button/SimpleButton/styles.ts
+++ src/components/common/Button/SimpleButton/styles.ts
@@ -14,10 +14,20 @@ const colorSchemeStyles = (theme: DefaultTheme) => ({
 });
 
 export const StyledButton = styled.button.withConfig({
-  shouldForwardProp: (prop) => !["colorScheme"].includes(prop)
-})<{ colorScheme: TColorScheme }>`
+  shouldForwardProp: (prop) => !["pt", "pb", "ph", "pr", "pl", "colorScheme"].includes(prop)
+})<{
+  pt?: number;
+  pb?: number;
+  ph?: number;
+  pr?: number;
+  pl?: number;
+  colorScheme: TColorScheme;
+}>`
   display: flex;
-  padding: 1px 12px;
+  padding-top: ${({ pt }) => (pt ? pt : 0.125)}rem;
+  padding-bottom: ${({ pb }) => (pb ? pb : 0.125)}rem;
+  padding-left: ${({ pl, ph }) => (pl ? pl : ph ? ph : 0.75)}rem;
+  padding-right: ${({ pr, ph }) => (pr ? pr : ph ? ph : 0.75)}rem;
   align-items: center;
   gap: 10px;
 
