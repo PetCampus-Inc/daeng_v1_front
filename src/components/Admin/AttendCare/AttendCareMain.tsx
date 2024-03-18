26eec7aecff51d40c8a78d0398ad1d2dad1a9893
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-18 15:47:29 +0900

    Feat: Add AddCaredogBottomSheet component and SelectedDogsProvider context

--- src/components/Admin/AttendCare/AttendCareMain.tsx
+++ src/components/Admin/AttendCare/AttendCareMain.tsx
@@ -3,12 +3,15 @@ import { PATH } from "constants/path";
 import AddIcon from "assets/svg/addIcon";
 import RightArrow from "assets/svg/right-arrow";
 import SimpleButton from "components/common/Button/SimpleButton";
+import useBottomSheet from "hooks/common/useBottomSheet";
 import { useNavigate } from "react-router-dom";
 import { ICareDogInfo } from "types/admin.caredog.type";
 
 import CareOptionDropdown from "./CareButton/CareOptionDropdown";
 import MainSendCard from "./CareButton/MainSendCard";
 import MainDogList from "./CareList/MainDogList";
+import AddCaredogBottomSheet from "./CareModal/AddCaredogBottomSheet";
+import { SelectedDogsProvider } from "./context/SelectedDogsProvider";
 import { ButtonWrapper } from "./styles";
 
 interface AttendCareMainProps {
@@ -17,6 +20,7 @@ interface AttendCareMainProps {
 
 const AttendCareMain = ({ data }: AttendCareMainProps) => {
   const navigate = useNavigate();
+  const { isVisible, open, close } = useBottomSheet();
 
   const CARE_OPTIONS: { [key: string]: () => void } = {
     "관리 강아지 삭제": () => navigate(PATH.ADMIN_CARE_DOG + "/delete"),
@@ -30,12 +34,19 @@ const AttendCareMain = ({ data }: AttendCareMainProps) => {
 
   return (
     <>
+      <SelectedDogsProvider>
+        <AddCaredogBottomSheet isVisible={isVisible} close={close} />
+      </SelectedDogsProvider>
       <MainSendCard
         text="견주에게 바로 사진을 보낼 수 있어요"
         onClick={() => navigate("강아지관리 상세정보")}
       />
       <ButtonWrapper>
-        <SimpleButton leftAddon={<AddIcon />} rightAddon={<RightArrow w={"20"} h={"20"} />}>
+        <SimpleButton
+          onClick={open}
+          leftAddon={<AddIcon />}
+          rightAddon={<RightArrow w={"20"} h={"20"} />}
+        >
           강아지 추가하기
         </SimpleButton>
         <CareOptionDropdown
