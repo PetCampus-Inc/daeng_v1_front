376bd56ccd3f76f24b9fc58a47508c9823d6a7f8
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-18 15:52:57 +0900

    Refactor: Update to use caredogLoader in AttendCarePage component

--- src/pages/AttendCarePage.tsx
+++ src/pages/AttendCarePage.tsx
@@ -3,16 +3,22 @@ import AttendCareMain from "components/Admin/AttendCare/AttendCareMain";
 import Header from "components/common/Header";
 import NavBar from "components/common/NavBar";
 import { useGetCareDogList } from "hooks/api/caredogQuery";
-import { useLocation } from "react-router-dom";
+import { useRouteLoaderData } from "react-router-dom";
 import { useRecoilValue } from "recoil";
+import caredogLoader from "routes/caredogLoader";
 import { adminLoginInfoAtom } from "store/admin";
 import { PageContainer } from "styles/StyleModule";
 
 const AttendCarePage = () => {
-  const { adminId } = useRecoilValue(adminLoginInfoAtom).data;
-  const { data } = useGetCareDogList(adminId);
-  const location = useLocation();
-  const isFirstEntry = location.state?.isFirstEntry ?? data.some((dog) => dog.adminName === null);
+  const { adminId } = useRecoilValue(adminLoginInfoAtom);
+  const initialData = useRouteLoaderData("caredog") as Awaited<ReturnType<typeof caredogLoader>>;
+
+  const { data } = useGetCareDogList(adminId, initialData);
+
+  // FIXME: 출석한 강아지가 없는 경우 [] 임.
+
+  const noCareDog = data.some((dog) => dog.adminName === null);
+  const isFirstEntry = noCareDog;
 
   // FIXME: get 강아지관리 api 수정되면 []로 최초진입 구분하기!!
   // const isFirstEntry = data.length === 0;
