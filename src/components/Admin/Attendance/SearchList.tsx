662af18d94d911bb9c1b00d48865c127fe434c5a
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-19 03:21:51 +0900

    Refactor: SearchList component to use memo and update prop types

--- src/components/Admin/Attendance/SearchList.tsx
+++ src/components/Admin/Attendance/SearchList.tsx
@@ -1,36 +1,33 @@
+import { memo } from "react";
+
 import DogCard from "./AttendanceCard/DogCard";
 import { ListWrapper, CardListWrapper, EmptyText } from "./styles";
 
+import type { IAttendDogInfo } from "types/admin.attendance.type";
+
 type SearchListProps = {
-  data?: any[];
+  data?: IAttendDogInfo[];
+  type: "search" | "list";
 };
 
-const SearchList = ({ data }: SearchListProps) => {
-  // TODO: 데이터패칭이 실패한 경우, 또는 패칭이 안된 경우 에러 핸들링 필요!
+const SearchList = memo(({ data, type }: SearchListProps) => {
   if (!data) return null;
 
-  return (
-    <>
-      {data.length > 0 ? (
-        <ListWrapper>
-          <CardListWrapper>
-            {data.map((item) => (
-              <DogCard
-                key={item.dogId}
-                dogId={item.dogId}
-                name={item.dogName}
-                allRounds={item.allRounds}
-                rounds={item.currentRounds}
-                monthly={item.monthlyTicket}
-              />
-            ))}
-          </CardListWrapper>
-        </ListWrapper>
-      ) : (
-        <EmptyText>검색 결과와 일치하는 강아지가 없어요</EmptyText>
-      )}
-    </>
-  );
-};
+  if (data.length > 0) {
+    return (
+      <ListWrapper>
+        <CardListWrapper>
+          {data.map((dog) => (
+            <DogCard key={dog.dogId} info={dog} />
+          ))}
+        </CardListWrapper>
+      </ListWrapper>
+    );
+  }
+
+  const emptyMessage =
+    type === "search" ? "검색 결과와 일치하는 강아지가 없어요" : "아직 등원한 강아지가 없어요";
+  return <EmptyText>{emptyMessage}</EmptyText>;
+});
 
 export default SearchList;
