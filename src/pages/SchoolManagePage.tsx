a283eda2c14a8ae9339aeb7a3b88d199303e267b
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-19 21:30:07 +0900

    Refactor: 사용하지 않는 프로퍼티 제거

--- src/pages/SchoolManagePage.tsx
+++ src/pages/SchoolManagePage.tsx
@@ -1,5 +1,3 @@
-import { PATH } from "constants/path";
-
 import MenuCard from "components/Admin/SchoolManage/MenuCard";
 import Header from "components/common/Header";
 import NavBar from "components/common/NavBar";
@@ -9,7 +7,7 @@ const SchoolManagePage = () => {
     <>
       <Header type="notice" text="유치원 운영" />
       <MenuCard />
-      <NavBar type="admin" attendance={PATH.ADMIN_DOG_INFO} />
+      <NavBar type="admin" />
     </>
   );
 };
