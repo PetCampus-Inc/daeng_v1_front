a6b9a1bb4093b2d052fceb2ba62124b979ebbe35
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-22 23:04:10 +0900

    Refactor: admin 라우터 분리, 출석페이지로 리디렉션 로더 추가

--- src/router.tsx
+++ src/router.tsx
@@ -3,7 +3,7 @@ import { PATH } from "constants/path";
 import { QueryClient } from "@tanstack/react-query";
 import * as Pages from "pages";
 import { Suspense } from "react";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter, redirect } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import caredogLoader from "routes/caredogLoader";
 import { adminLoginInfoAtom } from "store/admin";
@@ -14,156 +14,155 @@ const AppRouter = ({ queryClient }: { queryClient: QueryClient }) => {
   const [adminInfo] = useRecoilState(adminLoginInfoAtom);
   const router = createBrowserRouter([
     {
-      path: PATH.ROOT,
+      path: "/admin",
       element: <App />,
       errorElement: <Pages.NotFoundPage />,
       children: [
         {
+          path: PATH.ADMIN_ATTENDANCE,
+          children: [
+            {
+              index: true,
+              element: (
+                <Suspense>
+                  <Pages.AttendancePage />
+                </Suspense>
+              )
+            },
+            {
+              path: PATH.ADMIN_ATTENDANCE_INFO(),
+              element: (
+                <Suspense>
+                  <Pages.DogInfoPage />
+                </Suspense>
+              )
+            }
+          ]
+        },
+        {
+          path: PATH.ADMIN_CARE_DOG,
+          id: "caredog",
+          loader: () => caredogLoader({ adminId: adminInfo.adminId, queryClient }),
+          children: [
+            {
+              index: true,
+              element: (
+                <Suspense>
+                  <Pages.AttendCarePage />
+                </Suspense>
+              )
+            },
+            {
+              path: "delete",
+              element: <Pages.AttendCareDeletePage />
+            }
+          ]
+        },
+        {
+          path: PATH.ADMIN_CHAT,
+          element: <Pages.Chat />
+        },
+        {
+          path: PATH.ADMIN_SCHOOL_MANAGE,
+          children: [
+            {
+              index: true,
+              element: (
+                <Suspense>
+                  <Pages.SchoolManagePage />
+                </Suspense>
+              )
+            },
+            {
+              path: PATH.ADMIN_ENROLLMENT,
+              element: (
+                <Suspense>
+                  <Pages.SchoolManageEnrollmentPage />
+                </Suspense>
+              )
+            },
+            {
+              path: PATH.ADMIN_FORMS,
+              element: (
+                <Suspense>
+                  <Pages.EnrollmentFormListPage />
+                </Suspense>
+              )
+            },
+            {
+              path: PATH.ADMIN_CREATE_FORM,
+              element: (
+                <Suspense>
+                  <Pages.EnrollmentFormCreatePage />
+                </Suspense>
+              )
+            },
+            {
+              path: PATH.ADMIN_SUBMIT_FORM,
+              element: (
+                <Suspense>
+                  <Pages.EnrollmentFormSubmitPage />
+                </Suspense>
+              )
+            },
+            {
+              path: PATH.ADMIN_EDIT_FORM(),
+              element: (
+                <Suspense>
+                  <Pages.EnrollmentFormEditPage />
+                </Suspense>
+              )
+            },
+            {
+              path: PATH.ADMIN_FORM(),
+              element: (
+                <Suspense>
+                  <Pages.EnrollmentFormDetailPage />
+                </Suspense>
+              )
+            },
+            {
+              path: PATH.ADMIN_TEACHER_MANAGE,
+              element: (
+                <Suspense>
+                  <Pages.TeacherManagePage />
+                </Suspense>
+              )
+            }
+          ]
+        },
+        {
+          path: PATH.ADMIN_MY_PAGE,
+          element: <Pages.MyPage />
+        }
+      ]
+    },
+    {
+      path: PATH.ROOT,
+      element: <App />,
+      children: [
+        {
           index: true,
-          path: PATH.HOME,
+          // path: PATH.HOME,
+          loader: () => redirect(PATH.ADMIN_ATTENDANCE),
           element: (
             <Suspense>
               <Pages.HomePage />
             </Suspense>
           )
-        }
-      ]
-    },
-    {
-      path: PATH.LOGIN,
-      element: (
-        <Suspense>
-          <Pages.SignInPage />
-        </Suspense>
-      )
-    },
-    {
-      path: PATH.SIGNUP,
-      element: <Pages.SignUpPage />
-    },
-    {
-      path: "*",
-      element: <Pages.NotFoundPage />
-    },
-    {
-      path: PATH.ADMIN_ATTENDANCE,
-      children: [
+        },
         {
-          index: true,
+          path: PATH.LOGIN,
           element: (
             <Suspense>
-              <Pages.AttendancePage />
+              <Pages.SignInPage />
             </Suspense>
           )
         },
         {
-          path: PATH.ADMIN_ATTENDANCE_INFO(),
-          element: (
-            <Suspense>
-              <Pages.DogInfoPage />
-            </Suspense>
-          )
-        }
-      ]
-    },
-    {
-      path: PATH.ADMIN_CARE_DOG,
-      id: "caredog",
-      loader: () => caredogLoader({ adminId: adminInfo.adminId, queryClient }),
-      children: [
-        {
-          index: true,
-          element: (
-            <Suspense>
-              <Pages.AttendCarePage />
-            </Suspense>
-          )
+          path: PATH.SIGNUP,
+          element: <Pages.SignUpPage />
         },
-        {
-          path: "delete",
-          element: <Pages.AttendCareDeletePage />
-        }
-      ]
-    },
-    {
-      path: PATH.ADMIN_CHAT,
-      element: <Pages.Chat />
-    },
-    {
-      path: PATH.ADMIN_SCHOOL_MANAGE,
-      children: [
-        {
-          index: true,
-          element: (
-            <Suspense>
-              <Pages.SchoolManagePage />
-            </Suspense>
-          )
-        },
-        {
-          path: PATH.ADMIN_ENROLLMENT,
-          element: (
-            <Suspense>
-              <Pages.SchoolManageEnrollmentPage />
-            </Suspense>
-          )
-        },
-        {
-          path: PATH.ADMIN_FORMS,
-          element: (
-            <Suspense>
-              <Pages.EnrollmentFormListPage />
-            </Suspense>
-          )
-        },
-        {
-          path: PATH.ADMIN_CREATE_FORM,
-          element: (
-            <Suspense>
-              <Pages.EnrollmentFormCreatePage />
-            </Suspense>
-          )
-        },
-        {
-          path: PATH.ADMIN_SUBMIT_FORM,
-          element: (
-            <Suspense>
-              <Pages.EnrollmentFormSubmitPage />
-            </Suspense>
-          )
-        },
-        {
-          path: PATH.ADMIN_EDIT_FORM(),
-          element: (
-            <Suspense>
-              <Pages.EnrollmentFormEditPage />
-            </Suspense>
-          )
-        },
-        {
-          path: PATH.ADMIN_FORM(),
-          element: (
-            <Suspense>
-              <Pages.EnrollmentFormDetailPage />
-            </Suspense>
-          )
-        },
-        {
-          path: PATH.ADMIN_TEACHER_MANAGE,
-          element: (
-            <Suspense>
-              <Pages.TeacherManagePage />
-            </Suspense>
-          )
-        }
-      ]
-    },
-    {
-      path: PATH.ADMIN_MY_PAGE,
-      element: <Pages.MyPage />
-    },
-    {
-      children: [
         {
           path: PATH.OWNER_MA,
           element: (
