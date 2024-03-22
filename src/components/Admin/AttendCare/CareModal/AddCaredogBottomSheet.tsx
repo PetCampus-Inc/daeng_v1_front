143b0b0c10c44ecf88f31f13ce56a34d68a198a5
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-22 23:31:15 +0900

    Refactor: AddCaredogBottomSheet에 useOverlay 적용

--- src/components/Admin/AttendCare/CareModal/AddCaredogBottomSheet.tsx
+++ src/components/Admin/AttendCare/CareModal/AddCaredogBottomSheet.tsx
@@ -1,24 +1,26 @@
-import BottomSheet from "components/common/BottomSheet";
+import BottomSheet, { type IBottomSheetProps } from "components/common/BottomSheet";
 import { useCreateCareDogs } from "hooks/api/caredogQuery";
-import useModal from "hooks/common/useModal";
+import useOverlay from "hooks/common/useOverlay/useOverlay";
 import { useRecoilValue } from "recoil";
 import { adminLoginInfoAtom } from "store/admin";
 import { styled } from "styled-components";
 
 import AlertAlreadySelectedModal from "./AlertAlreadySelectedModal";
 import AddDogList from "../CareList/AddDogList";
-import { useSelectedDogs } from "../context/SelectedDogsProvider";
+import { useSelectedDogs } from "../hooks/useSelectedDogs";
 
-interface AddCaredogBottomSheetProps {
-  isVisible: boolean;
-  close: () => void;
-}
+const AddCaredogBottomSheet = ({ isOpen, close }: IBottomSheetProps) => {
+  const overlay = useOverlay();
 
-const AddCaredogBottomSheet = ({ isVisible, close }: AddCaredogBottomSheetProps) => {
-  const { adminId } = useRecoilValue(adminLoginInfoAtom);
+  const openPopup = () =>
+    overlay.open(({ isOpen, close }) => (
+      <AlertAlreadySelectedModal isOpen={isOpen} close={close} />
+    ));
+
+  const { mutateCreateCareDogs } = useCreateCareDogs(openPopup);
   const [selectedDogs, _] = useSelectedDogs();
-  const { isVisible: isModalOpen, open, close: modalClose } = useModal();
-  const { mutateCreateCareDogs } = useCreateCareDogs(open);
+
+  const { adminId } = useRecoilValue(adminLoginInfoAtom);
   const selectedDogId = selectedDogs.map((dog) => dog.attendanceId);
 
   // TODO: 명확한 네이밍, useCreateCareDogs 내부의 onSuccess 분리하기!
@@ -29,30 +31,26 @@ const AddCaredogBottomSheet = ({ isVisible, close }: AddCaredogBottomSheetProps)
         onSuccess: () => close()
       }
     );
-    // open();
   };
 
   return (
-    <>
-      <AlertAlreadySelectedModal isOpen={isModalOpen} close={modalClose} />
-      <BottomSheet isOpen={isVisible} onClose={() => close()}>
-        <BottomSheet.Content>
-          <BottomSheet.Control />
-          <BottomSheet.Title align="left">오늘 관리할 강아지</BottomSheet.Title>
-          <BottomSheet.Subtitle align="left">
-            관리할 강아지 목록에 추가할 강아지를 선택해 주세요
-          </BottomSheet.Subtitle>
-          <ListWrapper>
-            <AddDogList adminId={adminId} />
-          </ListWrapper>
-          <BottomSheet.Button
-            actionText="선택 완료"
-            actionFn={handleSubmit}
-            disabled={selectedDogs.length === 0}
-          />
-        </BottomSheet.Content>
-      </BottomSheet>
-    </>
+    <BottomSheet isOpen={isOpen} close={close}>
+      <BottomSheet.Content>
+        <BottomSheet.Control />
+        <BottomSheet.Title align="left">오늘 관리할 강아지</BottomSheet.Title>
+        <BottomSheet.Subtitle align="left">
+          관리할 강아지 목록에 추가할 강아지를 선택해 주세요
+        </BottomSheet.Subtitle>
+        <ListWrapper>
+          <AddDogList adminId={adminId} />
+        </ListWrapper>
+        <BottomSheet.Button
+          actionText="선택 완료"
+          actionFn={handleSubmit}
+          disabled={selectedDogs.length === 0}
+        />
+      </BottomSheet.Content>
+    </BottomSheet>
   );
 };
 
