77eeda84e6640d6a7f80823b311e7bcac92c6bc9
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-03-29 20:04:37 +0900

    feat: 깜빡임 제어를 위한 suspense 추가

--- src/components/Admin/DogDetailInfo/AttendanceRecord/index.tsx
+++ src/components/Admin/DogDetailInfo/AttendanceRecord/index.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+
 import DailyNotice from "./DailyNotice";
 import * as S from "./styles";
 import Calendar from "../DogInfo/Calendar";
@@ -8,8 +10,10 @@ const AttendanceRecord = () => {
       <S.TopContainer>
         <S.Image src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
       </S.TopContainer>
-      <Calendar />
-      <DailyNotice />
+      <Suspense>
+        <Calendar />
+        <DailyNotice />
+      </Suspense>
     </>
   );
 };
