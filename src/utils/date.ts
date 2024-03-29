ec4a3f10264560cd370e6b19d05b731acc234077
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-03-29 20:03:10 +0900

    feat: 숫자를 두 자리로 변환해주는 addZero util 추가

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -6,3 +6,11 @@ import { format } from "date-fns";
 export const getCurrentDate = () => {
   return format(new Date(), "yyyyMMdd");
 };
+
+/**
+ * 한 자리 숫자를 두 자리 숫자로 변환하는 함수
+ */
+export const addZero = (num: number | number[]) => {
+  if (Array.isArray(num)) return num.map((n) => (n < 10 ? `0${n}` : n));
+  return num < 10 ? `0${num}` : num;
+};
