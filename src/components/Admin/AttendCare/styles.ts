7e12e662b583d9074dbe74a9b96497931d2e2f3d
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-30 23:12:02 +0900

    Feat: 출석한 강아지가 없는 화면 컴포넌트 추가

--- src/components/Admin/AttendCare/styles.ts
+++ src/components/Admin/AttendCare/styles.ts
@@ -5,7 +5,7 @@ export const Title = styled.h1`
   color: ${({ theme }) => theme.colors.gray_1};
 `;
 
-export const SubTitle = styled.h3`
+export const SubTitle = styled.h2`
   ${({ theme }) => theme.typo.body2_16_R};
   color: ${({ theme }) => theme.colors.gray_2};
 `;
@@ -14,7 +14,7 @@ export const ListWrapper = styled.div`
   margin: 1.5rem 0;
 `;
 
-export const DescTitle = styled.h2`
+export const DescTitle = styled.h1`
   ${({ theme }) => theme.typo.body2_16_B};
   color: ${({ theme }) => theme.colors.darkBlack};
 `;
@@ -31,4 +31,33 @@ export const VStack = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+`;
+
+export const NoResultContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  height: 65%;
+  margin-block: 2.5rem;
+
+  border-radius: 16px;
+  background-color: ${({ theme }) => theme.colors.white};
+`;
+
+export const LinkText = styled.h3`
+  color: ${({ theme }) => theme.colors.gray_1};
+  ${({ theme }) => theme.typo.label1_16_B};
+
+  & > a {
+    display: flex;
+    align-items: center;
+    color: inherit;
+  }
+`;
+
+export const DescText = styled.p`
+  color: ${({ theme }) => theme.colors.gray_2};
+  ${({ theme }) => theme.typo.label2_14_R};
 `;
