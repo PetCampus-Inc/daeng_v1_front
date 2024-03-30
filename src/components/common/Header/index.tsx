2cd7178913665ed19647b495b6935e63ff7f0802
Jae <jaeyeonjung3@gmail.com>
2024-03-30 21:03:56 +0900

    Feat: 마이페이지 헤더 형식 추가

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,5 +1,6 @@
 import ArrowLeftIcon from "assets/svg/arrow-left-icon";
 import NoticeActiveIcon from "assets/svg/notice-active-icon";
+import SettingWhiteIcon from "assets/svg/setting-white";
 import { memo } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -19,22 +20,24 @@ header type
 - back : 뒤로기기
 - text : 뒤로가기 + 텍스트 + (오른쪽 버튼)
 - notice : 텍스트 + 알림 // TODO: 이러한 형태로 다른 아이콘 사용하는 경우가 있다면 수정하기
+- setting : 텍스트 + 세팅 (background-color, box-shadow 없음)
 */
 
 interface Props {
-  type: "main" | "back" | "text" | "notice";
+  type: "main" | "back" | "text" | "notice" | "setting";
   handleClick?: () => void | Promise<void>;
   text?: string;
   rightElement?: React.ReactNode;
+  transparent?: boolean;
 }
 
-const Header = ({ type, handleClick, text, rightElement }: Props) => {
+const Header = ({ type, handleClick, text, rightElement, transparent }: Props) => {
   const navigate = useNavigate();
 
   const click = handleClick ? handleClick : () => navigate(-1);
   return (
-    <Container>
-      <HeaderWrapper>
+    <Container className={transparent ? "transparent" : ""}>
+      <HeaderWrapper className={transparent ? "transparent" : ""}>
         {type === "main" && (
           <>
             <LogoWrapper to={"/home"}>
@@ -66,6 +69,18 @@ const Header = ({ type, handleClick, text, rightElement }: Props) => {
               }}
             >
               <NoticeActiveIcon />
+            </IconWrapper>
+          </TextWrapper>
+        )}
+        {type === "setting" && (
+          <TextWrapper className="setting-right">
+            <TitleText className="setting">{text}</TitleText>
+            <IconWrapper
+              onClick={() => {
+                // TODO: 세팅 페이지로 이동
+              }}
+            >
+              <SettingWhiteIcon />
             </IconWrapper>
           </TextWrapper>
         )}
