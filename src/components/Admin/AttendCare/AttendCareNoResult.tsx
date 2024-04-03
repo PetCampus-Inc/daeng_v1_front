e23636d5bbccedfb8e64acb046f1236103661444
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-04-03 18:10:50 +0900

    Feat: `강아지관리` 출석한 강아지가 없는 경우 화면 구현 (#88)
    * Feat: care-dog-icon 아이콘 추가
    
    * Feat: 출석한 강아지가 없는 화면 컴포넌트 추가
    
    * Feat: 강아지 관리 페이지에 AttendCareNoResult 컴포넌트 추가
    
    * Refactor: TimePicker 컴포넌트 useTimePicker 훅 분리
    
    * Refactor: TimePicker 컴포넌트 불필요한 코드 제거, ARIA 속성 추가
    
    * Chore: svg 파일 수정

---
+++ src/components/Admin/AttendCare/AttendCareNoResult.tsx
@@ -0,0 +1,27 @@
+import { PATH } from "constants/path";
+
+import CareDogIcon from "assets/svg/care-dog-icon";
+import RightArrow from "assets/svg/right-arrow";
+import { Link } from "react-router-dom";
+
+import { DescText, LinkText, NoResultContainer, SubTitle, Title } from "./styles";
+
+const AttendCareNoResult = () => {
+  return (
+    <>
+      <Title>오늘 관리할 강아지</Title>
+      <SubTitle role="alert">관리할 강아지가 없어요 출석 먼저 진행해 주세요</SubTitle>
+      <NoResultContainer>
+        <CareDogIcon aria-hidden="true" />
+        <LinkText>
+          <Link to={PATH.ADMIN_ATTENDANCE}>
+            출석 진행 하기 <RightArrow w={"20"} h={"20"} aria-hidden="true" />
+          </Link>
+        </LinkText>
+        <DescText>출석 후에 관리할 강아지를 선택할 수 있어요</DescText>
+      </NoResultContainer>
+    </>
+  );
+};
+
+export default AttendCareNoResult;
