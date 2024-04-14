b46f095e432fa8e9db5ac7851ca4f3e1b246a0c2
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-04-15 03:56:48 +0900

    refactor: mypage 쿼리함수를 customAxios -> request로 변경

--- src/apis/admin.mypage.api.ts
+++ src/apis/admin.mypage.api.ts
@@ -1,22 +1,25 @@
-import customAxios from "libs/CustomAxios";
-import { IPrincipalInfo, ITeacherInfo } from "types/admin.mypage.type";
+import { request } from "libs/CustomAxios/request";
+import { IOwnerInfo, ITeacherInfo } from "types/admin.mypage.type";
+import { IResponse } from "types/helper.type";
 
-export const handleGetPrincipalInfo = async (adminId: number): Promise<IPrincipalInfo> => {
+export const handleGetPrincipalInfo = async (adminId: number): Promise<IOwnerInfo> => {
   const url = `admin/owner/mypage`;
-  const { data } = await customAxios.get(url, {
+  const { data } = await request<IResponse<IOwnerInfo>>({
+    url,
     params: {
       adminId
     }
   });
-  return data.data;
+  return data;
 };
 
 export const handleGetTeacherInfo = async (adminId: number): Promise<ITeacherInfo> => {
   const url = `admin/teacher/mypage`;
-  const { data } = await customAxios.get(url, {
+  const { data } = await request<IResponse<ITeacherInfo>>({
+    url,
     params: {
       adminId
     }
   });
-  return data.data;
+  return data;
 };
