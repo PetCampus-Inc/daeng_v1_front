2ed6a523808173a1f9a0c27c953cc470a3fb93bb
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-04-15 03:57:11 +0900

    refactor: useQuery -> useSuspenseQuery 로 변경

--- src/hooks/api/useGetTeacherInfo.ts
+++ src/hooks/api/useGetTeacherInfo.ts
@@ -1,10 +1,10 @@
 import { QUERY_KEY } from "constants/queryKey";
 
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { handleGetTeacherInfo } from "apis/admin.mypage.api";
 
 const useGetTeacherInfo = (adminId: number) => {
-  return useQuery({
+  return useSuspenseQuery({
     queryKey: QUERY_KEY.TEACHER_INFO,
     queryFn: () => handleGetTeacherInfo(adminId)
   });
