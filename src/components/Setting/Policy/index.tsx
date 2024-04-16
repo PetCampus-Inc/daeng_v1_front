8b27dd468f9691d5ff20559ba8aa4ef2c7debedc
jieun419 <crg1050@naver.com>
2024-04-17 04:23:03 +0900

    Feat: PlicyInfo 컴포넌트 생성 및 PolicyPage에 추가

---
+++ src/components/Setting/Policy/index.tsx
@@ -0,0 +1,9 @@
+const PolicyInfo = () => {
+  return (
+    <div>
+      정책 내용
+    </div>
+  );
+};
+
+export default PolicyInfo;
--- src/pages/PolicyPage.tsx
+++ src/pages/PolicyPage.tsx
@@ -1,16 +1,14 @@
 import Header from "components/common/Header";
+import PolicyInfo from "components/Setting/Policy";
 import { PageContainer } from "styles/StyleModule";
 
 const PolicyPage = () => {
   return (
     <>
       <Header type="text" text="정책" />
-      <PageContainer color="white">
-        정책 내용
-      </PageContainer>
+      <PageContainer color="white"><PolicyInfo /></PageContainer>
     </>
-  )
-
+  );
 };
 
 export default PolicyPage;
