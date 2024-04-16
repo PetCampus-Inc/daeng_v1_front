ad999de51aeb825faa9505ef324ce9d9e99022e7
jieun419 <crg1050@naver.com>
2024-04-17 04:19:02 +0900

    Feat: PolicyPage에 Header, PageContainer 공통 컴포넌트 추가

--- src/pages/PolicyPage.tsx
+++ src/pages/PolicyPage.tsx
@@ -1,9 +1,16 @@
+import Header from "components/common/Header";
+import { PageContainer } from "styles/StyleModule";
+
 const PolicyPage = () => {
   return (
-    <div>
-      정책 페이지
-    </div>
-  );
+    <>
+      <Header type="text" text="정책" />
+      <PageContainer color="white">
+        정책 내용
+      </PageContainer>
+    </>
+  )
+
 };
 
 export default PolicyPage;
