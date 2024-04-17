281de7ee91248017051e4da5c5c595a79e0f4fda
jieun419 <crg1050@naver.com>
2024-04-17 16:09:19 +0900

    Feat: TextWrapper컴포넌트 추가로 인한 div에서 컴포넌트로 변경

--- src/components/Setting/Notification/index.tsx
+++ src/components/Setting/Notification/index.tsx
@@ -3,7 +3,7 @@ import CheckBoard from "assets/svg/check-board";
 import SchoolIcon from "assets/svg/school-icon";
 import Toggle from "components/common/Toggle/Toggle";
 
-import * as S from "../styles"
+import * as S from "../styles";
 
 // TODO 기능 추가 할 때 컴포넌트 분리 작업 필요합니다. (리팩토링)
 const NotificationInfo = () => {
@@ -18,33 +18,39 @@ const NotificationInfo = () => {
       </S.TopWrapper>
       <S.SettingList>
         <S.SettingItem type="notification">
-          <div>
+          <S.TextWrapper>
             <S.TextBox>
-              <S.StyledIconBox><AttendanceIcon /></S.StyledIconBox>
+              <S.StyledIconBox>
+                <AttendanceIcon />
+              </S.StyledIconBox>
               <S.Text>출석부 알림</S.Text>
             </S.TextBox>
             <S.SubTextPL>강아지 등록 정보, 이용권, 유의사항 등</S.SubTextPL>
-          </div>
+          </S.TextWrapper>
           <Toggle />
         </S.SettingItem>
         <S.SettingItem type="notification">
-          <div>
+          <S.TextWrapper>
             <S.TextBox>
-              <S.StyledIconBox><CheckBoard /></S.StyledIconBox>
+              <S.StyledIconBox>
+                <CheckBoard />
+              </S.StyledIconBox>
               <S.Text>강아지 관리 알림</S.Text>
             </S.TextBox>
             <S.SubTextPL>알림장 전송, 사진 전송 등</S.SubTextPL>
-          </div>
+          </S.TextWrapper>
           <Toggle />
         </S.SettingItem>
         <S.SettingItem type="notification">
-          <div>
+          <S.TextWrapper>
             <S.TextBox>
-              <S.StyledIconBox><SchoolIcon /></S.StyledIconBox>
+              <S.StyledIconBox>
+                <SchoolIcon />
+              </S.StyledIconBox>
               <S.Text>유치원 운영 알림</S.Text>
             </S.TextBox>
             <S.SubTextPL>신규 가입, 교사 관리 등</S.SubTextPL>
-          </div>
+          </S.TextWrapper>
           <Toggle />
         </S.SettingItem>
       </S.SettingList>
