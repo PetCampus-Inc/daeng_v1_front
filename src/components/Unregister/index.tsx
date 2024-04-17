33fa29c62552b34836c3d928691c163c7fd92cf8
jieun419 <crg1050@naver.com>
2024-04-17 17:37:39 +0900

    Feat: UnregisterInfo 컴포넌트 UI  스타일 작업

--- src/components/Unregister/index.tsx
+++ src/components/Unregister/index.tsx
@@ -6,36 +6,35 @@ import * as S from "./styles";
 const UnregisterInfo = () => {
   return (
     <>
-      <div>
-        <h3>똑독 탈퇴를 원하시나요?</h3>
-        <span>탈퇴 전, 아래 내용을 확인해 주세요</span>
-      </div>
-      <div>
-        <Checkbox />
-        <p>지금까지 주고받은 채팅내역, 알림장, 사진앨범 등의 모든 기록이 <em>영구 삭제</em>되며 복구할 수 없어요</p>
-      </div>
-      <div>
-        <Checkbox />
-        <p>탈퇴 후 사용했던 소셜 아이디로 <em>재가입 시 신규 회원으로 가입</em> 돼요</p>
-      </div>
-      <div>
-        <Checkbox />
-        <p>원장님이 등록한 유치원의 <em>활동 기록</em>과 <em>정보</em>, 가입된 <em>회원 및 교사 정보</em>는 모두 <em>초기화</em>되고 복구되지 않아요</p>
-      </div>
-      <div>
-        <Checkbox />
-        <p>모든 개인 정보가 삭제돼요</p>
-      </div>
-      <div>
-        <Checkbox />
-        <p>위 안내사항에 모두 동의해요</p>
-      </div>
+      <S.TopWrapper>
+        <S.Title>똑독 탈퇴를 원하시나요?</S.Title>
+        <S.SubText>탈퇴 전, 아래 내용을 확인해 주세요</S.SubText>
+      </S.TopWrapper>
+      <S.CheckBoxWrapper>
+        <S.CheckBoxItem>
+          <Checkbox />
+          <S.CautionText>
+            지금까지 주고받은 채팅내역, 알림장, 사진앨범 등의 모든 기록이 <S.AccentText>영구 삭제</S.AccentText>되며
+            복구할 수 없어요
+          </S.CautionText>
+        </S.CheckBoxItem>
+        <S.CheckBoxItem>
+          <Checkbox />
+          <S.CautionText>
+            탈퇴 후 사용했던 소셜 아이디로 <S.AccentText>재가입 시 신규 회원으로 가입</S.AccentText> 돼요
+          </S.CautionText>
+        </S.CheckBoxItem>
+      </S.CheckBoxWrapper>
 
-      <BackgroundGrayButton>
-        탈퇴하기
-      </BackgroundGrayButton>
+      <S.CheckBoxItem>
+        <Checkbox />
+        <S.CautionText>위 안내사항에 모두 동의해요</S.CautionText>
+      </S.CheckBoxItem>
+      <S.BackgroundButtonWrapper>
+        <BackgroundGrayButton>탈퇴하기</BackgroundGrayButton>
+      </S.BackgroundButtonWrapper>
+
     </>
-
   );
 };
 
--- src/components/Unregister/styles.ts
+++ src/components/Unregister/styles.ts
@@ -1,1 +1,53 @@
 import styled from "styled-components";
+
+export const TopWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+  padding-top: 1.5rem;
+  padding-bottom: 1.875rem;
+`;
+
+export const Title = styled.h3`
+  ${({ theme }) => theme.typo.title1_24_B};
+  color: ${({ theme }) => theme.colors.darkBlack};
+`;
+
+export const SubText = styled.span`
+  ${({ theme }) => theme.typo.body2_16_R};
+  color: ${({ theme }) => theme.colors.gray_2};
+`;
+
+export const CheckBoxWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.625rem;
+  margin-bottom: 2.25rem;
+`;
+
+export const CheckBoxItem = styled.div`
+  padding: 1rem 0.625rem;
+  border-radius: 0.5rem;
+  border: 1px solid ${({ theme }) => theme.colors.gray_5};
+  background-color: ${({ theme }) => theme.colors.white};
+  display: flex;
+  gap: 16px;
+  cursor: pointer;
+`;
+
+export const CautionText = styled.p`
+  ${({ theme }) => theme.typo.label1_16_R};
+  color: ${({ theme }) => theme.colors.gray_1};
+`;
+
+export const AccentText = styled.em`
+  color: ${({ theme }) => theme.colors.primaryColor};
+`;
+
+export const BackgroundButtonWrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 1rem 1rem 2rem 1rem;
+`;
