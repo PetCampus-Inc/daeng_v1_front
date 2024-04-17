3261b81de59fff9156b3e605ae8e336ad7a06ce1
jieun419 <crg1050@naver.com>
2024-04-17 16:00:24 +0900

    Feat: NotificationInfo컴포넌트 UI 구조 및 스타일 작업

--- src/components/Setting/Notification/index.tsx
+++ src/components/Setting/Notification/index.tsx
@@ -1,8 +1,54 @@
+import AttendanceIcon from "assets/svg/attendance-icon";
+import CheckBoard from "assets/svg/check-board";
+import SchoolIcon from "assets/svg/school-icon";
+import Toggle from "components/common/Toggle/Toggle";
+
+import * as S from "../styles"
+
+// TODO 기능 추가 할 때 컴포넌트 분리 작업 필요합니다. (리팩토링)
 const NotificationInfo = () => {
   return (
-    <div>
-      알림 설정
-    </div>
+    <>
+      <S.TopWrapper>
+        <div>
+          <S.Title>서비스 알림</S.Title>
+          <S.SubTitle>서비스 전체 알림</S.SubTitle>
+        </div>
+        <Toggle />
+      </S.TopWrapper>
+      <S.SettingList>
+        <S.SettingItem type="notification">
+          <div>
+            <S.TextBox>
+              <S.StyledIconBox><AttendanceIcon /></S.StyledIconBox>
+              <S.Text>출석부 알림</S.Text>
+            </S.TextBox>
+            <S.SubTextPL>강아지 등록 정보, 이용권, 유의사항 등</S.SubTextPL>
+          </div>
+          <Toggle />
+        </S.SettingItem>
+        <S.SettingItem type="notification">
+          <div>
+            <S.TextBox>
+              <S.StyledIconBox><CheckBoard /></S.StyledIconBox>
+              <S.Text>강아지 관리 알림</S.Text>
+            </S.TextBox>
+            <S.SubTextPL>알림장 전송, 사진 전송 등</S.SubTextPL>
+          </div>
+          <Toggle />
+        </S.SettingItem>
+        <S.SettingItem type="notification">
+          <div>
+            <S.TextBox>
+              <S.StyledIconBox><SchoolIcon /></S.StyledIconBox>
+              <S.Text>유치원 운영 알림</S.Text>
+            </S.TextBox>
+            <S.SubTextPL>신규 가입, 교사 관리 등</S.SubTextPL>
+          </div>
+          <Toggle />
+        </S.SettingItem>
+      </S.SettingList>
+    </>
   );
 };
 
--- src/components/Setting/styles.ts
+++ src/components/Setting/styles.ts
@@ -10,10 +10,18 @@ export const SettingItem = styled.li<ISettingProps>`
   padding: 16px;
   display: flex;
   flex-direction: ${(props) => (props.type === "verUpdate" ? "column" : "row")};
+  justify-content: ${(props) => (props.type === "notification" ? "space-between" : "normal")};
   background-color: ${({ theme }) => theme.colors.white};
   border-bottom: 1px solid ${({ theme }) => theme.colors.gray_5};
   ${({ theme }) => theme.typo.label1_16_M};
   color: ${({ theme }) => theme.colors.gray_1};
+`;
+
+export const TopWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 16px;
+  padding-bottom: 0;
 `;
 
 export const TopBox = styled.div`
@@ -27,9 +35,42 @@ export const ButtomBox = styled.div`
   flex-direction: column;
 `;
 
+export const TextBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+`;
+
+// TODO 추후 공통 컴포넌트 작업 필요합니다.
+export const StyledIconBox = styled.div`
+  display: flex;
+  width: 24px;
+  height: 24px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50px;
+  background-color: ${({ theme }) => theme.colors.yellow_3};
+`;
+
+export const Title = styled.p`
+  ${({ theme }) => theme.typo.body1_18_B}
+  color: ${({ theme }) => theme.colors.gray_1};
+`;
+
+export const SubTitle = styled.span`
+  ${({ theme }) => theme.typo.body2_16_R}
+  color: ${({ theme }) => theme.colors.gray_2};
+`;
+
 export const Text = styled.span`
   ${({ theme }) => theme.typo.label1_16_M};
   color: ${({ theme }) => theme.colors.gray_1};
+`;
+
+export const SubTextPL = styled.span`
+  ${({ theme }) => theme.typo.caption1_12_R};
+  color: ${({ theme }) => theme.colors.gray_2};
+  padding-left: 28px;
 `;
 
 export const SubText = styled.span`
