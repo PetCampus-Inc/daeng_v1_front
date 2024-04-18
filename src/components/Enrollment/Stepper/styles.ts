4d2b23d5acda103471c5fde6f2ae1766e0f76a57
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-04-18 16:25:34 +0900

    design: css 깨진 부분 수정

--- src/components/Enrollment/Stepper/styles.ts
+++ src/components/Enrollment/Stepper/styles.ts
@@ -3,7 +3,7 @@ import styled, { DefaultTheme, css } from "styled-components";
 export const ButtonContainer = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 3px;
+  gap: 4px;
 
   margin-top: 28px;
 `;
@@ -18,39 +18,6 @@ export const Caption = styled.div`
     ${({ theme }) => theme.typo.caption1_12_R};
     color: ${({ theme }) => theme.colors.gray_3};
   }
-`;
-
-export const ButtonWrapper = styled.div`
-  display: flex;
-  gap: 4px;
-`;
-
-export const Button = styled.button`
-  display: flex;
-  flex: 3;
-  width: 100%;
-  padding: 10px 0;
-  justify-content: center;
-  align-items: center;
-
-  border-radius: 8px;
-  background-color: ${({ theme }) => theme.colors.primaryColor};
-
-  ${({ theme }) => theme.typo.label1_16_B};
-  color: ${({ theme }) => theme.colors.white};
-
-  &[aria-disabled="true"] {
-    background-color: ${({ theme }) => theme.colors.gray_4};
-    color: ${({ theme }) => theme.colors.gray_2};
-    tabindex: -1;
-  }
-`;
-
-export const PrevButton = styled(Button)`
-  flex: 1;
-
-  background-color: ${({ theme }) => theme.colors.br_4};
-  color: ${({ theme }) => theme.colors.primaryColor};
 `;
 
 export const IndicatorContainer = styled.div`
