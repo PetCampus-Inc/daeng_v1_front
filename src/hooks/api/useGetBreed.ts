9b773b382c1dcf1dea50ef735af39eba769eb555
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-04-20 16:47:14 +0900

    feat: useGetBreed훅 캐싱 사용, keepPreviousData 사용

--- src/hooks/api/useGetBreed.ts
+++ src/hooks/api/useGetBreed.ts
@@ -5,9 +5,12 @@ import { handleGetBreed } from "apis/member/school.api";
 
 const useGetBreed = (inputValue: string) => {
   return useQuery({
-    queryKey: QUERY_KEY.BREED,
+    queryKey: QUERY_KEY.BREED(inputValue),
     queryFn: () => handleGetBreed(inputValue),
-    enabled: false
+    placeholderData: (previousData) => previousData,
+    enabled: !!inputValue, // 입력값이 있을 때만 실행,
+    staleTime: 60 * 1000, // 1분동안 캐시된 데이터 사용
+    gcTime: 5 * 60 * 1000 // 비활성 캐시는 5분동안 유지
   });
 };
 
