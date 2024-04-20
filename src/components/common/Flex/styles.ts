227d57b4a053a0a892938ca0ab6aa108084a07d5
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-04-21 00:43:26 +0900

    feat: Flex 컴포넌트 구현

---
+++ src/components/common/Flex/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+import type { FlexOptions } from ".";
+
+export const StyledFlex = styled.div.attrs<FlexOptions>((props) => ({
+  style: {
+    display: "flex",
+    gap: props.gap + "px" || undefined,
+    flexDirection: props.direction || undefined,
+    alignItems: props.align || undefined,
+    justifyContent: props.justify || undefined,
+    flexWrap: props.wrap || undefined,
+    flexBasis: props.basis || undefined,
+    flexGrow: props.grow || undefined,
+    flexShrink: props.shrink || undefined
+  }
+}))``;
