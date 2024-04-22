99778b3d4dccef5d36b181ad7f69dcb3c8db1240
jieun419 <crg1050@naver.com>
2024-04-23 03:03:57 +0900

    Chore: 견주 마이페이지 작업을 위한 컴포넌트 폴더 생성 및 컨테이너 스타일 추가

---
+++ src/components/Member/MyPage/Container/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const PageContainer = styled.div.withConfig({
+  shouldForwardProp: (prop) => !["pt", "imageUrl"].includes(prop)
+})<{
+  pt?: string;
+  imageUrl?: string;
+}>`
+  padding-top: ${({ pt }) => (pt ? `calc(5vh + ${pt}rem)` : "calc(5vh)")};
+  background: ${({ imageUrl, theme }) =>
+    imageUrl
+      ? `linear-gradient(0deg, rgba(0, 0, 0, 0.60) 0%, rgba(0, 0, 0, 0.60) 100%), url(${imageUrl}) lightgray 50% / cover no-repeat`
+      : theme.colors.br_2};
+  width: 100vw;
+  background-size: cover;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
--- src/pages/MemberMyPage.tsx
+++ src/pages/MemberMyPage.tsx
@@ -1,7 +1,19 @@
 import NavBar from "components/common/NavBar";
 
+import { PageContainer } from "../components/Member/MyPage/Container/styles";
+
 const MemberMyPage = () => {
-  return <NavBar />;
+  return (
+    <>
+      <NavBar />
+      <PageContainer
+        pt="7"
+        imageUrl={
+          "https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        }
+      ></PageContainer>
+    </>
+  );
 };
 
 export default MemberMyPage;
