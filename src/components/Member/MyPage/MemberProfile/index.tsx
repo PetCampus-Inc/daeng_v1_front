d19812edf187c366313811b95c22286402ba8053
jieun419 <crg1050@naver.com>
2024-04-23 20:14:41 +0900

    Refactor: 불필요한 Fragment 태그 및 공백 삭제

--- src/components/Member/MyPage/MemberProfile/index.tsx
+++ src/components/Member/MyPage/MemberProfile/index.tsx
@@ -9,24 +9,21 @@ import * as S from "./styles";
 
 const MemberProfile = () => {
   return (
-    <>
-      <S.ProfileWrapper>
-        <S.ProfileImage
-          src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="member-image"
-        />
-
-        <S.ProfileDetail>
-          <S.DetailItem>
-            <S.GotoInfoButton>
-              <S.Text className="name">뽀뽀의 언니</S.Text>
-              <ArrowRightSquare />
-            </S.GotoInfoButton>
-            <S.Text className="number">박유빈</S.Text>
-          </S.DetailItem>
-        </S.ProfileDetail>
-      </S.ProfileWrapper>
-    </>
+    <S.ProfileWrapper>
+      <S.ProfileImage
+        src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="member-image"
+      />
+      <S.ProfileDetail>
+        <S.DetailItem>
+          <S.GotoInfoButton>
+            <S.Text className="name">뽀뽀의 언니</S.Text>
+            <ArrowRightSquare />
+          </S.GotoInfoButton>
+          <S.Text className="number">박유빈</S.Text>
+        </S.DetailItem>
+      </S.ProfileDetail>
+    </S.ProfileWrapper>
   );
 };
 
