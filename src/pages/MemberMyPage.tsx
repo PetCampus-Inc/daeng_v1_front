c6ecf11673eb287a7f1e217cb4d57a908865bbc2
jieun419 <crg1050@naver.com>
2024-04-23 19:54:14 +0900

    Feat: Header 컴포넌트 추가

--- src/pages/MemberMyPage.tsx
+++ src/pages/MemberMyPage.tsx
@@ -1,17 +1,18 @@
+import Header from "components/common/Header";
 import NavBar from "components/common/NavBar";
-
-import { PageContainer } from "../components/Member/MyPage/Container/styles";
+import { PageContainer } from "components/Member/MyPage/Container/styles";
 
 const MemberMyPage = () => {
   return (
     <>
-      <NavBar />
+      <Header type="setting" text="마이페이지" transparent={true} />
       <PageContainer
         pt="7"
         imageUrl={
           "https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         }
       ></PageContainer>
+      <NavBar type="member" />
     </>
   );
 };
