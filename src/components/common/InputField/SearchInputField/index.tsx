07dd1d01117585a6150ddf59c9c8c34fc10cb1f6
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-04-24 01:20:29 +0900

    hotfix: 중복 코드 제거
    style: useS3 훅 린트 fix
    fix: 파일명 변경 사항 반영
    fix: 중복 코드 제거

--- src/components/common/InputField/SearchInputField/index.tsx
+++ src/components/common/InputField/SearchInputField/index.tsx
@@ -1,6 +1,5 @@
 import SearchIcon from "assets/svg/search-icon";
 import XCircleIcon from "assets/svg/x-circle-icon";
-import XCircleIcon from "assets/svg/x-circle-icon";
 
 import * as S from "./styles";
 import InputField from "../index";
@@ -10,15 +9,12 @@ import type { InputFieldProps } from "../index";
 export interface SearchInputFieldProps extends Omit<InputFieldProps, "type"> {
   onSearch?: (value: string) => void;
   onClear?: () => void;
-  onClear?: () => void;
 }
 
 const SearchInputField = ({
   name,
   value,
-  value,
   onSearch,
-  onClear,
   onClear,
   disabled = false,
   readOnly = false,
@@ -26,11 +22,9 @@ const SearchInputField = ({
 }: SearchInputFieldProps) => {
   const handleClear = () => {
     onClear?.();
-    onClear?.();
   };
 
   const handleSearch = () => {
-    value && onSearch?.(value.toString());
     value && onSearch?.(value.toString());
   };
 
@@ -49,20 +43,13 @@ const SearchInputField = ({
         disabled={disabled}
         readOnly={readOnly}
         {...props}
-        disabled={disabled}
-        {...props}
       />
-      {value ? (
-        <S.SearchInputButton onClick={handleClear}>
-          <XCircleIcon />
       {value ? (
         <S.SearchInputButton onClick={handleClear}>
           <XCircleIcon />
         </S.SearchInputButton>
       ) : (
         <S.SearchInputButton onClick={handleSearch} disabled={readOnly || disabled}>
-          <SearchIcon />
-        <S.SearchInputButton onClick={handleSearch} disabled={searchDisabled}>
           <SearchIcon />
         </S.SearchInputButton>
       )}
