9d1a3d3e5b1aea41499093c64859434ed6693af0
jieun419 <crg1050@naver.com>
2024-04-23 20:09:23 +0900

    Feat: MemberMyPage에 사용될 컴포넌트 추가

--- src/pages/MemberMyPage.tsx
+++ src/pages/MemberMyPage.tsx
@@ -1,6 +1,8 @@
 import Header from "components/common/Header";
 import NavBar from "components/common/NavBar";
 import { PageContainer } from "components/Member/MyPage/Container/styles";
+import MemberProfile from "components/Member/MyPage/MemberProfile";
+import { CardContainer, ContentContainer } from "components/Member/MyPage/styles";
 
 const MemberMyPage = () => {
   return (
@@ -11,7 +13,12 @@ const MemberMyPage = () => {
         imageUrl={
           "https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         }
-      ></PageContainer>
+      >
+        <ContentContainer>
+          <MemberProfile />
+          <CardContainer></CardContainer>
+        </ContentContainer>
+      </PageContainer>
       <NavBar type="member" />
     </>
   );
