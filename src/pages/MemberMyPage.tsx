4d69cee192d1f9cbc43f4185dce9da750b3c42b6
jieun419 <crg1050@naver.com>
2024-04-24 02:07:25 +0900

    Design: PageContainer padding-top 사이즈 임시 조절

--- src/pages/MemberMyPage.tsx
+++ src/pages/MemberMyPage.tsx
@@ -11,7 +11,7 @@ const MemberMyPage = () => {
     <>
       <Header type="setting" text="마이페이지" transparent={true} />
       <PageContainer
-        pt="7"
+        pt="4"
         imageUrl={
           "https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         }
