4a848ca571e1978df2722b4b612bfbd217bbcccb
jieun419 <crg1050@naver.com>
2024-04-24 01:39:54 +0900

    Refactor: Card 컴포넌트 분리 작업

--- src/components/Member/MyPage/MyDogInfo/index.tsx
+++ src/components/Member/MyPage/MyDogInfo/index.tsx
@@ -1,7 +1,6 @@
-import AddCIcon from "assets/svg/add-c-icon";
-import RightArrow from "assets/svg/right-arrow";
-
 import * as S from "./styles";
+import AddMyDogCard from "../Cards/AddMyDogCard";
+import MyDogCard from "../Cards/MyDogCard";
 
 const MyDogInfo = () => {
   return (
@@ -11,24 +10,8 @@ const MyDogInfo = () => {
         <S.DeleteDogButton>강아지 삭제</S.DeleteDogButton>
       </S.TitleBox>
       <S.MyDogInfoList>
-        <S.MyDogCard>
-          <S.InfoTextBox>
-            <S.DogName>뽀뽀</S.DogName>
-            <S.GotoSchoolInfoButton>
-              바나나 유치원 잠실점
-              <RightArrow />
-            </S.GotoSchoolInfoButton>
-            <S.CurrentStatusText>2023.12.20 등록</S.CurrentStatusText>
-          </S.InfoTextBox>
-          <S.MyDogImg
-            src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="my-dog"
-          />
-        </S.MyDogCard>
-        <S.AddMyDogCard>
-          <AddCIcon className="addIcon" />
-          <S.AddDogButton>강아지 추가하기</S.AddDogButton>
-        </S.AddMyDogCard>
+        <MyDogCard />
+        <AddMyDogCard />
       </S.MyDogInfoList>
     </S.DogInfoContainer>
   );
