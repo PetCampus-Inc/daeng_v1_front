c2b8f7a41567f053717805fafb8ab07f3c6d07ae
jieun419 <crg1050@naver.com>
2024-04-26 19:17:40 +0900

    Refactor: 불필요한 중괄호 삭제

--- src/components/Member/MyPage/Buttons/LogOutButton.tsx
+++ src/components/Member/MyPage/Buttons/LogOutButton.tsx
@@ -4,7 +4,7 @@ import { BackgroundButtonWrapper } from "components/Member/MyPage/styles";
 const LogOutButton = () => {
   return (
     <BackgroundButtonWrapper>
-      <BackgroundButton backgroundColor={"white"} buttonBackgroundColor={"gray_4"}>
+      <BackgroundButton backgroundColor="white" buttonBackgroundColor="gray_4">
         로그아웃
       </BackgroundButton>
     </BackgroundButtonWrapper>
