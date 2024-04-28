77e4fa79ce5c3b9ca1235168081059c17c261165
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-04-29 02:06:52 +0900

    refactor: CommentBox 개행 추가

--- src/components/Home/HomeImageSidler/CommentBox.tsx
+++ src/components/Home/HomeImageSidler/CommentBox.tsx
@@ -1,10 +1,20 @@
 import { CommentBoxWrapper, CommentText, CommentTextWrapper } from "./styles";
 
 const CommentBox = ({ comment }: { comment?: string }) => {
+  if (!comment) return null;
   return (
     <CommentBoxWrapper>
       <CommentTextWrapper>
-        <CommentText>{comment}</CommentText>
+        <CommentText>
+          {comment?.split("\n").map((line) => {
+            return (
+              <>
+                {line}
+                <br />
+              </>
+            );
+          })}
+        </CommentText>
       </CommentTextWrapper>
     </CommentBoxWrapper>
   );
