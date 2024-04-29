fce11dbc3bd790c002488529ab1b3b4474360031
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-04-29 17:04:26 +0900

    refactor: Text 컴포넌트 tag → as prop 이름 변경

--- src/components/Home/HomeHeader.tsx
+++ src/components/Home/HomeHeader.tsx
@@ -11,11 +11,11 @@ const HomeHeader = ({ data }: { data: IHome }) => {
         <ImgWrapper>
           <Img src="https://github.githubassets.com/assets/pair-extraordinaire-default-579438a20e01.png" />
         </ImgWrapper>
-        <Text tag="h2" typo="title2_20_B" color="darkBlack">
+        <Text as="h2" typo="title2_20_B" color="darkBlack">
           {data.dogName} {data.relation}님
         </Text>
       </Flex>
-      <Text tag="p" typo="body2_16_R" color="gray_2">
+      <Text as="p" typo="body2_16_R" color="gray_2">
         아래에서 {data.dogName} 유치원 일지를 확인할 수 있어요
       </Text>
     </StyledHgroup>
