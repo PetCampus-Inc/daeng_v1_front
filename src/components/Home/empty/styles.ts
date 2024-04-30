1a2e26c27a8985485707d90c3e762e6b36074f76
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-01 03:04:03 +0900

    design: 사진앨범 없는 경우 화면 구현

--- src/components/Home/empty/styles.ts
+++ src/components/Home/empty/styles.ts
@@ -18,7 +18,7 @@ export const EmptyContainer = styled.div`
     transform: translate(-50%, -50%);
     width: 100%;
     height: 100%;
-    background-image: url(images/empty-image.svg);
+    background-image: url(images/empty-image1.svg);
     background-size: 50% 50%;
     background-position: center 40%;
     background-repeat: no-repeat;
@@ -28,6 +28,38 @@ export const EmptyContainer = styled.div`
     height: 39%;
     margin-top: auto;
 
+    ${({ theme }) => theme.typo.label2_14_R};
+    color: ${({ theme }) => theme.colors.gray_1};
+  }
+`;
+
+export const EmptyAlbumContainer = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  min-height: 127px;
+  aspect-ratio: 2.8 / 1;
+
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.colors.white};
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+    height: 100%;
+    background-image: url(images/empty-image2.svg);
+    background-size: 52% 52%;
+    background-position: center 45%;
+    background-repeat: no-repeat;
+  }
+
+  & > span {
     ${({ theme }) => theme.typo.label2_14_R};
     color: ${({ theme }) => theme.colors.gray_1};
   }
