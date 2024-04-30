27d89b345e83261c106b74c1ec40033bb3154d77
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-01 00:04:30 +0900

    refactor: 추가할 강아지 없는 화면 컴포넌트 분리

---
+++ src/components/Admin/AttendCare/empty/EmptyDog.tsx
@@ -0,0 +1,26 @@
+import { PATH } from "constants/path";
+
+import CareDogIcon from "assets/svg/care-dog-icon";
+import RightArrow from "assets/svg/right-arrow";
+import { Text } from "components/common";
+import { Link } from "react-router-dom";
+
+import { LinkText } from "../card/styles";
+
+const EmptyDog = () => {
+  return (
+    <>
+      <CareDogIcon aria-hidden="true" />
+      <LinkText>
+        <Link to={PATH.ADMIN_ATTENDANCE}>
+          출석 진행 하기 <RightArrow w={"20"} h={"20"} aria-hidden="true" />
+        </Link>
+      </LinkText>
+      <Text typo="label2_14_R" color="gray_2">
+        출석 후에 관리할 강아지를 선택할 수 있어요
+      </Text>
+    </>
+  );
+};
+
+export default EmptyDog;
