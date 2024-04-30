cf3a386727acf24c7e923587273e45e4ce262c69
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-01 01:41:22 +0900

    design: 전송한 이미지가 없는 경우 화면 구현

---
+++ src/components/Home/empty/EmptyImg.tsx
@@ -0,0 +1,11 @@
+import { EmptyContainer } from "./styles";
+
+const EmptyImg = () => {
+  return (
+    <EmptyContainer>
+      <span>전송된 사진이 없어요</span>
+    </EmptyContainer>
+  );
+};
+
+export default EmptyImg;
---
+++ src/components/Home/empty/styles.ts
@@ -0,0 +1,33 @@
+import { styled } from "styled-components";
+
+export const EmptyContainer = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  aspect-ratio: 1 / 1;
+  border-radius: 8px;
+
+  ${({ theme }) => theme.typo.label2_14_R};
+  color: ${({ theme }) => theme.colors.gray_1};
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+    height: 100%;
+    background-image: url(images/empty-image.svg);
+    background-size: 50% 50%;
+    background-position: center 40%;
+    background-repeat: no-repeat;
+  }
+
+  & > span {
+    height: 39%;
+    margin-top: auto;
+  }
+`;
