b7b8cc708fb93d080d8dd09707a0f5d3ac310696
jieun419 <crg1050@naver.com>
2024-05-04 02:08:28 +0900

    Feat: BasicModal 공통 컴포넌트 생성

---
+++ src/components/common/Modal/BasicModal.tsx
@@ -0,0 +1,40 @@
+import Modal from "components/common/ButtonModal";
+
+type DisconnectModalProps = {
+  // TODO: 탈퇴 액션 추가
+  isOpen: boolean;
+  close: () => void;
+  action: () => void;
+  title: string;
+  subtitle: string;
+  closeText: string;
+  actionText: string;
+  colorScheme: "primary" | "red";
+};
+
+const BasicModal = ({
+  close,
+  action,
+  isOpen,
+  title,
+  subtitle,
+  closeText,
+  actionText,
+  colorScheme
+}: DisconnectModalProps) => {
+  return (
+    <Modal isOpen={isOpen} close={close}>
+      <Modal.Content variant="two">
+        <Modal.Title title={title} subtitle={subtitle} />
+        <Modal.Button
+          colorScheme={colorScheme}
+          closeText={closeText}
+          actionText={actionText}
+          actionFn={action}
+        />
+      </Modal.Content>
+    </Modal>
+  );
+};
+
+export default BasicModal;
