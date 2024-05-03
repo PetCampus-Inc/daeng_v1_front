b54abb8f5544d0bda3b7e411be2dc9b25f2135e3
jieun419 <crg1050@naver.com>
2024-05-03 17:58:56 +0900

    Refactor: IMemberInfoEdite interface 추가

--- src/types/Member.type.ts
+++ src/types/Member.type.ts
@@ -7,3 +7,10 @@ export interface IUserInfo {
   name: string;
   userId: number;
 }
+
+export interface IMemberInfoEdite {
+  requiredItems?: Map<number, boolean>;
+  handleFocus?: () => void;
+  handleBlur?: () => void;
+  isFocusing?: boolean;
+}
