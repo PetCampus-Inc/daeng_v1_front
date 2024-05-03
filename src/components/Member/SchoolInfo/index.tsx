a8b8afc25d4f463a094a3f2441bd7da1d419e6e4
jieun419 <crg1050@naver.com>
2024-05-04 02:24:12 +0900

    Feat: AlertBottomSheet 컴포넌트 추가 및 멘트 수정

--- src/components/Member/SchoolInfo/index.tsx
+++ src/components/Member/SchoolInfo/index.tsx
@@ -3,6 +3,7 @@ import List from "assets/svg/list-normal-school-icon";
 import Map from "assets/svg/map-pin-icon";
 import Phone from "assets/svg/phone-basic";
 import PhoneIcon from "assets/svg/phone-icon";
+import AlertBottomSheet from "components/common/BottomSheet/AlertBottomSheet";
 import CallSchoolBottomSheet from "components/common/BottomSheet/CallBottomSheet/CallSchoolBottomSheet";
 import BackgroundButton from "components/common/Button/BackgroundButton";
 import BasicModal from "components/common/Modal/BasicModal";
@@ -39,10 +40,25 @@ const SchoolInfo = () => {
           close();
         }}
         title="유치원과 연결을 끊으시겠습니까?"
-        subtitle="유치원과 연결이 끊겨 채팅내역, 알림장, 사진앨범 등의 모든 기록에 접근할 수 없어요 모든 기록은 유치원에서는 유지되니 걱정마세요"
+        subtitle="지금까지 주고 받은 채팅내역, 알림장, 사진앨범 등의 모든 기록은 유지되지만, 유치원과 연결이 끊겨 더 이상 해당 유치원의 소식을 받을 수 없어요"
         closeText="취소"
-        actionText="탈퇴하기"
+        actionText="연결 끊기"
         colorScheme="red"
+      />
+    ));
+
+  const openAlertPopup = () =>
+    overlay.open(({ isOpen, close }) => (
+      <AlertBottomSheet
+        title="이용권의 사용기한이 남아 있어요"
+        subtitle="유치원과의 연결을 끊으면 서비스 이용이 불가능해요 아직 이용권의 사용기한이 남아 있어요 그래도 연결을 끊으시겠습니까?"
+        isOpen={isOpen}
+        close={close}
+        actionText={"연결 끊기"}
+        actionFn={() => {
+          console.log("유치원 연결 끊기");
+          close();
+        }}
       />
     ));
 
@@ -79,11 +95,7 @@ const SchoolInfo = () => {
           <S.ListTitle>{data && data.enrollDate ? `${data.enrollDate}등록` : ""}</S.ListTitle>
         </S.InfoList>
       </S.InfoContainer>
-      <BackgroundButton
-        backgroundColor={"white"}
-        onClick={openDisconnectPopup}
-        className="disconnect"
-      >
+      <BackgroundButton backgroundColor={"white"} onClick={openAlertPopup} className="disconnect">
         유치원 연결 끊기
       </BackgroundButton>
     </S.CardContainer>
