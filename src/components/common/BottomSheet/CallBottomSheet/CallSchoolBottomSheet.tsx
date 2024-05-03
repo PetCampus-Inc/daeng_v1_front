982bffe7578b5a6090a3a8d3098175707f3b0a0f
jieun419 <crg1050@naver.com>
2024-05-04 02:09:00 +0900

    Feat: 유치원 전화 걸기 CallSchoolBottomSheet 공통 컴포넌트 생성

---
+++ src/components/common/BottomSheet/CallBottomSheet/CallSchoolBottomSheet.tsx
@@ -0,0 +1,33 @@
+import { memo } from "react";
+
+import CallBottomSheet from "./index";
+
+import type { ISchoolCallInfo } from "types/admin/mypage.type";
+
+interface CallSchoolBottomSheetProps {
+  info: ISchoolCallInfo | null;
+  isOpen: boolean;
+  close: () => void;
+}
+
+const CallSchoolBottomSheet = memo(({ info, isOpen, close }: CallSchoolBottomSheetProps) => {
+  if (!info || info.schoolName === undefined || info.schoolNumber === undefined) return null;
+
+  const handleCallMember = (info: ISchoolCallInfo) => {
+    console.log(info.schoolNumber);
+    // TODO: 해당 유치원 전화번호가 입력된 전화앱으로 바로 이동.
+    close();
+  };
+
+  return (
+    <CallBottomSheet
+      isOpen={isOpen}
+      schoolName={info.schoolName}
+      phoneNumber={info.schoolNumber}
+      close={close}
+      handleCall={() => handleCallMember(info)}
+    />
+  );
+});
+
+export default CallSchoolBottomSheet;
