b0cf3766647ed8e7216c4ce51402e3d88a086f95
jieun419 <crg1050@naver.com>
2024-05-05 05:27:48 +0900

    Feat: MemberInfo type 추가

--- src/types/member/home.types.ts
+++ src/types/member/home.types.ts
@@ -1,6 +1,7 @@
 export type TAttendanceStatus = "ATTENDED" | "NOT_ATTENDED";
 export type TAgendaStatus = "COMPLETE" | "NOT_YET" | "WRITING";
 export type TImageType = "IMAGE" | "PROFILE";
+export type TDogStatus = "ENROLLED" | "DROP_OUT";
 
 export interface IHome {
   dogId: number;
@@ -19,4 +20,21 @@ export interface ImageList {
   imageType: TImageType;
   comment?: string;
   createdTime: string;
+}
+
+export interface IDoglist {
+  dogId: number;
+  dogName: string;
+  status: TDogStatus;
+  schoolId: number;
+  schoolName: string;
+  registeredDate: number[];
+  dropOutDate: string;
+}
+export interface IMemberInfo {
+  memberId: number;
+  memberName: string;
+  relation: string;
+  fileUrl: string;
+  doglist: IDoglist[];
 }
