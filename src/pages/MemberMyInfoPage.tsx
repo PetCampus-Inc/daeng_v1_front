8fce49fa6594498d1bd8437e15f635a45b6d3507
jieun419 <crg1050@naver.com>
2024-05-05 03:27:48 +0900

    Feat/Refactor: Header 수정 버튼 클릭 시 프로필 수정 페이지로 이동 하도록 수정

--- src/pages/MemberMyInfoPage.tsx
+++ src/pages/MemberMyInfoPage.tsx
@@ -1,12 +1,21 @@
+import { PATH } from "constants/path";
+
 import Header from "components/common/Header";
 import MyInfo from "components/Member/MyPage/MyMemberinfo";
 import { FootIconItem } from "components/Member/MyPage/MyMemberinfo/styles";
+import { useNavigate } from "react-router-dom";
 import { PageContainer } from "styles/StyleModule";
 
 const MemberMyInfoPage = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <Header type="edite" text="내프로필" transparent={true} />
+      <Header
+        type="edite"
+        text="내프로필"
+        handleClick={() => navigate(PATH.MEMBER_MY_INFO_EDITE_PAGE)}
+        transparent
+      />
       <PageContainer pt="1" color="br_5">
         <MyInfo />
         <FootIconItem className="br4-foot" />
