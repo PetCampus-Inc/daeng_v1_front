18aeb350a76a2036bea75124fabafcf8ab6816b0
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-05 03:34:01 +0900

    feat: Flex 컴포넌트 props 확장

--- src/components/common/Flex/styles.ts
+++ src/components/common/Flex/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { remCalc } from "utils/calculator";
 
 import type { FlexOptions } from ".";
 
@@ -13,8 +14,6 @@ export const StyledFlex = styled.div.withConfig({
       "maxHeight",
       "overflowX",
       "overflowY",
-      "margin",
-      "padding",
       "display",
       "direction",
       "align",
@@ -22,7 +21,28 @@ export const StyledFlex = styled.div.withConfig({
       "wrap",
       "basis",
       "grow",
-      "shrink"
+      "shrink",
+      "marginBlock",
+      "marginInline",
+      "mr",
+      "mt",
+      "ml",
+      "mb",
+      "marginRight",
+      "marginTop",
+      "marginLeft",
+      "marginBottom",
+      "padding",
+      "paddingBlock",
+      "paddingInline",
+      "paddingTop",
+      "paddingRight",
+      "paddingBottom",
+      "paddingLeft",
+      "pt",
+      "pr",
+      "pb",
+      "pl"
     ].includes(prop)
 })<FlexOptions>`
   width: ${(props) => props.width || undefined};
@@ -31,8 +51,29 @@ export const StyledFlex = styled.div.withConfig({
   max-height: ${(props) => props.maxHeight || undefined};
   overflow-x: ${(props) => props.overflowX || undefined};
   overflow-y: ${(props) => props.overflowY || undefined};
-  margin: ${(props) => props.margin || undefined};
-  padding: ${(props) => props.padding || undefined};
+
+  margin: ${({ margin }) => (margin ? margin : undefined)};
+  margin-right: ${({ mr, marginRight }) =>
+    mr ? remCalc(mr) : marginRight ? remCalc(marginRight) : undefined};
+  margin-top: ${({ mt, marginTop }) =>
+    mt ? remCalc(mt) : marginTop ? remCalc(marginTop) : undefined};
+  margin-left: ${({ ml, marginLeft }) =>
+    ml ? remCalc(ml) : marginLeft ? remCalc(marginLeft) : undefined};
+  margin-bottom: ${({ mb, marginBottom }) =>
+    mb ? remCalc(mb) : marginBottom ? remCalc(marginBottom) : undefined};
+  margin-inline: ${({ marginInline }) => (marginInline ? remCalc(marginInline) : undefined)};
+  margin-block: ${({ marginBlock }) => (marginBlock ? remCalc(marginBlock) : undefined)};
+  padding: ${({ padding }) => (padding ? padding : undefined)};
+  padding-top: ${({ pt, paddingTop }) =>
+    pt ? remCalc(pt) : paddingTop ? remCalc(paddingTop) : undefined};
+  padding-right: ${({ pr, paddingRight }) =>
+    pr ? remCalc(pr) : paddingRight ? remCalc(paddingRight) : undefined};
+  padding-bottom: ${({ pb, paddingBottom }) =>
+    pb ? remCalc(pb) : paddingBottom ? remCalc(paddingBottom) : undefined};
+  padding-left: ${({ pl, paddingLeft }) =>
+    pl ? remCalc(pl) : paddingLeft ? remCalc(paddingLeft) : undefined};
+  padding-inline: ${({ paddingInline }) => (paddingInline ? remCalc(paddingInline) : undefined)};
+  padding-block: ${({ paddingBlock }) => (paddingBlock ? remCalc(paddingBlock) : undefined)};
 
   display: ${(props) => props.display || "flex"};
   gap: ${(props) => (props.gap ? props.gap + "px" : undefined)};
