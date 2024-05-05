95817047b88be7f58ce28515952afe8b26f69d2e
jieun419 <crg1050@naver.com>
2024-05-06 00:47:01 +0900

    Chore: Irelation -> IRelation 소문자에서 대문자로 오타 수정

--- src/constants/relation.ts
+++ src/constants/relation.ts
@@ -1,9 +1,9 @@
-interface Irelation {
+interface IRelation {
   [key: string]: string;
 }
 
 // TODO 언니/누나, 오빠/형의 경우 강아지 성별에 따라 판단되는지 확인 필요
-export const RELATION_DATA: Irelation = {
+export const RELATION_DATA: IRelation = {
   DAD: "아빠",
   MOM: "엄마",
   SISTER: "언니/누나",
