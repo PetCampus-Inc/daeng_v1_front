3f75b00219bda7c52bf4c8a33f80633a5a558b23
jieun419 <crg1050@naver.com>
2024-05-06 00:52:00 +0900

    Feat: SchoolSearchPage 컴포넌트 생성 및 기초 세팅

---
+++ src/pages/SchoolSearchPage.tsx
@@ -0,0 +1,13 @@
+import Header from "components/common/Header";
+import { PageContainer } from "styles/StyleModule";
+
+const SchoolSearchPage = () => {
+  return (
+    <>
+      <Header type="back" />
+      <PageContainer pt="2" color="white"></PageContainer>
+    </>
+  );
+};
+
+export default SchoolSearchPage;
