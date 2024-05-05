2fedf47d303561237ee1fa5c4d84091d7d372ccc
jieun419 <crg1050@naver.com>
2024-05-06 01:04:02 +0900

    Feat: SchoolSearchPage 필요한 props 데이터 추가

--- src/pages/SchoolSearchPage.tsx
+++ src/pages/SchoolSearchPage.tsx
@@ -1,11 +1,16 @@
 import Header from "components/common/Header";
+import DogOwner from "components/SignIn/DogOwner";
+import useSignIn from "hooks/api/useSignIn";
 import { PageContainer } from "styles/StyleModule";
 
 const SchoolSearchPage = () => {
+  const { setCurrentMainStep } = useSignIn();
   return (
     <>
       <Header type="back" />
-      <PageContainer pt="2" color="white"></PageContainer>
+      <PageContainer pt="2" color="white">
+        <DogOwner currentMainStep={1} setCurrentMainStep={setCurrentMainStep} />
+      </PageContainer>
     </>
   );
 };
