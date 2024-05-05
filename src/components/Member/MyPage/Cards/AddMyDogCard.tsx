0626a64d78b7e53bea06874dc9c44b9ccdadfc24
jieun419 <crg1050@naver.com>
2024-05-06 01:16:17 +0900

    Feat: 유치원 검색 페이지로 이동하도록 navigate 추가

--- src/components/Member/MyPage/Cards/AddMyDogCard.tsx
+++ src/components/Member/MyPage/Cards/AddMyDogCard.tsx
@@ -1,10 +1,14 @@
+import { PATH } from "constants/path";
+
 import AddCIcon from "assets/svg/add-c-icon";
+import { useNavigate } from "react-router-dom";
 
 import * as S from "./styles";
 
 const AddMyDogCard = () => {
+  const navigate = useNavigate();
   return (
-    <S.AddMyDogCard role="button">
+    <S.AddMyDogCard role="button" onClick={() => navigate(PATH.MEMBER_MY_SCHOOL_SEARCH)}>
       <AddCIcon className="addIcon" />
       <S.AddDogButton>강아지 추가하기</S.AddDogButton>
     </S.AddMyDogCard>
