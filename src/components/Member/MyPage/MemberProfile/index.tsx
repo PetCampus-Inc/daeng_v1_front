7f6dcbd5092bdfe61bd93b44a09e0073af8d3da2
jieun419 <crg1050@naver.com>
2024-05-05 22:54:40 +0900

    Refactor: 프로필 사진 조건문 추가

--- src/components/Member/MyPage/MemberProfile/index.tsx
+++ src/components/Member/MyPage/MemberProfile/index.tsx
@@ -14,7 +14,11 @@ const MemberProfile = ({ data }: MemberInfoProps) => {
   return (
     <S.ProfileWrapper>
       <S.ProfileImage
-        src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src={
+          data.fileUrl
+            ? data.fileUrl
+            : "https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        }
         alt="member-image"
       />
       <S.ProfileDetail>
