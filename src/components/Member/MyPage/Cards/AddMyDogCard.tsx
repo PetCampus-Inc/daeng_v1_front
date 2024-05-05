14ccafee7533fac1971fb5fb6928a8d4b8ac79fc
jieun419 <crg1050@naver.com>
2024-05-05 23:21:03 +0900

    Refactor: AddMyDogCard에  role="button" 속성 추가

--- src/components/Member/MyPage/Cards/AddMyDogCard.tsx
+++ src/components/Member/MyPage/Cards/AddMyDogCard.tsx
@@ -4,7 +4,7 @@ import * as S from "./styles";
 
 const AddMyDogCard = () => {
   return (
-    <S.AddMyDogCard>
+    <S.AddMyDogCard role="button">
       <AddCIcon className="addIcon" />
       <S.AddDogButton>강아지 추가하기</S.AddDogButton>
     </S.AddMyDogCard>
