f8e007238fd68f8641e9999d017b777ec5e5ad5f
jieun419 <crg1050@naver.com>
2024-05-06 22:18:42 +0900

    Chore: 불필요한 코드 삭제

--- src/hooks/api/member/member.ts
+++ src/hooks/api/member/member.ts
@@ -1,18 +1,11 @@
 import { QUERY_KEY } from "constants/queryKey";
 
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { handleGetMemberInfo, handleGetMemberMainDogInfo } from "apis/member/member.api";
+import { handleGetMemberInfo } from "apis/member/member.api";
 
 export const useGetMemberInfo = (memberId: number) => {
   return useSuspenseQuery({
     queryKey: QUERY_KEY.MEMBER_INFO(memberId),
     queryFn: () => handleGetMemberInfo(memberId)
-  });
-};
-
-export const useGetMemberMainDogInfo = (memberId: number) => {
-  return useSuspenseQuery({
-    queryKey: QUERY_KEY.MEMBER_MAIN_DOG_INFO(memberId),
-    queryFn: () => handleGetMemberMainDogInfo(memberId)
   });
 };
