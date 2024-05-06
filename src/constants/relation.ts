50e68ed93d6a7dad7b4fd0a486e0e2f1072d2ef4
jieun419 <crg1050@naver.com>
2024-05-06 18:12:24 +0900

    Chore: RELATION_DATA 오타 수정

--- src/constants/relation.ts
+++ src/constants/relation.ts
@@ -4,8 +4,8 @@ interface IRelation {
 
 // TODO 언니/누나, 오빠/형의 경우 강아지 성별에 따라 판단되는지 확인 필요
 export const RELATION_DATA: IRelation = {
-  DAD: "아빠",
-  MOM: "엄마",
+  FATHER: "아빠",
+  MOTHER: "엄마",
   SISTER: "언니/누나",
   BROTHER: "오빠/형",
   FRIEND: "친구"
