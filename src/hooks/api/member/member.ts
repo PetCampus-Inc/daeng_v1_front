4335a42b82dd625568984f60e6d0579e4351d965
jieun419 <crg1050@naver.com>
2024-05-06 18:59:41 +0900

    Feat: 마이페이지 강아지 유치원 정보 api 및 query 추가

--- src/hooks/api/member/member.ts
+++ src/hooks/api/member/member.ts
@@ -2,6 +2,7 @@ import { QUERY_KEY } from "constants/queryKey";
 
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { handleGetMemberInfo, handleGetMemberMainDogInfo } from "apis/member/member.api";
+import { handleGetSchoolInfo } from "apis/member/school.api";
 
 export const useGetMemberInfo = (memberId: number) => {
   return useSuspenseQuery({
@@ -14,5 +15,12 @@ export const useGetMemberMainDogInfo = (memberId: number) => {
   return useSuspenseQuery({
     queryKey: QUERY_KEY.MEMBER_MAIN_DOG_INFO(memberId),
     queryFn: () => handleGetMemberMainDogInfo(memberId)
+  });
+};
+
+export const useGetMemberSchoolInfo = (dogId: number) => {
+  return useSuspenseQuery({
+    queryKey: QUERY_KEY.MEMBER_SCHOOL_INFO(dogId),
+    queryFn: () => handleGetSchoolInfo(dogId)
   });
 };
