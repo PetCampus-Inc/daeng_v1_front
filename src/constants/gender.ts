97946532b607410180288ef34008f879c1e1e723
jieun419 <crg1050@naver.com>
2024-05-08 05:27:37 +0900

    Feat: GENDER_DATA_ARR 데이터 추가

--- src/constants/gender.ts
+++ src/constants/gender.ts
@@ -6,3 +6,14 @@ export const GENDER_DATA: IGender = {
   FEMALE: "여성",
   MALE: "남성"
 };
+
+export const GENDER_DATA_ARR: IGender[] = [
+  {
+    gender: "여성",
+    type: "FEMALE"
+  },
+  {
+    gender: "남성",
+    type: "MALE"
+  }
+];
