ba5c6fcef0b2603b5f34b8071af19524cce02549
jieun419 <crg1050@naver.com>
2024-05-09 03:04:30 +0900

    Refactor: 커스텀 훅 불필요로 판단하여 삭제

--- src/components/Member/MyPage/Buttons/SaveButton.tsx
+++ src/components/Member/MyPage/Buttons/SaveButton.tsx
@@ -1,24 +1,54 @@
 import BackgroundButton from "components/common/Button/BackgroundButton";
-import { usePostMemberProfileInfo } from "hooks/api/member/member";
-import useMemberInfoFormValid from "hooks/common/useMemberInfoFormValid";
-import { useForm } from "react-hook-form";
+import { useGetMemberProfileInfo, usePostMemberProfileInfo } from "hooks/api/member/member";
+import { useCallback, useEffect, useState } from "react";
+import { useForm, useFormContext } from "react-hook-form";
 import { useParams } from "react-router-dom";
 
 const SaveButton = () => {
+  const [isDisabled, setIsDisabled] = useState(true);
+  const { watch } = useFormContext();
   const { memberId } = useParams();
   const methods = useForm({ mode: "onSubmit" });
   const mutatePostMemberInfo = usePostMemberProfileInfo();
+  const { data: previousValues } = useGetMemberProfileInfo(String(memberId));
 
-  const {
-    isDisabled,
-    memberName,
-    memberGender,
-    nickName,
+  const memberName = watch("memberName");
+  const memberGender = watch("memberGender") === "여" ? "FEMALE" : "MALE";
+  const nickName = watch("nickName");
+  const address = `${watch("address.street")}${watch("address.detail")}`;
+  const phoneNumber = watch("phoneNumber");
+  const emergencyNumber = watch("emergencyNumber");
+  const relation = watch("relation");
+
+  const checkFormValidity = useCallback(() => {
+    if (
+      previousValues.memberName !== memberName ||
+      previousValues.memberGender !== memberGender ||
+      previousValues.nickName !== nickName ||
+      previousValues.address !== address ||
+      previousValues.phoneNumber !== phoneNumber ||
+      previousValues.emergencyNumber !== emergencyNumber ||
+      previousValues.relation !== relation
+    ) {
+      return false;
+    }
+    return true;
+  }, [
     address,
-    phoneNumber,
     emergencyNumber,
+    memberGender,
+    memberName,
+    nickName,
+    phoneNumber,
+    previousValues.address,
+    previousValues.emergencyNumber,
+    previousValues.memberGender,
+    previousValues.memberName,
+    previousValues.nickName,
+    previousValues.phoneNumber,
+    previousValues.relation,
     relation
-  } = useMemberInfoFormValid(String(memberId));
+  ]);
 
   const updatedMemberInfo = {
     memberId: String(memberId),
@@ -44,6 +74,11 @@ const SaveButton = () => {
     //   },
     // });
   });
+
+  useEffect(() => {
+    const isValid = checkFormValidity();
+    setIsDisabled(isValid);
+  }, [checkFormValidity]);
 
   return (
     <BackgroundButton
