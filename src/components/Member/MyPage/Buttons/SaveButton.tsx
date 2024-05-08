b64436d28859c9410dac9a073473a97ec6c9c960
jieun419 <crg1050@naver.com>
2024-05-09 00:48:52 +0900

    Feat: 수정 완료 버튼 클릭 시 데이터 전달 (수정 필요)

--- src/components/Member/MyPage/Buttons/SaveButton.tsx
+++ src/components/Member/MyPage/Buttons/SaveButton.tsx
@@ -1,8 +1,45 @@
 import BackgroundButton from "components/common/Button/BackgroundButton";
+import { usePostMemberProfileInfo } from "hooks/api/member/member";
+import { useForm, useFormContext } from "react-hook-form";
+import { useParams } from "react-router-dom";
 
 const SaveButton = () => {
+  const { memberId } = useParams();
+  const methods = useForm({ mode: "onSubmit" });
+  const { watch } = useFormContext();
+  const mutatePostMemberInfo = usePostMemberProfileInfo();
+
+  const updatedMemberInfo = {
+    memberId: String(memberId),
+    memberName: watch("memberName"),
+    memberGender: watch("memberGender") === "여" ? "FEMALE" : "MALE",
+    // memberProfileUri: "se_ryusun_smile_face",
+    nickName: watch("nickName"),
+    address: `${watch("address.street")} ${watch("address.detail")}`,
+    phoneNumber: watch("phoneNumber"),
+    emergencyNumber: watch("emergencyNumber"),
+    relation: watch("relation")
+  };
+
+  const onSubmit = methods.handleSubmit(() => {
+    console.log("updatedMemberInfo", updatedMemberInfo);
+    //FIXME 500에러 발생
+    // mutatePostMemberInfo.mutateAttend(updatedMemberInfo, {
+    //   onError: (err) => {
+    //     console.log(err)
+    //   },
+    //   onSuccess: () => {
+    //     console.log('성공')
+    //   },
+    // });
+  });
+
   return (
-    <BackgroundButton backgroundColor="white" buttonBackgroundColor="primaryColor" disabled>
+    <BackgroundButton
+      onClick={onSubmit}
+      backgroundColor="white"
+      buttonBackgroundColor="primaryColor"
+    >
       수정 완료
     </BackgroundButton>
   );
