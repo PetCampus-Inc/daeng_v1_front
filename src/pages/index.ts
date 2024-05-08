95587754561f99f96c6aca8dd780846c71e9deb4
jieun419 <crg1050@naver.com>
2024-05-09 05:24:31 +0900

    Feat: 강아지 추가 가입 신청서 페이지 추가

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -18,6 +18,7 @@ export { default as EnrollmentFormEditPage } from "./EnrollmentFormEditPage";
 export { default as EnrollmentFormCreatePage } from "./EnrollmentFormCreatePage";
 export { default as EnrollmentFormListPage } from "./EnrollmentFormListPage";
 export { default as EnrollmentFormSubmitPage } from "./EnrollmentFormSubmitPage";
+export { default as EnrollmentFormMemberDogAddPage } from "./EnrollmentFormMemberDogAddPage";
 export { default as TeacherManagePage } from "./TeacherManagePage";
 export { default as AttendCareDeletePage } from "./AttendCareDeletePage";
 export { default as DogGalleryPage } from "./DogGalleryPage";
