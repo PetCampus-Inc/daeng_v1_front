66f44208470dcd9a4e808ceb8caba49308c1c31a
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-09 12:00:05 +0900

    feat: Login, Redirect 페이지 추가

---
+++ src/pages/RedirectPage.tsx
@@ -0,0 +1,24 @@
+import { useLogInMutation } from "hooks/api/login";
+import { useEffect } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
+import { isProviderValid } from "utils/auth";
+
+const RedirectPage = () => {
+  const { provider } = useParams();
+  const [searchParams] = useSearchParams();
+
+  const code = searchParams.get("code");
+
+  if (!code || !provider || !isProviderValid(provider)) {
+    throw new Error("로그인/회원가입에 실패했습니다.");
+  }
+  const loginMutate = useLogInMutation();
+
+  useEffect(() => {
+    loginMutate({ provider, code });
+  }, [code, loginMutate, provider]);
+
+  return <div>로그인 중...</div>;
+};
+
+export default RedirectPage;
