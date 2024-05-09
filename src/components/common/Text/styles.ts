7c9b53e83cd5404f879e68f29f02f5a9797613f6
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-09 12:03:26 +0900

    feat: Text 컴포넌트 em 자식요소 스타일링 추가

--- src/components/common/Text/styles.ts
+++ src/components/common/Text/styles.ts
@@ -9,7 +9,7 @@ export interface IStyledTextProps {
 }
 
 export const StyledText = styled.span.withConfig({
-  shouldForwardProp: (prop) => !["color", "typo", "isEllipsis"].includes(prop)
+  shouldForwardProp: (prop) => !["color", "typo", "isEllipsis", "accent"].includes(prop)
 })<IStyledTextProps>`
   color: ${({ color, theme }) => color && theme.colors[color]};
   ${({ theme, typo }) => theme.typo[typo]};
@@ -19,5 +19,15 @@ export const StyledText = styled.span.withConfig({
       overflow: hidden;
       white-space: nowrap;
       text-overflow: ellipsis;
-    `}
+    `};
+`;
+
+export type StyledEmEmProps = {
+  color?: TColor;
+};
+
+export const StyledEm = styled.em.withConfig({
+  shouldForwardProp: (prop) => !["color"].includes(prop)
+})<StyledEmEmProps>`
+  color: ${({ theme, color }) => (color && theme.colors[color]) || "inherit"};
 `;
