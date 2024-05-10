af501ec0e175900eca21aa0c42f51c477b6c1e25
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-10 20:22:56 +0900

    feat: 유치원 검색 인풋창 구현

---
+++ src/components/SignUp/SchoolSearchInputBox.tsx
@@ -0,0 +1,75 @@
+import { Flex } from "components/common";
+import SearchInputField from "components/common/InputField/SearchInputField";
+import { useGetSchool } from "hooks/api/signup";
+import { useClickOutSide } from "hooks/common/useClickOutSide";
+import { useState, useRef, type RefObject, useCallback, useEffect } from "react";
+import { useRecoilState } from "recoil";
+import { schoolIdAtom } from "store/form";
+
+import SchoolListDropdown from "./dropdown/SchoolListDropdown";
+
+interface SchoolSearchInputBoxProps {
+  searchText: string;
+  setSearchText: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const SchoolSearchInputBox = ({ searchText, setSearchText }: SchoolSearchInputBoxProps) => {
+  const contentRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [showDropdown, setShowDropdown] = useState<boolean>(false);
+
+  const [schoolId, setSchoolId] = useRecoilState(schoolIdAtom);
+
+  const { data } = useGetSchool(searchQuery);
+
+  const handleSearch = (value: string) => {
+    setSearchQuery(value);
+    setShowDropdown(true);
+  };
+
+  const handleClear = () => {
+    setSearchText("");
+    setSearchQuery("");
+    setSchoolId(null);
+    setShowDropdown(false);
+  };
+
+  const handleItemSelect = useCallback((id: number, name: string) => {
+    setSchoolId(id);
+    setSearchText(name);
+    setShowDropdown(false);
+  }, []);
+
+  useClickOutSide({
+    enabled: showDropdown,
+    targetRef: contentRef,
+    onClickOutside: () => setShowDropdown(false)
+  });
+
+  useEffect(
+    function handleDropdown() {
+      const shouldCloseDropdown = !searchText || schoolId;
+      setShowDropdown(!shouldCloseDropdown);
+    },
+    [searchText, schoolId]
+  );
+
+  return (
+    <Flex ref={contentRef} direction="column" gap={8}>
+      <SearchInputField
+        name="schoolSearch"
+        placeholder="검색어를 입력해주세요"
+        value={searchText}
+        onSearch={handleSearch}
+        onClear={handleClear}
+        onChange={(e) => {
+          setSchoolId(null);
+          setSearchText(e.target.value);
+        }}
+      />
+      {data && showDropdown && <SchoolListDropdown list={data} handleSelected={handleItemSelect} />}
+    </Flex>
+  );
+};
+
+export default SchoolSearchInputBox;
