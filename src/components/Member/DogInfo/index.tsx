7a42741f1dc1510e6c6c7e2fe182c8366a0a3ff5
jieun419 <crg1050@naver.com>
2024-05-11 04:47:17 +0900

    Design: 강아지 정보 카드 디자인

--- src/components/Member/DogInfo/index.tsx
+++ src/components/Member/DogInfo/index.tsx
@@ -1,42 +1,73 @@
 import ArrowRightIcon from "assets/svg/arrow-right-icon";
+import CalendarIcon from "assets/svg/calendar";
 import CarIcon from "assets/svg/car-icon";
 import GirlNormalIcon from "assets/svg/girl-normal-icon";
+import { Flex } from "components/common";
+
+import * as S from "./styles";
 
 const DogInfo = () => {
   return (
     <>
-      <div>
-        <div>
+      <S.DogDetailBox>
+        <S.BgImg>
           <img
-            src="https://images.unsplash.com/photo-1591160690555-5debfba289f0?q=80&amp;w=2864&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="dog_img"
+            src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="dog_bg"
           />
-        </div>
-        <div>
-          <div>
-            <span>뽀뽀</span>
-            <span>소형견</span>
-          </div>
-          <div>
-            <div>
-              <GirlNormalIcon />
-              암컷 / 중성화 O
-            </div>
-            <div>
-              <GirlNormalIcon />
-              2008.09.10
-            </div>
-            <div>
-              <GirlNormalIcon />
-              블랙 러시안 테리어
-            </div>
-          </div>
-        </div>
-        <button>
+        </S.BgImg>
+        <S.DogInfoBox>
+          <S.ImageBox
+            width="52px"
+            height="52px"
+            overflow="hidden"
+            position="relative"
+            borderRadius="circle"
+          >
+            <img
+              src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="dog_img"
+            />
+          </S.ImageBox>
+          <S.TextWrapper>
+            <S.TopInfoBox>
+              <S.Title>
+                <S.DogName>뽀뽀</S.DogName>
+                <S.DogSize>소형견</S.DogSize>
+              </S.Title>
+              <S.Editebutton>
+                <span>수정</span>
+                <ArrowRightIcon />
+              </S.Editebutton>
+            </S.TopInfoBox>
+            <Flex wrap="wrap" gap="8">
+              <S.InfoText>
+                <S.Icon>
+                  <GirlNormalIcon />
+                </S.Icon>
+                암컷 / 중성화 O
+              </S.InfoText>
+              <S.InfoText>
+                <S.Icon>
+                  <CalendarIcon />
+                </S.Icon>
+                2008.09.10
+              </S.InfoText>
+              <S.InfoText>
+                <S.Icon>
+                  <CalendarIcon />
+                </S.Icon>
+                블랙 러시안 테리어
+              </S.InfoText>
+            </Flex>
+          </S.TextWrapper>
+        </S.DogInfoBox>
+
+        <S.GotoEnrollButton>
           <span>뽀뽀의 가입신청서</span>
           <ArrowRightIcon />
-        </button>
-      </div>
+        </S.GotoEnrollButton>
+      </S.DogDetailBox>
 
       <div>
         <section>
--- src/components/Member/DogInfo/styles.ts
+++ src/components/Member/DogInfo/styles.ts
@@ -1,4 +1,5 @@
 import FootIcon from "assets/svg/foot-icon";
+import { Box } from "components/common/Box";
 import styled from "styled-components";
 
 export const Nav = styled.nav`
@@ -41,4 +42,135 @@ export const FootIconItem = styled(FootIcon)`
       fill: ${({ theme }) => theme.colors.br_3};
     }
   }
+`;
+
+export const DogDetailBox = styled.section`
+  position: relative;
+  border-radius: 16px;
+  overflow: hidden;
+  padding: 16px 16px 48px;
+  z-index: 1;
+`;
+
+export const BgImg = styled.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  z-index: -1;
+
+  & > img {
+    object-fit: cover;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    filter: blur(15px);
+  }
+
+  &::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background-color: ${({ theme }) => theme.colors.black};
+    opacity: 0.6;
+    z-index: 1;
+  }
+`;
+
+export const DogInfoBox = styled.div`
+  display: flex;
+  gap: 16px;
+  margin-bottom: 33px;
+`;
+
+export const ImageBox = styled(Box)`
+  & > img {
+    object-fit: cover;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+`;
+
+export const TextWrapper = styled.div`
+  width: 100%;
+  flex: 2;
+`;
+
+export const Title = styled.div`
+  display: flex;
+  gap: 4px;
+`;
+
+export const TopInfoBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 13px;
+`;
+
+export const DogName = styled.span`
+  ${({ theme }) => theme.typo.body1_18_B};
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+export const DogSize = styled.span`
+  background-color: ${({ theme }) => theme.colors.white};
+  border-radius: 50px;
+  padding: 4px 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${({ theme }) => theme.colors.primaryColor};
+  ${({ theme }) => theme.typo.caption1_12_R};
+`;
+
+export const Icon = styled.span`
+  border-radius: 50px;
+  background-color: ${({ theme }) => theme.colors.yellow_3};
+  width: 20px;
+  height: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const InfoText = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  color: ${({ theme }) => theme.colors.white};
+  ${({ theme }) => theme.typo.label2_14_R};
+`;
+
+export const Editebutton = styled.button`
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.colors.white};
+  ${({ theme }) => theme.typo.label2_14_R};
+`;
+
+export const GotoEnrollButton = styled.button`
+  background-color: ${({ theme }) => theme.colors.gray_3};
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 14px 0;
+  ${({ theme }) => theme.typo.label2_14_R}
 `;
