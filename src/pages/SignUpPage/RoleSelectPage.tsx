5f989446fa3a4b7e62227ba0c737cf8f3e03b4c4
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-11 02:24:52 +0900

    refactor: 역할 선택 페이지 리팩터링

--- src/pages/SignUpPage/RoleSelectPage.tsx
+++ src/pages/SignUpPage/RoleSelectPage.tsx
@@ -1,16 +1,61 @@
-import React from "react";
+import { Box, Text, Layout, Flex } from "components/common";
+import Header from "components/common/Header";
+import { StyledButton } from "components/SignIn/styles";
+import RoleBox from "components/SignUp/RoleBox";
+import { useState } from "react";
 
 interface IStepProps {
   onNextStep: (role: "TEACHER" | "OWNER") => void;
 }
+export enum Role {
+  TEACHER = "TEACHER",
+  OWNER = "OWNER"
+}
 
 const RoleSelectPage = ({ onNextStep }: IStepProps) => {
+  const [selected, setSelected] = useState<Role | null>(null);
+
+  const handleSelected = (role: Role) => {
+    setSelected(role);
+  };
+
   return (
-    <div>
-      <button onClick={() => onNextStep("OWNER")}>원장으로 가입하기</button>
-      <br />
-      <button onClick={() => onNextStep("TEACHER")}>교사로 가입하기</button>
-    </div>
+    <>
+      <Header type="text" text="역할 선택" />
+      <Layout type="page" pt={44}>
+        <Flex justify="center" gap={16}>
+          <RoleBox
+            role="TEACHER"
+            mainText="선생님"
+            subText="강아지 유치원에
+근무중인 선생님"
+            selected={selected === Role.TEACHER}
+            handleClick={() => handleSelected(Role.TEACHER)}
+          />
+          <RoleBox
+            role="OWNER"
+            mainText="원장님"
+            subText="강아지 유치원을
+운영중인 원장님"
+            selected={selected === Role.OWNER}
+            handleClick={() => handleSelected(Role.OWNER)}
+          />
+        </Flex>
+
+        <Box position="absolute" left={16} right={16} bottom={24}>
+          <StyledButton
+            type="button"
+            bg="primaryColor"
+            onClick={() => selected && onNextStep(selected)}
+            disabled={!selected}
+          >
+            <Text className={selected ? "" : "inactive"} typo="label1_16_B" color="white">
+              다음
+            </Text>
+          </StyledButton>
+        </Box>
+      </Layout>
+    </>
   );
 };
 
