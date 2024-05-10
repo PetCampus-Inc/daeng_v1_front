e4152462c706f4d0122bf4c62413c3ea5ecb459b
jieun419 <crg1050@naver.com>
2024-05-11 02:42:05 +0900

    Feat: 강아지 추가 기능 함수만 추가 (적용 필요)

--- src/components/Enrollment/Stepper/SubmitButton.tsx
+++ src/components/Enrollment/Stepper/SubmitButton.tsx
@@ -1,9 +1,11 @@
 import { FIELD_TO_STEP } from "constants/step";
 
 import { usePostEnrollment } from "hooks/api/member/enroll";
+import { useGetMemberProfileInfo } from "hooks/api/member/member";
 import { Adapter } from "libs/Adapter";
 import { MemberFormToServerAdapter } from "libs/Adapter/FormToServerAdapter";
 import { FieldValues, useFormContext, type FieldErrors } from "react-hook-form";
+import { useParams } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
 import { currentStepState } from "store/form";
 import { FormButton } from "styles/StyleModule";
@@ -11,17 +13,43 @@ import { FormButton } from "styles/StyleModule";
 import type { IRequestEnrollment } from "types/member/enrollment.types";
 
 const SubmitButton = ({ openPopup }: { openPopup: (field: string) => void }) => {
+  const { memberId } = useParams();
   const { handleSubmit } = useFormContext();
   const enrollMutation = usePostEnrollment();
+  const { data: memberInfoData } = useGetMemberProfileInfo(String(memberId));
 
   const setStep = useSetRecoilState(currentStepState);
+
+  // TODO memberInfo데이터 recoil로 분리 등 리팩토링 필요
+  const memberInfo = {
+    memberId: Number(memberInfoData.memberId),
+    memberName: memberInfoData.memberName,
+    memberGender: memberInfoData.memberGender,
+    address: `${memberInfoData.address && memberInfoData.address} ${memberInfoData.address && memberInfoData.addressDetail}`,
+    phoneNumber: memberInfoData.phoneNumber,
+    emergencyNumber: memberInfoData.emergencyPhoneNumber
+      ? memberInfoData.emergencyPhoneNumber
+      : null
+  };
 
   const onSubmit = (data: FieldValues) => {
     // FIXME: memberId, fileUrl 추가 필요
     const requestData = Adapter.from(data).to<FieldValues, IRequestEnrollment>((item) =>
       new MemberFormToServerAdapter(item).adapt()
     );
+
     enrollMutation(requestData);
+  };
+
+  // TODO /member url 일 경우 해당 함수 실행 (member 강아지 추가)
+  const onSubmitMemberDogAdd = (data: FieldValues) => {
+    // FIXME: fileUrl 추가 필요
+    const requestData = Adapter.from(data).to<FieldValues, IRequestEnrollment>((item) =>
+      new MemberFormToServerAdapter(item).adapt()
+    );
+
+    const memberDogAddInfo = { ...requestData, ...memberInfo };
+    enrollMutation(memberDogAddInfo);
   };
 
   const onInvalid = (errors: FieldErrors) => {
