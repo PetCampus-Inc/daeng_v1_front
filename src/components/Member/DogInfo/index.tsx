50926d9421bd5d252c290d0db021fd4751fe0dcf
jieun419 <crg1050@naver.com>
2024-05-11 02:55:17 +0900

    Chore: 강아지 상제 정보 작업을 위한 DogInfo 컴포넌트 생성 및 추가

---
+++ src/components/Member/DogInfo/index.tsx
@@ -0,0 +1,5 @@
+const DogInfo = () => {
+  return <div>강아지 정보</div>;
+};
+
+export default DogInfo;
--- src/pages/MemberDogInfoPage.tsx
+++ src/pages/MemberDogInfoPage.tsx
@@ -1,6 +1,7 @@
 import { MEMBER_DOG_INFO_STEP } from "constants/step";
 
 import Header from "components/common/Header";
+import DogInfo from "components/Member/DogInfo";
 import SchoolInfo from "components/Member/DogInfo/SchoolInfo";
 import { FootIconItem, Nav, NavItem, NavWrapper } from "components/Member/DogInfo/styles";
 import { useState } from "react";
@@ -32,7 +33,7 @@ const MemberDogInfoPage = () => {
         </NavWrapper>
       </Nav>
       <PageContainer color="gray_5">
-        {currentStep === 0 && ""}
+        {currentStep === 0 && <DogInfo />}
         {currentStep === 1 && <SchoolInfo />}
         {currentStep === 2 && ""}
       </PageContainer>
