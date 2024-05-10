2ea3c4ce38793d053250b978c0d4fe0efffcd3be
jieun419 <crg1050@naver.com>
2024-05-11 05:46:45 +0900

    Design: 이용권 정보 기존 컴포넌트 활용해 작업

--- src/components/Member/DogInfo/AttendanceTicketInfo/index.tsx
+++ src/components/Member/DogInfo/AttendanceTicketInfo/index.tsx
@@ -1,5 +1,60 @@
+import AttendanceIcon from "assets/svg/attendance-icon";
+import CalendarIcon from "assets/svg/calendar";
+import CalendarExpireIcon from "assets/svg/calendar-expire";
+import { DetailItem, TextWrapper } from "components/Admin/DogDetailInfo/DogInfo/AboutDog/styles";
+import { FlexWrapper } from "components/Admin/DogDetailInfo/styles";
+import { Flex } from "components/common";
+
+import * as S from "./styles";
+import { Icon } from "../styles";
+
 const AttendanceTicketInfo = () => {
-  return <div>출결 및 이용권</div>;
+  return (
+    <Flex direction="column" gap="41">
+      <section>
+        <S.DogDetailInfoText className="big">출결</S.DogDetailInfoText>
+        <div>달력</div>
+      </section>
+
+      <section>
+        <S.DogDetailInfoText className="big">이용권 정보</S.DogDetailInfoText>
+        <FlexWrapper>
+          <S.Wrapper>
+            <S.UpperContainer>
+              <S.DogDetailInfoPointText>정기권</S.DogDetailInfoPointText>
+              <S.DogDetailInfoText className="big">12주</S.DogDetailInfoText>
+            </S.UpperContainer>
+            <S.BottomContainer>
+              <DetailItem className="row">
+                <TextWrapper>
+                  <Icon>
+                    <CalendarExpireIcon />
+                  </Icon>
+                  만료일 : 2024.02.01
+                </TextWrapper>
+              </DetailItem>
+              <DetailItem className="row">
+                <TextWrapper>
+                  <Icon>
+                    <AttendanceIcon />
+                  </Icon>
+                  시작일 : 2024.02.01
+                </TextWrapper>
+              </DetailItem>
+              <DetailItem>
+                <TextWrapper>
+                  <Icon>
+                    <CalendarIcon />
+                  </Icon>
+                  유치원 등원 요일 : 월.목.토
+                </TextWrapper>
+              </DetailItem>
+            </S.BottomContainer>
+          </S.Wrapper>
+        </FlexWrapper>
+      </section>
+    </Flex>
+  );
 };
 
 export default AttendanceTicketInfo;
---
+++ src/components/Member/DogInfo/AttendanceTicketInfo/styles.ts
@@ -0,0 +1,98 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  box-shadow: ${({ theme }) => theme.shadows.card};
+  border-radius: 12px;
+`;
+
+export const UpperContainer = styled.div`
+  padding: 12px 20px;
+  background-color: ${({ theme }) => theme.colors.yellow_3};
+  border: 1px solid ${({ theme }) => theme.colors.gray_5};
+  border-radius: 12px 12px 0 0;
+`;
+
+export const BottomContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  background-color: ${({ theme }) => theme.colors.white};
+  padding: 12px;
+
+  border: 1px solid ${({ theme }) => theme.colors.gray_5};
+  border-top: none;
+  border-radius: 0 0 12px 12px;
+
+  color: ${({ theme }) => theme.colors.darkBlack};
+  ${({ theme }) => theme.typo.label2_14_R};
+`;
+
+export const DogDetailInfoPointText = styled.span`
+  ${({ theme }) => theme.typo.caption1_12_B};
+  color: ${({ theme }) => theme.colors.primaryColor};
+`;
+
+export const DogDetailInfoText = styled.div`
+  color: ${({ theme }) => theme.colors.gray_1};
+  ${({ theme }) => theme.typo.label1_16_B};
+
+  &.big {
+    ${({ theme }) => theme.typo.body1_18_B};
+  }
+
+  &.explanation {
+    ${({ theme }) => theme.typo.label2_14_R};
+    color: ${({ theme }) => theme.colors.gray_2};
+  }
+`;
+
+export const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-radius: 8px;
+  border: 1px solid ${({ theme }) => theme.colors.gray_5};
+  background-color: ${({ theme }) => theme.colors.white};
+  box-shadow: ${({ theme }) => theme.shadows.card};
+  padding: 0 12px;
+  > div:last-child {
+    border-bottom: none;
+  }
+`;
+
+export const List = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 12px 0;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray_5};
+`;
+
+export const InnerFlexWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const FlexText = styled.p`
+  &.title {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+    ${({ theme }) => theme.typo.body2_16_R};
+    color: ${({ theme }) => theme.colors.gray_1};
+  }
+
+  &.re-agree {
+    display: flex;
+    align-items: center;
+    ${({ theme }) => theme.typo.caption1_12_R};
+    color: ${({ theme }) => theme.colors.primary_3};
+  }
+
+  &.date {
+    ${({ theme }) => theme.typo.caption1_12_R};
+    color: ${({ theme }) => theme.colors.gray_3};
+  }
+`;
