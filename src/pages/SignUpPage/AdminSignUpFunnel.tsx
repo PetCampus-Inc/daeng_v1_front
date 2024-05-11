7cd48173b6fc1d7dd0c551711ccf20c6afb7908a
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-12 00:47:59 +0900

    feat: 권한별 step 관리하도록 구현

--- src/pages/SignUpPage/AdminSignUpFunnel.tsx
+++ src/pages/SignUpPage/AdminSignUpFunnel.tsx
@@ -1,31 +1,57 @@
 import { ADMIN_SIGN_UP_STEP } from "constants/step";
 
 import { useFunnel } from "hooks/common/useFunnel";
-import { memo } from "react";
+import { memo, useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 
 import AccountSettingPage from "./AccountSettingPage";
 import AdminInfoPage from "./AdminInfoPage";
+import ApprovalStatusPage from "./ApprovalStatusPage";
 import EnrollSchoolPage from "./EnrollSchoolPage";
 import RoleSelectPage from "./RoleSelectPage";
+import SchoolRegistrationCompletePage from "./SchoolRegistrationCompletePage";
 import SearchSchoolPage from "./SearchSchoolPage";
 
+export enum Role {
+  TEACHER = "TEACHER",
+  OWNER = "OWNER"
+}
+
 const AdminSignUpFunnel = () => {
-  const { 역할_선택, 유치원_검색, 유치원_등록, 회원정보_입력, 계정설정 } = ADMIN_SIGN_UP_STEP;
+  const {
+    역할_선택,
+    유치원_검색,
+    유치원_등록,
+    회원정보_입력,
+    계정설정,
+    승인상태,
+    유치원_등록완료
+  } = ADMIN_SIGN_UP_STEP;
   const [Funnel, setStep] = useFunnel(
-    [역할_선택, 유치원_검색, 유치원_등록, 회원정보_입력, 계정설정] as const,
+    [
+      역할_선택,
+      유치원_검색,
+      유치원_등록,
+      회원정보_입력,
+      계정설정,
+      승인상태,
+      유치원_등록완료
+    ] as const,
     {
       initialStep: 역할_선택,
       stepQueryKey: "step"
     }
   );
 
+  const [role, setRole] = useState<Role | null>(null);
+
   const methods = useForm({
     mode: "onChange",
     reValidateMode: "onChange"
   });
 
-  const handleNextStep = (role: "TEACHER" | "OWNER") => {
+  const handleRoleSelection = (role: Role) => {
+    setRole(role);
     if (role === "TEACHER") {
       setStep(유치원_검색);
     } else {
@@ -33,26 +59,43 @@ const AdminSignUpFunnel = () => {
     }
   };
 
-  // 역할 선택 페이지 role: TEACHER -> 유치원 검색 페이지 -> 회원정보 입력 페이지 -> 계정 설정 페이지
-  // 역할 선택 페이지 role: OWNER -> 회원정보 입력 페이지 -> 계정 설정 페이지 -> 유치원 등록 페이지
+  const handleNextStep = () => {
+    if (role === "TEACHER") {
+      setStep(승인상태);
+    } else {
+      setStep(유치원_등록);
+    }
+  };
+
+  // 역할 선택 페이지 role: TEACHER -> 유치원 검색 페이지 -> 회원정보 입력 페이지 -> 계정 설정 페이지 -> 승인 상태 페이지(최종)
+  // 역할 선택 페이지 role: OWNER -> 회원정보 입력 페이지 -> 계정 설정 페이지 -> 유치원 등록 페이지 -> 유치원 등록 완료 페아지(최종)
 
   return (
     <FormProvider {...methods}>
       <Funnel>
         <Funnel.Step name={역할_선택}>
-          <RoleSelectPage onNextStep={handleNextStep} />
+          <RoleSelectPage onNextStep={handleRoleSelection} />
         </Funnel.Step>
+        {/* role: TEACHER인 경우 */}
         <Funnel.Step name={유치원_검색}>
-          <SearchSchoolPage type="TEACHER" onNextStep={() => setStep(회원정보_입력)} />
+          <SearchSchoolPage type={Role.TEACHER} onNextStep={() => setStep(회원정보_입력)} />
         </Funnel.Step>
         <Funnel.Step name={회원정보_입력}>
           <AdminInfoPage onNextStep={() => setStep(계정설정)} />
         </Funnel.Step>
         <Funnel.Step name={계정설정}>
-          <AccountSettingPage onNextStep={() => setStep(계정설정)} />
+          <AccountSettingPage type={role as Role} onNextStep={handleNextStep} />
         </Funnel.Step>
+        {/* role: TEACHER인 경우 */}
+        <Funnel.Step name={승인상태}>
+          <ApprovalStatusPage />
+        </Funnel.Step>
+        {/* role: OWNER인 경우 */}
         <Funnel.Step name={유치원_등록}>
-          <EnrollSchoolPage />
+          <EnrollSchoolPage onNextStep={() => setStep(유치원_등록완료)} />
+        </Funnel.Step>
+        <Funnel.Step name={유치원_등록완료}>
+          <SchoolRegistrationCompletePage />
         </Funnel.Step>
       </Funnel>
     </FormProvider>
