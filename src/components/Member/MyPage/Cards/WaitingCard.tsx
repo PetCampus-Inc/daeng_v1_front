99d312b99e4dd55238a08edfdf5e9fedbbebfbf3
jieun419 <crg1050@naver.com>
2024-05-12 03:15:42 +0900

    Feat: APPROVAL_PENDING 인 강아지 정보 가져오기

--- src/components/Member/MyPage/Cards/WaitingCard.tsx
+++ src/components/Member/MyPage/Cards/WaitingCard.tsx
@@ -1,5 +1,7 @@
 import ArrowRightIcon from "assets/svg/arrow-right-icon";
 import DogWaitingIcon from "assets/svg/dog-waiting-icon";
+import { useGetMemberInfo } from "hooks/api/member/member";
+import { useParams } from "react-router-dom";
 
 import * as S from "./styles";
 
@@ -9,13 +11,24 @@ interface IWaitingCardProps {
 }
 
 const WaitingCard = ({ dogName, registeredDate }: IWaitingCardProps) => {
+  const { memberId } = useParams();
+  const { data: memberInfo } = useGetMemberInfo(String(memberId));
+  const approvalPendingDog = memberInfo.doglist.filter(
+    (dog) => dog.status && dog.status === "APPROVAL_PENDING"
+  );
+  const approvalPendingSchoolName = approvalPendingDog?.map((dog) => dog.schoolName)[0];
+
+  const handleCancelApproval = () => {
+    console.log("승인취소");
+    console.log("approvalPendingSchoolName", approvalPendingSchoolName);
+  };
   return (
     <S.WaitingCard>
       <S.InfoTextBox mb="32">
         <S.DogName>{dogName}</S.DogName>
         <S.CurrentStatusBox>승인 대기중</S.CurrentStatusBox>
       </S.InfoTextBox>
-      <S.CancelApprovalButton>
+      <S.CancelApprovalButton onClick={handleCancelApproval}>
         <S.DateText>{registeredDate && registeredDate} 제출 | 승인 취소</S.DateText>
         <ArrowRightIcon />
       </S.CancelApprovalButton>
