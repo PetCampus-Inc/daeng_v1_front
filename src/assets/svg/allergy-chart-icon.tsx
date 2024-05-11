a01a30131b18bf889d74e691928609d5f44dc09b
jieun419 <crg1050@naver.com>
2024-05-11 18:09:18 +0900

    Chore: 강아지 상세 정보에 필요한 아이콘 추가

---
+++ src/assets/svg/allergy-chart-icon.tsx
@@ -0,0 +1,25 @@
+const AllergyChartIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="18" viewBox="0 0 14 18" fill="none">
+      <path
+        d="M12 0.5C12.8284 0.5 13.5 1.17157 13.5 2L13.5 16C13.5 16.8284 12.8284 17.5 12 17.5L2 17.5C1.17157 17.5 0.5 16.8284 0.5 16L0.500001 2C0.500001 1.17157 1.17157 0.499999 2 0.499999L12 0.5Z"
+        fill="white"
+        stroke="#E4CAB1"
+      />
+      <rect x="6.1084" y="2" width="2.10811" height="6.32432" rx="0.527027" fill="#956F4C" />
+      <rect
+        x="10.3242"
+        y="4.1084"
+        width="2.10811"
+        height="6.32432"
+        rx="0.527027"
+        transform="rotate(90 10.3242 4.1084)"
+        fill="#956F4C"
+      />
+      <rect x="2" y="10" width="8" height="1" fill="#E9E9E9" />
+      <rect x="2" y="12" width="6" height="1" fill="#E9E9E9" />
+    </svg>
+  );
+};
+
+export default AllergyChartIcon;
