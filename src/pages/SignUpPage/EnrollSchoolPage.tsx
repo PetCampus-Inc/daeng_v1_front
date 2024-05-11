e5a95eb34564a94736876ca02aa306730a91bee9
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-12 00:49:38 +0900

    feat: 유치원 등록 핸들러 구현

--- src/pages/SignUpPage/EnrollSchoolPage.tsx
+++ src/pages/SignUpPage/EnrollSchoolPage.tsx
@@ -2,15 +2,37 @@ import { Box, Layout, Text } from "components/common";
 import Header from "components/common/Header";
 import NextButton from "components/SignUp/form/NextButton";
 import SchoolInfo from "components/SignUp/form/SchoolInfo";
-import { useFormContext } from "react-hook-form";
+import { useOwnerSinUp } from "hooks/api/signup";
+import { type FieldValues, useFormContext } from "react-hook-form";
 
 interface IStepProps {
   onNextStep: () => void;
 }
 
 const EnrollSchoolPage = ({ onNextStep }: IStepProps) => {
-  const { getValues } = useFormContext();
+  const { getValues, handleSubmit } = useFormContext();
   const name = getValues("name");
+
+  const { mutateOwnerSignUp } = useOwnerSinUp();
+
+  const onSubmit = (data: FieldValues) => {
+    const req = {
+      id: data.id,
+      pwd: data.pwd,
+      name: data.name,
+      phoneNumber: data.phoneNumber,
+      schoolName: data.schoolName,
+      schoolPhoneNumber: data.schoolPhoneNumber,
+      schoolAddress: data.schoolAddress,
+      registrationNumber: data.registrationNumber
+    };
+    mutateOwnerSignUp(req, {
+      onSuccess: () => {
+        onNextStep();
+      }
+    });
+  };
+
   return (
     <>
       <Header type="back" />
@@ -23,7 +45,9 @@ const EnrollSchoolPage = ({ onNextStep }: IStepProps) => {
         <Box mt={56}>
           <SchoolInfo />
         </Box>
-        <NextButton onNextStep={onNextStep}>등록</NextButton>
+        <NextButton type="submit" onNextStep={handleSubmit(onSubmit)}>
+          등록
+        </NextButton>
       </Layout>
     </>
   );
