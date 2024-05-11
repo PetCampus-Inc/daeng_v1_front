6030168243649938c634fd6552fbcd732980b638
jieun419 <crg1050@naver.com>
2024-05-12 02:52:26 +0900

    Feat: 승인 대기중 가입신청서 삭제 query 추가

--- src/hooks/api/member/member.ts
+++ src/hooks/api/member/member.ts
@@ -1,7 +1,11 @@
 import { QUERY_KEY } from "constants/queryKey";
 
-import { useSuspenseQuery } from "@tanstack/react-query";
-import { handleGetMemberInfo, handleGetMemberProfileInfo } from "apis/member/member.api";
+import { useMutation, useSuspenseQuery } from "@tanstack/react-query";
+import {
+  handleGetMemberInfo,
+  handleGetMemberProfileInfo,
+  handlePostMemberDogEnrollment
+} from "apis/member/member.api";
 
 export const useGetMemberInfo = (memberId: string) => {
   return useSuspenseQuery({
@@ -15,4 +19,14 @@ export const useGetMemberProfileInfo = (memberId: string) => {
     queryKey: QUERY_KEY.MEMBER_PROFILE_INFO(memberId),
     queryFn: () => handleGetMemberProfileInfo(memberId)
   });
+};
+
+// 견주 가입신청서 취소
+export const usePostMemberDogEnrollment = () => {
+  const enrollMemberDOgMutation = useMutation({
+    mutationFn: (enrollmentFormId: string) => handlePostMemberDogEnrollment(enrollmentFormId),
+    throwOnError: true
+  });
+
+  return enrollMemberDOgMutation.mutate;
 };
