834137c9ee96675789c15fc61e66396b365301bd
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-11 23:02:54 +0900

    feat: 일반전화 번호 하이픈 생성 formatter 추가

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -24,6 +24,37 @@ export const formatPhoneNumber = (value: string): string => {
     .replace(/(-{1,2})$/g, "");
 };
 
+export const formatSchoolNumber = (value: string) => {
+  value = value.replace(/\D/g, "");
+  if (value.length > 11) {
+    value = value.substring(0, 11);
+  }
+
+  if (value.length > 7) {
+    // 8-10자리 숫자 (000-0000-0000 형식)
+    value = `${value.substring(0, 3)}-${value.substring(3, 7)}-${value.substring(7)}`;
+  } else if (value.length > 6) {
+    // 7자리 숫자 (000-000-0000 형식)
+    value = `${value.substring(0, 3)}-${value.substring(3, 6)}-${value.substring(6)}`;
+  } else if (value.length > 3) {
+    // 4-6자리 숫자 (00-000-0000 형식)
+    value = `${value.substring(0, 2)}-${value.substring(2, 5)}-${value.substring(5)}`;
+  } else {
+    // 3자리 이하 숫자
+    value;
+  }
+
+  return value.replace(/-$/, "");
+};
+
+export const formatBusinessNumber = (value: string) => {
+  value = value.replace(/\D/g, "");
+  if (value.length > 10) {
+    value = value.substring(0, 10);
+  }
+  return value.replace(/^(\d{0,3})(\d{0,2})(\d{0,5})$/, "$1-$2-$3").replace(/(-)+$/, "");
+};
+
 type TItem = Record<string, string>[];
 export const extractTicketValues = (items: TItem = []) => {
   return items.map((item) => parseInt(item.value, 10));
