ada003211c35c840f94381f03ebac54bd594d897
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-12 00:48:13 +0900

    feat: NextButton type 추가

--- src/components/SignUp/form/NextButton.tsx
+++ src/components/SignUp/form/NextButton.tsx
@@ -1,21 +1,24 @@
+import type { PropsWithChildren } from "react";
+
 import { Box, Text } from "components/common";
 import { StyledButton } from "components/SignIn/styles";
 import { useFormContext } from "react-hook-form";
 
 type ButtonProps = {
+  type?: "button" | "submit";
   onNextStep: () => void;
 };
 
-const NextButton = ({ onNextStep }: ButtonProps) => {
+const NextButton = ({ onNextStep, type, children }: PropsWithChildren<ButtonProps>) => {
   const {
     formState: { isValid }
   } = useFormContext();
 
   return (
     <Box position="absolute" left={16} right={16} bottom={24}>
-      <StyledButton type="submit" bg="primaryColor" onClick={onNextStep} disabled={!isValid}>
+      <StyledButton type={type} bg="primaryColor" onClick={onNextStep} disabled={!isValid}>
         <Text className={isValid ? "" : "inactive"} typo="label1_16_B" color="white">
-          다음
+          {children}
         </Text>
       </StyledButton>
     </Box>
@@ -23,3 +26,7 @@ const NextButton = ({ onNextStep }: ButtonProps) => {
 };
 
 export default NextButton;
+
+NextButton.defaultProps = {
+  type: "button"
+};
