d1dab9d35d9cd794e6b73596196274ec106ce3d7
jieun419 <crg1050@naver.com>
2024-05-11 17:54:42 +0900

    Design: 스타일 적용되도록 수정

--- src/components/common/BottomSheet/InfoBottomSheet/styles.ts
+++ src/components/common/BottomSheet/InfoBottomSheet/styles.ts
@@ -2,13 +2,16 @@ import TextArea from "components/common/TextArea";
 import styled from "styled-components";
 
 export const TextAreaBox = styled(TextArea)`
-  background-color: ${({ theme }) => theme.colors.gray_5};
-  border: 1px solid ${({ theme }) => theme.colors.gray_5};
-  padding: 10px 12px;
-  height: 140px;
+  &.grayArea {
+    background-color: ${({ theme }) => theme.colors.gray_5};
+    border: 1px solid ${({ theme }) => theme.colors.gray_5};
+    padding: 10px 12px;
+    height: 140px;
 
-  ${({ theme }) => theme.typo.label2_14_R};
-  &:not(:focus):not(:placeholder-shown) {
+    ${({ theme }) => theme.typo.label2_14_R};
+  }
+
+  &.grayArea:not(:focus):not(:placeholder-shown) {
     border: 1px solid ${({ theme }) => theme.colors.gray_5};
   }
 `;
