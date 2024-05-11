53d05dec7e93fcca876e96ea67b469fe79208a8b
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-11 20:56:20 +0900

    feat: admin 회원정보 입력 폼 구현

--- src/pages/SignUpPage/AdminInfoPage.tsx
+++ src/pages/SignUpPage/AdminInfoPage.tsx
@@ -1,11 +1,27 @@
-import React from "react";
-
+import { Box, Layout, Text } from "components/common";
+import Header from "components/common/Header";
+import NextButton from "components/SignUp/form/NextButton";
+import PersonalInfo from "components/SignUp/form/PersonalInfo";
+import { memo } from "react";
 interface IStepProps {
   onNextStep: () => void;
 }
 
 const AdminInfoPage = ({ onNextStep }: IStepProps) => {
-  return <div>AdminInfoPage</div>;
+  return (
+    <>
+      <Header type="back" />
+      <Layout type="page" pt={76} pb={24}>
+        <Text typo="title1_24_B" color="darkBlack">
+          정보를 입력해주세요
+        </Text>
+        <Box mt={56}>
+          <PersonalInfo />
+        </Box>
+        <NextButton onNextStep={onNextStep} />
+      </Layout>
+    </>
+  );
 };
 
-export default AdminInfoPage;
+export default memo(AdminInfoPage);
--- src/pages/SignUpPage/AdminSignUpFunnel.tsx
+++ src/pages/SignUpPage/AdminSignUpFunnel.tsx
@@ -1,6 +1,8 @@
 import { ADMIN_SIGN_UP_STEP } from "constants/step";
 
 import { useFunnel } from "hooks/common/useFunnel";
+import { memo } from "react";
+import { FormProvider, useForm } from "react-hook-form";
 
 import AccountSettingPage from "./AccountSettingPage";
 import AdminInfoPage from "./AdminInfoPage";
@@ -10,11 +12,17 @@ import SearchSchoolPage from "./SearchSchoolPage";
 
 const AdminSignUpFunnel = () => {
   const { 역할_선택, 유치원_검색, 유치원_등록, 회원정보_입력, 계정설정 } = ADMIN_SIGN_UP_STEP;
-  const funnelSteps = [역할_선택, 유치원_검색, 유치원_등록, 회원정보_입력, 계정설정] as const;
+  const [Funnel, setStep] = useFunnel(
+    [역할_선택, 유치원_검색, 유치원_등록, 회원정보_입력, 계정설정] as const,
+    {
+      initialStep: 역할_선택,
+      stepQueryKey: "step"
+    }
+  );
 
-  const [Funnel, setStep] = useFunnel(funnelSteps, {
-    initialStep: 역할_선택,
-    stepQueryKey: "step"
+  const methods = useForm({
+    mode: "onChange",
+    reValidateMode: "onChange"
   });
 
   const handleNextStep = (role: "TEACHER" | "OWNER") => {
@@ -29,24 +37,26 @@ const AdminSignUpFunnel = () => {
   // 역할 선택 페이지 role: OWNER -> 회원정보 입력 페이지 -> 계정 설정 페이지 -> 유치원 등록 페이지
 
   return (
-    <Funnel>
-      <Funnel.Step name={역할_선택}>
-        <RoleSelectPage onNextStep={handleNextStep} />
-      </Funnel.Step>
-      <Funnel.Step name={유치원_검색}>
-        <SearchSchoolPage type="TEACHER" onNextStep={() => setStep(회원정보_입력)} />
-      </Funnel.Step>
-      <Funnel.Step name={회원정보_입력}>
-        <AdminInfoPage onNextStep={() => setStep(계정설정)} />
-      </Funnel.Step>
-      <Funnel.Step name={계정설정}>
-        <AccountSettingPage onNextStep={() => setStep(유치원_등록)} />
-      </Funnel.Step>
-      <Funnel.Step name={유치원_등록}>
-        <EnrollSchoolPage />
-      </Funnel.Step>
-    </Funnel>
+    <FormProvider {...methods}>
+      <Funnel>
+        <Funnel.Step name={역할_선택}>
+          <RoleSelectPage onNextStep={handleNextStep} />
+        </Funnel.Step>
+        <Funnel.Step name={유치원_검색}>
+          <SearchSchoolPage type="TEACHER" onNextStep={() => setStep(회원정보_입력)} />
+        </Funnel.Step>
+        <Funnel.Step name={회원정보_입력}>
+          <AdminInfoPage onNextStep={() => setStep(계정설정)} />
+        </Funnel.Step>
+        <Funnel.Step name={계정설정}>
+          <AccountSettingPage onNextStep={() => setStep(계정설정)} />
+        </Funnel.Step>
+        <Funnel.Step name={유치원_등록}>
+          <EnrollSchoolPage />
+        </Funnel.Step>
+      </Funnel>
+    </FormProvider>
   );
 };
 
-export default AdminSignUpFunnel;
+export default memo(AdminSignUpFunnel);
