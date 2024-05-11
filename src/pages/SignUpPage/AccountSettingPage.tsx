def23e4ed2965020e074e7679d137b2f61c7fcad
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-11 20:56:46 +0900

    feat: admin 계정 등록  폼 구현

--- src/pages/SignUpPage/AccountSettingPage.tsx
+++ src/pages/SignUpPage/AccountSettingPage.tsx
@@ -1,11 +1,26 @@
-import React from "react";
-
+import { Box, Text, Layout } from "components/common";
+import Header from "components/common/Header";
+import AccountInfo from "components/SignUp/form/AccountInfo";
+import NextButton from "components/SignUp/form/NextButton";
 interface IStepProps {
   onNextStep: () => void;
 }
 
 const AccountSettingPage = ({ onNextStep }: IStepProps) => {
-  return <div>AccountSettingPage</div>;
+  return (
+    <>
+      <Header type="back" />
+      <Layout type="page" pt={76} pb={24}>
+        <Text typo="title1_24_B" color="darkBlack">
+          회원가입을 완료해주세요
+        </Text>
+        <Box mt={56}>
+          <AccountInfo />
+        </Box>
+        <NextButton onNextStep={onNextStep} />
+      </Layout>
+    </>
+  );
 };
 
 export default AccountSettingPage;
