648b62c5075ad20591daa7dd0078dfe9339bd9a1
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-12 01:03:07 +0900

    Revert "chore: sentry 설치"
    This reverts commit ff063ea20674e24e1fe0c8d58e073aa23cba3456.

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import * as Sentry from "@sentry/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import ReactDOM from "react-dom/client";
@@ -21,18 +20,6 @@ const queryClient = new QueryClient({
       throwOnError: true
     }
   }
-});
-
-Sentry.init({
-  dsn: process.env.REACT_APP_SENTRY_DSN,
-  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", process.env.REACT_APP_SENTRY_BASE_URL || ""],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
