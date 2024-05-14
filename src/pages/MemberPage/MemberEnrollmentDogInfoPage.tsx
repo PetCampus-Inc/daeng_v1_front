1a80ba47fbe39bf5c6ec023ac38a0abe4fe7a668
jieun419 <crg1050@naver.com>
2024-05-15 05:49:41 +0900

    Feat: EnrollmentPage 임시 추가

--- src/pages/MemberPage/MemberEnrollmentDogInfoPage.tsx
+++ src/pages/MemberPage/MemberEnrollmentDogInfoPage.tsx
@@ -1,11 +1,21 @@
 import Header from "components/common/Header";
+import EnrollmentDogDetail from "components/Member/DogInfo/Enrollment/EnrollmentDogDetail";
+import EnrollmentPage from "pages/SignUpPage/EnrollmentPage";
+import { useLocation } from "react-router-dom";
 import { PageContainer } from "styles/StyleModule";
 
 const MemberEnrollmentDogInfoPage = () => {
+  const isDoginfo = useLocation()
+    .pathname.split("/")
+    .some((url) => url === "dogInfo");
+
   return (
     <>
       <Header type="text" text="뽀뽀의 가입신청서" />
-      <PageContainer pt="1">가입신청서</PageContainer>
+      <PageContainer pt="1">
+        {/* <EnrollmentDogDetail /> */}
+        <EnrollmentPage isMemberAddDog={true} />
+      </PageContainer>
     </>
   );
 };
