d45876b80bea1e5dad3f9c63718ccd91de4196a7
jieun419 <crg1050@naver.com>
2024-05-14 21:14:44 +0900

    Feat: MemberDogInfoEditePage 컴포넌트 추가 및 path 설정

---
+++ src/pages/MemberPage/MemberDogInfoEditePage.tsx
@@ -0,0 +1,13 @@
+import Header from "components/common/Header";
+import { PageContainer } from "styles/StyleModule";
+
+const MemberDogInfoEditePage = () => {
+  return (
+    <>
+      <Header type="text" text="뽀뽀의 가입정보 수정" />
+      <PageContainer pt="1">데이터</PageContainer>
+    </>
+  );
+};
+
+export default MemberDogInfoEditePage;
