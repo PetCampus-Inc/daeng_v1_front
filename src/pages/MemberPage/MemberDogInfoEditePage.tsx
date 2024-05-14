197d13019a079ded1fbd01ad51ad16980cee5b11
jieun419 <crg1050@naver.com>
2024-05-15 01:00:56 +0900

    Feat: 뒤로가기 버튼 클릭 시 안내 modal 활성화

--- src/pages/MemberPage/MemberDogInfoEditePage.tsx
+++ src/pages/MemberPage/MemberDogInfoEditePage.tsx
@@ -1,14 +1,19 @@
+import PreventLeaveModal from "components/common/ButtonModal/PreventLeaveModal";
 import Header from "components/common/Header";
 import SaveButton from "components/Member/DogInfo/DogDetailInfoEdite/Buttons/SaveButton";
 import DogDetailInfoEdite from "components/Member/DogInfo/DogDetailInfoEdite/DogDetailInfoEdite";
 import { useGetMemberDogDetailnfo } from "hooks/api/member/member";
+import { useOverlay } from "hooks/common/useOverlay/useOverlay";
 import { FormProvider, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import { PageContainer } from "styles/StyleModule";
 
 const MemberDogInfoEditePage = () => {
   const dogId = 1;
+  const overlay = useOverlay();
   const { data } = useGetMemberDogDetailnfo(dogId);
   const { ...rest } = data;
+  const navigate = useNavigate();
 
   const methods = useForm({
     mode: "onBlur",
@@ -16,9 +21,18 @@ const MemberDogInfoEditePage = () => {
     defaultValues: { ...rest }
   });
 
+  const openPreventLeavePopup = () =>
+    overlay.open(({ isOpen, close }) => (
+      <PreventLeaveModal isOpen={isOpen} close={close} action={() => navigate(-1)} />
+    ));
+
   return (
     <>
-      <Header type="text" text={`${data.dogName}의 가입정보 수정`} />
+      <Header
+        type="text"
+        text={`${data.dogName}의 가입정보 수정`}
+        handleClick={openPreventLeavePopup}
+      />
       <PageContainer pt="1">
         <FormProvider {...methods}>
           <DogDetailInfoEdite />
