bb46849d892c400c1201b90beb9b3c170d1c7eb6
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-18 02:15:20 +0900

    refactor: 출석부 Empty text 로직 변경

--- src/components/Admin/Attendance/AttendanceList/AttendanceSearchList.tsx
+++ src/components/Admin/Attendance/AttendanceList/AttendanceSearchList.tsx
@@ -14,33 +14,32 @@ interface AttendanceSearchListProps {
 const AttendanceSearchList = memo(({ data, type }: AttendanceSearchListProps) => {
   const [selectedDogs, dispatch] = useSelectedDogs();
 
-  if (!data) return null;
-
   const addDog = (dog: AttendData) => {
     dispatch({ type: "ADD_DOG", payload: dog });
   };
 
-  if (data.length > 0) {
-    return (
-      <ListWrapper>
-        <CardListWrapper>
-          {data.map((item) => (
-            <AttendanceDogCard
-              key={item.dogId}
-              attendanceId={item.attendanceId}
-              dogName={item.dogName}
-              onClick={() => addDog(item)}
-              isSelected={selectedDogs.some((dog) => dog.dogId === item.dogId)}
-            />
-          ))}
-        </CardListWrapper>
-      </ListWrapper>
-    );
-  }
-
   const emptyMessage =
     type === "search" ? "검색 결과와 일치하는 강아지가 없어요" : "아직 등원한 강아지가 없어요";
-  return <EmptyText>{emptyMessage}</EmptyText>;
+
+  if (!data || data.length === 0) {
+    return <EmptyText>{emptyMessage}</EmptyText>;
+  }
+
+  return (
+    <ListWrapper>
+      <CardListWrapper>
+        {data.map((item) => (
+          <AttendanceDogCard
+            key={item.dogId}
+            attendanceId={item.attendanceId}
+            dogName={item.dogName}
+            onClick={() => addDog(item)}
+            isSelected={selectedDogs.some((dog) => dog.dogId === item.dogId)}
+          />
+        ))}
+      </CardListWrapper>
+    </ListWrapper>
+  );
 });
 
 export default AttendanceSearchList;
