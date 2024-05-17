87c5b295d0c95807a077a29e7f1bc67f2e9de873
jieun419 <crg1050@naver.com>
2024-05-17 18:09:06 +0900

    Chore: MemberEnrollmentDogInfoPage 불필요한 코드 삭제

--- src/pages/MemberPage/MemberEnrollmentDogInfoPage.tsx
+++ src/pages/MemberPage/MemberEnrollmentDogInfoPage.tsx
@@ -1,8 +1,5 @@
 import Header from "components/common/Header";
 import EnrollmentDogDetail from "components/Member/DogInfo/Enrollment/EnrollmentDogDetail";
-import { useGetMemberDogEnrollmemntInfo } from "hooks/api/member/member";
-import EnrollmentPage from "pages/SignUpPage/EnrollmentPage";
-import { useLocation } from "react-router-dom";
 import { PageContainer } from "styles/StyleModule";
 
 const MemberEnrollmentDogInfoPage = () => {
