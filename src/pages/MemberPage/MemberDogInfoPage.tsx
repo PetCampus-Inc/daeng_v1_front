2ead352859eca516275f198f0df91db067571b31
jieun419 <crg1050@naver.com>
2024-05-18 04:18:11 +0900

    Feat: MemberDogInfoPage Header 강아지 이름 연동

--- src/pages/MemberPage/MemberDogInfoPage.tsx
+++ src/pages/MemberPage/MemberDogInfoPage.tsx
@@ -5,17 +5,19 @@ import DogInfo from "components/Member/DogInfo";
 import AttendanceTicketInfo from "components/Member/DogInfo/AttendanceTicketInfo";
 import SchoolInfo from "components/Member/DogInfo/SchoolInfo";
 import { FootIconItem, Nav, NavItem, NavWrapper } from "components/Member/DogInfo/styles";
+import { useGetMemberDogDetailnfo } from "hooks/api/member/member";
 import { useState } from "react";
 import { PageContainer } from "styles/StyleModule";
 
 const MemberDogInfoPage = () => {
   const currentSteps = MEMBER_DOG_INFO_STEP;
   const [currentStep, setCurrentStep] = useState(0);
-  const dogId = 1;
+  const dogId = 1; //TODO url로 전달 받을 수 있도로 수정
+  const { data } = useGetMemberDogDetailnfo(dogId);
 
   return (
     <>
-      <Header type="text" text="뽀뽀의 상세정보" shadow={true} />
+      <Header type="text" text={`${data.dogName}의 상세정보`} shadow={true} />
       <Nav>
         <NavWrapper>
           {currentSteps.map((item, index) => (
