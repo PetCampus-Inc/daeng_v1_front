37ecd33b77f9944b5f26dee7ff7822fa3a38db1c
jieun419 <crg1050@naver.com>
2024-05-18 03:20:49 +0900

    Feat: remainingDaysDogInfo utill 및 코멘트 추가

--- src/utils/remainingDays.ts
+++ src/utils/remainingDays.ts
@@ -1,3 +1,4 @@
+// MEMO expiration 데이터가 없고 monthlyNumber로 계산하는 경우 (주수)
 export const remainingDays = (StartDate: number[], monthlyNumber: number) => {
   // 등록 날짜
   const startDate = new Date(StartDate[0], StartDate[1] - 1, StartDate[2]);
@@ -9,6 +10,22 @@ export const remainingDays = (StartDate: number[], monthlyNumber: number) => {
 
   const totalDays = monthlyNumber * 7;
   const remainingDays = totalDays - elapsedDays;
+
+  return remainingDays;
+};
+
+// MEMO expiration 데이터가 있는 경우
+export const remainingDaysDogInfo = (expirationArr: number[] | null) => {
+  if (!expirationArr) return null;
+
+  const [year, month, day] = expirationArr;
+  // 마감 날짜
+  const expirationDate = new Date(year, month - 1, day);
+  // 현재 날짜
+  const currentDate = new Date();
+
+  const elapsedTime = expirationDate.getTime() - currentDate.getTime();
+  const remainingDays = Math.floor(elapsedTime / (1000 * 60 * 60 * 24)) + 1;
 
   return remainingDays;
 };
