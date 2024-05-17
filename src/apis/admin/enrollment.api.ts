f9e0e6ca83cd15f01629e0fd47e202369f61d245
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-18 00:44:59 +0900

    fix: 견주 가입신청서 조회 쿼리 인터페이스 변경

--- src/apis/admin/enrollment.api.ts
+++ src/apis/admin/enrollment.api.ts
@@ -1,13 +1,14 @@
 import { request } from "libs/CustomAxios/request";
 import { IResponse } from "types/helper.type";
 
-import type { IResponseMemberForm } from "types/admin/enrollment.types";
+import type { MemberFormData } from "types/admin/enrollment.types";
 
 // 대기 목록 가입신청서 보기
+// description: 승인 대기중인 견주의 가입신청서를 보여줍니다.
 export const handleGetMemberEnrollmentForm = async (
   enrollmentFormId: string
-): Promise<IResponseMemberForm> => {
+): Promise<MemberFormData> => {
   const url = `admin/enrollment/${enrollmentFormId}`;
-  const { data } = await request<IResponse<IResponseMemberForm>>({ url });
+  const { data } = await request<IResponse<MemberFormData>>({ url });
   return data;
 };
