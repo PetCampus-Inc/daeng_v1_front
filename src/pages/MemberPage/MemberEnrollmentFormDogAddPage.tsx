04f4bf38621e922ddb8dd39ebc07d0815eea37de
jieun419 <crg1050@naver.com>
2024-05-19 00:33:41 +0900

    Fix: MemberschoolId 추가

--- src/pages/MemberPage/MemberEnrollmentFormDogAddPage.tsx
+++ src/pages/MemberPage/MemberEnrollmentFormDogAddPage.tsx
@@ -2,12 +2,15 @@ import PreventLeaveModal from "components/common/ButtonModal/PreventLeaveModal";
 import Header from "components/common/Header";
 import { useOverlay } from "hooks/common/useOverlay";
 import { useLocation, useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { schoolIdAtom } from "store/form";
 
 import EnrollmentPage from "../SignUpPage/EnrollmentPage";
 
 const MemberEnrollmentFormDogAddPage = () => {
   const navigate = useNavigate();
   const overlay = useOverlay();
+  const MemberschoolId = useRecoilValue(schoolIdAtom); // member에서 유치원 검색시 추출
   const isMypage = useLocation()
     .pathname.split("/")
     .some((url) => url === "mypage");
@@ -20,7 +23,7 @@ const MemberEnrollmentFormDogAddPage = () => {
   return (
     <>
       <Header type="text" text="가입신청서" handleClick={openPreventLeavePopup} />
-      <EnrollmentPage isMemberAddDog={isMypage} />
+      <EnrollmentPage isMemberAddDog={isMypage} schoolId={Number(MemberschoolId)} />
     </>
   );
 };
