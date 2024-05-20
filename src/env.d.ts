f5ecba3fbd841c84eddadcc551a5b7da1a461b67
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-05-20 23:35:37 +0900

    feat: apple 소셜 로그인 구현 (#152)
    * feat: Add environment variables for server, business API, Apple login, AWS, and Sentry
    
    * feat: Add Apple login types for client configuration and response handling
    
    * feat: apple 로그인 html 임베디드
    
    * feat: apple 로그인 핸들러 추가
    
    * feat: 리다이렉트 페이지에서 인가코드 추출하는 로직 추가
    
    * feat: apple 로그인 인가코드, 사용자 정보 서버로 전송
    
    * feat: Add environment variables for server, business API, Apple login, AWS, and Sentry
    
    * feat: Add Apple login types for client configuration and response handling
    
    * feat: apple 로그인 html 임베디드
    
    * feat: apple 로그인 핸들러 추가
    
    * feat: 리다이렉트 페이지에서 인가코드 추출하는 로직 추가
    
    * feat: apple 로그인 인가코드, 사용자 정보 서버로 전송

---
+++ src/env.d.ts
@@ -0,0 +1,20 @@
+/// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    REACT_APP_SERVER_HOST: string;
+    REACT_APP_SERVER_DOMAIN: string;
+    REACT_APP_BUSINESS_API_KEY: string;
+
+    REACT_APP_APPLE_CLIENT_ID: string;
+    REACT_APP_APPLE_REDIRECT_URI: string;
+
+    REACT_APP_AWS_BUCKET: string;
+    REACT_APP_AWS_REGION: string;
+    REACT_APP_AWS_ACCESS_KEY_ID: string;
+    REACT_APP_AWS_SECRET_ACCESS_KEY: string;
+
+    REACT_APP_SENTRY_DSN: string;
+    REACT_APP_SENTRY_BASE_URL: string;
+  }
+}
