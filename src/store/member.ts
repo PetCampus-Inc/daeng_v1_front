8696d01b5dcaaf82dcc29cf0c36f59a06a09d122
jieun419 <crg1050@naver.com>
2024-05-24 05:00:58 +0900

    Feat: 강아지 승인 거절 데이터 저장을 위한 recoil 추가

--- src/store/member.ts
+++ src/store/member.ts
@@ -5,7 +5,15 @@ interface ISchool {
   schoolId: number;
 }
 
+const IS_REJECTED = localStorage.getItem("IS_REJECTED");
+
 export const memberEnrollmentSchoolAtom = atom<ISchool | null>({
   key: "memberEnrollmentSchool",
   default: null
+});
+
+// 강아지 승인 거절 데이터 atom
+export const memberEnrollmentRejectedAtom = atom({
+  key: "isRejectedState",
+  default: IS_REJECTED ? JSON.parse(IS_REJECTED) : false
 });
