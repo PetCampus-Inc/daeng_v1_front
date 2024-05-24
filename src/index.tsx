f869226b14844c83de8c8a067d33aa565d9ea2c4
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-24 15:03:56 +0900

    chore: 불필요한 파일 제거

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
-import AppRouter from "router";
+import AppRouter from "routes/AppRouter";
 import { ThemeProvider } from "styled-components";
 import mediaQueries from "styles/MediaQuery";
 
