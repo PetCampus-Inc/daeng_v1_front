0fc739cf7122d02919c616bd94bf0f4427ef3e87
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-24 14:57:17 +0900

    feat: isCustomError 타입 가드 추가

--- src/utils/typeGuard.ts
+++ src/utils/typeGuard.ts
@@ -7,3 +7,25 @@ export function isAdmin(role: unknown): role is Role {
 export function isNumber(value: unknown): value is number {
   return typeof value === "number";
 }
+
+interface CustomError {
+  status: number;
+  data: {
+    status: number;
+    message: string;
+    code: string;
+  };
+}
+
+export function isCustomError(error: any): error is CustomError {
+  return (
+    typeof error === "object" &&
+    error !== null &&
+    typeof error.status === "number" &&
+    typeof error.data === "object" &&
+    error.data !== null &&
+    typeof error.data.status === "number" &&
+    typeof error.data.message === "string" &&
+    typeof error.data.code === "string"
+  );
+}
