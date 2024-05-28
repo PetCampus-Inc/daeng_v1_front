edc4647a92dcfd47136ea3ccdafbd85935af64f7
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-29 04:20:56 +0900

    refactor: MainAlbum 타입 수정

--- src/hooks/api/member/useGetMainAlbum.ts
+++ src/hooks/api/member/useGetMainAlbum.ts
@@ -1,11 +1,11 @@
 import { QUERY_KEY } from "constants/queryKey";
 
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { handleGetAlbum } from "apis/member/member.api";
 import { IMainAlbum } from "types/member/main.types";
 
 const useGetMainAlbum = (req: IMainAlbum) => {
-  return useQuery({
+  return useSuspenseQuery({
     queryKey: QUERY_KEY.MEMBER_MAIN_ALBUM(req.dogId, req.date),
     queryFn: () => handleGetAlbum(req),
     gcTime: 1000 * 60 * 60,
