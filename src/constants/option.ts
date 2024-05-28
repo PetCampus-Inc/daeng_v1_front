d916cdc0b8c672fc4c9305ce8e24e2dffdb1c151
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-29 02:44:59 +0900

    feat: ViewMode 탭 구현

--- src/constants/option.ts
+++ src/constants/option.ts
@@ -10,3 +10,10 @@ export const LIST = {
 export type TSortOptionList = PropertyValues<typeof LIST>;
 
 export const INIT_COUNTER = 2;
+
+export const GALLERY_VIEW = {
+  PHOTO: "photo",
+  ALBUM: "album"
+} as const;
+
+export type GalleryViewType = (typeof GALLERY_VIEW)[keyof typeof GALLERY_VIEW];
