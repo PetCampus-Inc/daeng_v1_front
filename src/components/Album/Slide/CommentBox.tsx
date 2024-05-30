6c0bcae25948fb82ebe129206cd87032caede1d4
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-31 03:46:43 +0900

    feat: CommentBox accordion 적용

--- src/components/Album/Slide/CommentBox.tsx
+++ src/components/Album/Slide/CommentBox.tsx
@@ -1,22 +1,25 @@
 import FootRoundIcon from "assets/svg/foot-round-icon";
-import { Box, Flex, Text } from "components/common";
+import { Accordion, Flex, Text } from "components/common";
 
 import type { ImageAlbumType } from "types/member/main.types";
 
 const CommentBox = ({ commentList }: { commentList: ImageAlbumType[] }) => {
-  console.log(commentList);
   return (
-    <Box bg="gray_5" paddingBlock={12} paddingInline={12} borderRadius={8}>
-      <Flex gap={5.5} align="center" marginBottom={10}>
-        <FootRoundIcon w="20" h="20" colorScheme="basic" />
-        <Text typo="body2_16_B" color="darkBlack">
-          함께 온 코멘트
+    <Accordion>
+      <Accordion.Title>
+        <Flex gap={5.5} align="center">
+          <FootRoundIcon w="20" h="20" colorScheme="basic" />
+          <Text typo="body2_16_B" color="darkBlack">
+            함께 온 코멘트
+          </Text>
+        </Flex>
+      </Accordion.Title>
+      <Accordion.Content>
+        <Text typo="label1_16_R" color="gray_1">
+          {commentList[0].comment}
         </Text>
-      </Flex>
-      <Text typo="label1_16_R" color="gray_1">
-        {commentList[0].comment}
-      </Text>
-    </Box>
+      </Accordion.Content>
+    </Accordion>
   );
 };
 
