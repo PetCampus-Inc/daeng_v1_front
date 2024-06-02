4f04699d1182a1cf19089613712f64166ff91adc
jieun419 <crg1050@naver.com>
2024-06-03 02:43:09 +0900

    Chore: 오타 수정

--- src/components/Member/Profile/ProfileEdite.tsx
+++ src/components/Member/Profile/ProfileEdite.tsx
@@ -95,7 +95,7 @@ const ProfileEdite = () => {
           {dogProfile.length > 0 ? (
             <>
               <S.UploadImage src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-              {!isMyActive && <ProfileActiveBox />}
+              {!isDogActive && <ProfileActiveBox />}
             </>
           ) : (
             <AddCIcon />
