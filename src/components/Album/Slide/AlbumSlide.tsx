8d0e864c8663ac9275d0e7d96536a5a20ee95247
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-06-04 06:21:13 +0900

    refactor: Update AlbumView and AlbumSlide components to use single image array instead of nested arrays

--- src/components/Album/Slide/AlbumSlide.tsx
+++ src/components/Album/Slide/AlbumSlide.tsx
@@ -11,7 +11,7 @@ import LightBoxPopup from "../LightBoxPopup";
 
 import type { ImageAlbumType } from "types/member/main.types";
 
-const AlbumSlide = ({ imageList }: { imageList: ImageAlbumType[][] }) => {
+const AlbumSlide = ({ images }: { images: ImageAlbumType[] }) => {
   const [isSaveMode, setSaveMode] = useState<boolean>(false);
   const overlay = useOverlay();
 
@@ -31,28 +31,24 @@ const AlbumSlide = ({ imageList }: { imageList: ImageAlbumType[][] }) => {
     ));
 
   return (
-    <>
-      {imageList.map((images) => (
-        <Flex direction="column" gap={16}>
-          <Flex direction="column" gap={4}>
-            <Flex justify="space-between">
-              <Text typo="body2_16_R" color="darkBlack">
-                {getTimeAgo(images[0].createdTime)}
-              </Text>
-              <SaveButton isSaveMode={isSaveMode} handleSaveMode={handleSaveMode} />
-            </Flex>
-            <DragCarousel gap={12}>
-              {images.map((item, index) => (
-                <SlideWrapper onClick={() => openLightBoxPopup({ images, currentSlide: index })}>
-                  <Img src={item.imageUri} alt={`${item.imageId} + 번째 강아지 사진`} />
-                </SlideWrapper>
-              ))}
-            </DragCarousel>
-          </Flex>
-          <CommentBox commentList={images} />
+    <Flex direction="column" gap={16}>
+      <Flex direction="column" gap={4}>
+        <Flex justify="space-between">
+          <Text typo="body2_16_R" color="darkBlack">
+            {getTimeAgo(images[0].createdTime)}
+          </Text>
+          <SaveButton isSaveMode={isSaveMode} handleSaveMode={handleSaveMode} />
         </Flex>
-      ))}
-    </>
+        <DragCarousel gap={12}>
+          {images.map((item, index) => (
+            <SlideWrapper onClick={() => openLightBoxPopup({ images, currentSlide: index })}>
+              <Img src={item.imageUri} alt={`${item.imageId} + 번째 강아지 사진`} />
+            </SlideWrapper>
+          ))}
+        </DragCarousel>
+      </Flex>
+      <CommentBox commentList={images} />
+    </Flex>
   );
 };
 
