35e757f965d52da4c955410b0da9f157e8229dc7
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-06-04 05:41:25 +0900

    feat: Add LightBoxPopup component in AlbumSlide

--- src/components/Album/Slide/AlbumSlide.tsx
+++ src/components/Album/Slide/AlbumSlide.tsx
@@ -1,4 +1,5 @@
 import { DragCarousel, Flex, Text } from "components/common";
+import { useOverlay } from "hooks/common/useOverlay";
 import { useState } from "react";
 import { Img } from "styles/StyleModule";
 import { getTimeAgo } from "utils/date";
@@ -6,36 +7,49 @@ import { getTimeAgo } from "utils/date";
 import CommentBox from "./CommentBox";
 import SaveButton from "./SaveButton";
 import { SlideWrapper } from "./styles";
+import LightBoxPopup from "../LightBoxPopup";
 
 import type { ImageAlbumType } from "types/member/main.types";
 
 const AlbumSlide = ({ imageList }: { imageList: ImageAlbumType[][] }) => {
   const [isSaveMode, setSaveMode] = useState<boolean>(false);
+  const overlay = useOverlay();
 
   const handleSaveMode = () => {
     setSaveMode(!isSaveMode);
   };
 
+  const openLightBoxPopup = ({
+    images,
+    currentSlide
+  }: {
+    images: ImageAlbumType[];
+    currentSlide: number;
+  }) =>
+    overlay.open(({ isOpen, close }) => (
+      <LightBoxPopup isOpen={isOpen} close={close} images={images} currentSlide={currentSlide} />
+    ));
+
   return (
     <>
-      {imageList.map((list) => (
+      {imageList.map((images) => (
         <Flex direction="column" gap={16}>
           <Flex direction="column" gap={4}>
             <Flex justify="space-between">
               <Text typo="body2_16_R" color="darkBlack">
-                {getTimeAgo(list[0].createdTime)}
+                {getTimeAgo(images[0].createdTime)}
               </Text>
               <SaveButton isSaveMode={isSaveMode} handleSaveMode={handleSaveMode} />
             </Flex>
             <DragCarousel gap={12}>
-              {list.map((item) => (
-                <SlideWrapper>
+              {images.map((item, index) => (
+                <SlideWrapper onClick={() => openLightBoxPopup({ images, currentSlide: index })}>
                   <Img src={item.imageUri} alt={`${item.imageId} + 번째 강아지 사진`} />
                 </SlideWrapper>
               ))}
             </DragCarousel>
           </Flex>
-          <CommentBox commentList={list} />
+          <CommentBox commentList={images} />
         </Flex>
       ))}
     </>
