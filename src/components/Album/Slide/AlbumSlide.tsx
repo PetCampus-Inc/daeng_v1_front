3ff818814fe11aa6376092646773db9b8b38af0c
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-06-04 07:12:25 +0900

    feat: Improve AlbumSlide component by adding save mode and image selection functionality

--- src/components/Album/Slide/AlbumSlide.tsx
+++ src/components/Album/Slide/AlbumSlide.tsx
@@ -1,37 +1,49 @@
 import { DragCarousel, Flex, Text } from "components/common";
+import { Checkbox } from "components/common";
 import { useOverlay } from "hooks/common/useOverlay";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { Img } from "styles/StyleModule";
 import { getTimeAgo } from "utils/date";
 
 import CommentBox from "./CommentBox";
 import SaveButton from "./SaveButton";
-import { SlideWrapper } from "./styles";
+import { Dimmer, SlideWrapper } from "./styles";
 import LightBoxPopup from "../LightBoxPopup";
 
 import type { ImageAlbumType } from "types/member/main.types";
 
 const AlbumSlide = ({ images }: { images: ImageAlbumType[] }) => {
   const [isSaveMode, setSaveMode] = useState<boolean>(false);
+  const [selectedImages, setSelectedImages] = useState(new Set());
+
   const overlay = useOverlay();
 
   const handleSaveMode = () => {
     setSaveMode(!isSaveMode);
+    if (!isSaveMode) {
+      setSelectedImages(new Set());
+    }
   };
 
-  const openLightBoxPopup = ({
-    images,
-    currentSlide
-  }: {
-    images: ImageAlbumType[];
-    currentSlide: number;
-  }) =>
+  const handleSelectImage = useCallback((imageId: number) => {
+    setSelectedImages((prev) => {
+      const newSet = new Set(prev);
+      if (newSet.has(imageId)) {
+        newSet.delete(imageId);
+      } else {
+        newSet.add(imageId);
+      }
+      return newSet;
+    });
+  }, []);
+
+  const openLightBoxPopup = (currentSlide: number) =>
     overlay.open(({ isOpen, close }) => (
       <LightBoxPopup isOpen={isOpen} close={close} images={images} currentSlide={currentSlide} />
     ));
 
   return (
-    <Flex direction="column" gap={16}>
+    <Flex direction="column" gap={12}>
       <Flex direction="column" gap={4}>
         <Flex justify="space-between">
           <Text typo="body2_16_R" color="darkBlack">
@@ -39,9 +51,22 @@ const AlbumSlide = ({ images }: { images: ImageAlbumType[] }) => {
           </Text>
           <SaveButton isSaveMode={isSaveMode} handleSaveMode={handleSaveMode} />
         </Flex>
-        <DragCarousel gap={12}>
+        <DragCarousel gap={8}>
           {images.map((item, index) => (
-            <SlideWrapper onClick={() => openLightBoxPopup({ images, currentSlide: index })}>
+            <SlideWrapper
+              onClick={() => !isSaveMode && openLightBoxPopup(index)}
+              active={selectedImages.has(item.imageId)}
+              isSaveMode={isSaveMode}
+            >
+              {isSaveMode && (
+                <>
+                  <Dimmer onClick={(e) => e.stopPropagation()} />
+                  <Checkbox
+                    isChecked={selectedImages.has(item.imageId)}
+                    onChange={() => handleSelectImage(item.imageId)}
+                  />
+                </>
+              )}
               <Img src={item.imageUri} alt={`${item.imageId} + 번째 강아지 사진`} />
             </SlideWrapper>
           ))}
