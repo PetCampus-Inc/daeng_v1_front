64a0444dd0061a7ce52e1c619805e6e3755936e0
jieun419 <crg1050@naver.com>
2024-06-04 06:58:39 +0900

    Refactor: 회원가입, 강아지 추가 제출하기 버튼 로직 분리

---
+++ src/components/Enrollment/Stepper/MemberSubmitButton.tsx
@@ -0,0 +1,87 @@
+import { PATH } from "constants/path";
+import { QUERY_KEY } from "constants/queryKey";
+import { FIELD_TO_STEP } from "constants/step";
+
+import { useQueryClient } from "@tanstack/react-query";
+import { usePostEnrollment } from "hooks/api/member/enroll";
+import { useGetMemberProfileInfo } from "hooks/api/member/member";
+import { Adapter } from "libs/Adapter";
+import { MemberFormToServerAdapter } from "libs/Adapter/FormToServerAdapter";
+import { FieldValues, useFormContext, type FieldErrors } from "react-hook-form";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
+import { currentStepState } from "store/form";
+import { FormButton } from "styles/StyleModule";
+
+import type { EnrollmentInfo, MemberGenderType } from "types/member/enrollment.types";
+
+// FIXME: 회원가입과 강아지 추가 로직 분리 필요
+const MemberSubmitButton = ({ openPopup }: { openPopup: (field: string) => void }) => {
+  const queryClient = useQueryClient();
+  const { memberId } = useParams();
+  const navigate = useNavigate();
+  const { handleSubmit } = useFormContext();
+  const enrollMutation = usePostEnrollment();
+  const { data: memberInfoData } = useGetMemberProfileInfo(String(memberId));
+
+  const setStep = useSetRecoilState(currentStepState);
+
+  // TODO memberInfo데이터 recoil로 분리 등 리팩토링 필요
+  const memberInfo = {
+    dogId: 0, // 신규 강아지의 경우
+    memberId: Number(memberInfoData.memberId),
+    memberName: memberInfoData.memberName,
+    memberGender: memberInfoData.memberGender as MemberGenderType,
+    address: `${memberInfoData.address && memberInfoData.address}`,
+    addressDetail: `${memberInfoData.address && memberInfoData.addressDetail}`,
+    phoneNumber: memberInfoData.phoneNumber,
+    emergencyNumber: memberInfoData.emergencyPhoneNumber
+      ? memberInfoData.emergencyPhoneNumber
+      : null
+  };
+
+  // 공통 requestData
+  const getSubmitFormInfo = (data: FieldValues) => {
+    return Adapter.from(data).to<FieldValues, EnrollmentInfo>((item) =>
+      new MemberFormToServerAdapter(item).adapt()
+    );
+  };
+
+  // member 강아지 추가
+  const onSubmitMemberDogAdd = (data: FieldValues) => {
+    // FIXME: fileUrl 추가 필요
+    const requestData = getSubmitFormInfo(data);
+
+    const memberDogAddInfo = { ...requestData, ...memberInfo };
+    enrollMutation(memberDogAddInfo, {
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: QUERY_KEY.MEMBER_INFO(String(memberId)) });
+        navigate(PATH.MEMBER_MY_PAGE(memberId));
+      }
+    });
+    console.log("memberDogAddInfo", memberDogAddInfo);
+  };
+
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+    const firstErrorField = Object.keys(errors)[0];
+    const step = FIELD_TO_STEP.get(firstErrorField);
+
+    if (step !== undefined) {
+      openPopup(firstErrorField);
+      setStep(step);
+    }
+  };
+
+  return (
+    <FormButton
+      type="submit"
+      onClick={handleSubmit(onSubmitMemberDogAdd, onInvalid)}
+      aria-label="제출하기"
+    >
+      제출하기1
+    </FormButton>
+  );
+};
+
+export default MemberSubmitButton;
--- src/components/Enrollment/Stepper/SubmitButton.tsx
+++ src/components/Enrollment/Stepper/SubmitButton.tsx
@@ -1,47 +1,20 @@
-import { PATH } from "constants/path";
-import { QUERY_KEY } from "constants/queryKey";
 import { FIELD_TO_STEP } from "constants/step";
 
-import { useQueryClient } from "@tanstack/react-query";
 import { usePostEnrollment } from "hooks/api/member/enroll";
-import { useGetMemberProfileInfo } from "hooks/api/member/member";
 import { Adapter } from "libs/Adapter";
 import { MemberFormToServerAdapter } from "libs/Adapter/FormToServerAdapter";
 import { FieldValues, useFormContext, type FieldErrors } from "react-hook-form";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
 import { currentStepState } from "store/form";
 import { FormButton } from "styles/StyleModule";
 
-import type { EnrollmentInfo, MemberGenderType } from "types/member/enrollment.types";
+import type { EnrollmentInfo } from "types/member/enrollment.types";
 
 // FIXME: 회원가입과 강아지 추가 로직 분리 필요
 const SubmitButton = ({ openPopup }: { openPopup: (field: string) => void }) => {
-  const queryClient = useQueryClient();
-  const { memberId } = useParams();
-  const navigate = useNavigate();
-  const isMypage = useLocation()
-    .pathname.split("/")
-    .some((url) => url === "mypage");
   const { handleSubmit } = useFormContext();
   const enrollMutation = usePostEnrollment();
-  const { data: memberInfoData } = useGetMemberProfileInfo(String(memberId));
-
   const setStep = useSetRecoilState(currentStepState);
-
-  // TODO memberInfo데이터 recoil로 분리 등 리팩토링 필요
-  const memberInfo = {
-    dogId: 0, // 신규 강아지의 경우
-    memberId: Number(memberInfoData.memberId),
-    memberName: memberInfoData.memberName,
-    memberGender: memberInfoData.memberGender as MemberGenderType,
-    address: `${memberInfoData.address && memberInfoData.address}`,
-    addressDetail: `${memberInfoData.address && memberInfoData.addressDetail}`,
-    phoneNumber: memberInfoData.phoneNumber,
-    emergencyNumber: memberInfoData.emergencyPhoneNumber
-      ? memberInfoData.emergencyPhoneNumber
-      : null
-  };
 
   // 공통 requestData
   const getSubmitFormInfo = (data: FieldValues) => {
@@ -57,20 +30,6 @@ const SubmitButton = ({ openPopup }: { openPopup: (field: string) => void }) => 
     enrollMutation(requestData);
   };
 
-  // member 강아지 추가
-  const onSubmitMemberDogAdd = (data: FieldValues) => {
-    // FIXME: fileUrl 추가 필요
-    const requestData = getSubmitFormInfo(data);
-
-    const memberDogAddInfo = { ...requestData, ...memberInfo };
-    enrollMutation(memberDogAddInfo, {
-      onSuccess: () => {
-        queryClient.invalidateQueries({ queryKey: QUERY_KEY.MEMBER_INFO(String(memberId)) });
-        navigate(PATH.MEMBER_MY_PAGE(memberId));
-      }
-    });
-  };
-
   const onInvalid = (errors: FieldErrors) => {
     console.log(errors);
     const firstErrorField = Object.keys(errors)[0];
@@ -83,11 +42,7 @@ const SubmitButton = ({ openPopup }: { openPopup: (field: string) => void }) => 
   };
 
   return (
-    <FormButton
-      type="submit"
-      onClick={handleSubmit(isMypage ? onSubmitMemberDogAdd : onSubmit, onInvalid)}
-      aria-label="제출하기"
-    >
+    <FormButton type="submit" onClick={handleSubmit(onSubmit, onInvalid)} aria-label="제출하기">
       제출하기
     </FormButton>
   );
