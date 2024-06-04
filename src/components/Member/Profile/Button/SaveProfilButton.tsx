12699a323eb65bc11662200e7bfb62a89d80011c
jieun419 <crg1050@naver.com>
2024-06-04 21:55:32 +0900

    Feat: 데이터 추가 하기 (임시)

--- src/components/Member/Profile/Button/SaveProfilButton.tsx
+++ src/components/Member/Profile/Button/SaveProfilButton.tsx
@@ -1,8 +1,26 @@
 import BackgroundButton from "components/common/Button/BackgroundButton";
+import { usePostMemberProfile } from "hooks/api/member/member";
+import { Adapter } from "libs/Adapter";
+import { FieldErrors, FieldValues, useFormContext } from "react-hook-form";
+import { IMemberProfile } from "types/member/home.types";
 
 import * as S from "../styles";
 
 const SaveProfilButton = () => {
+  const { register, setValue, watch, handleSubmit } = useFormContext();
+  const mutateMemberProfile = usePostMemberProfile();
+
+  const handleSubmitData = () => {
+    const data = {
+      memberId: 1,
+      dogId: 1,
+      memberProfileUri: watch("myProfile"),
+      dogProfileUri: watch("dogProfile"),
+      dogName: watch("nickName"),
+      relation: watch("relation")
+    };
+  };
+
   return (
     <S.SavaProfileButton>
       <BackgroundButton backgroundColor="transparent" disabled>
