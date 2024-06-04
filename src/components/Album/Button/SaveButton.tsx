d276887aaea76abcb8d467de9f5c0860aca04af5
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-06-04 10:47:11 +0900

    feat: Add SaveButton component for downloading selected images in the Album context

---
+++ src/components/Album/Button/SaveButton.tsx
@@ -0,0 +1,37 @@
+import { ProgressTemplate } from "components/common";
+import BackgroundButton from "components/common/Button/BackgroundButton";
+import { useFileDownload } from "hooks/common/useS3";
+
+import { useSelectedImages } from "../context/SelectedImageProvider";
+
+const SaveButton = () => {
+  const { imageMap, selectedImgIds } = useSelectedImages();
+  const { isLoading, progress, downloaded, downloadFile } = useFileDownload();
+
+  const handleDownload = () => {
+    const urls = Array.from(imageMap.values());
+    downloadFile({ urls });
+  };
+
+  return (
+    <>
+      {isLoading && (
+        <ProgressTemplate
+          progress={progress}
+          currentIdx={downloaded}
+          totalFiles={selectedImgIds.size}
+        />
+      )}
+      <BackgroundButton
+        backgroundColor="white"
+        pb={32}
+        onClick={handleDownload}
+        disabled={selectedImgIds.size === 0}
+      >
+        저장
+      </BackgroundButton>
+    </>
+  );
+};
+
+export default SaveButton;
