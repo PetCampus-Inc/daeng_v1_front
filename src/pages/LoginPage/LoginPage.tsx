fc7f4b52e391027114a96d8d009aff6eb5d1beff
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-06-04 19:54:54 +0900

    refactor: 로그인 페이지 소셜로그인버튼 컴포넌트 분리

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,8 +1,42 @@
-import SignIn from "components/SignIn";
-import React from "react";
+import { PATH } from "constants/path";
+
+import { Box, Flex, Layout, Text } from "components/common";
+import LoginButtonGroup from "components/SignIn/LoginButtonGroup";
+import { StyledButton } from "components/SignIn/styles";
+import { Link, useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
-  return <SignIn />;
+  const navigate = useNavigate();
+
+  return (
+    <Layout type="page" pt={76}>
+      <Text typo="title1_24_B" color="darkBlack">
+        <em color="primaryColor">우리 강아지</em>의 유치원
+        <br /> 생활을 보러 갈까요?
+      </Text>
+      <Flex align="center" justify="center">
+        <Link to={"서비스 체험하기"}>
+          <Text typo="label2_14_B" color="gray_1">
+            서비스 체험하기
+          </Text>
+        </Link>
+      </Flex>
+      <LoginButtonGroup />
+      <Box position="absolute" left={16} right={16} bottom={24}>
+        <Flex direction="column" align="center" gap={8}>
+          <Text typo="label2_14_M" color="gray_2">
+            <Link to={"이용약관"}>이용약관</Link> |{" "}
+            <Link to={"개인정보 처리 방침"}>개인정보 처리 방침</Link>
+          </Text>
+          <StyledButton bg="primaryColor" onClick={() => navigate(PATH.ADMIN_LOGIN)}>
+            <Text typo="label1_16_B" color="white">
+              관리자로 시작하기
+            </Text>
+          </StyledButton>
+        </Flex>
+      </Box>
+    </Layout>
+  );
 };
 
 export default LoginPage;
