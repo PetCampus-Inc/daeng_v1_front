7944920f0f2fd67cd1a1112c50ad971b703d9710
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-06 05:44:02 +0900

    refactor: 가입신청서 필드 이름 상수로 관리

--- src/constants/field.ts
+++ src/constants/field.ts
@@ -1,3 +1,49 @@
+const AGREEMENT_FIELD = {
+  TICKET_INFO_TERM: "ticketInfo_agreement",
+  LIMITS_INFO_TERM: "limitsInfo_agreement",
+  ACCIDENT_INFO_TERM: "accidentInfo_agreement",
+  ABANDONMENT_INFO_TERM: "abandonmentInfo_agreement",
+  PICKDROP_INFO_TERM: "pickDropInfo_agreement"
+} as const;
+
+/**
+ * @description 가입신청서 필드 이름
+ */
+export const FIELD = {
+  MEMBER_NAME: "memberName",
+  MEMBER_GENDER: "memberGender",
+  MEMBER_ADDRESS: "address",
+  MEMBER_ADDRESS_DETAIL: "addressDetail",
+  MEMBER_PHONE: "phoneNumber",
+  EMERGENCY_NUMBER: "emergencyPhoneNumber",
+  DOG_NAME: "dogName",
+  DOG_GENDER: "dogGender",
+  DOG_SIZE: "dogSize",
+  DOG_BREED: "breedId",
+  NEW_BREED: "newBreed",
+  DOG_BIRTHDAY: "birthDate",
+  NEUTRALIZATION: "nationalization",
+  VACCINATION: "vaccination",
+  VACCINATION_URL: "vaccinationUri",
+  ALLERGY_DISEASE: "allergyDisease",
+  PRICE_INFO: "priceInfo",
+  TICKET_TYPE: "ticketType",
+  ROUND_TICKET_NUMBER: "roundTicketNumber",
+  MONTHLY_TICKET_NUMBER: "monthlyTicketNumber",
+  OPEN_DAYS: "openDays",
+  TICKET_INFO: "ticketInfo",
+  LIMITS_INFO: "limitsInfo",
+  ACCIDENT_INFO: "accidentInfo",
+  ABANDONMENT_INFO: "abandonmentInfo",
+  PICDROP_STATE: "pickDropState",
+  PICKDROP_NOTICE: "pickDropNotice",
+  PICKDROP_REQUEST: "pickDropRequest",
+  PICKDROP_TYPE: "pickDropType",
+  PICKDROP_MEMO: "pickDropMemo",
+  PICKDROP_INFO: "pickDropInfo",
+  ...AGREEMENT_FIELD
+} as const;
+
 export const FIELD_KEYS = {
   MEMBER_NAME: 1,
   MEMBER_GENDER: 2,
@@ -12,7 +58,7 @@ export const FIELD_KEYS = {
   DOG_BIRTHDAY: 11,
   NEUTRALIZATION: 12,
   VACCINATION: 13,
-  VACCINATION_FILE: 14,
+  VACCINATION_URL: 14,
   ALLERGY_DISEASE: 15,
   PRICE_INFO: 16,
   TICKET_TYPE: 17,
@@ -23,7 +69,7 @@ export const FIELD_KEYS = {
   LIMITS_INFO: 22,
   ACCIDENT_INFO: 23,
   ABANDONMENT_INFO: 24,
-  PICKDROP_OPERATION: 25,
+  PICDROP_STATE: 25,
   PICKDROP_NOTICE: 26,
   PICKDROP_REQUEST: 27,
   PICKDROP_TYPE: 28,
@@ -32,7 +78,7 @@ export const FIELD_KEYS = {
 } as const;
 
 /**
- * @description
+ * @description 이넘 객체로 필드 아이템을 정의
  */
 export const FIELD_ITEMS = {
   memberGender: { MALE: "남", FEMALE: "여" },
