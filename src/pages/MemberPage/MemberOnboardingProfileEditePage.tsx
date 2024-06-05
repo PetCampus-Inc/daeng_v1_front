3c2a0a0607fc645dbecce6ce0bbcc0f17248cd41
jieun419 <crg1050@naver.com>
2024-06-06 04:26:01 +0900

    Feat: memberProfile 수정 전 데이터 가져오기

--- src/pages/MemberPage/MemberOnboardingProfileEditePage.tsx
+++ src/pages/MemberPage/MemberOnboardingProfileEditePage.tsx
@@ -1,13 +1,19 @@
 import SaveProfilButton from "components/Member/Profile/Button/SaveProfilButton";
 import OnboardingProfile from "components/Member/Profile/OnboardingProfile";
+import { useGetMemberProfile } from "hooks/api/member/member";
 import { FormProvider, useForm } from "react-hook-form";
 import { PageContainer } from "styles/StyleModule";
 
 const MemberOnboardingProfileEditePage = () => {
+  // TODO memeberId 데이터 가져오기
+  const memberId = 11;
+  const { data } = useGetMemberProfile(memberId);
+  const { ...rest } = data;
+
   const methods = useForm({
     mode: "onChange",
     shouldUnregister: false,
-    defaultValues: {}
+    defaultValues: { ...rest }
   });
 
   return (
