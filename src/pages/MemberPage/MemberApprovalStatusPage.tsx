8aa5e21b8b02d97382a1fe2ec7cb10cd9f256ebe
jieun419 <crg1050@naver.com>
2024-06-07 02:16:31 +0900

    Feat: MemberApprovalStatusPage 작업 (임시)

--- src/pages/MemberPage/MemberApprovalStatusPage.tsx
+++ src/pages/MemberPage/MemberApprovalStatusPage.tsx
@@ -1,7 +1,7 @@
 import { Layout } from "components/common";
-import ApprovalSuccess from "components/Enrollment/ApprovalStatus/ApprovalSuccess";
-import ApprovalFailed from "components/SignUp/ApprovalStatus/ApprovalFailed";
-import ApprovalPending from "components/SignUp/ApprovalStatus/ApprovalPending";
+import MemberApprovalSuccess from "components/Enrollment/ApprovalStatus/MemberApprovalSuccess";
+// import ApprovalFailed from "components/SignUp/ApprovalStatus/ApprovalFailed";
+// import ApprovalPending from "components/SignUp/ApprovalStatus/ApprovalPending";
 import { useAdminInfo } from "hooks/common/useAdminInfo";
 import { ITeacherInfo } from "pages/SignUpPage/AdminSignUpFunnel";
 import { useLocation } from "react-router-dom";
@@ -22,11 +22,9 @@ const MemberApprovalStatusPage = ({
   const searchParams = new URLSearchParams(location.search);
   const isLoginSource = searchParams.get("source") === "login";
 
-  const loginInfo = useAdminInfo();
-
-  const schoolName = isLoginSource ? loginInfo.schoolName : info?.schoolName;
-  const adminId = isLoginSource ? loginInfo.adminId : info?.adminId;
-  const status = isLoginSource ? loginInfo.role : info?.role;
+  // const schoolName = isLoginSource ? loginInfo.schoolName : info?.schoolName;
+  // const adminId = isLoginSource ? loginInfo.adminId : info?.adminId;
+  // const status = isLoginSource ? loginInfo.role : info?.role;
 
   // MEMO: 승인 상태에 따라 다른 컴포넌트를 보여줄 수 있도록 구현
   // 최초 페이지 접근 시(가입신청 단계) ApprovalPending 컴포넌트를 보여줌
@@ -35,13 +33,14 @@ const MemberApprovalStatusPage = ({
 
   return (
     <Layout type="page" pt={76} position="relative">
-      {status === Role.APPROVAL_PENDING && (
+      {/* {status === Role.APPROVAL_PENDING && (
         <ApprovalPending adminId={adminId} schoolName={schoolName} onNextStep={onSelectRoleClick} />
-      )}
-      {status === Role.ROLE_TEACHER && <ApprovalSuccess schoolName={schoolName} />}
-      {status === Role.APPROVAL_DENIED && (
+      )} */}
+      <MemberApprovalSuccess schoolName={"강아지월드"} />
+      {/* {status === Role.ROLE_TEACHER && <ApprovalSuccess schoolName={schoolName} />} */}
+      {/* {status === Role.APPROVAL_DENIED && (
         <ApprovalFailed schoolName={schoolName} onNextStep={onSearchSchoolClick} />
-      )}
+      )} */}
     </Layout>
   );
 };
