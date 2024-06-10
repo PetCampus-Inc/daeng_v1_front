74460f0987212b4362cf303656458d21148512c4
hyerani <hran9404@naver.com>
2024-06-11 01:28:22 +0900

    feat: add image box

--- src/components/Admin/MyPage/EditProfile/EditProfile.tsx
+++ src/components/Admin/MyPage/EditProfile/EditProfile.tsx
@@ -1,8 +1,10 @@
+import PencilBrownNormalIcon from "assets/svg/pencil-brown-normal-icon";
 import { Layout, Text, TextInput } from "components/common";
-import Button from "components/common/Button";
 import BackgroundButton from "components/common/Button/BackgroundButton";
 import { IOwnerInfo } from "types/admin/mypage.type";
 import { ITeacherInfo } from "types/admin/mypage.type";
+
+import * as S from "./styles";
 
 interface ProfileInfoProps {
   principalData?: IOwnerInfo;
@@ -12,15 +14,32 @@ interface ProfileInfoProps {
 const EditProfile = ({ principalData, teacherData }: ProfileInfoProps) => {
   return (
     <Layout type="page" pt="5vh">
-      <Text typo="label2_14_R" color="darkBlack">
-        이름
-      </Text>
-      <TextInput value={principalData ? principalData.adminName : teacherData?.adminName} />
-      <Text typo="label2_14_R" color="darkBlack">
-        전화번호
-      </Text>
-      <TextInput value={principalData ? principalData.phoneNumber : teacherData?.phoneNumber} />
-      <BackgroundButton>수정 완료</BackgroundButton>
+      <S.ProfileWrapper>
+        <S.ProfileBox>
+          <S.ProfileEditeBox>
+            <S.UserImage
+              src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="user_profile"
+            />
+            <S.ProfileEditeButton>
+              <PencilBrownNormalIcon />
+            </S.ProfileEditeButton>
+          </S.ProfileEditeBox>
+        </S.ProfileBox>
+        <S.contentWrapper>
+          <Text typo="label2_14_R" color="darkBlack">
+            이름
+          </Text>
+          <TextInput value={principalData ? principalData.adminName : teacherData?.adminName} />
+        </S.contentWrapper>
+        <S.contentWrapper>
+          <Text typo="label2_14_R" color="darkBlack">
+            전화번호
+          </Text>
+          <TextInput value={principalData ? principalData.phoneNumber : teacherData?.phoneNumber} />
+        </S.contentWrapper>
+        <BackgroundButton backgroundColor="white">수정 완료</BackgroundButton>
+      </S.ProfileWrapper>
     </Layout>
   );
 };
---
+++ src/components/Admin/MyPage/EditProfile/styles.ts
@@ -0,0 +1,51 @@
+import { styled } from "styled-components";
+
+export const ProfileWrapper = styled.div`
+  width: 100%;
+  height: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+export const ProfileBox = styled.div`
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  grid-row: 1 / 3;
+`;
+
+export const ProfileEditeBox = styled.div`
+  position: relative;
+  width: 107px;
+  height: 0;
+  padding-bottom: 107px;
+`;
+
+export const UserImage = styled.img`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  inset: 0px;
+  color: transparent;
+  object-fit: cover;
+  border-radius: 40px;
+`;
+
+export const ProfileEditeButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  border-radius: 50px;
+  width: 28px;
+  height: 28px;
+  background-color: ${({ theme }) => theme.colors.br_4};
+  border: 2px solid ${({ theme }) => theme.colors.white};
+`;
+
+export const contentWrapper = styled.div`
+  width: 100%;
+`;
