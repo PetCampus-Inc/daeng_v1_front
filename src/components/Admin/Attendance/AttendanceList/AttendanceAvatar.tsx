c2c2b6d1d739885260a35be6dafb8fb2d60fdce3
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-12 17:42:11 +0900

    fix:  슬라이더에서 모든 페이지가 균일하게 5개의 슬라이드를 보여주도록 플레이스 홀더 생성

--- src/components/Admin/Attendance/AttendanceList/AttendanceAvatar.tsx
+++ src/components/Admin/Attendance/AttendanceList/AttendanceAvatar.tsx
@@ -9,20 +9,54 @@ import { useSelectedDogs } from "../context/SelectedDogProvider";
 import { Spacing } from "../styles";
 
 const AttendanceAvatar = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
   const sliderRef = useRef<Slider>(null);
 
   const [selectedDogs, dispatch] = useSelectedDogs();
 
-  const removeDog = useCallback((dogId: number) => {
+  const handleRemoveDog = useCallback((dogId: number) => {
     dispatch({ type: "REMOVE_DOG", payload: dogId });
   }, []);
 
-  const next = () => {
-    sliderRef.current?.slickNext();
+  const navigate = (direction: "next" | "prev") => {
+    direction === "next" ? sliderRef.current?.slickNext() : sliderRef.current?.slickPrev();
   };
-  const previous = () => {
-    sliderRef.current?.slickPrev();
+
+  if (selectedDogs.length === 0) return <Spacing />;
+
+  const adjustedSlides = () => {
+    const slides = selectedDogs.map((dog) => (
+      <S.Avatar key={dog.dogId}>
+        <S.AvatarWrapper>
+          <S.AvatarImgWrapper>
+            <S.Image
+              src={
+                "https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              }
+              alt={`${dog.dogName} 이미지`}
+            />
+          </S.AvatarImgWrapper>
+          <S.Name>{dog.dogName}</S.Name>
+          <S.RemoveButton onClick={() => handleRemoveDog(dog.dogId)}>
+            <CloseIcon />
+          </S.RemoveButton>
+        </S.AvatarWrapper>
+      </S.Avatar>
+    ));
+
+    // MEMO: 슬라우드 수가 5의 배수가 되도록 필요한 만큼의 빈 슬라이드(placeholder)를 추가는 로직
+    const remainder = slides.length % 5;
+    if (remainder > 0) {
+      const placeholders = Array.from({ length: 5 - remainder }, (_, i) => (
+        <S.Avatar key={`placeholder-${i}`} className="placeholder">
+          <S.AvatarWrapper>
+            <S.AvatarImgWrapper />
+          </S.AvatarWrapper>
+        </S.Avatar>
+      ));
+      return [...slides, ...placeholders];
+    }
+    return slides;
   };
 
   const settings = {
@@ -32,48 +66,33 @@ const AttendanceAvatar = () => {
     speed: 500,
     slidesToShow: Math.min(5, selectedDogs.length),
     slidesToScroll: 5,
-    // variableWidth: true,
-    afterChange: (current: number) => {
-      setCurrentIndex(current);
+    beforeChange: (_: never, next: number) => {
+      setCurrentIndex(next);
     }
   };
 
-  if (selectedDogs.length === 0) return <Spacing />;
+  const totalPageIndex = Math.ceil(selectedDogs.length / 5);
+  const currentPageIndex = Math.ceil(currentIndex / 5) + 1;
 
   return (
     <S.Container>
       <S.TopWrapper>
         <S.Text>출석이 완료된 강아지들이에요</S.Text>
         <S.SliderPagination>
-          <S.ArrowButton type="button" className="prev" onClick={previous}>
+          <S.ArrowButton type="button" className="prev" onClick={() => navigate("prev")}>
             <ArrowLeftSquare />
           </S.ArrowButton>
           <span>
-            {currentIndex + 1}/{Math.ceil(selectedDogs.length / 5)}
+            {currentPageIndex} / {totalPageIndex}
           </span>
-          <S.ArrowButton type="button" className="next" onClick={next}>
+          <S.ArrowButton type="button" className="next" onClick={() => navigate("next")}>
             <ArrowRightSquare />
           </S.ArrowButton>
         </S.SliderPagination>
       </S.TopWrapper>
       <S.SliderWrapper>
         <Slider ref={sliderRef} {...settings}>
-          {selectedDogs.map((dog) => (
-            <S.Avatar key={dog.dogId}>
-              <S.AvatarWrapper>
-                <S.AvatarImgWrapper>
-                  <S.Image
-                    src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    alt={dog.dogName + " 이미지"}
-                  />
-                </S.AvatarImgWrapper>
-                <S.Name>{dog.dogName}</S.Name>
-                <S.RemoveButton type="button" onClick={() => removeDog(dog.dogId)}>
-                  <CloseIcon />
-                </S.RemoveButton>
-              </S.AvatarWrapper>
-            </S.Avatar>
-          ))}
+          {adjustedSlides()}
         </Slider>
       </S.SliderWrapper>
     </S.Container>
