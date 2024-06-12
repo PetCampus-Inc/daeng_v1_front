ce4d61d108528b56a51403d56460184a4897c229
jieun419 <crg1050@naver.com>
2024-06-12 19:21:58 +0900

    Refactor/Chore: 불필요한 코드 삭제

--- src/store/member.ts
+++ src/store/member.ts
@@ -20,8 +20,3 @@ export const dogIdState = atom<number | null>({
   key: "dogId",
   default: null
 });
-
-export const memberProfileAtom = atom<string | null>({
-  key: "memberProfile",
-  default: null
-});
