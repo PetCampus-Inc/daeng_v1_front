5cea6d189ee8ca32835a3278260206ce1ed99b54
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-14 18:48:30 +0900

    feat: admin teacher, owner 권한에 따른 nav 렌더 로직 추가

--- src/components/common/NavBar/index.tsx
+++ src/components/common/NavBar/index.tsx
@@ -1,61 +1,73 @@
 import { MENU_ITEMS } from "constants/navBar";
 import { PATH } from "constants/path";
 
+import { useLocalStorageValue } from "hooks/common/useLocalStorage";
 import usePathParams from "hooks/common/usePathParams";
-import React, { memo } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { Fragment, memo, useCallback, useMemo } from "react";
+import { useLocation } from "react-router-dom";
+import { useAuth } from "routes/AuthProvider";
+import { AUTH_MEMBER_ID } from "store/auth";
+import { Role } from "types/admin/admin.types";
 
 import * as S from "./styles";
 
-interface Props {
-  type?: string;
-  show?: string;
-  attendance?: string;
-}
-
-// * typeof menuItem.path === 'function' -> path가 function인지에 따라 path 매개 변수 전달
-
-// TODO: 로그인 시 원장/선생님 구분 데이터를 어떻게 저장하느냐에 따라 '유치원 운영' 노출 여부 변경하기
-const Navbar = ({ type, show, attendance }: Props) => {
+// FIXME: admin nav 와 member nav 구분이 필요합니다.
+// FIXME: routes가 변경될 때 마다 매번 새로 그려지고 있습니다. 함수호출을 줄일 수 있는 방법을 찾아야합니다.
+const Navbar = () => {
   const path: string = usePathParams();
   const location = useLocation();
-  const memberId = 1; //임시 memberId
+  const memberId = useLocalStorageValue<string>(AUTH_MEMBER_ID);
+  const adminContext = useAuth();
 
-  const setIsPath = (path: string) => {
-    return location.pathname === path;
-  };
+  const isCurrentPath = useCallback(
+    (path: string) => {
+      return location.pathname === path;
+    },
+    [location.pathname]
+  );
+
+  const getFilteredMenuItems = useMemo(() => {
+    if (!path.includes(PATH.ADMIN)) {
+      return;
+    }
+    if (adminContext?.auth?.role === Role.ROLE_TEACHER) {
+      return MENU_ITEMS.admin.filter((item) => item.text !== "유치원 운영");
+    }
+    return MENU_ITEMS.admin;
+  }, [adminContext?.auth?.role]);
 
   return (
-    <S.Container radius={`${path.includes("/admin") ? "0" : "20px 20px 0 0"}`}>
-      {path.includes("/admin")
-        ? MENU_ITEMS.admin.map((menuItem) => (
+    <S.Container radius={`${path.includes(PATH.ADMIN) ? "0" : "20px 20px 0 0"}`}>
+      {path.includes(PATH.ADMIN)
+        ? getFilteredMenuItems?.map((menuItem) => (
             <S.NavButton key={menuItem.text} to={menuItem.path}>
               {path === menuItem.path ? menuItem.colorImage : menuItem.blackImage}
               <S.Text className={path === menuItem.path ? "active" : ""}>{menuItem.text}</S.Text>
             </S.NavButton>
           ))
         : MENU_ITEMS.member.map((menuItem, index) => (
-            <React.Fragment key={menuItem.text}>
-              {typeof menuItem.path === "function" && (
+            <Fragment key={menuItem.text}>
+              {menuItem.text === "마이페이지" ? (
                 <S.NavButton to={menuItem.path(String(memberId))}>
-                  {setIsPath(menuItem.path(String(memberId)))
+                  {isCurrentPath(menuItem.path(String(memberId)))
                     ? menuItem.colorImage
                     : menuItem.blackImage}
-                  <S.Text className={setIsPath(menuItem.path(String(memberId))) ? "active" : ""}>
+                  <S.Text
+                    className={isCurrentPath(menuItem.path(String(memberId))) ? "active" : ""}
+                  >
                     {menuItem.text}
                   </S.Text>
                 </S.NavButton>
-              )}
-              {typeof menuItem.path !== "function" && (
-                <S.NavButton to={String(menuItem.path)}>
-                  {setIsPath(String(menuItem.path)) ? menuItem.colorImage : menuItem.blackImage}
-                  <S.Text className={setIsPath(String(menuItem.path)) ? "active" : ""}>
+              ) : (
+                <S.NavButton to={menuItem.path}>
+                  {isCurrentPath(menuItem.path) ? menuItem.colorImage : menuItem.blackImage}
+                  <S.Text className={isCurrentPath(menuItem.path) ? "active" : ""}>
                     {menuItem.text}
                   </S.Text>
                 </S.NavButton>
               )}
               {index === 0 && <S.Circle />}
-            </React.Fragment>
+            </Fragment>
           ))}
     </S.Container>
   );
