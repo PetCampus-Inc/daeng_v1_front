f0438f17cacc2e046fdcfac7baef4db2b39323f6
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-16 23:35:27 +0900

    refactor: defaultValue, defaultSelect 제거

--- src/components/Member/MyPage/MyMemberInfoEdite/MyInfoEdite.tsx
+++ src/components/Member/MyPage/MyMemberInfoEdite/MyInfoEdite.tsx
@@ -1,5 +1,4 @@
 import { FIELD, FIELD_KEYS } from "constants/field";
-import { GENDER_DATA } from "constants/gender";
 import { PHONE_REGEX } from "constants/validCheck";
 
 import { Text } from "components/common";
@@ -9,20 +8,15 @@ import SearchInputField from "components/common/Input/SearchInputField";
 import Postcode from "components/common/Postcode";
 import SingleRadio from "components/common/Select/SingleRadio";
 import * as useOverlay from "hooks/common/useOverlay/useOverlay";
-import { useState } from "react";
 import { useFormContext } from "react-hook-form";
 import { formatPhoneNumber } from "utils/formatter";
 
 import * as S from "./styles";
 
-import type { IMemberInfoEdite } from "types/member/member.types";
-
 // TODO 코드 리팩토링 필요
-// NOTE: 여기서 handleFocus, handleBlur는 어떤 동작을 하고 있는건가요?
 
-const MyInfoEdite = ({ requiredItems, handleFocus, handleBlur, memberData }: IMemberInfoEdite) => {
+const MyInfoEdite = ({ requiredItems }: { requiredItems: Map<number, boolean> }) => {
   const { register, setValue, watch } = useFormContext();
-  const [isAddressActive, setIsAddressActive] = useState(false);
   const overlay = useOverlay.useOverlay();
 
   const addressStreet = FIELD.MEMBER_ADDRESS;
@@ -41,18 +35,11 @@ const MyInfoEdite = ({ requiredItems, handleFocus, handleBlur, memberData }: IMe
   const handleClear = () => {
     setValue(addressStreet, "");
     setValue(FIELD.MEMBER_ADDRESS_DETAIL, "");
-    setIsAddressActive(false);
   };
 
   const openPopup = () =>
     overlay.open(({ isOpen, close }) => (
-      <Postcode
-        isOpen={isOpen}
-        close={close}
-        field={addressStreet}
-        setValue={setValue}
-        setIsAddressActive={setIsAddressActive}
-      />
+      <Postcode isOpen={isOpen} close={close} field={addressStreet} setValue={setValue} />
     ));
 
   //TODO input value 연동 작업하기
@@ -67,9 +54,6 @@ const MyInfoEdite = ({ requiredItems, handleFocus, handleBlur, memberData }: IMe
           register={register}
           required
           placeholder="견주 이름을 입력해주세요"
-          defaultValue={memberData.memberName}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
           onChange={handleChangeInput}
           className="defaultValue"
         />
@@ -79,11 +63,7 @@ const MyInfoEdite = ({ requiredItems, handleFocus, handleBlur, memberData }: IMe
         <Text typo="label2_14_R" color="darkBlack">
           성별
         </Text>
-        <SingleRadio
-          name={FIELD.MEMBER_GENDER}
-          radiosText={["남", "여"]}
-          defaultSelect={GENDER_DATA[memberData.memberGender][0]}
-        />
+        <SingleRadio name={FIELD.MEMBER_GENDER} radiosText={["남", "여"]} />
       </Flex>
 
       <Flex direction="column" gap={7}>
@@ -96,7 +76,6 @@ const MyInfoEdite = ({ requiredItems, handleFocus, handleBlur, memberData }: IMe
           onSearch={openPopup}
           onClick={openPopup}
           onClear={handleClear}
-          defaultValue={memberData.address}
           value={watchAddress}
           required={requiredItems?.get(FIELD_KEYS.MEMBER_ADDRESS)}
           readOnly
@@ -107,10 +86,7 @@ const MyInfoEdite = ({ requiredItems, handleFocus, handleBlur, memberData }: IMe
         <TextInput
           name={FIELD.MEMBER_ADDRESS_DETAIL}
           register={register}
-          defaultValue={memberData.addressDetail}
           placeholder="상세 주소를 입력해주세요"
-          onFocus={handleFocus}
-          onBlur={handleBlur}
           onChange={handleChangeInput}
           className="defaultValue"
         />
@@ -128,10 +104,7 @@ const MyInfoEdite = ({ requiredItems, handleFocus, handleBlur, memberData }: IMe
           }}
           onChange={handleChangeNumber(FIELD.MEMBER_PHONE)}
           placeholder="연락처를 입력해주세요"
-          defaultValue={memberData.phoneNumber}
           type="tel"
-          onFocus={handleFocus}
-          onBlur={handleBlur}
           className="defaultValue"
           required
         />
@@ -149,10 +122,7 @@ const MyInfoEdite = ({ requiredItems, handleFocus, handleBlur, memberData }: IMe
             pattern: PHONE_REGEX
           }}
           placeholder="비상 연락처를 입력해주세요"
-          defaultValue={memberData.emergencyPhoneNumber}
           type="tel"
-          onFocus={handleFocus}
-          onBlur={handleBlur}
           className="defaultValue"
           required
         />
