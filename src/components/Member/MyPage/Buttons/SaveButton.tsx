2ebd03716705d4f3491b4109838dbe0ad183d9b5
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-16 23:40:45 +0900

    refactor: SaveButton 컴포넌트 리팩터링
    * isDisabled 상태 대신 isValid, isDirty 상태로 교체
    
    * watch 대신 getValues로 모든 필드의 값 가져오도록 교체

--- src/components/Member/MyPage/Buttons/SaveButton.tsx
+++ src/components/Member/MyPage/Buttons/SaveButton.tsx
@@ -1,93 +1,47 @@
 import { FIELD } from "constants/field";
 
 import BackgroundButton from "components/common/Button/BackgroundButton";
-import { useGetMemberProfileInfo, usePostMemberProfileInfo } from "hooks/api/member/member";
-import { useCallback, useEffect, useState } from "react";
-import { useForm, useFormContext } from "react-hook-form";
-import { useParams } from "react-router-dom";
+import { usePostMemberProfileInfo } from "hooks/api/member/member";
+import { useFormContext } from "react-hook-form";
 
-const SaveButton = () => {
-  const [isDisabled, setIsDisabled] = useState(true);
-  const { memberId } = useParams();
-  const methods = useForm({ mode: "onSubmit" });
-  const { watch } = useFormContext();
-  const mutatePostMemberInfo = usePostMemberProfileInfo(String(memberId));
-  const { data: previousValues } = useGetMemberProfileInfo(String(memberId));
+import type { IMemberProfilePostInfo } from "types/member/main.types";
 
-  const memberName = watch(FIELD.MEMBER_NAME);
-  const memberGender = watch(FIELD.MEMBER_GENDER) === "여" ? "FEMALE" : "MALE";
-  const nickName = watch(FIELD.NICK_NAME);
-  const address = watch(FIELD.MEMBER_ADDRESS);
-  const addressDetail = watch(FIELD.MEMBER_ADDRESS_DETAIL);
-  const phoneNumber = watch(FIELD.MEMBER_PHONE);
-  const emergencyPhoneNumber = watch(FIELD.EMERGENCY_NUMBER);
-  const relation = watch(FIELD.RELATION);
+const SaveButton = ({ memberId }: { memberId: string }) => {
+  const {
+    getValues,
+    handleSubmit,
+    formState: { isDirty, isValid, isSubmitting }
+  } = useFormContext();
 
-  const updatedMemberInfo = {
-    memberId: String(memberId),
-    memberName: memberName,
-    memberGender: memberGender,
-    memberProfileUri: "s3_uri_yubin's_cute_face", // TODO 데이터 연동 필요
-    nickName: nickName,
-    address: address,
-    addressDetail: addressDetail,
-    phoneNumber: phoneNumber,
-    emergencyPhoneNumber: emergencyPhoneNumber,
-    relation: relation
+  const { mutateProfileInfo } = usePostMemberProfileInfo(memberId);
+
+  const getFormValues = (): IMemberProfilePostInfo => {
+    const formData = getValues();
+    return {
+      memberId,
+      memberName: formData[FIELD.MEMBER_NAME],
+      memberGender: formData[FIELD.MEMBER_GENDER] === "여" ? "FEMALE" : "MALE",
+      memberProfileUri: "", // TODO 데이터 연동 필요
+      nickName: formData[FIELD.NICK_NAME],
+      address: formData[FIELD.MEMBER_ADDRESS],
+      addressDetail: formData[FIELD.MEMBER_ADDRESS_DETAIL],
+      phoneNumber: formData[FIELD.MEMBER_PHONE],
+      emergencyPhoneNumber: formData[FIELD.EMERGENCY_NUMBER],
+      relation: formData[FIELD.RELATION]
+    };
   };
 
-  const checkFormValidity = useCallback(() => {
-    if (
-      previousValues.memberName !== memberName ||
-      previousValues.memberGender !== memberGender ||
-      previousValues.nickName !== nickName ||
-      previousValues.address !== address ||
-      previousValues.addressDetail !== addressDetail ||
-      previousValues.phoneNumber !== phoneNumber ||
-      previousValues.emergencyPhoneNumber !== emergencyPhoneNumber ||
-      previousValues.relation !== relation
-    ) {
-      return false;
-    }
-    return true;
-  }, [
-    address,
-    addressDetail,
-    emergencyPhoneNumber,
-    memberGender,
-    memberName,
-    nickName,
-    phoneNumber,
-    previousValues.address,
-    previousValues.addressDetail,
-    previousValues.emergencyPhoneNumber,
-    previousValues.memberGender,
-    previousValues.memberName,
-    previousValues.nickName,
-    previousValues.phoneNumber,
-    previousValues.relation,
-    relation
-  ]);
-
-  const onSubmit = methods.handleSubmit(() => {
-    mutatePostMemberInfo.mutateAttend(updatedMemberInfo, {
-      onError: (err) => {
-        console.log(err);
-      }
-    });
-  });
-
-  useEffect(() => {
-    const isValid = checkFormValidity();
-    setIsDisabled(isValid);
-  }, [checkFormValidity]);
+  const onSubmit = () => {
+    const requestData = getFormValues();
+    mutateProfileInfo(requestData);
+  };
 
   return (
     <BackgroundButton
-      onClick={onSubmit}
+      onClick={handleSubmit(onSubmit)}
       backgroundColor="white"
       buttonBackgroundColor="primaryColor"
-      disabled={isDisabled}
+      disabled={!isDirty || !isValid || isSubmitting}
     >
       수정 완료
     </BackgroundButton>
