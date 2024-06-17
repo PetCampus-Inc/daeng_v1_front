e9e76e953e61d7ca52fe502ddba96098cfcbfe59
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-18 07:21:18 +0900

    refactor: actionFn, closeFn 콜백 타입변경, shouldPropagate 속성추가

--- src/components/common/Modal/ModalButton.tsx
+++ src/components/common/Modal/ModalButton.tsx
@@ -8,9 +8,10 @@ export type TColorScheme = "primary" | "red";
 export interface TicketInfo {
   actionText: string;
   closeText?: string;
-  actionFn: () => void | Promise<void>;
-  closeFn?: () => void | Promise<void>;
+  actionFn: (e: React.MouseEvent) => void | Promise<void>;
+  closeFn?: (e: React.MouseEvent) => void | Promise<void>;
   colorScheme?: TColorScheme;
+  shouldPropagate?: boolean;
 }
 
 export interface ModalButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, TicketInfo {}
@@ -21,20 +22,35 @@ export const ModalButton = ({
   closeFn,
   actionFn,
   colorScheme = "primary",
+  shouldPropagate = false,
   ...props
 }: ModalButtonProps) => {
   const modal = useModal();
 
   const defaultCloseFn = closeFn ? closeFn : modal?.onClose;
 
+  const handleActionClick = (e: React.MouseEvent) => {
+    if (!shouldPropagate) {
+      e.stopPropagation();
+    }
+    actionFn(e);
+  };
+
+  const handleCloseClick = (e: React.MouseEvent) => {
+    if (!shouldPropagate) {
+      e.stopPropagation();
+    }
+    defaultCloseFn(e);
+  };
+
   return (
     <ButtonGroup>
       {closeText && (
-        <CloseButton onClick={defaultCloseFn} {...props}>
+        <CloseButton onClick={handleCloseClick} {...props}>
           {closeText}
         </CloseButton>
       )}
-      <ActionButton colorScheme={colorScheme} onClick={actionFn} {...props}>
+      <ActionButton colorScheme={colorScheme} onClick={handleActionClick} {...props}>
         {actionText}
       </ActionButton>
     </ButtonGroup>
