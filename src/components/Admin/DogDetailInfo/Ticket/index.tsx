6403c15e3ceac9dac664bdf45d9191972e4cd756
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-18 23:54:41 +0900

    refactor: useParams로 dogId 받아오도록 변경, 코드 정리

--- src/components/Admin/DogDetailInfo/Ticket/index.tsx
+++ src/components/Admin/DogDetailInfo/Ticket/index.tsx
@@ -1,31 +1,34 @@
-import useGetTicketDetail from "hooks/api/useGetTicketDetail";
-import { useLocation } from "react-router-dom";
+import { Flex, Text } from "components/common";
+import { useGetTicketDetail } from "hooks/api/admin/attendance";
+import { useParams } from "react-router-dom";
 
 import PastTicketCard from "./PastTicketCard";
 import TicketCard from "./TicketCard";
-import { DogDetailInfoText } from "../DogInfo/styles";
-import { FlexWrapper, InnerContainer } from "../styles";
+import { InnerContainer } from "../styles";
 
 const Ticket = () => {
-  const dogId = useLocation().pathname.split("/").pop();
-  const { data } = useGetTicketDetail(Number(dogId) || -1);
+  const { dogId } = useParams();
+
+  if (!dogId) throw new Error("id가 없습니다");
+
+  const { data } = useGetTicketDetail(Number(dogId));
   const { ticketHistory, ...ticketInfo } = data;
 
   return (
-    <>
-      <InnerContainer>
-        <FlexWrapper>
-          <DogDetailInfoText className="big">이용권 상세정보</DogDetailInfoText>
-          <TicketCard data={ticketInfo} />
-        </FlexWrapper>
-        <FlexWrapper>
-          <DogDetailInfoText className="big">과거 이용권 정보</DogDetailInfoText>
-          <PastTicketCard data={ticketHistory} />
-        </FlexWrapper>
-      </InnerContainer>
-
-      {/* <NewTicketBottomSheet isVisible={isVisible} close={close} currentData={ticketInfo} /> */}
-    </>
+    <InnerContainer>
+      <Flex direction="column" gap={12}>
+        <Text typo="body1_18_B" color="darkBlack">
+          이용권 상세정보
+        </Text>
+        <TicketCard dogId={Number(dogId)} data={ticketInfo} />
+      </Flex>
+      <Flex direction="column" gap={12}>
+        <Text typo="body1_18_B" color="darkBlack">
+          과거 이용권 정보
+        </Text>
+        <PastTicketCard data={ticketHistory} />
+      </Flex>
+    </InnerContainer>
   );
 };
 
