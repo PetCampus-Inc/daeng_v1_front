e1e5831772b3eaae1b2db83a6da743a13d086e61
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-19 03:59:22 +0900

    refactor: style foundations 정리

---
+++ src/styles/foundations/index.ts
@@ -0,0 +1,14 @@
+import { colors } from "./colors";
+import { shadows } from "./shadows";
+import { transition } from "./transition";
+import { typo } from "./typography";
+import { zIndex } from "./zIndex";
+
+export const foundations = {
+  zIndex,
+  blur,
+  colors,
+  typo,
+  shadows,
+  transition
+};
