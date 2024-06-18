fbb194debc3ce4328a4aad9bf713f69a6219c4d4
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-19 01:04:09 +0900

    chore: 불필요한 파일 제거

--- src/store/admin.ts
+++ src/store/admin.ts
@@ -2,7 +2,7 @@ import { atom } from "recoil";
 import { recoilPersist } from "recoil-persist";
 
 import type { INewEnrollmentList } from "../types/admin/school.types";
-import type { ITicketDetail } from "types/admin/attendance.type";
+import type { TicketDetailData } from "types/admin/attendance.type";
 
 const { persistAtom } = recoilPersist();
 
@@ -12,7 +12,7 @@ export const newEnrollmentListAtom = atom<INewEnrollmentList | null>({
   effects_UNSTABLE: [persistAtom]
 });
 
-export const newTicketCardDataAtom = atom<Omit<ITicketDetail, "ticketHistory"> | null>({
+export const newTicketCardDataAtom = atom<Omit<TicketDetailData, "ticketHistory"> | null>({
   key: "newTicketCardData",
   default: null,
   effects_UNSTABLE: [persistAtom]
