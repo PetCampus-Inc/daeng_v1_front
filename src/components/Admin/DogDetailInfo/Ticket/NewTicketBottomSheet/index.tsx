11b5c15d9d38ab46bb051fdbfdba688000fcb56d
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-07-10 23:59:20 +0900

    refactor: 이용권 갱신 팝업 수정
    * design: UI 수정
    * feat: 티켓 유형에 따른 전략 패턴 적용

--- src/components/Admin/DogDetailInfo/Ticket/NewTicketBottomSheet/index.tsx
+++ src/components/Admin/DogDetailInfo/Ticket/NewTicketBottomSheet/index.tsx
@@ -1,84 +1,72 @@
-import { Checkbox } from "components/common";
-import { addWeeks, format } from "date-fns";
-import { useCreateNewTicket } from "hooks/api/admin/attendance";
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useRecoilState } from "recoil";
-import { newTicketCardDataAtom } from "store/admin";
+import { PATH } from "constants/path";
 
-import { NewTicketBottomSheetWrapper } from "./styles";
-import { BottomSheet, type BottomSheetProps } from "../../../../common/BottomSheet";
+import { Box, Flex } from "components/common";
+import { BottomSheet, type BottomSheetProps } from "components/common/BottomSheet";
+import { WideButton } from "components/common/Button/Templates";
+import { Text } from "components/common/Text";
+import { useCreateNewTicket } from "hooks/api/admin/ticket";
+import { useMemo } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { calculateRenewal } from "../newTicket";
 import TicketCard from "../TicketCard";
 
 import type { TicketDetailData } from "types/admin/attendance.type";
 
 interface AddCaredogBottomSheetProps extends BottomSheetProps {
-  currentData: Omit<TicketDetailData, "ticketHistory"> & {
+  info: TicketDetailData & {
     dogId: number;
   };
 }
 
-const NewTicketBottomSheet = ({ isOpen, close, currentData }: AddCaredogBottomSheetProps) => {
-  const [isChecked, setIsChecked] = useState(false);
-  const [, setData] = useRecoilState(newTicketCardDataAtom); //TODO: set만 가져오게
+const NewTicketBottomSheet = ({ isOpen, close, info }: AddCaredogBottomSheetProps) => {
   const navigate = useNavigate();
-  const { mutateNewTicket } = useCreateNewTicket();
+  const { mutateNewTicket } = useCreateNewTicket(info.dogId);
 
-  const today = new Date();
-  const futureDate = addWeeks(today, currentData.monthlyTicketNumber || 1);
-  const todayArray = format(today, "yyyy, M, d").split(",").map(Number);
-  const startDateString = format(today, "yyyy-MM-dd");
-  const futureDateArray = format(futureDate, "yyyy, M, d").split(",").map(Number);
+  const newTicketData = useMemo(() => calculateRenewal(info), [info]);
 
   const handleSubmit = () => {
-    if (isChecked) {
-      navigate(`attendance/${currentData.dogId}/newTicket`);
-      return;
-    }
-    mutateNewTicket({
-      dogId: currentData.dogId,
-      ticketType: currentData.ticketType,
-      roundTicketNumber: currentData.allRoundTicket,
-      monthlyTicketNumber: currentData.monthlyTicketNumber,
-      startDate: startDateString,
-      attendanceDays: currentData.attendanceDays
-    });
+    mutateNewTicket(
+      {
+        dogId: info.dogId,
+        ticketType: newTicketData.ticketType,
+        roundTicketNumber: newTicketData.allRoundTicket,
+        monthlyTicketNumber: newTicketData.monthlyTicketNumber,
+        startDate: newTicketData.ticketStartDate.join("-"),
+        attendanceDays: newTicketData.attendanceDays ?? []
+      },
+      {
+        onSuccess: () => {
+          close();
+        }
+      }
+    );
   };
 
-  const cardData = {
-    ticketType: currentData.ticketType,
-    currentRoundTicket: currentData.allRoundTicket,
-    ticketExpirationDate: futureDateArray,
-    allRoundTicket: currentData.allRoundTicket,
-    monthlyTicketNumber: currentData.monthlyTicketNumber,
-    ticketStartDate: todayArray,
-    attendanceDays: currentData.attendanceDays
-  };
-
-  // useEffect(() => {
-  //   setData(cardData);
-  // }, []);
+  const handleRenewal = () => navigate(PATH.ADMIN_ATTENDANCE_INFO_NEW_TICKET(info.dogId));
 
   return (
     <BottomSheet isOpen={isOpen} close={() => close()}>
       <BottomSheet.Content>
-        <NewTicketBottomSheetWrapper>
-          <BottomSheet.Control />
-          <BottomSheet.Title align="left">갱신될 이용권 내역이에요</BottomSheet.Title>
-
-          <TicketCard dogId={currentData.dogId} data={cardData} />
-          <BottomSheet.Subtitle>
-            갱신될 이용권 내역을 변경 하려면 아래에 체크해 주세요
-          </BottomSheet.Subtitle>
-
-          <Checkbox
-            label="갱신될 이용권 정보 변경"
-            variant="fill"
-            onClick={() => setIsChecked(!isChecked)}
-            isChecked={isChecked}
-          />
-        </NewTicketBottomSheetWrapper>
-        <BottomSheet.Button actionText="갱신하기" actionFn={handleSubmit} />
+        <BottomSheet.Control />
+        <Box marginBlock={12}>
+          <Text as="p" typo="title2_20_B" color="darkBlack">
+            갱신될 이용권 내역이에요
+          </Text>
+        </Box>
+        <TicketCard dogId={info.dogId} data={newTicketData} />
+        <Flex direction="column" gap={8} marginTop="12%">
+          <Text as="p" typo="label2_14_R" color="gray_2" textAlign="center" whiteSpace="pre-wrap">
+            위의 갱신될 이용권 내역을 확인해보시고 수정을 원하시는 경우, 수정하기 버튼을 수정
+            페이지로 이동해주세요
+          </Text>
+          <Flex gap={8} width="full">
+            <WideButton colorScheme="br_5" onClick={handleRenewal}>
+              수정
+            </WideButton>
+            <WideButton onClick={handleSubmit}>갱신</WideButton>
+          </Flex>
+        </Flex>
       </BottomSheet.Content>
     </BottomSheet>
   );
