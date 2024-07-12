f5b5e3d62e9bae6582e59496af6f594af01a5643
jieun419 <crg1050@naver.com>
2024-07-12 14:20:02 +0900

    Feat: SaveButton profile 업로드 가능하도록 코드 수정

--- src/components/Member/DogInfo/DogDetailInfoEdit/Buttons/SaveButton.tsx
+++ src/components/Member/DogInfo/DogDetailInfoEdit/Buttons/SaveButton.tsx
@@ -1,8 +1,9 @@
 import { FIELD } from "constants/field";
-import { FILE_URI_NAME } from "constants/profile";
+import { ACCEPT_FILE_TYPE, FILE_URI_NAME, PROFILE_NAME, PROFILE_PATHS } from "constants/profile";
 
 import { BackgroundButton } from "components/common/Button";
 import { usePostMemberDogDetailInfo } from "hooks/api/member/member";
+import useSubmitProfile from "hooks/api/member/useSubmitProfile";
 import { useFormContext } from "react-hook-form";
 import { type MemberDogInfoReq } from "types/member/main.types";
 import { getKeyForLabel } from "utils/formatter";
@@ -13,7 +14,13 @@ const SaveButton = ({ dogId }: { dogId: number }) => {
     handleSubmit,
     formState: { isDirty, isValid, isSubmitting }
   } = useFormContext();
+
+  const { uploadFiles, s3ProfileData } = useSubmitProfile();
   const { mutatePostDogDetailInfo } = usePostMemberDogDetailInfo(dogId);
+
+  const convertProfileUri = (name: string) => {
+    return s3ProfileData.find((file) => file.split("/").includes(name)) || "";
+  };
 
   const getFormValues = (): MemberDogInfoReq => {
     const formData = getValues();
@@ -30,14 +37,43 @@ const SaveButton = ({ dogId }: { dogId: number }) => {
     };
   };
 
+  const handleSubmitInfo = () => {
+    const { profileUri } = getFormValues();
+    // profileUri 파일 수정 할 경우/안 할 경우
+    if (typeof profileUri !== "string") uploadProfileFiles(profileUri);
+    else onSubmit();
+  };
+
+  const uploadProfileFiles = async (profileUri: FileList) => {
+    const dogParams = {
+      name: PROFILE_NAME.DOG,
+      id: dogId,
+      files: profileUri,
+      accept: ACCEPT_FILE_TYPE.IMAGE,
+      path: PROFILE_PATHS.DOG
+    };
+
+    const params = [dogParams];
+
+    await uploadFiles(params, {
+      onSuccess: () => {
+        onSubmit();
+      }
+    });
+  };
+
   const onSubmit = () => {
-    const requestData = getFormValues();
+    const formData = getFormValues();
+    const requestData = Object.assign(formData, {
+      profileUri: convertProfileUri(PROFILE_NAME.DOG)
+    });
     mutatePostDogDetailInfo(requestData);
+    console.log("requestData", requestData);
   };
 
   return (
     <BackgroundButton
-      onClick={handleSubmit(onSubmit)}
+      onClick={handleSubmit(handleSubmitInfo)}
       backgroundColor="white"
       buttonBackgroundColor="primaryColor"
       disabled={!isDirty || !isValid || isSubmitting}
