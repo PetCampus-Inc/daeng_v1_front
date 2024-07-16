fb592f784b0fcc61210ca8d6aecff9d5a6e8dd24
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-07-17 06:00:31 +0900

    refactor: BackgroundButton 불필요한 스타일 제거

--- src/components/common/Button/BackgroundButton/styles.ts
+++ src/components/common/Button/BackgroundButton/styles.ts
@@ -33,12 +33,9 @@ export const Button = styled.button.withConfig({
     color 0.3s;
 `;
 
-export const BackgroundButtonWrapper = styled.div.withConfig({
-  shouldForwardProp: (prop) => !["hasNav"].includes(prop)
-})<{ hasNav?: boolean }>`
+export const BackgroundButtonWrapper = styled.div`
   position: absolute;
   bottom: 0;
   left: 0;
   right: 0;
-  padding-bottom: ${({ hasNav }) => (hasNav ? "78px" : 0)};
 `;
