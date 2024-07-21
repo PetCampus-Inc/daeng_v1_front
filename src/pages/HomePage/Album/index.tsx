4dacddf7402514a8735f483598b7807a3ddffaa6
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-07-21 16:59:46 +0900

    refactor: Home 디렉터리 구조 수정

---
+++ src/pages/HomePage/Album/index.tsx
@@ -0,0 +1,34 @@
+import { GALLERY_VIEW, type GalleryViewType } from "constants/option";
+
+import AlbumHeader from "components/Album/AlbumHeader";
+import AlbumView from "components/Album/AlbumView";
+import PhotoView from "components/Album/PhotoView";
+import ViewTabs from "components/Album/ViewTabs";
+import { Layout } from "components/common";
+import Header from "components/common/Header";
+import { useGetMainAlbum } from "hooks/api/member/member";
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
+const Album = () => {
+  const [searchParams] = useSearchParams();
+  const dogId = parseInt(searchParams.get("dogId") ?? "");
+  const dogName = searchParams.get("dogName");
+  const { data: imageList } = useGetMainAlbum({ dogId });
+
+  const [mode, setMode] = useState<GalleryViewType>(GALLERY_VIEW.PHOTO);
+
+  return (
+    <>
+      <Header type="text" text="사진 앨범" />
+      <Layout px={16} pt={42} bgColor="white">
+        <AlbumHeader dogId={dogId} dogName={dogName} />
+        <ViewTabs mode={mode} setMode={setMode} />
+        {mode === GALLERY_VIEW.PHOTO && <PhotoView imageList={imageList} />}
+        {mode === GALLERY_VIEW.ALBUM && <AlbumView imageList={imageList} />}
+      </Layout>
+    </>
+  );
+};
+
+export default Album;
