23d4ebbdd6cbeb3ce5c91f4d5a68a7337c52ef96
hyeran.lee <hyeran.lee@giantstepcorp.com>
2024-07-23 18:55:50 +0900

    feat: add markup

--- src/components/Admin/MyPage/DeleteAccount/index.tsx
+++ src/components/Admin/MyPage/DeleteAccount/index.tsx
@@ -1,3 +1,4 @@
+import { Checkbox, Flex, Layout, Text } from "components/common";
 import Header from "components/common/Header";
 
 interface DeleteAccountProps {
@@ -7,7 +8,24 @@ interface DeleteAccountProps {
 const DeleteAccount = ({ setStep }: DeleteAccountProps) => {
   return (
     <>
-      <Header type="back" handleClick={() => setStep(0)} />
+      <Header type="back" handleClick={() => setStep(0)} shadow={true} />
+      <Layout type="detail" paddingTop="1.5rem" paddingX="1rem">
+        <Flex direction="column" gap={3}>
+          <Text as="p" typo="title1_24_B">
+            똑독 탈퇴를 원하시나요?
+          </Text>
+          <Text as="p" typo="body2_16_R" color="gray_2">
+            탈퇴 전, 아래 내용을 확인해 주세요
+          </Text>
+        </Flex>
+        <Flex justify="flex-start">
+          <Checkbox isChecked />
+          <Text>
+            지금까지 주고받은 채팅내역, 알림장, 사진앨범 등의 모든 기록이 <Text>영구 삭제</Text>되며
+            복구할 수 없어요
+          </Text>
+        </Flex>
+      </Layout>
     </>
   );
 };
