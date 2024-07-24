0690ff82c3c81cb3fb1bd31deff18dd50149d166
jsn0509 <jsn_@naver.com>
2024-07-25 00:22:02 +0900

    refactor: 메모이제이션 hook 추가

--- src/hooks/native/useNative.tsx
+++ src/hooks/native/useNative.tsx
@@ -1,5 +1,5 @@
 import nativeReceiver from "libs/nativeReceiver";
-import { useCallback } from "react";
+import { useCallback, useMemo } from "react";
 import { MessageData, MessageType, NativeMessage } from "types/native/message.types";
 
 const useNative = () => {
@@ -13,51 +13,57 @@ const useNative = () => {
     []
   );
 
-  const nativeRequest = async <T extends MessageType["Request"]>(
-    type: T,
-    data: MessageData["Request"][T],
-    timeout?: number
-  ): Promise<NativeMessage<T>["data"]> => {
-    return new Promise((resolve, reject) => {
-      let timer: NodeJS.Timeout | null = null;
+  const nativeRequest = useCallback(
+    async <T extends MessageType["Request"]>(
+      type: T,
+      data: MessageData["Request"][T],
+      timeout?: number
+    ): Promise<NativeMessage<T>["data"]> => {
+      return new Promise((resolve, reject) => {
+        let timer: NodeJS.Timeout | null = null;
 
-      if (typeof timeout === "number" && timeout > 0) {
-        timer = setTimeout(() => {
-          nativeReceiver.unregisterCallback(handler);
-          reject(new Error("Native 응답 시간 초과"));
-        }, timeout);
-      }
-
-      const handler = (message: NativeMessage) => {
-        const { type: resType, data: resData } = message;
-
-        if (resType === type) {
-          resolve(resData);
-          cleanup();
-        } else if (resType === "ERROR") {
-          reject(resData);
-          cleanup();
+        if (typeof timeout === "number" && timeout > 0) {
+          timer = setTimeout(() => {
+            nativeReceiver.unregisterCallback(handler);
+            reject(new Error("Native 응답 시간 초과"));
+          }, timeout);
         }
-      };
 
-      const cleanup = () => {
-        nativeReceiver.unregisterCallback(handler);
-        if (timer) clearTimeout(timer);
-      };
+        const handler = (message: NativeMessage) => {
+          const { type: resType, data: resData } = message;
 
-      postMessage(type, data);
-      nativeReceiver.registerCallback(handler);
-    });
-  };
+          if (resType === type) {
+            resolve(resData);
+            cleanup();
+          } else if (resType === "ERROR") {
+            reject(resData);
+            cleanup();
+          }
+        };
 
-  const nativeEvent = {
-    goBack: () => nativeRequest("GO_BACK", null, 2000),
-    getIdToken: () => nativeRequest("GET_ID_TOKEN", null, 2000),
-    getDeviceId: () => nativeRequest("GET_DEVICE_ID", null, 2000),
-    saveImage: (uri: string | string[]) => nativeRequest("SAVE_IMAGE", uri),
-    selectImage: () => nativeRequest("SELECT_IMAGE", null),
-    launchCamera: () => nativeRequest("LAUNCH_CAMERA", null)
-  };
+        const cleanup = () => {
+          nativeReceiver.unregisterCallback(handler);
+          if (timer) clearTimeout(timer);
+        };
+
+        postMessage(type, data);
+        nativeReceiver.registerCallback(handler);
+      });
+    },
+    [postMessage]
+  );
+
+  const nativeEvent = useMemo(
+    () => ({
+      goBack: () => nativeRequest("GO_BACK", null, 2000),
+      getIdToken: () => nativeRequest("GET_ID_TOKEN", null, 2000),
+      getDeviceId: () => nativeRequest("GET_DEVICE_ID", null, 2000),
+      saveImage: (uri: string | string[]) => nativeRequest("SAVE_IMAGE", uri),
+      selectImage: () => nativeRequest("SELECT_IMAGE", null),
+      launchCamera: () => nativeRequest("LAUNCH_CAMERA", null)
+    }),
+    [nativeRequest]
+  );
 
   return { postMessage, nativeRequest, nativeEvent };
 };
