0eb4e8f172228a4e858c13641c79fb0ea11e9df6
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-07-24 19:51:49 +0900

    feat: poop box variant 추가

--- src/components/common/PoopBox/index.tsx
+++ src/components/common/PoopBox/index.tsx
@@ -8,29 +8,30 @@ import { Poop } from "types/admin/attendance.type";
 import * as S from "./styles";
 
 interface PoopBoxProps {
+  variant?: "sm" | "lg";
   selected?: Poop;
 }
 
-const PoopBox = ({ selected }: PoopBoxProps) => {
+const PoopBox = ({ variant = "lg", selected }: PoopBoxProps) => {
   return (
-    <S.PoopCardContainer>
-      <S.PoopCard>
+    <S.PoopCardContainer variant={variant}>
+      <S.PoopCard active={selected === Poop.HARD}>
         <PoopHard poop={selected} />
         딱딱함
       </S.PoopCard>
-      <S.PoopCard>
+      <S.PoopCard active={selected === Poop.HEALTHY}>
         <PoopHealthy poop={selected} />
         건강함
       </S.PoopCard>
-      <S.PoopCard>
+      <S.PoopCard active={selected === Poop.WATERY}>
         <PoopWatery poop={selected} />
         묽은 변
       </S.PoopCard>
-      <S.PoopCard>
+      <S.PoopCard active={selected === Poop.NOT_BROWN}>
         <PoopNotBrown poop={selected} />
         갈색이 아닌
       </S.PoopCard>
-      <S.PoopCard>
+      <S.PoopCard active={selected === Poop.WARNING}>
         <PoopWarning poop={selected} />
         주의필요
       </S.PoopCard>
--- src/components/common/PoopBox/styles.ts
+++ src/components/common/PoopBox/styles.ts
@@ -1,23 +1,48 @@
-import styled from "styled-components";
+import styled, { css, DefaultTheme } from "styled-components";
 
-export const PoopCardContainer = styled.div`
+type Variant = "sm" | "lg";
+
+const getVariantStyle = (theme: DefaultTheme, variant: Variant) => {
+  switch (variant) {
+    case "sm":
+      return css`
+        ${theme.typo.caption1_12_R};
+      `;
+    case "lg":
+      return css`
+        gap: 3px;
+        ${theme.typo.label2_14_R};
+      `;
+  }
+};
+
+export const PoopCardContainer = styled.div.withConfig({
+  shouldForwardProp: (props) => !["variant"].includes(props)
+})<{
+  variant: Variant;
+}>`
   display: flex;
   width: 100%;
-  gap: 14px;
+  gap: ${({ variant }) => (variant === "sm" ? "16px" : "14px")};
   margin-top: 8px;
+
+  & > div {
+    ${({ theme, variant }) => getVariantStyle(theme, variant)}
+  }
 `;
 
-export const PoopCard = styled.div`
+export const PoopCard = styled.div.withConfig({
+  shouldForwardProp: (prop) => !["active"].includes(prop)
+})<{ active: boolean }>`
   display: flex;
   flex-direction: column;
-  gap: 3px;
-  width: 100%;
   text-align: center;
-  white-space: nowrap;
-  color: ${({ theme }) => theme.colors.gray_2};
-  ${({ theme }) => theme.typo.label2_14_R};
+  width: 100%;
+  flex: 1;
 
-  > svg {
+  color: ${({ active, theme }) => (active ? theme.colors.primaryColor : theme.colors.gray_3)};
+
+  & > svg {
     width: 100%;
     border-radius: 10px;
   }
