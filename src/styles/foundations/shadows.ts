ec2b87d1c3260c27e5b47e45415e8c515158946b
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-07-24 19:57:04 +0900

    fix: shadows 토큰 수정

--- src/styles/foundations/shadows.ts
+++ src/styles/foundations/shadows.ts
@@ -1,7 +1,7 @@
 export const shadows = {
   upper: "0px 5px 10px rgba(0, 0, 0, 0.04)",
   card: "0px 8px 15px 0px rgba(0, 0, 0, 0.04)",
-  smallMenu: "-2px 8px 30px 45px rgba(0, 0, 0, 0.35)",
+  smallMenu: "2px 8px 30px 0px rgba(82, 82, 82, 0.35);",
   alertCard: "0px 8px 15px 10px rgba(156, 155, 155, 0.07)",
   overlap: "-4px 0px 12px rgba(0,0,0,0.35)",
   input: "0px -5px 10px rgba(0,0,0,0.04)",
