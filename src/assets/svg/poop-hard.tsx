600262443668228db2d345df30128a0ece8090d6
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-07-24 19:50:43 +0900

    fix: poop box icon fill 변경

--- src/assets/svg/poop-hard.tsx
+++ src/assets/svg/poop-hard.tsx
@@ -10,11 +10,11 @@ const PoopHard = ({ poop }: { poop?: Poop }) => {
         width="60"
         height="62"
         rx="8"
-        fill={isSelected ? themeConfig.colors.yellow_3 : "#E9E9E9"}
+        fill={isSelected ? themeConfig.colors.yellow_3 : "#F6F6F6"}
       />
       <path
         d="M30.5 14L44.7894 22.25V38.75L30.5 47L16.2106 38.75V22.25L30.5 14Z"
-        fill={isSelected ? themeConfig.colors.primaryColor : "#B5B5B5"}
+        fill={isSelected ? themeConfig.colors.primaryColor : "#E9E9E9"}
       />
     </svg>
   );
