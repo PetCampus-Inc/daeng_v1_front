ef9170242af509af05352d9660fc05a140ce201e
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-07-27 23:46:24 +0900

    Feat: `홈` 알림장 마크업 (#196)
    * refactor: Home 디렉터리 구조 수정
    
    * feat: Home Agenda 페이지 추가
    
    * feat: Agenda 영역 컨테이너 추가
    
    * feat: Agenda 기록 영역 마크업
    
    * feat: Empty Agenda 영역 추가
    
    * feat: Agenda 컴포넌트 re export  추가
    
    * chore: public assets 클라이언트 url 추가
    
    * env.d.ts client_url 추가
    * assets url 추가
    
    * fix: poop box icon fill 변경
    
    * feat: poop box variant 추가
    
    * refactor: poop box variant 사용
    
    * feat: style props radius type 추가

---
+++ src/components/Agenda/DailyAgenda/EmptyAgenda.tsx
@@ -0,0 +1,11 @@
+import { EmptyContainer } from "./styles";
+
+export const EmptyAgenda = () => {
+  return (
+    <>
+      <EmptyContainer>
+        <span>작성된 알림장이 없어요</span>
+      </EmptyContainer>
+    </>
+  );
+};
