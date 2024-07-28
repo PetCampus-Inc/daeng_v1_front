49693226350ecd88a86349698d590e807042a1ba
hyerani <hran9404@naver.com>
2024-07-29 08:28:41 +0900

    refactor: edit profile src

--- src/components/Admin/MyPage/EditProfile/index.tsx
+++ src/components/Admin/MyPage/EditProfile/index.tsx
@@ -18,7 +18,11 @@ const EditProfile = ({ principalData, teacherData }: ProfileInfoProps) => {
         <S.ProfileBox>
           <S.ProfileEditeBox>
             <S.UserImage
-              src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              src={
+                principalData?.profileUri
+                  ? `${principalData.profileUri}`
+                  : "https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              }
               alt="user_profile"
             />
             <S.ProfileEditeButton>
