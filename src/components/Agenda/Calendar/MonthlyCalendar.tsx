509deb18344f9d3bf9b2212d693bc6b2ee596819
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-08-07 20:33:49 +0900

    refactor: 공통 props에 맞춰 월간 캘린더 변경

--- src/components/Agenda/Calendar/MonthlyCalendar.tsx
+++ src/components/Agenda/Calendar/MonthlyCalendar.tsx
@@ -1,127 +1,53 @@
 import ArrowLeftIcon from "assets/svg/arrow-left-icon";
 import ArrowRightIcon from "assets/svg/arrow-right-icon";
 import FootIcon from "assets/svg/foot-icon";
-import XIcon from "assets/svg/x-icon";
 import { Box, Text } from "components/common";
-import { FloatingOverlay } from "components/common/FloatingOverlay";
 import { format, isSameDay } from "date-fns";
-import { useClickOutSide } from "hooks/common/useClickOutSide";
-import React, {
-  type ForwardedRef,
-  type RefObject,
-  forwardRef,
-  useCallback,
-  useEffect,
-  useRef,
-  useState
-} from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import Calendar, { type OnArgs } from "react-calendar";
 
-import {
-  StyledMonthlyCalendar,
-  StyledDate,
-  StyledYearView,
-  PopupContainer,
-  ControlWrapper,
-  ControlButton,
-  PopupWrapper
-} from "./styles";
+import { StyledMonthlyCalendar, GoToTodayButton } from "./styles";
 
-type ValuePiece = Date | null;
-type Value = ValuePiece | [ValuePiece, ValuePiece];
+import type { Value } from "react-calendar/dist/cjs/shared/types";
 
-interface YearPopupProps {
-  isOpen: boolean;
-  close: () => void;
-  activeStartDate: Date | null;
-  onSelect: (value: Value) => void;
-}
-
-const YearPopup = forwardRef(function YearPopup(
-  { isOpen, close, activeStartDate, onSelect }: YearPopupProps,
-  ref: ForwardedRef<HTMLDivElement>
-) {
-  const popupRef = ref as RefObject<HTMLDivElement>;
-
-  useClickOutSide({
-    enabled: isOpen,
-    targetRef: popupRef,
-    onClickOutside: close
-  });
-
-  if (!isOpen) return null;
-
-  return (
-    <>
-      <FloatingOverlay type="none" />
-      <PopupContainer>
-        <PopupWrapper ref={ref}>
-          <StyledYearView>
-            <Calendar
-              value={activeStartDate}
-              onChange={onSelect}
-              view="year"
-              maxDetail="year"
-              minDetail="month"
-              formatYear={(locale, date) => format(date, "yyyy")}
-              prevLabel={<ArrowLeftIcon w={20} colorScheme="darkBlack" />}
-              nextLabel={<ArrowRightIcon w={20} colorScheme="darkBlack" />}
-              prev2Label={null}
-              next2Label={null}
-            />
-          </StyledYearView>
-          <ControlWrapper>
-            <ControlButton type="button" onClick={close}>
-              <XIcon />
-            </ControlButton>
-          </ControlWrapper>
-        </PopupWrapper>
-      </PopupContainer>
-    </>
-  );
-});
+const ATTEND_DAYS = ["2024-07-17", "2024-07-17", "2024-07-21", "2024-07-23"];
 
 interface MonthlyCalendarProps {
   data: {
     today: Date;
+    onDateClick: (newDate: Value) => void;
+    onTodayClick: () => void;
     activeStartDate: Date | null;
-    handleDateChange: (newDate: Value) => void;
-    handleTodayClick: () => void;
-    setActiveStartDate: React.Dispatch<React.SetStateAction<Date | null>>;
+    onActiveStartDateChange: React.Dispatch<React.SetStateAction<Date | null>>;
+    onShowMonthSelectChange: React.Dispatch<React.SetStateAction<boolean>>;
+    headerRef: React.RefObject<HTMLDivElement>;
   };
 }
 
-export const MonthlyCalendar = ({ data }: MonthlyCalendarProps) => {
-  const [showYearPopup, setShowYearPopup] = useState(false);
-  const calendarRef = useRef<HTMLDivElement>(null);
-  const popupRef = useRef<HTMLDivElement>(null);
-  const todayRef = useRef<HTMLButtonElement>(null);
+export const MonthlyCalendar: React.FC<MonthlyCalendarProps> = ({ data }) => {
+  const {
+    today,
+    activeStartDate,
+    onDateClick,
+    onTodayClick,
+    onActiveStartDateChange,
+    onShowMonthSelectChange,
+    headerRef: calendarRef
+  } = data;
 
-  const { today, activeStartDate, handleDateChange, handleTodayClick, setActiveStartDate } = data;
-
-  const ATTEND_DAYS = ["2024-07-17", "2024-07-17", "2024-07-21", "2024-07-23"];
+  const todayButtonRef = useRef<HTMLButtonElement>(null);
 
   const handleDrillUp = useCallback(() => {
-    setShowYearPopup(true);
-  }, []);
+    onShowMonthSelectChange(true);
+  }, [onShowMonthSelectChange]);
 
   const handleActiveStartDateChange = useCallback(
     ({ view, activeStartDate }: OnArgs) => {
       if (view === "month") {
-        setActiveStartDate(activeStartDate);
+        onActiveStartDateChange(activeStartDate);
       }
     },
-    [setActiveStartDate]
-  );
-
-  const handleYearSelect = useCallback(
-    (value: Value) => {
-      if (value instanceof Date) {
-        setActiveStartDate(value);
-        setShowYearPopup(false);
-      }
-    },
-    [setActiveStartDate]
+    [onActiveStartDateChange]
   );
 
   const renderTileContent = useCallback(
@@ -130,7 +56,7 @@ export const MonthlyCalendar = ({ data }: MonthlyCalendarProps) => {
 
       if (view === "month" && isSameDay(date, today)) {
         contents.push(
-          <Text typo="caption1_12_R" color="primaryColor" key="today">
+          <Text key="today" typo="caption1_12_R" color="primaryColor">
             오늘
           </Text>
         );
@@ -138,7 +64,7 @@ export const MonthlyCalendar = ({ data }: MonthlyCalendarProps) => {
 
       if (ATTEND_DAYS.some((day) => isSameDay(new Date(day), date))) {
         contents.push(
-          <Box as="span" color="br_3" key="footIcon">
+          <Box key="footIcon" as="span" color="br_3">
             <FootIcon w={15} h={12} />
           </Box>
         );
@@ -150,9 +76,9 @@ export const MonthlyCalendar = ({ data }: MonthlyCalendarProps) => {
   );
 
   const adjustTodayButtonPosition = useCallback(() => {
-    if (calendarRef.current && todayRef.current) {
+    if (calendarRef.current && todayButtonRef.current) {
       const calendarNavigation = calendarRef.current.querySelector(".react-calendar__navigation");
-      const todayButton = todayRef.current;
+      const todayButton = todayButtonRef.current;
 
       if (calendarNavigation) {
         const navRect = calendarNavigation.getBoundingClientRect();
@@ -174,61 +100,31 @@ export const MonthlyCalendar = ({ data }: MonthlyCalendarProps) => {
     };
   }, [adjustTodayButtonPosition]);
 
-  useEffect(() => {
-    if (showYearPopup && calendarRef.current && popupRef.current) {
-      const calendarNavigation = calendarRef.current.querySelector(".react-calendar__navigation");
-      const popupNavigation = popupRef.current.querySelector(".react-calendar__navigation");
-
-      if (calendarNavigation && popupNavigation) {
-        const calendarRect = calendarNavigation.getBoundingClientRect();
-        const popupRect = popupRef.current.getBoundingClientRect();
-
-        const topOffset = calendarRect.top - popupRect.top;
-
-        const popupStyle = getComputedStyle(popupRef.current);
-        const paddingTop = parseFloat(popupStyle.paddingTop);
-
-        popupRef.current.style.transform = `translateY(${topOffset - paddingTop}px)`;
-      }
-    }
-  }, [showYearPopup]);
-
   return (
-    <>
-      <StyledMonthlyCalendar ref={calendarRef}>
-        <Calendar
-          value={activeStartDate}
-          onChange={handleDateChange}
-          activeStartDate={activeStartDate || undefined}
-          onActiveStartDateChange={handleActiveStartDateChange}
-          formatDay={(locale, date) => format(date, "d")}
-          formatWeekday={(locale, date) => format(date, "E")}
-          formatMonthYear={(locale, date) => format(date, "yyyy. MM")}
-          formatYear={(locale, date) => format(date, "yyyy")}
-          calendarType="gregory"
-          showNeighboringMonth={true}
-          prevLabel={<ArrowLeftIcon w={24} colorScheme="darkBlack" />}
-          nextLabel={<ArrowRightIcon w={24} colorScheme="darkBlack" />}
-          next2Label={null}
-          prev2Label={null}
-          minDetail="year"
-          view="month"
-          onDrillUp={handleDrillUp}
-          tileContent={renderTileContent}
-        />
-        <StyledDate type="button" ref={todayRef} onClick={handleTodayClick}>
-          오늘
-        </StyledDate>
-      </StyledMonthlyCalendar>
-      {showYearPopup && (
-        <YearPopup
-          ref={popupRef}
-          isOpen={showYearPopup}
-          close={() => setShowYearPopup(false)}
-          activeStartDate={activeStartDate}
-          onSelect={handleYearSelect}
-        />
-      )}
-    </>
+    <StyledMonthlyCalendar ref={calendarRef}>
+      <Calendar
+        value={activeStartDate}
+        onChange={onDateClick}
+        activeStartDate={activeStartDate || undefined}
+        onActiveStartDateChange={handleActiveStartDateChange}
+        formatDay={(locale, date) => format(date, "d")}
+        formatWeekday={(locale, date) => format(date, "E")}
+        formatMonthYear={(locale, date) => format(date, "yyyy. MM")}
+        formatYear={(locale, date) => format(date, "yyyy")}
+        calendarType="gregory"
+        showNeighboringMonth={true}
+        prevLabel={<ArrowLeftIcon w={24} colorScheme="darkBlack" />}
+        nextLabel={<ArrowRightIcon w={24} colorScheme="darkBlack" />}
+        next2Label={null}
+        prev2Label={null}
+        minDetail="year"
+        view="month"
+        onDrillUp={handleDrillUp}
+        tileContent={renderTileContent}
+      />
+      <GoToTodayButton type="button" ref={todayButtonRef} onClick={onTodayClick}>
+        오늘
+      </GoToTodayButton>
+    </StyledMonthlyCalendar>
   );
 };
