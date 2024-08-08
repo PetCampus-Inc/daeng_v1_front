c19483363421998819b30ce51897d8319421b4e5
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-08-08 22:21:23 +0900

    feat: Agenda 페이지 라우터 키 추가

---
+++ src/pages/AgendaPage/AgendaPage.tsx
@@ -0,0 +1,27 @@
+import { Calendar, DailyAgenda } from "components/Agenda";
+import { Box, Layout } from "components/common";
+import Header from "components/common/Header";
+import { Suspense } from "react";
+import { useParams } from "react-router-dom";
+
+const Agenda = () => {
+  const { dogId } = useParams<{ dogId: string }>();
+
+  return (
+    <>
+      <Header type="text" text="알림장" />
+      <Layout bgColor="BGray" pb={40}>
+        <Suspense fallback={<div>로딩중...</div>}>
+          <Calendar id={Number(dogId)} />
+        </Suspense>
+        <Box mx={16} mt={40}>
+          <Suspense fallback={<div>로딩중...</div>}>
+            <DailyAgenda id={Number(dogId)} />
+          </Suspense>
+        </Box>
+      </Layout>
+    </>
+  );
+};
+
+export default Agenda;
