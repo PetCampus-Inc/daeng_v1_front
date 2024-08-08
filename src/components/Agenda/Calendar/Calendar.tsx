92757b24fd92aef56b3f289b75d2a4feab169747
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-08-08 09:13:09 +0900

    refactor: 추상화, 코드 정리

--- src/components/Agenda/Calendar/Calendar.tsx
+++ src/components/Agenda/Calendar/Calendar.tsx
@@ -5,7 +5,7 @@ import React, { useState, useRef } from "react";
 import { useSearchParams } from "react-router-dom";
 
 import { MonthlyCalendar } from "./MonthlyCalendar";
-import { MonthSelector } from "./MonthSelector";
+import { MonthPicker } from "./MonthPicker";
 import { ToggleViewButton } from "./styles";
 import { WeeklyCalendar } from "./WeeklyCalendar";
 
@@ -14,15 +14,15 @@ import type { Value } from "react-calendar/dist/cjs/shared/types";
 export const Calendar: React.FC = () => {
   const today = new Date();
   const [expanded, setExpanded] = useState(false);
-  const [activeStartDate, setActiveStartDate] = useState<Date | null>(new Date());
+  const [activeDate, setActiveDate] = useState<Date | null>(new Date());
   const [searchParams, setSearchParams] = useSearchParams();
-  const [showMonthSelector, setShowMonthSelector] = useState(false);
+  const [showMonthPicker, setShowMonthPicker] = useState(false);
   const calendarHeaderRef = useRef<HTMLDivElement>(null);
 
-  const handleDateClick = (newDate: Value) => {
-    if (newDate instanceof Date) {
-      setActiveStartDate(newDate);
-      const formattedDate = format(newDate as Date, "yyyy-MM-dd");
+  const handleDateClick = (date: Value) => {
+    if (date instanceof Date) {
+      setActiveDate(date);
+      const formattedDate = format(date as Date, "yyyy-MM-dd");
       searchParams.set("date", formattedDate);
       setSearchParams(searchParams);
     }
@@ -30,46 +30,48 @@ export const Calendar: React.FC = () => {
 
   const handleTodayClick = () => {
     const today = new Date();
-    setActiveStartDate(today);
+    handleDateClick(today);
   };
 
   const toggleExpanded = () => setExpanded((prev) => !prev);
 
-  const handleMonthSelect = (value: Value) => {
-    if (value instanceof Date) {
-      setActiveStartDate(value);
-      setShowMonthSelector(false);
-    }
+  const handleMonthClick = (date: Value) => {
+    handleDateClick(date);
+    setShowMonthPicker(false);
   };
 
-  const handleCloseMonthSelector = () => {
-    setShowMonthSelector(false);
+  const handleOpenMonthPicker = () => {
+    setShowMonthPicker(true);
   };
 
-  const calendarData = {
+  const handleCloseMonthPicker = () => {
+    setShowMonthPicker(false);
+  };
+
+  const calendarProps = {
     today,
     onDateClick: handleDateClick,
     onTodayClick: handleTodayClick,
-    activeStartDate,
-    onActiveStartDateChange: setActiveStartDate,
-    onShowMonthSelectChange: setShowMonthSelector,
+    activeDate,
+    onActiveDateChange: setActiveDate,
+    onOpenMonthPicker: handleOpenMonthPicker,
     headerRef: calendarHeaderRef
   };
 
   return (
     <Box bgColor="white" pt={28} radius="0px 0px 20px 20px">
-      {expanded ? <MonthlyCalendar data={calendarData} /> : <WeeklyCalendar data={calendarData} />}
+      {expanded ? <MonthlyCalendar {...calendarProps} /> : <WeeklyCalendar {...calendarProps} />}
       <ToggleViewButton type="button" onClick={toggleExpanded} expand={expanded}>
         {expanded ? "닫기" : "펼쳐보기"}
         <span>
           <ArrowDownIcon w={20} h={20} />
         </span>
       </ToggleViewButton>
-      <MonthSelector
-        isOpen={showMonthSelector}
-        onClose={handleCloseMonthSelector}
-        activeStartDate={activeStartDate}
-        onSelect={handleMonthSelect}
+      <MonthPicker
+        isOpen={showMonthPicker}
+        onClose={handleCloseMonthPicker}
+        onMonthClick={handleMonthClick}
+        activeDate={activeDate}
         anchorRef={calendarHeaderRef}
       />
     </Box>
