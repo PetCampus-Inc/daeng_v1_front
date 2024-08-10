ce40682eadcf040b2570669c7aef08b198e5f84b
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-08-10 19:46:25 +0900

    refactor: arrow icon svg fill 속성 제거

--- src/assets/svg/arrow-left-icon.tsx
+++ src/assets/svg/arrow-left-icon.tsx
@@ -2,15 +2,6 @@ import type { IconSize } from "./types";
 
 interface Props extends IconSize {
   className?: string;
-  colorScheme?:
-    | "gray_1"
-    | "gray_2"
-    | "gray_3"
-    | "gray_4"
-    | "gray_5"
-    | "black"
-    | "darkBlack"
-    | "none";
 }
 
 const ArrowLeftIcon = ({ className = "", w = "24", h = "24" }: Props) => {
@@ -20,11 +11,10 @@ const ArrowLeftIcon = ({ className = "", w = "24", h = "24" }: Props) => {
       className={className}
       width={w}
       height={h}
-      fill="none"
       viewBox="0 0 24 24"
+      fill="currentColor"
     >
       <path
-        fill="currentColor"
         fillRule="evenodd"
         d="M15.778 5.707a1 1 0 0 0-1.414 0l-5.651 5.651a1 1 0 0 0-.006 1.42l5.657 5.657a1 1 0 0 0 1.414-1.414l-4.95-4.95 4.95-4.95a1 1 0 0 0 0-1.414Z"
         clipRule="evenodd"
