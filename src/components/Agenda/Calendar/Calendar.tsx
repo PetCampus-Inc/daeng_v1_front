4b7a53a37d1123a61c6b13ac1a4d6a0ac6e75e09
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-08-10 19:55:05 +0900

    refactor: calendar 공통 props 네이밍 변경
    * useDogInfoRecord 추가 및 주석처리

--- src/components/Agenda/Calendar/Calendar.tsx
+++ src/components/Agenda/Calendar/Calendar.tsx
@@ -1,21 +1,37 @@
 import ArrowDownIcon from "assets/svg/arrow-down-icon";
 import { Box } from "components/common";
-import { format } from "date-fns";
-import React, { useState, useRef } from "react";
+import { format, parse } from "date-fns";
+import { useDogInfoRecord } from "hooks/api/member/dogs";
+import { useState, useRef } from "react";
 import { useSearchParams } from "react-router-dom";
+import { AGENDA_STATUS, DogInfoRecord } from "types/member/dogs";
 
 import { MonthlyCalendar } from "./MonthlyCalendar";
 import { MonthPicker } from "./MonthPicker";
-import { ToggleViewButton } from "./styles";
+import { CalendarSection, ToggleViewButton } from "./styles";
 import { WeeklyCalendar } from "./WeeklyCalendar";
 
 import type { Value } from "react-calendar/dist/cjs/shared/types";
 
-export const Calendar: React.FC = () => {
+export const Calendar = ({ id }: { id: number }) => {
   const today = new Date();
-  const [expanded, setExpanded] = useState(false);
-  const [activeDate, setActiveDate] = useState<Date | null>(new Date());
   const [searchParams, setSearchParams] = useSearchParams();
+
+  const currentDateParam = searchParams.get("date");
+  const currentDate = currentDateParam ? parse(currentDateParam, "yyyy-MM-dd", new Date()) : today;
+  const [activeDate, setActiveDate] = useState<Date | null>(currentDate);
+
+  // const { data } = useDogInfoRecord(id);
+
+  const data: DogInfoRecord[] = [
+    {
+      date: [2024, 8, 8],
+      status: AGENDA_STATUS.COMPLETE,
+      registeredDate: [2024, 1, 11]
+    }
+  ];
+
+  const [expanded, setExpanded] = useState(false);
   const [showMonthPicker, setShowMonthPicker] = useState(false);
   const calendarHeaderRef = useRef<HTMLDivElement>(null);
 
@@ -50,30 +66,36 @@ export const Calendar: React.FC = () => {
 
   const calendarProps = {
     today,
-    onDateClick: handleDateClick,
+    onDateChange: handleDateClick,
     onTodayClick: handleTodayClick,
     activeDate,
-    onActiveDateChange: setActiveDate,
     onOpenMonthPicker: handleOpenMonthPicker,
     headerRef: calendarHeaderRef
   };
 
   return (
-    <Box bgColor="white" pt={28} radius="0px 0px 20px 20px">
-      {expanded ? <MonthlyCalendar {...calendarProps} /> : <WeeklyCalendar {...calendarProps} />}
-      <ToggleViewButton type="button" onClick={toggleExpanded} expand={expanded}>
-        {expanded ? "닫기" : "펼쳐보기"}
-        <span>
-          <ArrowDownIcon w={20} h={20} />
-        </span>
-      </ToggleViewButton>
-      <MonthPicker
-        isOpen={showMonthPicker}
-        onClose={handleCloseMonthPicker}
-        onMonthClick={handleMonthClick}
-        activeDate={activeDate}
-        anchorRef={calendarHeaderRef}
-      />
-    </Box>
+    <CalendarSection>
+      <Box bgColor="white" pt={28} radius="0px 0px 20px 20px" overflow="hidden">
+        {expanded ? (
+          <MonthlyCalendar data={data} {...calendarProps} />
+        ) : (
+          <WeeklyCalendar data={data} {...calendarProps} />
+        )}
+        <ToggleViewButton type="button" onClick={toggleExpanded} expand={expanded}>
+          {expanded ? "닫기" : "펼쳐보기"}
+          <span>
+            <ArrowDownIcon w={20} h={20} />
+          </span>
+        </ToggleViewButton>
+        <MonthPicker
+          data={data}
+          isOpen={showMonthPicker}
+          onClose={handleCloseMonthPicker}
+          onMonthClick={handleMonthClick}
+          activeDate={activeDate}
+          anchorRef={calendarHeaderRef}
+        />
+      </Box>
+    </CalendarSection>
   );
 };
