76b4b4c5316381e3a04b21de74f3d809ab3f7529
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-08-14 05:47:31 +0900

    refactor: 사용하지 않는 스타일 코드 제거

--- src/components/Admin/Attendance/AttendanceModal/styles.ts
+++ src/components/Admin/Attendance/AttendanceModal/styles.ts
@@ -1,29 +1,5 @@
 import styled from "styled-components";
 
-export const TitleWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 8px;
-`;
-
-export const Title = styled.span`
-  ${({ theme }) => theme.typo.title2_20_B};
-  color: ${({ theme }) => theme.colors.darkBlack};
-`;
-
-export const SubTitle = styled.p`
-  ${({ theme }) => theme.typo.body2_16_R};
-  color: ${({ theme }) => theme.colors.gray_2};
-`;
-
-export const Container = styled.div`
-  margin: 44px 0 32px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 8px;
-`;
-
 export const ListWrapper = styled.ul`
   display: flex;
   flex-direction: column;
--- src/pages/AdminAttendancePage/AttendancePage.tsx
+++ src/pages/AdminAttendancePage/AttendancePage.tsx
@@ -7,11 +7,33 @@ import {
   MainSearchContext,
   ModeSearchContext
 } from "components/Admin/Attendance";
-import { Layout } from "components/common";
+import { Box, Layout } from "components/common";
 import Header from "components/common/Header";
 import { AdminNavBar } from "components/common/NavBar";
 import { Suspense, useMemo } from "react";
 import { useSearchParams } from "react-router-dom";
+
+export default function AttendancePage() {
+  const [searchParams] = useSearchParams();
+  const mode = searchParams.get("mode");
+  return (
+    <>
+      <Header type="notice" text="출석부" />
+      <Layout type="main" bgColor="BGray" pt={32}>
+        {mode !== "attend" ? (
+          <MainSearchContext.SearchContextProvider>
+            <AttendanceContent mode={mode} />
+          </MainSearchContext.SearchContextProvider>
+        ) : (
+          <ModeSearchContext.SearchContextProvider>
+            <AttendanceContent mode={mode} />
+          </ModeSearchContext.SearchContextProvider>
+        )}
+      </Layout>
+      <AdminNavBar />
+    </>
+  );
+}
 
 // FIXME: 각 context를 독립적인 상태로 관리했어야 해서 컴포넌트를 분리했습니다. 더 좋은 방법이 있다면 수정해주세요.
 function AttendanceContent({ mode }: { mode: string | null }) {
@@ -26,8 +48,10 @@ function AttendanceContent({ mode }: { mode: string | null }) {
 
   return (
     <>
-      <AttendanceHeader {...headerProps} mode={mode} />
-      <AttendanceSearch {...searchProps} />
+      <Box px={16}>
+        <AttendanceHeader {...headerProps} mode={mode} />
+        <AttendanceSearch {...searchProps} />
+      </Box>
       {mode !== "attend" ? (
         <Suspense fallback={<div>출석부 로딩중...</div>}>
           <AttendanceMain />
@@ -39,28 +63,6 @@ function AttendanceContent({ mode }: { mode: string | null }) {
           </Suspense>
         </AttendanceModeProvider>
       )}
-    </>
-  );
-}
-
-export default function AttendancePage() {
-  const [searchParams] = useSearchParams();
-  const mode = searchParams.get("mode");
-  return (
-    <>
-      <Header type="notice" text="출석부" />
-      <Layout type="main" bgColor="BGray" pt={32} px={16}>
-        {mode !== "attend" ? (
-          <MainSearchContext.SearchContextProvider>
-            <AttendanceContent mode={mode} />
-          </MainSearchContext.SearchContextProvider>
-        ) : (
-          <ModeSearchContext.SearchContextProvider>
-            <AttendanceContent mode={mode} />
-          </ModeSearchContext.SearchContextProvider>
-        )}
-      </Layout>
-      <AdminNavBar />
     </>
   );
 }
