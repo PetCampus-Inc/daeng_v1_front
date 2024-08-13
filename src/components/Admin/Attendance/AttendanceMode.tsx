f84d9a6856e5983bd93e0322ae0040ba5a3abd04
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-08-13 22:16:44 +0900

    refactor: AttendanceMangement → AttendanceMode 리네이밍, blocking 로직 추가

---
+++ src/components/Admin/Attendance/AttendanceMode.tsx
@@ -0,0 +1,56 @@
+import { useAttendDogSearchQuery, useGetAttendDogList } from "hooks/api/admin/attendance";
+import { useAdminInfo } from "hooks/common/useAdminInfo";
+import { useCallback, useState } from "react";
+import { useBlocker, useSearchParams } from "react-router-dom";
+
+import AttendDogSubmitButton from "./AttendanceButton/AttendDogSubmitButton";
+import AttendanceAvatar from "./AttendanceList/AttendanceAvatar";
+import { AttendanceSearchList } from "./AttendanceList/AttendanceSearchList";
+import AttendanceCloseModal from "./AttendanceModal/AttendanceCloseModal";
+import { EmptyList } from "./EmptyList";
+import { useAttendanceModeContext } from "./hooks/useAttendanceModeContext";
+import { useSearchContext } from "./hooks/useSearchContext";
+import { List } from "./styles";
+
+export function AttendanceMode() {
+  const { schoolId, adminId } = useAdminInfo();
+  const { data: dogList } = useGetAttendDogList(schoolId);
+
+  const { searchText, isFocused } = useSearchContext();
+  const { data: searchList } = useAttendDogSearchQuery(schoolId, searchText);
+
+  const selectedDogs = useAttendanceModeContext();
+  const [, setSearchParams] = useSearchParams();
+
+  const data = searchText ? searchList : dogList;
+
+  const handleModeChange = useCallback(() => {
+    setSearchParams({});
+  }, [setSearchParams]);
+
+  const blocker = useBlocker(() => selectedDogs.length > 0);
+
+  return (
+    <>
+      <List isFocus={isFocused}>
+        <AttendanceAvatar />
+        {!data || data.length === 0 ? (
+          <EmptyList isSearching={!!searchList} />
+        ) : (
+          <AttendanceSearchList data={data} />
+        )}
+        <AttendDogSubmitButton schoolId={schoolId} adminId={adminId} />
+      </List>
+      {blocker.state === "blocked" && (
+        <AttendanceCloseModal
+          isOpen={true}
+          close={() => blocker.reset()}
+          action={() => {
+            blocker.proceed();
+            handleModeChange();
+          }}
+        />
+      )}
+    </>
+  );
+}
