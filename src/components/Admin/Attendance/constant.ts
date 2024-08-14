d476028d78c0cd991e15d3892c9194efbf0a9989
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-08-14 17:41:30 +0900

    refactor: CardOptionMenu action 직접 처리

--- src/components/Admin/Attendance/constant.ts
+++ src/components/Admin/Attendance/constant.ts
@@ -7,3 +7,10 @@ export const SORT_OPTIONS = {
   CHARGE: "자주 맡은 강아지순"
 } as const;
 export type SortOptions = PropertyValues<typeof SORT_OPTIONS>;
+
+export const CARD_OPTIONS = {
+  CALL: "견주에게 전화 걸기",
+  SEND_ALARM: "이용권 알림 전송하기",
+  DELETE: "강아지 삭제"
+} as const;
+export type CardOptions = PropertyValues<typeof CARD_OPTIONS>;
