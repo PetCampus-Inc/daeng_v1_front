03c5092f0657109f453ef0e15d08522e4e69859a
Choi Jieun <crg1050@gmail.com>
2024-08-18 17:48:02 +0900

    Feat: 견주 프로필 사진 등록 되도록 기능 추가

--- src/components/Member/MyPage/Buttons/SaveButton.tsx
+++ src/components/Member/MyPage/Buttons/SaveButton.tsx
@@ -1,7 +1,9 @@
 import { FIELD } from "constants/field";
+import { ACCEPT_FILE_TYPE, FILE_NAME, PATHS, TYPE_NAME } from "constants/s3File";
 
 import { BottomButton } from "components/common/Button";
 import { usePostMemberProfileInfo } from "hooks/api/member/member";
+import useUploadProfile from "hooks/common/useUploadProfile";
 import { useFormContext } from "react-hook-form";
 
 import type { IMemberProfilePostInfo } from "types/member/main.types";
@@ -13,6 +15,7 @@ const SaveButton = ({ memberId }: { memberId: string }) => {
     formState: { isDirty, isValid, isSubmitting }
   } = useFormContext();
 
+  const { convertProfileUri, uploadFiles } = useUploadProfile();
   const { mutateProfileInfo } = usePostMemberProfileInfo(memberId);
 
   const getFormValues = (): IMemberProfilePostInfo => {
@@ -21,7 +24,7 @@ const SaveButton = ({ memberId }: { memberId: string }) => {
       memberId,
       memberName: formData[FIELD.MEMBER_NAME],
       memberGender: formData[FIELD.MEMBER_GENDER] === "여" ? "FEMALE" : "MALE",
-      memberProfileUri: "", // TODO 데이터 연동 필요
+      memberProfileUri: formData[FILE_NAME.PROFILE_MEMBER], // TODO 데이터 연동 필요
       nickName: formData[FIELD.NICK_NAME],
       address: formData[FIELD.MEMBER_ADDRESS],
       addressDetail: formData[FIELD.MEMBER_ADDRESS_DETAIL],
@@ -31,13 +34,49 @@ const SaveButton = ({ memberId }: { memberId: string }) => {
     };
   };
 
+  const handleSubmitData = () => {
+    const { memberProfileUri } = getFormValues();
+    console.log(memberProfileUri);
+    // profileUri 파일 수정 할 경우
+    if (memberProfileUri && typeof memberProfileUri !== "string")
+      uploadProfileFiles(memberProfileUri);
+    // profileUri 파일 수정 안 할 경우
+    else onSubmit();
+  };
+
+  const uploadProfileFiles = async (profileUri: FileList) => {
+    const memberParam = {
+      name: TYPE_NAME.MEMBER,
+      id: Number(memberId),
+      files: profileUri,
+      accept: ACCEPT_FILE_TYPE.IMAGE,
+      path: PATHS.PROFILE
+    };
+
+    const params = [memberParam];
+
+    await uploadFiles(params, {
+      onSuccess: () => {
+        onSubmit();
+      }
+    });
+  };
+
   const onSubmit = () => {
-    const requestData = getFormValues();
+    const formData = getFormValues();
+    const memberProfileUri = convertProfileUri(TYPE_NAME.MEMBER);
+    const requestData = Object.assign(formData, {
+      memberProfileUri: memberProfileUri
+    });
+
     mutateProfileInfo(requestData);
   };
 
   return (
-    <BottomButton onClick={handleSubmit(onSubmit)} disabled={!isDirty || !isValid || isSubmitting}>
+    <BottomButton
+      onClick={handleSubmit(handleSubmitData)}
+      disabled={!isDirty || !isValid || isSubmitting}
+    >
       수정 완료
     </BottomButton>
   );
