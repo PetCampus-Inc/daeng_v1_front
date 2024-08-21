027333aa1c43d080ab7d7b7b172f69e332159316
hyeran.lee <hyeran.lee@giantstepcorp.com>
2024-08-21 17:33:10 +0900

    refactor: edit alarm path

--- src/constants/adminNotification.tsx
+++ src/constants/adminNotification.tsx
@@ -13,7 +13,7 @@ interface NotificationItem {
   id: number;
   title: (name: string) => string;
   text: (expired: string) => string;
-  path: string;
+  path: string | ((dogId: number, params: string) => string);
   icon: React.ReactNode;
   subtext?: (expired: string) => string;
 }
@@ -27,46 +27,47 @@ export const ADMIN_NOTIFICATION = {
       id: 0,
       title: "유치원 신규 가입 신청이 있어요!",
       text: "가입 신청 목록에서 승인 혹은 거절을 해주세요",
-      path: PATH.ADMIN_ATTENDANCE,
+      path: PATH.ADMIN_ENROLLMENT,
       icon: <AddBrownIcon />
     },
     {
       id: 1,
       title: (name: string) => `${name} 선생님이 유치원 가입 신청을 했어요`,
       text: "가입 신청 목록에서 승인 혹은 거절을 해주세요",
-      path: PATH.ADMIN_ATTENDANCE,
+      path: PATH.ADMIN_TEACHER_MANAGE,
       icon: <SendAlarmIcon borderStyle="50%" w={28} h={28} />
     },
     {
       id: 2,
       title: (name: string) => `${name} 선생님이 유치원을 탈퇴했어요`,
       text: "교사 목록에서 확인해 보세요",
-      path: PATH.ADMIN_ATTENDANCE,
+      path: PATH.ADMIN_TEACHER_MANAGE,
       icon: <AlertBrownIcon />
     }
   ],
-  teacher: [
-    {
-      id: 0,
-      title: (school: string) => `${school} 유치원 가입 신청이 승인되었습니다!`,
-      text: "등원한 강아지들을 관리해 보세요",
-      path: PATH.ADMIN_ATTENDANCE,
-      icon: <CheckCircleBrown />
-    },
-    {
-      id: 1,
-      title: (school: string) => `${school} 유치원 가입 신청이 거절되었습니다`,
-      text: "다시 가입을 신청하거나 유치원에 문의해 보세요",
-      path: PATH.ADMIN_ATTENDANCE,
-      icon: <AlertBrownIcon />
-    }
-  ],
+  // 선생님은 푸시 알림만 있음!
+  // teacher: [
+  //   {
+  //     id: 0,
+  //     title: (school: string) => `${school} 유치원 가입 신청이 승인되었습니다!`,
+  //     text: "등원한 강아지들을 관리해 보세요",
+  //     path: PATH.ADMIN_ATTENDANCE,
+  //     icon: <CheckCircleBrown />
+  //   },
+  //   {
+  //     id: 1,
+  //     title: (school: string) => `${school} 유치원 가입 신청이 거절되었습니다`,
+  //     text: "다시 가입을 신청하거나 유치원에 문의해 보세요",
+  //     path: PATH.ADMIN_ATTENDANCE,
+  //     icon: <AlertBrownIcon />
+  //   }
+  // ],
   common: [
     {
       id: 0,
       title: (dog: string) => `${dog}의 정보가 수정되었어요`,
       text: "강아지 정보 수정은 견주가 할 수 있어요",
-      path: PATH.ADMIN_ATTENDANCE,
+      path: (dogId, params) => PATH.ADMIN_ATTENDANCE_INFO(`${dogId}?${params}`),
       icon: <CareDogIcon borderStyle="50%" w={28} h={28} />
     },
     {
@@ -74,7 +75,7 @@ export const ADMIN_NOTIFICATION = {
       title: (dog: string) => `${dog}의 이용권 만료일이 임박했어요`,
       text: (expired: string) =>
         expired === "date" ? `[만료 ${expired}일 전]` : `[잔여 횟수:${expired}회]`,
-      path: PATH.ADMIN_ATTENDANCE,
+      path: (dogId, params) => PATH.ADMIN_ATTENDANCE_INFO(`${dogId}?${params}`),
       icon: <CalendarExpireIcon borderStyle="50%" w={28} h={28} />
     },
     {
@@ -83,7 +84,7 @@ export const ADMIN_NOTIFICATION = {
       text: "이용권 갱신은 원장님만 가능해요",
       subtext: (expired: string) =>
         expired === "date" ? `[시작일: ${expired}]` : `[만료된 이용권 정보:회차권 ${expired}회]`,
-      path: PATH.ADMIN_ATTENDANCE,
+      path: (dogId, params) => PATH.ADMIN_ATTENDANCE_INFO(`${dogId}?${params}`),
       icon: <SendAlarmIcon borderStyle="50%" w={28} h={28} />
     },
     {
@@ -93,13 +94,13 @@ export const ADMIN_NOTIFICATION = {
         newTicket === "date"
           ? `[시작일:${newTicket}]`
           : `[갱신된 이용권 정보:회차권 ${newTicket}회]`,
-      path: PATH.ADMIN_ATTENDANCE,
+      path: (dogId, params) => PATH.ADMIN_ATTENDANCE_INFO(`${dogId}?${params}`),
       icon: <SendAlarmIcon borderStyle="50%" w={28} h={28} />
     },
     {
       id: 4,
       title: "유치원 유의사항이 업데이트 되었어요!",
-      text: "견주들에게 픽드랍 유의사항 재동의 알림을 전송해 주세요", // 어떤 유의사항이 변동되었는지 값을 가져오는건지 확인 필요
+      text: (alarm) => `견주들에게 ${alarm} 재동의 알림을 전송해 주세요`,
       path: PATH.ADMIN_ATTENDANCE,
       icon: <ListYellowCircleIcon />
     },
@@ -115,14 +116,14 @@ export const ADMIN_NOTIFICATION = {
       title: "아직 전송되지 않은 알림장이 있어요!",
       text: "오늘이 지나면 알림장이 초기화 돼요 작성 중인 알림장을",
       subtext: "견주에게 전송해 주세요",
-      path: PATH.ADMIN_ATTENDANCE,
+      path: PATH.ADMIN_CARE,
       icon: <AgendaIcon bg={true} w={28} h={28} />
     },
     {
       id: 7,
       title: (dog: string) => `${dog}의 사진을 아직 한장도 전송하지 않았어요`,
       text: "강아지의 유치원 활동 상황을 견주와 공유해 보세요",
-      path: PATH.ADMIN_ATTENDANCE,
+      path: (dogId) => PATH.ADMIN_CARE_INFO_GALLERY(dogId),
       icon: <AgendaIcon bg={true} w={28} h={28} />
     }
   ] as NotificationItem[]
