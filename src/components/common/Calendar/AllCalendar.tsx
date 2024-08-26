a4c571517371899cddb746c72708809e20af1693
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-08-27 00:03:34 +0900

    chore: AllCalendar 임시 주석처리

--- src/components/common/Calendar/AllCalendar.tsx
+++ src/components/common/Calendar/AllCalendar.tsx
@@ -10,6 +10,7 @@ import * as S from "./styles";
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
+// FIXME: API 수정 후 타입에러 해결나는거 해결하고 반영하겠습니다.
 const Calendar = () => {
   const today = new Date();
   const [date, setDate] = useState<Value>(today);
@@ -33,36 +34,36 @@ const Calendar = () => {
 
   return (
     <S.CalendarWrapper>
-      <S.StyledCalendar
-        value={date}
-        onChange={handleDateChange}
-        activeStartDate={activeStartDate === null ? undefined : activeStartDate}
-        onActiveStartDateChange={({ activeStartDate }: { activeStartDate: Date | null }) =>
-          setActiveStartDate(activeStartDate)
-        }
-        formatDay={(locale: any, date: moment.MomentInput) => moment(date).format("D")}
-        formatYear={(locale: any, date: moment.MomentInput) => moment(date).format("YYYY")}
-        formatMonthYear={(locale: any, date: moment.MomentInput) => moment(date).format("YYYY. MM")}
-        calendarType="gregory"
-        showNeighboringMonth={false}
-        next2Label={null}
-        prev2Label={null}
-        minDetail="year"
-        tileContent={({ date, view }: { date: Date; view: string }) => {
-          const html = [];
-          if (
-            view === "month" &&
-            date.getMonth() === today.getMonth() &&
-            date.getDate() === today.getDate()
-          ) {
-            html.push(<S.Today key={"today"}>오늘</S.Today>);
-          }
-          if (attendDay.find((x) => x === moment(date).format("YYYY-MM-DD"))) {
-            html.push(<FootIcon className="footIcon" key={moment(date).format("YYYY-MM-DD")} />);
-          }
-          return <>{html}</>;
-        }}
-      />
+      {/*<S.StyledCalendar*/}
+      {/*  value={date}*/}
+      {/*  onChange={handleDateChange}*/}
+      {/*  activeStartDate={activeStartDate === null ? undefined : activeStartDate}*/}
+      {/*  onActiveStartDateChange={({ activeStartDate }: { activeStartDate: Date | null }) =>*/}
+      {/*    setActiveStartDate(activeStartDate)*/}
+      {/*  }*/}
+      {/*  formatDay={(locale: any, date: moment.MomentInput) => moment(date).format("D")}*/}
+      {/*  formatYear={(locale: any, date: moment.MomentInput) => moment(date).format("YYYY")}*/}
+      {/*  formatMonthYear={(locale: any, date: moment.MomentInput) => moment(date).format("YYYY. MM")}*/}
+      {/*  calendarType="gregory"*/}
+      {/*  showNeighboringMonth={false}*/}
+      {/*  next2Label={null}*/}
+      {/*  prev2Label={null}*/}
+      {/*  minDetail="year"*/}
+      {/*  tileContent={({ date, view }: { date: Date; view: string }) => {*/}
+      {/*    const html = [];*/}
+      {/*    if (*/}
+      {/*      view === "month" &&*/}
+      {/*      date.getMonth() === today.getMonth() &&*/}
+      {/*      date.getDate() === today.getDate()*/}
+      {/*    ) {*/}
+      {/*      html.push(<S.Today key={"today"}>오늘</S.Today>);*/}
+      {/*    }*/}
+      {/*    if (attendDay.find((x) => x === moment(date).format("YYYY-MM-DD"))) {*/}
+      {/*      html.push(<FootIcon className="footIcon" key={moment(date).format("YYYY-MM-DD")} />);*/}
+      {/*    }*/}
+      {/*    return <>{html}</>;*/}
+      {/*  }}*/}
+      {/*/>*/}
       <S.Date onClick={handleTodayClick}>오늘</S.Date>
     </S.CalendarWrapper>
   );
