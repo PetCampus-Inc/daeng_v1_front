ea742e79983124335d9a3472147045860bcbcedf
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-08-27 00:14:02 +0900

    style: 빈 fragment 제거

--- src/components/Agenda/DailyAgenda/EmptyAgenda.tsx
+++ src/components/Agenda/DailyAgenda/EmptyAgenda.tsx
@@ -2,10 +2,8 @@ import { EmptyContainer } from "./styles";
 
 export const EmptyAgenda = () => {
   return (
-    <>
-      <EmptyContainer>
-        <span>작성된 알림장이 없어요</span>
-      </EmptyContainer>
-    </>
+    <EmptyContainer>
+      <span>작성된 알림장이 없어요</span>
+    </EmptyContainer>
   );
 };
