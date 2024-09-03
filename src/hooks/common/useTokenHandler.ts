875788aa2511279543121330268158e0b50e0861
jsn0509 <jsn_@naver.com>
2024-09-04 02:08:21 +0900

    feat: AccessToken 핸들러 커스텀 훅 추가

---
+++ src/hooks/common/useTokenHandler.ts
@@ -0,0 +1,29 @@
+import { ACCESS_TOKEN_KEY } from "constants/storage";
+
+import { useLocalStorage } from "hooks/common/useLocalStorage";
+import { useMemo } from "react";
+import { User, Role } from "types/common/role.types";
+import { extractRoleByToken, extractUserByToken } from "utils/token";
+
+export const useTokenHandler = () => {
+  const [accessToken, setAccessToken] = useLocalStorage<string | null>(ACCESS_TOKEN_KEY, null);
+
+  const role = useMemo(() => {
+    if (!accessToken) return Role.ROLE_ANONYMOUS;
+    const role = extractRoleByToken(accessToken);
+    return role ? role : Role.ROLE_ANONYMOUS;
+  }, [accessToken]);
+
+  const user = useMemo(() => {
+    if (!accessToken) return User.GUEST;
+    const user = extractUserByToken(accessToken);
+    return user ? user : User.GUEST;
+  }, [accessToken]);
+
+  return {
+    role,
+    user,
+    accessToken,
+    setAccessToken
+  };
+};
