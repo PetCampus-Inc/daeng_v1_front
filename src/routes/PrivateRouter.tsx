141b9c88955dd8c74d9f2fb94e5f36f254c5dfd4
jsn0509 <jsn_@naver.com>
2024-09-04 17:29:37 +0900

    refactor: redirectPath 값이 없을 시, Role 값에 따라 리다이렉트 되도록 변경

--- src/routes/PrivateRouter.tsx
+++ src/routes/PrivateRouter.tsx
@@ -1,5 +1,4 @@
-import { PATH } from "constants/path";
-
+import { useRoleBasedPath } from "hooks/common/useRoleBasedPath";
 import { useTokenHandler } from "hooks/common/useTokenHandler";
 import { Navigate, Outlet } from "react-router-dom";
 import { Role } from "types/common/role.types";
@@ -9,13 +8,17 @@ interface PrivateRouterProps {
   redirectPath?: string;
 }
 
-export default function PrivateRouter({ roles, redirectPath = PATH.LOGIN }: PrivateRouterProps) {
+export default function PrivateRouter({ roles, redirectPath }: PrivateRouterProps) {
   const { role } = useTokenHandler();
-  console.log({
+  const getRoleBasedPath = useRoleBasedPath();
+  const basedPath = getRoleBasedPath(role);
+
+  console.info({
     현재권한: role,
     접근가능권한: roles,
     접근경로: location.pathname,
-    접근가능여부: roles.includes(role)
+    접근가능여부: roles.includes(role),
+    리다이렉트경로: basedPath
   });
-  return roles.includes(role) ? <Outlet /> : <Navigate to={redirectPath} />;
+  return roles.includes(role) ? <Outlet /> : <Navigate to={redirectPath ?? basedPath} />;
 }
