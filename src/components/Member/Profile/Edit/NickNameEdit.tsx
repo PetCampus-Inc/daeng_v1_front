c3a9764f1ba9ab6bf994ede4afe5e6acc7d4abaa
Choi Jieun <crg1050@gmail.com>
2024-09-14 02:20:27 +0900

    Refactor: nickName defaultValues 코드 개선

--- src/components/Member/Profile/Edit/NickNameEdit.tsx
+++ src/components/Member/Profile/Edit/NickNameEdit.tsx
@@ -6,12 +6,11 @@ import { css } from "styled-components";
 
 // FIXME 닉네임 기본 데이터 값은 dogName으로 들어가야합니다! (api 데이터 수정 필요)
 const NickNameEdit = () => {
-  const { register, getValues } = useFormContext();
+  const { register } = useFormContext();
   return (
     <>
       <TextInput
         {...register(FIELD.NICK_NAME, { required: true })}
-        defaultValue={getValues(FIELD.DOG_NAME)}
         register={register}
         placeholder="닉네임을 입력해주세요"
         css={InputStyle}
--- src/pages/MemberProfileEditPage/MemberProfileEditPage.tsx
+++ src/pages/MemberProfileEditPage/MemberProfileEditPage.tsx
@@ -1,6 +1,6 @@
 import { LayoutContainer } from "components/Member/MyPage/Container/styles";
 import OnboardingProfile from "components/Member/Profile/OnboardingProfile";
-import { useGetMemberProfileInfo } from "hooks/api/member/member";
+import { useGetMemberProfile } from "hooks/api/member/member";
 import { FormProvider, useForm } from "react-hook-form";
 import { useRecoilValue } from "recoil";
 import { dogIdState } from "store/member";
@@ -9,20 +9,22 @@ import SaveProfileButton from "../../components/Member/Profile/Button/SaveProfil
 
 const MemberProfileEditPage = () => {
   const dogId = useRecoilValue(dogIdState);
-  const { data } = useGetMemberProfileInfo();
+  // const { data } = useGetMemberProfile();
 
   const defaultValues = {
-    dogId: dogId ?? 1, // FIXME dogId 어디서 가져와야하는지?
+    dogId: 1, // FIXME dogId 어디서 가져와야하는지?
     memberProfileUri: "",
     dogProfileUri: "",
-    nickName: data.memberProfileUri,
+    dogName: "거튼이",
     relation: ""
   };
+
+  const { dogName, ...rest } = defaultValues;
 
   const methods = useForm({
     mode: "onChange",
     shouldUnregister: false,
-    defaultValues: { ...defaultValues }
+    defaultValues: { ...rest, nickName: dogName }
   });
 
   return (
