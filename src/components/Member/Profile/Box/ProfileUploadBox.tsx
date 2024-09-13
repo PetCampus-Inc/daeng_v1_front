35b3bc13059d6a2c6d93bd955bd44a49d1941c65
Choi Jieun <crg1050@gmail.com>
2024-09-13 13:42:29 +0900

    Feat/Refactor: profile 관련 코드 개선

--- src/components/Member/Profile/Box/ProfileUploadBox.tsx
+++ src/components/Member/Profile/Box/ProfileUploadBox.tsx
@@ -1,5 +1,5 @@
 import { IFile } from "components/Admin/AttendCare/AttendCareGallery/upload";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useCallback, useState } from "react";
 import { useFormContext } from "react-hook-form";
 import showToast from "utils/showToast";
 import { getFilePreview } from "utils/thumb";
@@ -18,29 +18,15 @@ interface ProfileUploadProps {
   type: string;
   isActive?: boolean;
   setIsActive?: React.Dispatch<React.SetStateAction<boolean>>;
-  fileRef: React.RefObject<HTMLInputElement>;
   fileName: string;
   mode: Mode;
 }
 
-const ProfileUploadBox = ({
-  type,
-  isActive,
-  setIsActive,
-  fileRef,
-  fileName,
-  mode
-}: ProfileUploadProps) => {
+const ProfileUploadBox = ({ type, isActive, setIsActive, fileName, mode }: ProfileUploadProps) => {
   const { setValue } = useFormContext();
   const [profile, setProfile] = useState<IFile[]>([]);
 
-  const handleClick = () => {
-    if (fileRef && fileRef.current) {
-      mode === "create" && isActive ? setIsActive && setIsActive(false) : fileRef.current.click();
-    }
-  };
-
-  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     const fileList = e.target.files;
 
     if (!fileList) {
@@ -50,7 +36,7 @@ const ProfileUploadBox = ({
 
     // 파일 변경 없을 경우
     if (fileList.length <= 0) {
-      if (!isActive && setIsActive) setIsActive(true);
+      setIsActive && setIsActive(true);
       return;
     }
 
@@ -58,7 +44,7 @@ const ProfileUploadBox = ({
       updateFilePreview(fileList);
       if (mode === "create" && setIsActive) setIsActive(true);
     }
-  };
+  }, []);
 
   const updateFilePreview = async (fileList: FileList) => {
     const newFiles = Array.from(fileList);
@@ -75,9 +61,7 @@ const ProfileUploadBox = ({
           isActive={isActive}
           setIsActive={setIsActive}
           profile={profile}
-          fileInputRef={fileRef}
           handleFileChange={handleFileChange}
-          handleClick={handleClick}
           registerText={fileName}
           type={type}
         />
--- src/components/Member/Profile/Onboarding/Profile.tsx
+++ src/components/Member/Profile/Onboarding/Profile.tsx
@@ -1,28 +1,24 @@
 import { TYPE_NAME, FILE_NAME } from "constants/s3File";
 
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 import ProfileUploadBox from "../Box/ProfileUploadBox";
 
 const Profile = () => {
   const [isMyActive, setIsMyActive] = useState(false);
   const [isDogActive, setIsDogActive] = useState(false);
-  const myFileInputRef = useRef<HTMLInputElement>(null);
-  const dogFileInputRef = useRef<HTMLInputElement>(null);
 
   const profileDatas = [
     {
       type: TYPE_NAME.MEMBER,
       isActive: isMyActive,
       setIsActive: setIsMyActive,
-      fileRef: myFileInputRef,
       fileName: FILE_NAME.PROFILE_MEMBER
     },
     {
       type: TYPE_NAME.DOG,
       isActive: isDogActive,
       setIsActive: setIsDogActive,
-      fileRef: dogFileInputRef,
       fileName: FILE_NAME.PROFILE_DOG
     }
   ];
@@ -35,7 +31,6 @@ const Profile = () => {
           type={item.type}
           isActive={item.isActive}
           setIsActive={item.setIsActive}
-          fileRef={item.fileRef}
           fileName={item.fileName}
           mode="create"
         />
