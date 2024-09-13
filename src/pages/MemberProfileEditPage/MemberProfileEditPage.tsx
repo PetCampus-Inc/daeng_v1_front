f54823bb7b65b745a166288736421608ab815f46
Choi Jieun <crg1050@gmail.com>
2024-09-13 17:59:37 +0900

    Feat: defaultValues 데이터 수정

--- src/pages/MemberProfileEditPage/MemberProfileEditPage.tsx
+++ src/pages/MemberProfileEditPage/MemberProfileEditPage.tsx
@@ -1,18 +1,22 @@
 import OnboardingProfile from "components/Member/Profile/OnboardingProfile";
 import { useGetMemberProfileInfo } from "hooks/api/member/member";
 import { FormProvider, useForm } from "react-hook-form";
+import { useRecoilValue } from "recoil";
+import { dogIdState } from "store/member";
 import { PageContainer } from "styles/StyleModule";
 
 import SaveProfileButton from "../../components/Member/Profile/Button/SaveProfileButton";
 
 const MemberProfileEditPage = () => {
+  const dogId = useRecoilValue(dogIdState);
   const { data } = useGetMemberProfileInfo();
 
   const defaultValues = {
-    memberProfileUri: data.memberProfileUri,
+    dogId: dogId ?? 1, // FIXME dogId 어디서 가져와야하는지?
+    memberProfileUri: "",
     dogProfileUri: "",
     nickName: data.memberProfileUri,
-    relation: data.memberProfileUri
+    relation: ""
   };
 
   const methods = useForm({
