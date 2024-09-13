ddff8d7f30e086016daf87c63b99ecdf1f24ec10
Choi Jieun <crg1050@gmail.com>
2024-09-13 13:43:12 +0900

    Chore: NickNameEdit 이전 코드로 돌리기 및 코멘트 추가

--- src/components/Member/Profile/Edit/NickNameEdit.tsx
+++ src/components/Member/Profile/Edit/NickNameEdit.tsx
@@ -4,14 +4,14 @@ import { TextInput } from "components/common";
 import { useFormContext } from "react-hook-form";
 import { css } from "styled-components";
 
+// FIXME 닉네임 기본 데이터 값은 dogName으로 들어가야합니다! (api 데이터 수정 필요)
 const NickNameEdit = () => {
   const { register } = useFormContext();
   return (
     <>
       <TextInput
+        {...register(FIELD.NICK_NAME, { required: true })}
         register={register}
-        name={FIELD.NICK_NAME}
-        required={true}
         placeholder="닉네임을 입력해주세요"
         css={InputStyle}
         className="defaultValue"
