f166dd59abb78b15b606053431febf2b2c7c779e
jsn0509 <jsn_@naver.com>
2024-09-17 02:04:59 +0900

    fix: 예방접종 했을 경우에만 imageUrls 폼데이터로 변환

--- src/components/Enrollment/Stepper/SubmitButton.tsx
+++ src/components/Enrollment/Stepper/SubmitButton.tsx
@@ -5,6 +5,7 @@ import { usePostEnrollment } from "hooks/api/member/enroll";
 import { useS3Upload } from "hooks/common/useS3";
 import { Adapter, MemberFormToServerAdapter } from "libs/adapters";
 import { FieldValues, useFormContext, type FieldErrors } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
 import { currentStepState } from "store/form";
 import { FormButton } from "styles/StyleModule";
@@ -12,7 +13,6 @@ import showToast from "utils/showToast";
 
 import type { ImageFile } from "../ImageUpload/ImageUploadInput";
 import type { EnrollmentInfoType } from "types/member/enrollment.types";
-import { useNavigate } from "react-router-dom";
 
 const SubmitButton = ({ openPopup }: { openPopup: (field: string) => void }) => {
   const { handleSubmit } = useFormContext();
@@ -40,11 +40,17 @@ const SubmitButton = ({ openPopup }: { openPopup: (field: string) => void }) => 
   };
 
   const onSubmit = async (data: FieldValues) => {
-    const files = data.vaccinationUri.map((item: ImageFile) => item.file);
-    const imageUrls = await uploadImages(files, data.dogId);
+    let imageUrls;
+    if (data.vaccination === "했어요" && data.vaccinationUri) {
+      const files = data.vaccinationUri.map((item: ImageFile) => item.file);
+      imageUrls = await uploadImages(files, data.dogId);
+    }
+
     const formData = getFormData({ ...data, vaccinationUri: imageUrls });
+
+    console.log(formData);
     mutateEnrollment(formData, {
-      onSuccess: () => navigate(routes.login.root),
+      onSuccess: () => navigate(routes.approval.root),
       onError: () => showToast("제출 중 오류가 발생했습니다. 다시 시도해주세요.", "ownerNav")
     });
   };
