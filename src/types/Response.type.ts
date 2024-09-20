183656894000e2f7a6513168917b90ff6ed17c12
jsn0509 <jsn_@naver.com>
2024-09-21 02:56:53 +0900

    fix: 불필요한 인터페이스 제거

--- src/types/Response.type.ts
+++ src/types/Response.type.ts
@@ -1,16 +1,20 @@
 /**
- * @deprecated Since version 1.0.0 Will be removed in version 2.0. Use {@link Response} instead.
- * @see {@link Response}
+ * @deprecated Since version 1.0.0 Will be removed in version 2.0. Use {@link ApiResponse} instead.
+ * @see {@link ApiResponse}
  */
 export interface IResponse {
   status: number;
   message: string;
 }
 
-export interface IError {
-  data: {
-    status: number;
-    message: string;
-    code: string;
-  };
+export interface ApiResponse<T> {
+  status: number;
+  message: string;
+  data: T;
+}
+
+export interface ApiErrorResponse {
+  status: number;
+  message: string;
+  code: string;
 }
