2ecec1bcbe92d4cfebceed006673d54f2f2c252d
Choi Jieun <crg1050@gmail.com>
2024-09-30 04:28:40 +0900

    Refactor: 불필요한 코드 정리 및 getLabelForValue를 활용한 데이터 UI 표시 수정

--- src/components/Enrollment/MemberDogReadForm/DogInfo.tsx
+++ src/components/Enrollment/MemberDogReadForm/DogInfo.tsx
@@ -9,90 +9,84 @@ import TextArea from "components/common/TextArea";
 import Title from "components/common/Title";
 import { Controller, useFormContext } from "react-hook-form";
 import { padToTwoDigits } from "utils/date";
-import { handlePreventDefault } from "utils/preventDefault";
+import { getLabelForValue } from "utils/formatter";
 
 import { Caption, Card } from "./styles";
 import { ImageUploadInput } from "../ImageUpload/ImageUploadInput";
 import BreedInput from "../Input/BreedInput";
 
 const DogInfo = () => {
-  const { control, register, watch, setValue } = useFormContext();
-  const [birthYear, birthMonth, birthDay] = watch(FIELD.BIRTHDAY);
-
+  const { control, register, setValue, watch, getValues } = useFormContext();
+  const { dogName, dogSize, allergyDisease, birthDate, dogGender, neutralization, vaccination } =
+    getValues();
+  const [birthYear, birthMonth, birthDay] = getValues(FIELD.BIRTHDAY).map(String);
   const dogBirthData = {
     year: birthYear,
     month: padToTwoDigits(birthMonth),
     day: padToTwoDigits(birthDay)
   };
+  const formatDogGender = getLabelForValue(FIELD.DOG_GENDER, dogGender);
+  const formatDogSize = getLabelForValue(FIELD.DOG_SIZE, dogSize);
+  const formatNeutralization = getLabelForValue(FIELD.NEUTRALIZATION, neutralization);
+  const formatVaccination = getLabelForValue(FIELD.VACCINATION, vaccination);
 
   return (
     <>
-      <Card>
-        <Title isRequired={REQUIRED_ITEMS_DOG_MAP?.get(FIELD_KEYS.DOG_NAME)}>이름</Title>
-        <TextInput
-          name={FIELD.DOG_NAME}
-          placeholder="강아지 이름을 입력해주세요"
-          register={register}
-          required
-          readOnly
-        />
-      </Card>
+      {dogName && (
+        <Card>
+          <Title isRequired={REQUIRED_ITEMS_DOG_MAP?.get(FIELD_KEYS.DOG_NAME)}>이름</Title>
+          <TextInput
+            name={FIELD.DOG_NAME}
+            placeholder="강아지 이름을 입력해주세요"
+            register={register}
+            readOnly
+          />
+        </Card>
+      )}
+
       <Card>
         <Title isRequired={REQUIRED_ITEMS_DOG_MAP?.get(FIELD_KEYS.DOG_GENDER)}>성별</Title>
         <SingleRadio
           name={FIELD.DOG_GENDER}
           radiosText={["수컷", "암컷"]}
-          isRequired
-          defaultSelect={ITEM_ENGLISH_TO_KOREAN[watch(FIELD.DOG_GENDER)]}
-          preventDefaultClick={handlePreventDefault}
+          defaultSelect={formatDogGender}
         />
       </Card>
-      <Card>
-        <Title isRequired={REQUIRED_ITEMS_DOG_MAP?.get(FIELD_KEYS.DOG_SIZE)}>크기</Title>
-        <SingleRadio
-          name={FIELD.DOG_SIZE}
-          caption="~7kg 소형견 / ~ 15kg 중형견 / 15kg 이상 대형견"
-          radiosText={["소형견", "중형견", "대형견"]}
-          isRequired
-          defaultSelect={ITEM_ENGLISH_TO_KOREAN[watch(FIELD.DOG_SIZE)]}
-          preventDefaultClick={handlePreventDefault}
-        />
-      </Card>
+
+      {dogSize && (
+        <Card>
+          <Title isRequired={REQUIRED_ITEMS_DOG_MAP?.get(FIELD_KEYS.DOG_SIZE)}>크기</Title>
+          <SingleRadio
+            name={FIELD.DOG_SIZE}
+            caption="~7kg 소형견 / ~ 15kg 중형견 / 15kg 이상 대형견"
+            radiosText={["소형견", "중형견", "대형견"]}
+            defaultSelect={formatDogSize}
+          />
+        </Card>
+      )}
+
       <Card>
         <Title isRequired={REQUIRED_ITEMS_DOG_MAP?.get(FIELD_KEYS.BREED_ID)}>견종</Title>
-        {/* // TODO 수정되지 않도록 추후 작업 필요 */}
         <BreedInput
           name={FIELD.BREED_NAME}
           register={register}
           setValue={setValue}
           watch={watch}
-          isRequired={REQUIRED_ITEMS_DOG_MAP?.get(FIELD_KEYS.BREED_ID)}
           readOnly
         />
       </Card>
-      <Card>
-        <Title isRequired={REQUIRED_ITEMS_DOG_MAP?.get(FIELD_KEYS.BIRTHDAY)}>생일</Title>
-        <div style={{ display: "flex", gap: "5px" }}>
-          <SelectNumber
-            name="year"
-            defaultValue={dogBirthData.year}
-            watch={watch}
-            setValue={setValue}
-          />
-          <SelectNumber
-            name="month"
-            defaultValue={String(dogBirthData.month)}
-            watch={watch}
-            setValue={setValue}
-          />
-          <SelectNumber
-            name="day"
-            defaultValue={String(dogBirthData.day)}
-            watch={watch}
-            setValue={setValue}
-          />
-        </div>
-      </Card>
+
+      {birthDate && (
+        <Card>
+          <Title isRequired={REQUIRED_ITEMS_DOG_MAP?.get(FIELD_KEYS.BIRTHDAY)}>생일</Title>
+          <div style={{ display: "flex", gap: "5px" }}>
+            <SelectNumber name="year" defaultValue={dogBirthData.year} setValue={setValue} />
+            <SelectNumber name="month" defaultValue={dogBirthData.month} setValue={setValue} />
+            <SelectNumber name="day" defaultValue={dogBirthData.day} setValue={setValue} />
+          </div>
+        </Card>
+      )}
+
       <Card>
         <Title isRequired={REQUIRED_ITEMS_DOG_MAP?.get(FIELD_KEYS.NEUTRALIZATION)}>
           중성화 여부
@@ -100,9 +94,7 @@ const DogInfo = () => {
         <SingleRadio
           name={FIELD.NEUTRALIZATION}
           radiosText={["했어요", "안했어요"]}
-          isRequired={REQUIRED_ITEMS_DOG_MAP?.get(FIELD_KEYS.NEUTRALIZATION)}
-          defaultSelect={ITEM_ENGLISH_TO_KOREAN[watch(FIELD.NEUTRALIZATION)]}
-          preventDefaultClick={handlePreventDefault}
+          defaultSelect={formatNeutralization}
         />
       </Card>
       <Card>
@@ -112,12 +104,10 @@ const DogInfo = () => {
         <SingleRadio
           name={FIELD.VACCINATION}
           radiosText={["했어요", "안했어요"]}
-          isRequired={REQUIRED_ITEMS_DOG_MAP?.get(FIELD_KEYS.VACCINATION)}
-          defaultSelect={ITEM_ENGLISH_TO_KOREAN[watch(FIELD.VACCINATION)]}
-          preventDefaultClick={handlePreventDefault}
+          defaultSelect={formatVaccination}
         />
       </Card>
-      {watch(FIELD.VACCINATION) === "했어요" && (
+      {getValues(FIELD.VACCINATION) === "했어요" && (
         <Card>
           <Title isRequired={REQUIRED_ITEMS_DOG_MAP?.get(FIELD_KEYS.VACCINATION_URL)}>
             예방접종 파일 첨부
@@ -130,18 +120,21 @@ const DogInfo = () => {
           />
         </Card>
       )}
-      <Card>
-        <Title isRequired={REQUIRED_ITEMS_DOG_MAP?.get(FIELD_KEYS.ALLERGY_DISEASE)}>
-          알러지 및 질병 유무
-        </Title>
-        <TextArea
-          placeholder="알러지나 질병이 있다면 상세히 입력해주세요."
-          {...register(FIELD.ALLERGY_DISEASE, {
-            required: REQUIRED_ITEMS_DOG_MAP?.get(FIELD_KEYS.ALLERGY_DISEASE)
-          })}
-          readOnly
-        />
-      </Card>
+
+      {allergyDisease && (
+        <Card>
+          <Title isRequired={REQUIRED_ITEMS_DOG_MAP?.get(FIELD_KEYS.ALLERGY_DISEASE)}>
+            알러지 및 질병 유무
+          </Title>
+          <TextArea
+            placeholder="알러지나 질병이 있다면 상세히 입력해주세요."
+            {...register(FIELD.ALLERGY_DISEASE, {
+              required: REQUIRED_ITEMS_DOG_MAP?.get(FIELD_KEYS.ALLERGY_DISEASE)
+            })}
+            readOnly
+          />
+        </Card>
+      )}
     </>
   );
 };
