3b205492f12168ac853ff0304224c877411afd57
Choi Jieun <crg1050@gmail.com>
2024-10-02 05:40:56 +0900

    Feat: 강아지 추가, 유치원 재신청 예방접종 이미지 업로드 기능 추가

--- src/components/Enrollment/Stepper/MemberSubmitButton.tsx
+++ src/components/Enrollment/Stepper/MemberSubmitButton.tsx
@@ -1,16 +1,21 @@
 import { getFieldStep } from "constants/step";
 
 import { usePostMemberEnrollment } from "hooks/api/member/enroll";
+import { useS3Upload } from "hooks/common/useS3";
 import { Adapter, MemberFormToServerAdapter } from "libs/adapters";
 import { FieldValues, useFormContext, type FieldErrors } from "react-hook-form";
 import { useSetRecoilState } from "recoil";
 import { currentStepState } from "store/form";
 import { FormButton } from "styles/StyleModule";
+import showToast from "utils/showToast";
+
+import { ImageFile } from "../ImageUpload/ImageUploadInput";
 
 import type { EnrollmentInfoType, MemberGenderType } from "types/member/enrollment.types";
 
 const MemberSubmitButton = ({ openPopup }: { openPopup: (field: string) => void }) => {
   const { handleSubmit, getValues } = useFormContext();
+  const { uploadToS3 } = useS3Upload();
   const { mutateMemberEnrollment } = usePostMemberEnrollment();
 
   const setStep = useSetRecoilState(currentStepState);
@@ -21,11 +26,22 @@ const MemberSubmitButton = ({ openPopup }: { openPopup: (field: string) => void 
     );
   };
 
+  const uploadImages = async (files: FileList, dogId: string) => {
+    const params = {
+      files,
+      accept: ["image/*"],
+      path: `vaccination/${dogId}`
+    };
+
+    return await uploadToS3(params, {
+      onError: () => showToast("사진 업로드에 실패했습니다. 다시 시도해주세요.", "ownerNav")
+    });
+  };
+
   const getMemberData = () => {
     const { memberName, memberGender, address, addressDetail, phoneNumber, emergencyPhoneNumber } =
       getValues();
 
-    // FIXME: fileUrl 추가 필요
     const memberData = {
       memberName: memberName,
       memberGender: memberGender as MemberGenderType,
@@ -39,12 +55,20 @@ const MemberSubmitButton = ({ openPopup }: { openPopup: (field: string) => void 
   };
 
   // member - 강아지 추가 및 유치원 재등록
-  const onSubmitMember = (data: FieldValues) => {
-    const requestData = getSubmitFormInfo(data);
+  const onSubmitMember = async (data: FieldValues) => {
     const memberData = getMemberData();
-    const reqData = { ...requestData, ...memberData };
 
-    mutateMemberEnrollment(reqData, {});
+    let imageUrls;
+    if (data.vaccination === "했어요" && data.vaccinationUri) {
+      const files = data.vaccinationUri.map((item: ImageFile) => item.file);
+      imageUrls = await uploadImages(files, data.dogId);
+    }
+
+    const formData = getSubmitFormInfo({ ...data, ...memberData, vaccinationUri: imageUrls });
+
+    mutateMemberEnrollment(formData, {
+      onError: () => showToast("제출 중 오류가 발생했습니다. 다시 시도해주세요.", "ownerNav")
+    });
   };
 
   const onInvalid = (errors: FieldErrors) => {
