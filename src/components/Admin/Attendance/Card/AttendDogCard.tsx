48e92dc48ed80648985e3fc5d9d3cddd1ce8965d
jsn0509 <jsn_@naver.com>
2024-10-01 18:12:48 +0900

    fix: 관리자 출석부 강아지 프로필 사진 표시

--- src/components/Admin/Attendance/Card/AttendDogCard.tsx
+++ src/components/Admin/Attendance/Card/AttendDogCard.tsx
@@ -4,20 +4,23 @@ import * as S from "./styles";
 
 interface AttendanceCardProps {
   attendanceId: number;
+  dogProfileUri: string;
   dogName: string;
   onClick: () => void;
   isSelected: boolean;
 }
 
-export function AttendDogCard({ dogName, onClick, isSelected }: AttendanceCardProps) {
+export function AttendDogCard({
+  dogProfileUri,
+  dogName,
+  onClick,
+  isSelected
+}: AttendanceCardProps) {
   return (
     <S.CardContainer hasAvatar onClick={onClick}>
       <S.Stack>
         <S.ImageWrapper>
-          <S.Image
-            src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt={dogName + " 이미지"}
-          />
+          <S.Image src={dogProfileUri} alt={dogName + " 이미지"} />
         </S.ImageWrapper>
         <S.DogName>{dogName}</S.DogName>
       </S.Stack>
