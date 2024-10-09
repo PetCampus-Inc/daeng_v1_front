6f8e3c140693b5d7077a80d4e678d3274c95225e
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-10-09 17:36:51 +0900

    fix: umount 시 value reset 로직 추가

--- src/components/Admin/EnrollmentForm/CreateForm/TicketInfo.tsx
+++ src/components/Admin/EnrollmentForm/CreateForm/TicketInfo.tsx
@@ -4,37 +4,38 @@ import { Checkbox } from "components/common";
 import DayMultiCheck from "components/common/Select/DayMultiCheck";
 import MultiCheck from "components/common/Select/MultiCheck";
 import { Textarea } from "components/common/Textarea";
-import AdminTitle from "components/common/Title/AdminTitle";
 import { useEffect } from "react";
 import { useFormContext } from "react-hook-form";
 
+import { Label } from "../Label/Label";
 import { Card, Caption, Stack } from "../styles";
-import TicketType from "../TicketType";
+import { TicketType } from "../TicketType/TicketType";
 
-const TicketInfo = () => {
-  const { register, control, watch, setValue } = useFormContext();
+export function TicketInfo() {
+  const { register, control, watch, unregister } = useFormContext();
 
   const selectedTicketTypes = watch(FIELD.TICKET_TYPE);
 
-  const isMonthlySelected = selectedTicketTypes?.includes("정기권");
-  const isRoundSelected = selectedTicketTypes?.includes("회차권");
+  const isMonthlySelected = selectedTicketTypes?.includes?.("정기권");
+  const isRoundSelected = selectedTicketTypes?.includes?.("회차권");
 
   useEffect(() => {
     if (!isMonthlySelected) {
-      setValue(FIELD.MONTHLY_TICKET_NUMBER, []);
+      unregister(`${FIELD.REQUEST_ITEMS}.${FIELD_KEYS.MONTHLY_TICKET_NUMBER}`);
+      unregister(FIELD.MONTHLY_TICKET_NUMBER);
     }
     if (!isRoundSelected) {
-      setValue(FIELD.ROUND_TICKET_NUMBER, []);
+      unregister(`${FIELD.REQUEST_ITEMS}.${FIELD_KEYS.ROUND_TICKET_NUMBER}`);
+      unregister(FIELD.ROUND_TICKET_NUMBER);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isMonthlySelected, isRoundSelected]);
 
   return (
     <>
       <Card>
-        <AdminTitle name="null" control={control} hasBadge noToggle>
+        <Label control={control} showBadge>
           가격 안내
-        </AdminTitle>
+        </Label>
         <Caption>견주에게 안내할 가격 내용을 입력해 주세요</Caption>
         <Textarea
           {...register(FIELD.PRICE_INFO, { required: true })}
@@ -42,25 +43,25 @@ const TicketInfo = () => {
         />
       </Card>
       <Card>
-        <AdminTitle
+        <Label
           name={`${FIELD.REQUEST_ITEMS}.${FIELD_KEYS.TICKET_TYPE}`}
           control={control}
-          hasBadge
+          showBadge
         >
           이용권 종류
-        </AdminTitle>
+        </Label>
         <Caption>복수 선택이 가능해요</Caption>
         <MultiCheck name={FIELD.TICKET_TYPE} radiosText={["정기권", "회차권"]} isRequired />
       </Card>
       {isMonthlySelected && (
         <Card>
-          <AdminTitle
+          <Label
             name={`${FIELD.REQUEST_ITEMS}.${FIELD_KEYS.MONTHLY_TICKET_NUMBER}`}
             control={control}
-            hasBadge
+            showBadge
           >
             정기권 유형
-          </AdminTitle>
+          </Label>
           <Caption>최대 6개까지 추가 가능하며, 최소 1개의 선택지가 있어야해요</Caption>
           <TicketType
             ticketType="MONTHLY"
@@ -72,13 +73,13 @@ const TicketInfo = () => {
       )}
       {isRoundSelected && (
         <Card>
-          <AdminTitle
+          <Label
             name={`${FIELD.REQUEST_ITEMS}.${FIELD_KEYS.ROUND_TICKET_NUMBER}`}
             control={control}
-            hasBadge
+            showBadge
           >
             회차권 유형
-          </AdminTitle>
+          </Label>
           <Caption>최대 6개까지 추가 가능하며, 최소 1개의 선택지가 있어야해요</Caption>
           <TicketType
             ticketType="ROUND"
@@ -89,24 +90,20 @@ const TicketInfo = () => {
         </Card>
       )}
       <Card>
-        <AdminTitle
-          name={`${FIELD.REQUEST_ITEMS}.${FIELD_KEYS.OPEN_DAYS}`}
-          control={control}
-          hasBadge
-        >
+        <Label name={`${FIELD.REQUEST_ITEMS}.${FIELD_KEYS.OPEN_DAYS}`} control={control} showBadge>
           등원 요일 선택
-        </AdminTitle>
+        </Label>
         <Caption>유치원 휴무날처럼 견주가 신청하면 안 되는 요일을 해제해 주세요</Caption>
         <DayMultiCheck name={FIELD.OPEN_DAYS} defaultChecked isRequired />
       </Card>
       <Card>
-        <AdminTitle
+        <Label
           name={`${FIELD.REQUEST_ITEMS}.${FIELD_KEYS.TICKET_INFO}`}
           control={control}
-          hasBadge
+          showBadge
         >
           유의사항
-        </AdminTitle>
+        </Label>
         <Textarea
           {...register(FIELD.TICKET_INFO, { required: true })}
           placeholder="유의사항을 입력해 주세요"
@@ -117,6 +114,4 @@ const TicketInfo = () => {
       </Card>
     </>
   );
-};
-
-export default TicketInfo;
+}
--- src/components/Admin/EnrollmentForm/EditForm/TicketInfo.tsx
+++ src/components/Admin/EnrollmentForm/EditForm/TicketInfo.tsx
@@ -1,28 +1,38 @@
 import { FIELD, FIELD_KEYS } from "constants/field";
 
-import { Checkbox } from "components/common";
+import { Checkbox, ToggleLabel } from "components/common";
 import DayMultiCheck from "components/common/Select/DayMultiCheck";
 import MultiCheck from "components/common/Select/MultiCheck";
 import { Textarea } from "components/common/Textarea";
-import AdminTitle from "components/common/Title/AdminTitle";
-import { useFormContext } from "react-hook-form";
+import { useEffect } from "react";
+import { Controller, useFormContext } from "react-hook-form";
 
 import { Card, Caption, Stack } from "../styles";
-import TicketType from "../TicketType";
+import { TicketType } from "../TicketType/TicketType";
 
-const TicketInfo = () => {
-  const { register, control, watch } = useFormContext();
+export function TicketInfo() {
+  const { register, control, unregister, watch } = useFormContext();
 
   const selectedTicketTypes = watch(FIELD.TICKET_TYPE);
+
   const isMonthlySelected = selectedTicketTypes?.includes("정기권");
   const isRoundSelected = selectedTicketTypes?.includes("회차권");
+
+  useEffect(() => {
+    if (!isMonthlySelected) {
+      unregister(`${FIELD.REQUEST_ITEMS}.${FIELD_KEYS.MONTHLY_TICKET_NUMBER}`);
+      unregister(FIELD.MONTHLY_TICKET_NUMBER);
+    }
+    if (!isRoundSelected) {
+      unregister(`${FIELD.REQUEST_ITEMS}.${FIELD_KEYS.ROUND_TICKET_NUMBER}`);
+      unregister(FIELD.ROUND_TICKET_NUMBER);
+    }
+  }, [isMonthlySelected, isRoundSelected]);
 
   return (
     <>
       <Card>
-        <AdminTitle name="null" control={control} hasBadge noToggle>
-          가격 안내
-        </AdminTitle>
+        <ToggleLabel showBadge>가격 안내</ToggleLabel>
         <Caption>견주에게 안내할 가격 내용을 입력해 주세요</Caption>
         <Textarea
           {...register(FIELD.PRICE_INFO, { required: true })}
@@ -30,61 +40,81 @@ const TicketInfo = () => {
         />
       </Card>
       <Card>
-        <AdminTitle
+        <Controller
           name={`${FIELD.REQUEST_ITEMS}.${FIELD_KEYS.TICKET_TYPE}`}
           control={control}
-          hasBadge
-        >
-          이용권 종류
-        </AdminTitle>
+          render={({ field }) => (
+            <ToggleLabel showBadge showToggle {...field}>
+              이용권 종류
+            </ToggleLabel>
+          )}
+        />
         <Caption>복수 선택이 가능해요</Caption>
         <MultiCheck name={FIELD.TICKET_TYPE} radiosText={["정기권", "회차권"]} isRequired />
       </Card>
       {isMonthlySelected && (
         <Card>
-          <AdminTitle
+          <Controller
             name={`${FIELD.REQUEST_ITEMS}.${FIELD_KEYS.MONTHLY_TICKET_NUMBER}`}
             control={control}
-            hasBadge
-          >
-            정기권 유형
-          </AdminTitle>
+            render={({ field }) => (
+              <ToggleLabel showBadge showToggle {...field}>
+                정기권 유형
+              </ToggleLabel>
+            )}
+          />
           <Caption>최대 6개까지 추가 가능하며, 최소 1개의 선택지가 있어야해요</Caption>
-          <TicketType ticketType="MONTHLY" control={control} name={FIELD.MONTHLY_TICKET_NUMBER} />
+          <TicketType
+            ticketType="MONTHLY"
+            control={control}
+            name={FIELD.MONTHLY_TICKET_NUMBER}
+            defaultValues={[1, 2, 4, 8]}
+          />
         </Card>
       )}
       {isRoundSelected && (
         <Card>
-          <AdminTitle
+          <Controller
             name={`${FIELD.REQUEST_ITEMS}.${FIELD_KEYS.ROUND_TICKET_NUMBER}`}
             control={control}
-            hasBadge
-          >
-            회차권 유형
-          </AdminTitle>
+            render={({ field }) => (
+              <ToggleLabel showBadge showToggle {...field}>
+                회차권 유형
+              </ToggleLabel>
+            )}
+          />
           <Caption>최대 6개까지 추가 가능하며, 최소 1개의 선택지가 있어야해요</Caption>
-          <TicketType ticketType="ROUND" control={control} name={FIELD.ROUND_TICKET_NUMBER} />
+          <TicketType
+            ticketType="ROUND"
+            control={control}
+            name={FIELD.ROUND_TICKET_NUMBER}
+            defaultValues={[1, 3, 5, 10]}
+          />
         </Card>
       )}
       <Card>
-        <AdminTitle
+        <Controller
           name={`${FIELD.REQUEST_ITEMS}.${FIELD_KEYS.OPEN_DAYS}`}
           control={control}
-          hasBadge
-        >
-          등원 요일 선택
-        </AdminTitle>
+          render={({ field }) => (
+            <ToggleLabel showBadge showToggle {...field}>
+              등원 요일 선택
+            </ToggleLabel>
+          )}
+        />
         <Caption>유치원 휴무날처럼 견주가 신청하면 안 되는 요일을 해제해 주세요</Caption>
         <DayMultiCheck name={FIELD.OPEN_DAYS} isRequired />
       </Card>
       <Card>
-        <AdminTitle
+        <Controller
           name={`${FIELD.REQUEST_ITEMS}.${FIELD_KEYS.TICKET_INFO}`}
           control={control}
-          hasBadge
-        >
-          유의사항
-        </AdminTitle>
+          render={({ field }) => (
+            <ToggleLabel showBadge showToggle {...field}>
+              유의사항
+            </ToggleLabel>
+          )}
+        />
         <Textarea
           {...register(FIELD.TICKET_INFO, { required: true })}
           placeholder="유의사항을 입력해 주세요"
@@ -95,6 +125,4 @@ const TicketInfo = () => {
       </Card>
     </>
   );
-};
-
-export default TicketInfo;
+}
