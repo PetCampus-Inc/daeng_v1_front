d8184c80e659747f1a329d3cff1e1eb1a5ae8791
jsn0509 <jsn_@naver.com>
2024-10-09 16:28:20 +0900

    feat: 이미지 컴포넌트 추가 (비디오 대응)

---
+++ src/components/common/Image/index.tsx
@@ -0,0 +1,55 @@
+import { ImgHTMLAttributes, useCallback, useEffect, useState } from "react";
+import styled from "styled-components";
+import { getVideoThumb } from "utils/thumb";
+
+interface ImageProps extends ImgHTMLAttributes<HTMLImageElement> {
+  src: string;
+  ratio?: string;
+}
+
+export const Image = ({ src, ratio, ...props }: ImageProps) => {
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
+
+  const getVideoThumbnail = useCallback(async () => {
+    try {
+      const response = await fetch(src);
+      const blob = await response.blob();
+      const file = new File([blob], "video.mp4", { type: "video/mp4" });
+
+      const videoThumb = await getVideoThumb(file);
+      return videoThumb.thumbnail;
+    } catch (error) {
+      return src;
+    }
+  }, [src]);
+
+  useEffect(() => {
+    const loadSrc = async () => {
+      if (src.endsWith(".mp4")) {
+        const videoThumbnail = await getVideoThumbnail();
+        setImageSrc(videoThumbnail);
+      } else setImageSrc(src);
+    };
+
+    loadSrc();
+  }, [src, getVideoThumbnail]);
+
+  if (!imageSrc) return <ImageSkeleton ratio={ratio} />;
+  return <StyledImage src={imageSrc} ratio={ratio} {...props} />;
+};
+
+const StyledImage = styled.img.withConfig({
+  shouldForwardProp: (prop) => prop !== "ratio"
+})<{ ratio?: string }>`
+  width: 100%;
+  object-fit: cover;
+  aspect-ratio: ${({ ratio }) => ratio};
+`;
+
+const ImageSkeleton = styled.div.withConfig({
+  shouldForwardProp: (prop) => prop !== "ratio"
+})<{ ratio?: string }>`
+  width: 100%;
+  aspect-ratio: ${({ ratio }) => ratio};
+  background-color: ${({ theme }) => theme.colors.gray_4};
+`;
