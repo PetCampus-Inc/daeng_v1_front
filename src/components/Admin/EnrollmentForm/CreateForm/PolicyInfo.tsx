9daaefdfe85c754701ab33416c289d075ca78026
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-10-09 17:36:19 +0900

    refactor: ToggleLabel 적용

--- src/components/Admin/EnrollmentForm/CreateForm/PolicyInfo.tsx
+++ src/components/Admin/EnrollmentForm/CreateForm/PolicyInfo.tsx
@@ -1,25 +1,26 @@
 import { FIELD, FIELD_KEYS } from "constants/field";
 
-import { Checkbox } from "components/common";
+import { Checkbox, ToggleLabel } from "components/common";
 import { Textarea } from "components/common/Textarea";
-import AdminTitle from "components/common/Title/AdminTitle";
-import { useFormContext } from "react-hook-form";
+import { Controller, useFormContext } from "react-hook-form";
 
 import { Card, Stack } from "../styles";
 
-const PolicyInfo = () => {
+export function PolicyInfo() {
   const { register, control } = useFormContext();
 
   return (
     <>
       <Card>
-        <AdminTitle
+        <Controller
           name={`${FIELD.REQUEST_ITEMS}.${FIELD_KEYS.LIMITS_INFO}`}
           control={control}
-          hasBadge
-        >
-          이용 제한 유의 사항
-        </AdminTitle>
+          render={({ field }) => (
+            <ToggleLabel showBadge showToggle {...field}>
+              이용 제한 유의 사항
+            </ToggleLabel>
+          )}
+        />
         <Textarea
           {...register(FIELD.LIMITS_INFO, { required: true })}
           placeholder="이용 제한 관련 유의사항을 입력해 주세요"
@@ -29,13 +30,15 @@ const PolicyInfo = () => {
         </Stack>
       </Card>
       <Card>
-        <AdminTitle
+        <Controller
           name={`${FIELD.REQUEST_ITEMS}.${FIELD_KEYS.ACCIDENT_INFO}`}
           control={control}
-          hasBadge
-        >
-          상해 유의사항
-        </AdminTitle>
+          render={({ field }) => (
+            <ToggleLabel showBadge showToggle {...field}>
+              상해 유의사항
+            </ToggleLabel>
+          )}
+        />
         <Textarea
           {...register(FIELD.ACCIDENT_INFO, { required: true })}
           placeholder="상해 관련 유의사항을 입력해 주세요"
@@ -45,13 +48,15 @@ const PolicyInfo = () => {
         </Stack>
       </Card>
       <Card>
-        <AdminTitle
+        <Controller
           name={`${FIELD.REQUEST_ITEMS}.${FIELD_KEYS.ABANDONMENT_INFO}`}
           control={control}
-          hasBadge
-        >
-          유기 유의사항
-        </AdminTitle>
+          render={({ field }) => (
+            <ToggleLabel showBadge showToggle {...field}>
+              유기 유의사항
+            </ToggleLabel>
+          )}
+        />
         <Textarea
           {...register(FIELD.ABANDONMENT_INFO, { required: true })}
           placeholder="유기 관련 유의사항을 입력해 주세요"
@@ -62,6 +67,4 @@ const PolicyInfo = () => {
       </Card>
     </>
   );
-};
-
-export default PolicyInfo;
+}
--- src/components/Admin/EnrollmentForm/EditForm/PolicyInfo.tsx
+++ src/components/Admin/EnrollmentForm/EditForm/PolicyInfo.tsx
@@ -1,27 +1,28 @@
 import { FIELD, FIELD_KEYS } from "constants/field";
 
-import { Checkbox } from "components/common";
+import { Checkbox, ToggleLabel } from "components/common";
 import { Textarea } from "components/common/Textarea";
-import AdminTitle from "components/common/Title/AdminTitle";
-import { useFormContext } from "react-hook-form";
+import { Controller, useFormContext } from "react-hook-form";
 
 import { Card, Stack } from "../styles";
 
-const PolicyInfo = () => {
+export function PolicyInfo() {
   const { register, control } = useFormContext();
 
   return (
     <>
       <Card>
-        <AdminTitle
+        <Controller
           name={`${FIELD.REQUEST_ITEMS}.${FIELD_KEYS.LIMITS_INFO}`}
           control={control}
-          hasBadge
-        >
-          이용 제한 유의 사항
-        </AdminTitle>
+          render={({ field }) => (
+            <ToggleLabel showBadge showToggle {...field}>
+              이용 제한 유의 사항
+            </ToggleLabel>
+          )}
+        />
         <Textarea
-          {...register("limitsInfo", { required: true })}
+          {...register(FIELD.LIMITS_INFO, { required: true })}
           placeholder="이용 제한 관련 유의사항을 입력해 주세요"
         />
         <Stack>
@@ -29,15 +30,17 @@ const PolicyInfo = () => {
         </Stack>
       </Card>
       <Card>
-        <AdminTitle
+        <Controller
           name={`${FIELD.REQUEST_ITEMS}.${FIELD_KEYS.ACCIDENT_INFO}`}
           control={control}
-          hasBadge
-        >
-          상해 유의사항
-        </AdminTitle>
+          render={({ field }) => (
+            <ToggleLabel showBadge showToggle {...field}>
+              상해 유의사항
+            </ToggleLabel>
+          )}
+        />
         <Textarea
-          {...register("accidentInfo", { required: true })}
+          {...register(FIELD.ACCIDENT_INFO, { required: true })}
           placeholder="상해 관련 유의사항을 입력해 주세요"
         />
         <Stack>
@@ -45,15 +48,17 @@ const PolicyInfo = () => {
         </Stack>
       </Card>
       <Card>
-        <AdminTitle
+        <Controller
           name={`${FIELD.REQUEST_ITEMS}.${FIELD_KEYS.ABANDONMENT_INFO}`}
           control={control}
-          hasBadge
-        >
-          유기 유의사항
-        </AdminTitle>
+          render={({ field }) => (
+            <ToggleLabel showBadge showToggle {...field}>
+              유기 유의사항
+            </ToggleLabel>
+          )}
+        />
         <Textarea
-          {...register("abandonmentInfo", { required: true })}
+          {...register(FIELD.ABANDONMENT_INFO, { required: true })}
           placeholder="유기 관련 유의사항을 입력해 주세요"
         />
         <Stack>
@@ -62,6 +67,4 @@ const PolicyInfo = () => {
       </Card>
     </>
   );
-};
-
-export default PolicyInfo;
+}
