a0240a8f56d919a483156c635be24fbed50bce12
yeomgahui <ykh4933@naver.com>
2024-10-10 15:45:01 +0900

    Fix/UI: 어드민 미디어 상세 페이지 이미지 리스트 가운데로 정렬되도록 수정

--- src/components/Admin/DogGallery/GalleryViewer/MediaThumbnailGallery/index.tsx
+++ src/components/Admin/DogGallery/GalleryViewer/MediaThumbnailGallery/index.tsx
@@ -20,56 +20,28 @@ const MediaThumbnailGallery: React.FC<MediaThumbnailGalleryProps> = ({
   handleClick
 }) => {
   const selectedThumbnailRef = useRef<HTMLDivElement | null>(null);
-  const containerRef = useRef<HTMLDivElement | null>(null); // 썸네일 컨테이너의 ref
 
   useEffect(() => {
-    // if (selectedThumbnailRef.current) {
-    //   selectedThumbnailRef.current.scrollIntoView({
-    //     behavior: "smooth",
-    //     block: "center",
-    //     inline: "center"
-    //   });
-    // }
+    if (selectedThumbnailRef.current) {
+      selectedThumbnailRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "nearest",
+        inline: "center"
+      });
+    }
   }, [selectedIndex]);
 
-  const handleScroll = useCallback(() => {
-    if (containerRef.current) {
-      const thumbnails = containerRef.current.children;
-      const containerRect = containerRef.current.getBoundingClientRect();
-      const containerCenter = containerRect.left + containerRect.width / 2;
-
-      // 중앙에 있는 썸네일을 찾음
-      let closestIndex = selectedIndex;
-      let closestDistance = Number.MAX_VALUE;
-
-      for (let i = 0; i < thumbnails.length; i++) {
-        const thumbnailRect = thumbnails[i].getBoundingClientRect();
-        const thumbnailCenter = thumbnailRect.left + thumbnailRect.width / 2;
-
-        const distance = Math.abs(thumbnailCenter - containerCenter);
-        if (distance < closestDistance) {
-          closestDistance = distance;
-          closestIndex = i;
-        }
-      }
-
-      if (closestIndex !== selectedIndex) {
-        handleClick(closestIndex); // 중앙에 있는 항목을 selectedIndex로 업데이트
-      }
-    }
-  }, [handleClick, selectedIndex]);
-
   return (
-    <S.ThumbnailContainer ref={containerRef}>
-      <S.ThumbnailItemsContainer width={`${items.length * 4.5}rem`}>
+    <S.ThumbnailContainer>
+      <S.ThumbnailItemsContainer>
         {items.map((item, index) => (
           <S.ThumbnailItemWrapper
             key={index}
-            isSelected={index === selectedIndex}
-            ref={index === selectedIndex ? selectedThumbnailRef : null} // 선택된 항목에만 ref 할당
             onClick={() => handleClick(index)}
+            isSelected={index === selectedIndex}
+            ref={index === selectedIndex ? selectedThumbnailRef : null} // Assign ref to selected item
           >
-            <img src={item.src} />
+            <img src={item.src} alt={`Thumbnail ${index + 1}`} />
           </S.ThumbnailItemWrapper>
         ))}
       </S.ThumbnailItemsContainer>
--- src/components/Admin/DogGallery/GalleryViewer/MediaThumbnailGallery/styles.ts
+++ src/components/Admin/DogGallery/GalleryViewer/MediaThumbnailGallery/styles.ts
@@ -4,10 +4,10 @@ export const ThumbnailContainer = styled.div`
   width: 100%;
   position: absolute;
   bottom: 0;
-  scroll-snap-type: x mandatory; // 스크롤 스냅 활성화
   position: relative;
   height: 4.5rem;
   overflow-x: auto;
+  margin-bottom: 1rem;
 
   // 스크롤바 제거
   -ms-overflow-style: none;
@@ -15,29 +15,31 @@ export const ThumbnailContainer = styled.div`
   &::-webkit-scrollbar {
     display: none;
   }
-  margin-bottom: 1rem;
 `;
 
-export const ThumbnailItemsContainer = styled.div<{ width: string }>`
+export const ThumbnailItemsContainer = styled.div`
   display: flex;
   overflow-x: auto;  
   padding-bottom: 0;
-  scroll-snap-type: x mandatory; // X축 스크롤 스냅 활성화
-  scroll-padding: 50%; // 중앙에 정렬되도록 스크롤 패딩 설정
   position: absolute;
+
   left:48%;
   translateX(-48%);
+  & > :last-child {
+    padding-right: calc(100% - 78%);
+  }
 `;
 
 export const ThumbnailItemWrapper = styled.div<{ isSelected: boolean }>`
   width: 3.5rem;
   height: 4.5rem;
   display: flex;
-  scroll-snap-align: center; // 선택된 요소가 중앙에 위치하도록 설정
   padding-left: ${({ isSelected }) => (isSelected ? "6px" : "0")};
   padding-right: ${({ isSelected }) => (isSelected ? "6px" : "0")};
   img {
     width: 100%;
+    object-fit: cover;
+    height
   }
   flex: 1;
   margin-right: 4px;
