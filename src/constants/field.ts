48a9db67fc138ff061097af6ce0657d8597ab5d6
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-10-13 17:15:23 +0900

    refactor: 사용하지 않는 코드 제거

--- src/constants/field.ts
+++ src/constants/field.ts
@@ -86,23 +86,6 @@ export const FIELD_KEYS = {
   PICKDROP_INFO: 30
 } as const;
 
-/**
- * @description 이넘 객체로 필드 아이템을 정의
- */
-export const FIELD_ITEMS = {
-  memberGender: { MALE: "남", FEMALE: "여" },
-  dogGender: { MALE: "수컷", FEMALE: "암컷" },
-  dogSize: { BIG: "대형견", MEDIUM: "중형견", SMALL: "소형견" },
-  neutralization: { NEUTERED: "했어요", NOT_NEUTERED: "안했어요" },
-  vaccination: { VACCINATED: "했어요", NOT_VACCINATED: "안했어요" },
-  pickDropRequest: { REQUEST: "신청", NOT_REQUEST: "미신청" },
-  pickDropType: { ONE_WAY: "편도", ROUND: "왕복" },
-  pickDropState: { RUNNING: "운영", NOT_RUNNING: "미운영" },
-  ticketType: { ROUND: "회차권", MONTHLY: "정기권" }
-} as const;
-export type FieldItemLabels = typeof FIELD_ITEMS;
-export type FieldItemKeys<K extends keyof FieldItemLabels> = keyof FieldItemLabels[K];
-
 export const AGREEMENT_ITEM: Map<string, number> = new Map([
   [FIELD.TICKET_INFO_TERM, 21],
   [FIELD.LIMITS_INFO_TERM, 22],
--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,5 +1,3 @@
-import { FIELD_ITEMS, type FieldItemKeys, type FieldItemLabels } from "constants/field";
-
 /**
  * 문자열에서 `/n`을 개행 문자로 대체하는 함수
  *
@@ -61,43 +59,4 @@ export const formatBusinessNumber = (value: string) => {
     value = value.substring(0, 10);
   }
   return value.replace(/^(\d{0,3})(\d{0,2})(\d{0,5})$/, "$1-$2-$3").replace(/(-)+$/, "");
-};
-
-/**
- * 지정된 카테고리와 값에 해당하는 레이블을 반환합니다.
- *
- * @param {K} category - 값을 조회할 카테고리의 키입니다.
- * @param {V} value - 카테고리 내에서 조회할 값의 키입니다.
- * @returns {FieldItemLabels[K][V]} - 조회된 레이블 문자열을 반환합니다. 카테고리와 값에 해당하는 레이블이 존재할 경우 해당 레이블을 반환하고, 그렇지 않으면 입력된 값을 그대로 반환합니다.
- *
- * @example
- * // '남'을 반환합니다.
- * getLabelForValue('memberGender', 'MALE');
- */
-
-export const getLabelForValue = <K extends keyof FieldItemLabels, V extends FieldItemKeys<K>>(
-  category: K,
-  value: V
-): FieldItemLabels[K][V] => {
-  return FIELD_ITEMS[category][value];
-};
-
-/**
- * 주어진 레이블에 해당하는 키를 찾아 반환합니다.
- *
- * @param {K} category - 키를 찾을 카테고리의 키입니다.
- * @param {string} label - 찾고자 하는 레이블의 문자열입니다.
- * @returns {keyof FieldItemLabels[K] | undefined} - 찾은 키를 반환합니다. 해당 레이블을 가진 키가 있으면 그 키를, 없으면 undefined를 반환합니다.
- *
- * @example
- * // 'MALE'을 반환합니다.
- * getKeyForLabel('memberGender', '남');
- */
-export const getKeyForLabel = <K extends keyof FieldItemLabels>(
-  category: K,
-  label: string
-): keyof FieldItemLabels[K] | undefined => {
-  const entries = Object.entries(FIELD_ITEMS[category]) as [keyof FieldItemLabels[K], string][];
-  const entry = entries.find(([, v]) => v === label);
-  return entry ? entry[0] : undefined;
 };
