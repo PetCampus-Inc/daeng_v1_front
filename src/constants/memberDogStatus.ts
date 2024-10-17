b69130fecd095145123aa7cdc94baac80ba0cc6c
Choi Jieun <crg1050@gmail.com>
2024-10-17 14:41:51 +0900

    Chore: memberDogStatus 사용 하지 않는 코드 정리

--- src/constants/memberDogStatus.ts
+++ src/constants/memberDogStatus.ts
@@ -2,11 +2,7 @@
  * 견주 마이페이지에서 사용되는 localStorage key를 관리합니다.
  */
 export const STORAGE_KEY = {
-  VISIT_MYPAGE: "VISIT-MYPAGE",
-  ENROLLMENT_FORM_ID: "ENROLLMENT-FORM-ID",
-  APPROVAL_DENIED: "APPROVAL-DENIED",
-  DOG_ENROLLMENT_DATA: "DOG-ENROLLMENT-DATA",
-  CURRENT_DOG_ID: "CURRENT-DOG-ID"
+  VISIT_MYPAGE: "VISIT-MYPAGE"
 };
 
 /**
@@ -17,6 +13,6 @@ export const DOG_STATUS = {
   APPROVAL_PENDING: "APPROVAL_PENDING", // 승인 대기
   APPROVAL_CANCEL: "APPROVAL_CANCEL", // 승인 취소
   APPROVAL_DENIED: "APPROVAL_DENIED", // 승인 거부
-  DIS_CONNECTED: "DIS_CONNECTED", // 유치원 연결 끊김(변경)
-  DROP_OUT: "DROP_OUT" // 유치원 연결 끊김(이제 안쓰는지..??)
+  DIS_CONNECTED: "DIS_CONNECTED", // 유치원 연결 끊김(견주가 끊을 경우)
+  DROP_OUT: "DROP_OUT" // 유치원 연결 끊김(관리자가 끊을 경우)
 };
