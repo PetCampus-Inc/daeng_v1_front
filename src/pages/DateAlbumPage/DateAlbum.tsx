260d80711d5c8a8b2bcd005f1a8b648f9f8e0260
Choi Jieun <crg1050@gmail.com>
2024-10-18 11:41:38 +0900

    Feat: 날짜별 사진 앨범페이지 DisconnectionNotice 추가

--- src/pages/DateAlbumPage/DateAlbum.tsx
+++ src/pages/DateAlbumPage/DateAlbum.tsx
@@ -2,16 +2,21 @@ import { Calendar } from "components/Agenda";
 import GridView from "components/Album/GridView";
 import { Layout } from "components/common";
 import Header from "components/common/Header";
+import DisconnectionNotice from "components/Home/DisconnectionNotice/DisconnectionNotice";
+import { useDogDisconnected } from "hooks/member/useDogDisconnected";
 import { Suspense } from "react";
 import { useParams } from "react-router-dom";
 
 export default function DateAlbum() {
   const { dogId } = useParams<{ dogId: string }>();
+  // 유치원 끊긴 강아지 여부
+  const { isDisconnected } = useDogDisconnected();
 
   return (
     <>
       <Header type="text" text="날짜별 사진 앨범" />
-      <Layout bgColor="BGray">
+      {isDisconnected && <DisconnectionNotice />}
+      <Layout bgColor="BGray" isDisconnected={isDisconnected}>
         {/* 캘린더 */}
         <Suspense fallback={<div>캘린더 로딩중..</div>}>
           <Calendar dogId={Number(dogId)} />
